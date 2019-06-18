da3bbff29940077fc3927ffd14a129460d43bc9a
Jon Schipp <jonschipp@gmail.com>
2019-06-18 12:51:38 -0500

    Gitignore bug causing actions named run to be added to git

---
+++ tshark/komand_tshark/actions/run/action.py
@@ -0,0 +1,17 @@
+import komand
+from .schema import RunInput, RunOutput, Input, Output
+# Custom imports below
+
+
+class Run(komand.Action):
+
+    def __init__(self):
+        super(self.__class__, self).__init__(
+                name='run',
+                description='Run Tshark on a pcap',
+                input=RunInput(),
+                output=RunOutput())
+
+    def run(self, params={}):
+        # TODO: Implement run function
+        return {}
