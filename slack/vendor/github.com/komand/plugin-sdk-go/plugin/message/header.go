4e031fbbba45d83c87a572a7569aff6cd4fd23ef
Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
2019-11-05 17:08:27 -0600

    DF-2771: remove slack (#84)
    Remove non-FOSS plugins

--- slack/vendor/github.com/komand/plugin-sdk-go/plugin/message/header.go
+++
@@ -1,7 +0,0 @@
-package message
-
-// Header is appended to the top of every message
-type Header struct {
-	Version string `json:"version"` // version of messages
-	Type    string `json:"type"`    // message type
-}
