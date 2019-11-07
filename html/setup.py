1d0928b91da263158edb06f73ec2869b02f15754
Joey McAdams <jmcadams@rapid7.com>
2019-11-07 14:38:56 -0600

    version bump

--- html/setup.py
+++ html/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='html-rapid7-plugin',
-      version='1.1.0',
+      version='1.2.0',
       description='Validate or convert an HTML document',
       author='rapid7',
       author_email='',
