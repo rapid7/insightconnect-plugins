a4a1620a4b62944dba0c034e11a962e2c008a4ba
Joey McAdams <jmcadams@rapid7.com>
2019-11-08 10:03:00 -0600

    changing error messaging

--- html/komand_html/actions/docx/action.py
+++ html/komand_html/actions/docx/action.py
@@ -21,8 +21,8 @@ class Docx(komand.Action):
     tags = re.findall(tag_parser, params.get('doc'))
     try:
         if not len(tags):
-            raise PluginException(cause='Run: Input Invalid',
-                                  assistance='Input must be of type HTML')
+            raise PluginException(cause='Run: Input Invalid.',
+                                  assistance='Input must be of type HTML.')
         pypandoc.convert(params.get('doc'), 'docx', outputfile=temp_file, format='html')
         with open(temp_file, 'rb') as output:
             #Reading the output and sending it in base64
