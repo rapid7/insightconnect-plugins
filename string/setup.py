6d41ad36002d3e3a7a26481266b5e5a3dbfe2c07
Joey McAdams <jmcadams@rapid7.com>
2019-11-11 16:23:08 -0600

    version bump

--- string/setup.py
+++ string/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='string-rapid7-plugin',
-      version='1.1.0',
+      version='1.2.0',
       description='Provides common programmatic string operations',
       author='rapid7',
       author_email='',
