9ef3e845f6a176f5ddd48992428b84368030d858
Mateusz Górny <divo@divo.net.pl>
2019-11-30 22:00:22 +0100

    #38 Change docker image from `komand/python-pypy3-plugin:2` to `komand/python-3-37-plugin` | Removed duplicated code | Changed string action to use bare string instead temporary file | Changed bare strings in params.get and output to static fields from schema | Repair coding style

--- awk/komand_awk/util/utils.py
+++ awk/komand_awk/util/utils.py
@@ -1,21 +1,22 @@
 import komand
 import base64
 
-
 # Specify path for unix utils grep and wc:
 awk = "gawk"
 
+
 # Preprocess expression
-def preprocess_expression(log, expression):
-    if expression[0].isalnum()\
-       or expression.startswith('=')\
-       or expression.endswith('='):
-        expression = base64.b64decode(expression)
+def preprocess_expression(expression):
+    if expression[0].isalnum() \
+            or expression.startswith('=') \
+            or expression.endswith('='):
+        expression = base64.b64decode(expression).decode('utf-8')
     return expression
+
 
 # Run awk on text for pattern, return stdout
 def process_lines(log, text, expression):
-    cmd = 'echo "%s" | %s %s' % (text, awk, expression)
+    cmd = 'echo "{}" | {} {}'.format(text, awk, expression)
     log.info('ProcessLines: awk %s', expression)
     r = komand.helper.exec_command(cmd)
     return r['stdout']
