95d670b8ced444b00856fa78327c7af4daf9ba66
Mateusz Górny <divo@divo.net.pl>
2019-12-04 20:54:52 +0100

    #41 datetime amendments - update branch | make regenerate | update version

--- datetime/setup.py
+++ datetime/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 
 setup(name='datetime-rapid7-plugin',
-      version='2.0.5',
-      description='This plugin manipulates timestamps using Python's maya library',
+      version='2.0.6',
+      description='This plugin manipulates timestamps using Pythons maya library',
       author='rapid7',
       author_email='',
       url='',
