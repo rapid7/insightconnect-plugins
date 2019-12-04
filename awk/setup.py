8329bad9b8394e7064f79866687b11d8086fe5c4
Mateusz Górny <divo@divo.net.pl>
2019-12-04 19:49:34 +0100

    #38 - awk amendments - rename test | make regenerate

--- awk/setup.py
+++ awk/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='awk-rapid7-plugin',
-      version='1.2.1',
+      version='1.2.2',
       description='This plugin manipulates an input string or file with the GNU awk programming language',
       author='rapid7',
       author_email='',
