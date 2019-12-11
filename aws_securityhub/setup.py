fd9372caa73dfef890f65fabd27f9eec831aab93
Luke Milby <luke_milby@rapid7.com>
2019-12-11 12:21:37 -0600

    update spelling

--- aws_securityhub/setup.py
+++ aws_securityhub/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='aws_securityhub-rapid7-plugin',
-      version='1.0.1',
+      version='1.0.2',
       description='This plugin utilizes AWS Security Hub to lists and describes security hub-aggregated findings and retrieve SQS messages',
       author='rapid7',
       author_email='',
