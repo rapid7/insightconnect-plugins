32562bf69a093094e303c6ae712b241a55311630
Luke Milby <luke_milby@rapid7.com>
2019-12-11 16:06:07 -0600

    description update

--- azure_ad_admin/setup.py
+++ azure_ad_admin/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='azure_ad_admin-rapid7-plugin',
       version='1.4.1',
-      description='Preform administrative operations in Azure AD',
+      description='Perform administrative operations in Azure AD',
       author='rapid7',
       author_email='',
       url='',
