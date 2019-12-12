130d113385fcf04e0f5b294f4741f7e5e0d110fd
matt hofert <mhofert@rapid7.com>
2019-12-12 13:29:55 -0600

    typo

--- mimecast/komand_mimecast/actions/add_group_member/schema.py
+++ mimecast/komand_mimecast/actions/add_group_member/schema.py
@@ -82,7 +82,7 @@ class AddGroupMemberOutput(komand.Output):
     "internal": {
       "type": "boolean",
       "title": "Internal",
-      "description": "If the user / doamin is internal or not",
+      "description": "If the user / domain is internal or not",
       "order": 4
     }
   }
