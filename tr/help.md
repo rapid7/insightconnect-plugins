f5102fa32f10c10b152fc73baf99c27cc86b20cc
Jakob Herlitz <jakob_herlitz@rapid7.com>
2019-12-12 09:27:21 -0500

    versioning fixes

--- tr/help.md
+++ tr/help.md
@@ -64,7 +64,6 @@ In case an error is raised, make sure that the given expression can be correctly
 
 # Version History
 
-* 2.0.3 - Help updates
 * 2.0.2 - New spec and help.md format for the Hub
 * 2.0.1 - Add `utilities` plugin tag for Marketplace searchability
 * 2.0.0 - Rename "Tr" plugin title to "Translate"
--- viper/setup.py
+++ viper/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='viper-rapid7-plugin',
       version='2.0.1',
-      description='Utilize Viper's platform to store, search, and analyze files',
+      description='Utilize the Viper platform to store, search, and analyze files',
       author='rapid7',
       author_email='',
       url='',
