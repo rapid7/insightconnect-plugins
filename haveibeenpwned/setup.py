bc5e794bc3e9c9d290ed5a75f4ab421f100bb913
Tyler Schmidtke <tyler_schmidtke@rapid7.com>
2019-12-16 12:09:04 -0600

    GRR, hashit, hibp

--- haveibeenpwned/setup.py
+++ haveibeenpwned/setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(name='haveibeenpwned-rapid7-plugin',
       version='4.0.2',
-      description='The HaveIBeenPwned plugin allows you find out if a user, domain, or password has been leaked',
+      description='Determine if a user, domain, or password has been leaked via data available in the Have I Been Pwned database',
       author='rapid7',
       author_email='',
       url='',
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/komand_haveibeenpwned']
+      scripts=['bin/icon_haveibeenpwned']
       )
