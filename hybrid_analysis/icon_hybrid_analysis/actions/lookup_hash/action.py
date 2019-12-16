1fcb5f67e76ee151d0239cb8da09b6123521956b
Tyler Schmidtke <tyler_schmidtke@rapid7.com>
2019-12-16 14:42:52 -0600

    Hybrid Analysis

--- hybrid_analysis/icon_hybrid_analysis/actions/lookup_hash/action.py
+++ hybrid_analysis/icon_hybrid_analysis/actions/lookup_hash/action.py
@@ -1,18 +1,20 @@
 import komand
 from .schema import LookupHashInput, LookupHashOutput
+
+
 # Custom imports below
 
 
 class LookupHash(komand.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='lookup_hash',
-                description='Lookup By Hash',
-                input=LookupHashInput(),
-                output=LookupHashOutput())
+            name='lookup_hash',
+            description='Lookup By Hash',
+            input=LookupHashInput(),
+            output=LookupHashOutput())
 
     def normalize(self, result):
-        formatted = { 'found': False, 'threatscore': 0, 'reports': [] }
+        formatted = {'found': False, 'threatscore': 0, 'reports': []}
         if result['response_code']:
             return formatted
 
@@ -24,19 +26,19 @@ class LookupHash(komand.Action):
         return formatted
 
     def lookup(self, hash=''):
-        r  = self.connection.get(hash)
+        r = self.connection.get(hash)
         if r.status_code == 200:
             results = r.json()
             self.logger.debug("Got results %s", results)
-            return self.normalize(results) 
-        return { 'found': False, 'threatscore': 0, 'reports': [] } 
+            return self.normalize(results)
+        return {'found': False, 'threatscore': 0, 'reports': []}
 
     def run(self, params={}):
         """Run action"""
-        return self.lookup(params['hash']) 
+        return self.lookup(params['hash'])
 
     def test(self):
         """Test action"""
-        result = self.lookup('040c0111aef474d8b7bfa9a7caa0e06b4f1049c7ae8c66611a53fc2599f0b90f') 
+        result = self.lookup('040c0111aef474d8b7bfa9a7caa0e06b4f1049c7ae8c66611a53fc2599f0b90f')
         self.logger.info("Got test result: %s", result)
         return result
