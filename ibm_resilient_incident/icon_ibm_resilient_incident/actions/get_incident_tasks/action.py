2bfa5310d8bb47483fca119e4f205fa93aa7dff6
Tyler Schmidtke <tyler_schmidtke@rapid7.com>
2019-12-16 15:00:04 -0600

    IBM Resilient

--- ibm_resilient_incident/icon_ibm_resilient_incident/actions/get_incident_tasks/action.py
+++ ibm_resilient_incident/icon_ibm_resilient_incident/actions/get_incident_tasks/action.py
@@ -6,17 +6,17 @@ from .schema import GetIncidentTasksInput, GetIncidentTasksOutput
 class GetIncidentTasks(komand.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='get_incident_tasks',
-                description='Gets the list of tasks for the incident.',
-                input=GetIncidentTasksInput(),
-                output=GetIncidentTasksOutput())
+            name='get_incident_tasks',
+            description='Gets the list of tasks for the incident.',
+            input=GetIncidentTasksInput(),
+            output=GetIncidentTasksOutput())
 
     def run(self, params={}):
         org_id = params.get("organization_id")
         inc_id = params.get("incident_id")
 
         url = self.connection.API_BASE + "/orgs/{org_id}/incidents/{inc_id}/tasks".format(org_id=org_id,
-                                                                                            inc_id=inc_id)
+                                                                                          inc_id=inc_id)
 
         self.logger.info("Retrieving tasks for incident %s..." % inc_id)
         try:
