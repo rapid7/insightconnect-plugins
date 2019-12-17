ed00f71e6a354f1d89577bdae0f58173fc759769
Jon Schipp <jonschipp@gmail.com>
2019-12-17 17:03:05 -0600

    Updates

--- awk/setup.py
+++ awk/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='awk-rapid7-plugin',
-      version='1.2.2',
-      description='This plugin manipulates an input string or file with the GNU awk programming language',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="awk-rapid7-plugin",
+      version="1.2.1",
+      description="This plugin manipulates an input string or file with the GNU awk programming language",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_awk']
