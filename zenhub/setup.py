5f56ea20911876c8b425a1c58df1ed35342afc58
Tyler Slijboom <tyler_slijboom@rapid.com>
2019-12-19 18:47:51 -0500

    one more rogue plugin failing spec

--- zenhub/setup.py
+++ zenhub/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='zenhub-rapid7-plugin',
       version='1.0.1',
-      description='Agile project management in GitHub, project road maps, kaban boards all attached to repositories',
+      description='Agile project management in GitHub.   Manage issues and epics',
       author='rapid7',
       author_email='',
       url='',
