59fc9fa952f01c7e761f2d4660a1eb68165db503
Gursimran Singh <gursimran_singh@rapid7.com>
2019-12-20 10:22:22 -0500

    Add action's missing titles

--- cef/setup.py
+++ cef/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='cef-rapid7-plugin',
-      version='1.0.1',
-      description='This plugin used for generating and manipulating event in a Common Event Format (CEF)',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="cef-rapid7-plugin",
+      version="1.0.2",
+      description="This plugin used for generating and manipulating event in a Common Event Format (CEF)",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_cef']
--- csv/setup.py
+++ csv/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='csv-rapid7-plugin',
-      version='1.1.4',
-      description='CSV (Comma Separated Values) is a simple data format for storing data. This plugin allows one to extract fields from CSV strings and files',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="csv-rapid7-plugin",
+      version="1.1.5",
+      description="CSV (Comma Separated Values) is a simple data format for storing data. This plugin allows one to extract fields from CSV strings and files",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_csv']
