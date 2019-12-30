d57ad16c816ad5ef199016c658d74a4300585e0e
Mateusz Górny <mateusz.gorny@morecode.ai>
2019-12-30 16:51:26 +0100

    #141 chaosreader amendments - Reformat file | Changed bare strings in params.get and output to static fields from schema | Removed unnecessarily comments and variables | Changed `Exception` to `PluginException` | Changed deprecated `decodestring` to `decodebytes`

--- chaosreader/setup.py
+++ chaosreader/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='chaosreader-rapid7-plugin',
-      version='1.0.2',
-      description='The plugin is used to trace sessions and fetch application data from snoop or tcpdump logs',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="chaosreader-rapid7-plugin",
+      version="1.0.3",
+      description="The plugin is used to trace sessions and fetch application data from snoop or tcpdump logs",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_chaosreader']
