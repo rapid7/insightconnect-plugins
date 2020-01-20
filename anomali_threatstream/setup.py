7ee41e40406eb51ac34efcd9ed9425b6c87e8a1c
Gursimran Singh <gursimran_singh@rapid7.com>
2020-01-20 12:14:32 -0500

    Update vendor in plugin.spec.yaml

--- anomali_threatstream/setup.py
+++ anomali_threatstream/setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup, find_packages
 
 
-setup(name="anomali_threatstream-rapid-plugin",
+setup(name="anomali_threatstream-rapid7-plugin",
       version="3.1.0",
       description="Anomali ThreatStream operationalizes threat intelligence, automating collection and integration that enables security teams to analyze and respond to threats",
-      author="rapid",
+      author="rapid7",
       author_email="",
       url="",
       packages=find_packages(),
