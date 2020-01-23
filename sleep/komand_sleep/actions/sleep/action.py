d7e9453ebe0d75c67eabad687b8160cc43f689b5
Mateusz Górny <mateusz.gorny@morecode.ai>
2020-01-24 00:06:02 +0100

    #173 sleep amendments - Use input and output constants | Change docker image from `komand/python-pypy3-plugin:2` to `komand/python-3-37-slim-plugin:3` to reduce plugin image size | Add interval input validation

--- sleep/komand_sleep/actions/sleep/action.py
+++ sleep/komand_sleep/actions/sleep/action.py
@@ -1,21 +1,21 @@
 import komand
 import time
-from .schema import SleepInput, SleepOutput
+from .schema import SleepInput, SleepOutput, Input, Output, Component
+from komand.exceptions import PluginException
 
 
 class Sleep(komand.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='sleep',
-                description='Suspend execution for an interval of time',
-                input=SleepInput(),
-                output=SleepOutput())
+            name='sleep',
+            description=Component.DESCRIPTION,
+            input=SleepInput(),
+            output=SleepOutput())
 
     def run(self, params={}):
-        _time = params.get('interval')
+        _time = params.get(Input.INTERVAL)
+        if int(_time) < 0:
+            raise PluginException(cause='Wrong input',
+                                  assistance=f"{Input.INTERVAL.capitalize()} should not be less than zero")
         time.sleep(_time)
-        return { 'slept': _time }
-
-    def test(self):
-        """TODO: Test action"""
-        return {}
+        return {Output.SLEPT: _time}
