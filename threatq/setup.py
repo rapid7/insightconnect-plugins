030c8b93a289e9445ed188f50f1bf18f5df5f341
Mike Rinehart <mike_rinehart@rapid7.com>
2020-01-26 14:26:30 -0600

    Update changelog and version

--- threatq/setup.py
+++ threatq/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='threatq-rapid7-plugin',
-      version='1.0.1',
-      description='The ThreatQ plugin provides ability to work with Threat Quotient indicators and events',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="threatq-rapid7-plugin",
+      version="1.0.2",
+      description="The ThreatQ plugin provides ability to work with Threat Quotient indicators and events",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_threatq']
