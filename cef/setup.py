9b41053f351692b3be31f470562180045c74b4de
Luke Milby <luke_milby@rapid7.com>
2020-02-13 15:48:13 -0600

    update version and regenerate

--- cef/setup.py
+++ cef/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="cef-rapid7-plugin",
-      version="1.0.2",
+      version="2.0.0",
       description="This plugin used for generating and manipulating event in a Common Event Format (CEF)",
       author="rapid7",
       author_email="",
