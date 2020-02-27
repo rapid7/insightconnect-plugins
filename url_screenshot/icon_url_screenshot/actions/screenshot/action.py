2c1c1de32b801b48807df51cb98528bb63e1ba7c
Elijah Martin-Merrill <emerrill@rapid7.com>
2020-02-27 09:44:22 -0500

    handled edge-cases with chrome, added fall-back if full-page screenshot fails

--- url_screenshot/icon_url_screenshot/actions/screenshot/action.py
+++ url_screenshot/icon_url_screenshot/actions/screenshot/action.py
@@ -22,35 +22,40 @@ class Screenshot(komand.Action):
                 input=ScreenshotInput(),
                 output=ScreenshotOutput())
 
+    def screenshot_to_file(self, driver):
+        with tempfile.NamedTemporaryFile(suffix=".png") as t:
+            driver.save_screenshot(t.name)
+            with open(t.name, "rb") as file:
+                contents = file.read()
+                file_str = base64.b64encode(contents).decode('ascii')
+        return file_str
+
     def run(self, params={}):
+        with tempfile.TemporaryDirectory() as tempdir:
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--window-size=%s" % self.WINDOW_SIZE)
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument(f"--user-data-dir={tempdir}")
+            chrome_options.binary_location = self.CHROME_PATH
 
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--window-size=%s" % self.WINDOW_SIZE)
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.binary_location = self.CHROME_PATH
-
-        url = params.get(Input.URL, "")
-        delay = params.get(Input.DELAY, 0)
-        if not url.lower().startswith('http'):
-            raise PluginException(cause="Input Error:", assistance="URLs need to start with 'http'")
-        driver = webdriver.Chrome(executable_path=self.CHROMEDRIVER_PATH, options=chrome_options)
-        driver.get(url)
-        time.sleep(delay)
-        full_page = params.get(Input.FULL_PAGE, False)
-        if full_page:
-            body = driver.find_element_by_tag_name('body')
-            element_png = body.screenshot_as_png
-            img_str = base64.b64encode(element_png).decode('ascii')
-            driver.close()
-            return {Output.SCREENSHOT: img_str}
-        else:
-            with tempfile.NamedTemporaryFile(suffix=".png") as t:
-                driver.save_screenshot(t.name)
-                driver.close()
-
-                with open(t.name, "rb") as file:
-                    contents = file.read()
-                    file_str = base64.b64encode(contents).decode('ascii')
-            driver.close()
-            return {Output.SCREENSHOT: file_str}
+            url = params.get(Input.URL, "")
+            delay = params.get(Input.DELAY, 0)
+            if not url.lower().startswith('http'):
+                raise PluginException(cause="Input Error:", assistance="URLs need to start with 'http'")
+            driver = webdriver.Chrome(executable_path=self.CHROMEDRIVER_PATH, options=chrome_options)
+            driver.get(url)
+            time.sleep(delay)
+            full_page = params.get(Input.FULL_PAGE, False)
+            if full_page:
+                try:
+                    body = driver.find_element_by_tag_name('body')
+                    img_str = body.screenshot_as_base64
+                except Exception:
+                    img_str = self.screenshot_to_file(driver)
+            else:
+                img_str = self.screenshot_to_file(driver)
+            driver.quit()
+        return {Output.SCREENSHOT: img_str}
