caaa3178656c35f6ab74adc96ee514715fb578ad
matt hofert <mhofert@rapid7.com>
2020-03-05 11:19:04 -0600

    revert vendor

--- palo_alto_pan_os/setup.py
+++ palo_alto_pan_os/setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup, find_packages
 
 
-setup(name="palo_alto_pan_os-rapid7-custom-plugin",
+setup(name="palo_alto_pan_os-rapid7-plugin",
       version="1.5.6",
       description="Manage Palo Alto Networks devices via PAN-OS",
-      author="rapid7-custom",
+      author="rapid7",
       author_email="",
       url="",
       packages=find_packages(),
