97d34ae16e226c42496a84014b9dc651c5bfc035
norenstrakh-r7 <nikolai_orenstrakh@rapid7.com>
2020-03-05 19:06:19 -0500

    WIP - make search and conent work

--- rapid7_vulndb/komand_rapid7_vulndb/actions/search_db/action.py
+++ rapid7_vulndb/komand_rapid7_vulndb/actions/search_db/action.py
@@ -15,79 +15,17 @@ class SearchDb(komand.Action):
             input=SearchDbInput(),
             output=SearchDbOutput())
 
-    # def run1(self, params={}):
-    #     # Get params
-    #     search = params.get(Input.SEARCH)
-    #     data_base = params.get(Input.DATABASE)
-    #
-    #     search = search.replace(" ", "+")
-    #     self.logger.info(f'Searching for {search}')
-    #
-    #     # Set database xpath
-    #     temp = path_helper.set_xpath(data_base)
-    #     xpath = temp['xpath']
-    #     db = temp['db']
-    #     self.logger.info('Database set')
-    #
-    #     # Create web browser
-    #     base_url = f'https://www.rapid7.com/db/search?utf8=%E2%9C%93&q={search}&t={db}'
-    #     vuldb_browser = browser.VulnDBBrowser(data_base)
-    #     results = vuldb_browser.scrape_vuldb(base_url, xpath)
-    #     print(f"RESULTS: {results}")
-    #     if results['results']:
-    #         return {Output.SEARCH_RESULTS: results['results'],
-    #                 Output.RESULTS_FOUND: results['found']}
-    #     else:
-    #         return {Output.RESULTS_FOUND: results['found']}
-
-    # def run(self, params={}):
-    #
-    #     # Get params
-    #     search = params.get(Input.SEARCH)
-    #     data_base = params.get(Input.DATABASE)
-    #
-    #     base_url = "https://vdb-kasf1i23nr1kl2j4.rapid7.com/v1/search"
-    #     vuldb_browser = browser.VulnDBBrowser(data_base)
-    #     results = vuldb_browser.scrape_vuldb(base_url, xpath)
-    #     print(f"RESULTS: {results}")
-    #     if results['results']:
-    #         return {Output.SEARCH_RESULTS: results['results'],
-    #                 Output.RESULTS_FOUND: results['found']}
-    #     else:
-    #         return {Output.RESULTS_FOUND: results['found']}
-
-
-    def t(params: Dict):
+    def run(self, params={}):
         # Get params
         search = params.get(Input.SEARCH)
         data_base = params.get(Input.DATABASE)
 
         q = {"query": search}
-        q = utils.R7DB.set_type(q, data_base)
-        data = utils.R7DB.get_query(q)
-        metadata = data['metadata']
-        results = utils.R7DB.paginate(q, metadata['total_pages'])
-        print(results)
-        return {Output.RESULTS_FOUND: None}
-
-
-
-def t(params: Dict):
-    params = {"query": "microsoft", "type": "Nexpose"}
-    URL = "https://vdb-kasf1i23nr1kl2j4.rapid7.com/v1/search"
-    resp = requests.get(URL, params=params, allow_redirects=False)
-    data = resp.json()
-    md = (data['metadata'])
-    for p in range(md['total_pages']):
-        if p > 10:
-            break
-        params["page"] = p
-        resp = requests.get(URL, params=params, allow_redirects=False)
-        data = resp.json()['data']
-        for vd in data:
-            print(f"type:{vd['type']}, content_type: {vd['content_type']}")
-
-    print(data['data'][0].keys())
-    # dict_keys(['id', 'type', 'identifier', 'title', 'description', 'data',
-    #            'references', 'created_at', 'updated_at', 'published_at',
-    #            'content_type'])
+        q = utils.R7VDB.set_query_db_type(q, data_base)
+        data = utils.R7VDB.get_query(q)
+        num_of_pages = data['metadata']['total_pages']
+        results = utils.R7VDB.paginate_search(q, num_of_pages)
+        return {
+            Output.RESULTS_FOUND: len(results),
+            Output.SEARCH_RESULTS: results,
+        }
--- rapid7_vulndb/komand_rapid7_vulndb/actions/search_db/schema.py
+++ rapid7_vulndb/komand_rapid7_vulndb/actions/search_db/schema.py
@@ -27,11 +27,9 @@ class SearchDbInput(komand.Input):
       "type": "string",
       "title": "Database",
       "description": "Name of the database",
-      "default": "All",
       "enum": [
         "Vulnerability Database",
-        "Metasploit Modules",
-        "All"
+        "Metasploit Modules"
       ],
       "order": 1
     },
@@ -60,17 +58,17 @@ class SearchDbOutput(komand.Output):
   "title": "Variables",
   "properties": {
     "results_found": {
-      "type": "boolean",
+      "type": "integer",
       "title": "Results Found",
-      "description": "Will return false if no results are found",
+      "description": "Will return number of results",
       "order": 2
     },
     "search_results": {
       "type": "array",
-      "title": "Vulnerability",
+      "title": "Results",
       "description": "Vulnerability and exploits found",
       "items": {
-        "$ref": "#/definitions/vuln_found"
+        "$ref": "#/definitions/search_result"
       },
       "order": 1
     }
@@ -79,81 +77,27 @@ class SearchDbOutput(komand.Output):
     "results_found"
   ],
   "definitions": {
-    "vuln_found": {
+    "search_result": {
       "type": "object",
-      "title": "vuln_found",
+      "title": "search_result",
       "properties": {
-        "history": {
-          "type": "string",
-          "title": "Metasploit History",
-          "description": "The history of the Metasploit module",
-          "order": 12
-        },
         "link": {
           "type": "string",
           "title": "Link",
           "description": "Link to vulnerability",
           "order": 2
         },
-        "module": {
+        "published_at": {
           "type": "string",
-          "title": "Metasploit Module",
-          "description": "Path to Metasploit module",
-          "order": 7
-        },
-        "module_name": {
-          "type": "string",
-          "title": "Metasploit Module Name",
-          "description": "The name of the Metasploit module",
-          "order": 9
-        },
-        "module_options": {
-          "type": "string",
-          "title": "Metasploit Module Options",
-          "description": "The options offered for the Metasploit module",
-          "order": 8
-        },
-        "name": {
-          "type": "string",
-          "title": "Name",
-          "description": "Name of vulnerability",
-          "order": 1
-        },
-        "published": {
-          "type": "string",
-          "title": "Published",
+          "title": "Published_at",
           "description": "Published date of vulnerability",
-          "order": 6
-        },
-        "reliability": {
-          "type": "string",
-          "title": "Reliability of Metasploit Module",
-          "description": "The reliability of running a Metasploit module",
-          "order": 10
-        },
-        "severity": {
-          "type": "string",
-          "title": "Severity",
-          "description": "Severity of vulnerability",
           "order": 3
         },
-        "source_code": {
+        "title": {
           "type": "string",
-          "title": "Source Code",
-          "description": "Source code of Metasploit module",
-          "order": 11
-        },
-        "summary": {
-          "type": "string",
-          "title": "Summary",
-          "description": "Brief summary of vulnerability",
-          "order": 5
-        },
-        "type": {
-          "type": "string",
-          "title": "Type",
-          "description": "Type of vulnerability",
-          "order": 4
+          "title": "Title",
+          "description": "Title of Vulnerability",
+          "order": 1
         }
       }
     }
