d582c6ab0af17948cf9e810c8af6e2141ebf3235
Joey McAdams <jmcadams@rapid7.com>
2020-03-13 10:15:31 -0500

    added get_address_objects action

--- fortinet_fortigate/icon_fortinet_fortigate/actions/__init__.py
+++ fortinet_fortigate/icon_fortinet_fortigate/actions/__init__.py
@@ -2,4 +2,5 @@
 from .add_address_object_to_address_group.action import AddAddressObjectToAddressGroup
 from .create_address_object.action import CreateAddressObject
 from .delete_address_object.action import DeleteAddressObject
+from .get_address_objects.action import GetAddressObjects
 from .get_policies.action import GetPolicies
