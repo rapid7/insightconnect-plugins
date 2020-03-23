7720826291726b8b878fd98e0a351d710af4c559
Mike Rinehart <mike_rinehart@rapid7.com>
2020-03-23 15:58:39 -0500

    Improve conn testt

--- jenkins/komand_jenkins/connection/connection.py
+++ jenkins/komand_jenkins/connection/connection.py
@@ -2,6 +2,8 @@ import komand
 from .schema import ConnectionSchema
 # Custom imports below
 import jenkins
+from jenkins import EmptyResponseException, BadHTTPException
+from komand.exceptions import ConnectionTestException
 
 
 class Connection(komand.Connection):
@@ -9,7 +11,7 @@ class Connection(komand.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
-    def connect(self, params):
+    def connect(self, params={}):
         username = params.get('credentials').get('username')
         password = params.get('credentials').get('password')
         host = params.get('host')
@@ -19,6 +21,17 @@ class Connection(komand.Connection):
         self.server = jenkins.Jenkins(host, username=username, password=password)
 
     def test(self):
-        user = self.server.get_whoami()
-        return {'user': user}
+        try:
+            self.server.get_whoami()
+        except EmptyResponseException as e:
+            raise ConnectionTestException(cause="An empty response was received while attempting to connect to Jenkins.",
+                                          assistance="Double-check your Jenkins server configuration.",
+                                          data=e)
+        except BadHTTPException as e:
+            raise ConnectionTestException(
+                cause="A bad HTTP response was received while attempting to connect to Jenkins.",
+                assistance="Double-check your Jenkins server configuration and ensure it is reachable.",
+                data=e)
+
+        return {"success": True}
 
