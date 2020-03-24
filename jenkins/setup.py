af5ead30efcf161e3d4912e65bbf5b13a03c5941
Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
2020-03-24 12:28:12 -0500

    DF-4019: Update Jenkins Plugin Connection Test (#395)
    * Update connection tests and add example inputs
    
    * Improve conn testt
    
    * Fix description
    
    * Fix invalid input
    
    * Fix help
    
    * Update title
    
    * Clear creds
    
    * Add example keys
    
    * Remove connection example
    
    * Checksum
    
    * Clear credentials
    
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>

--- jenkins/setup.py
+++ jenkins/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='jenkins-rapid7-plugin',
-      version='1.1.1',
-      description='Reliably build, test, and deploy their software',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="jenkins-rapid7-plugin",
+      version="1.1.2",
+      description="Reliably build, test, and deploy their software",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_jenkins']
