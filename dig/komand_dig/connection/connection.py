c4767acc94c428f647a66c30b2158894b5fe8c01
Zachary Youtz <zachary_youtz@rapid7.com>
2020-04-06 13:44:13 -0400

    Update dig plugin to use v4 plugin runtime

--- dig/komand_dig/connection/connection.py
+++ dig/komand_dig/connection/connection.py
@@ -1,8 +1,8 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import ConnectionSchema
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
 
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
