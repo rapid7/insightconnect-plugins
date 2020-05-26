3e254759c64d385ebf0a07168f4db8b1b019a89e
Mike Rinehart <mike_rinehart@rapid7.com>
2020-05-26 13:45:07 -0500

    Check if in subnets

--- checkpoint_ngfw/icon_checkpoint_ngfw/util/utils.py
+++ checkpoint_ngfw/icon_checkpoint_ngfw/util/utils.py
@@ -16,7 +16,17 @@ def check_if_ip_in_whitelist(ip_address: str, whitelist: [str]) -> bool:
     :return: True if the IP address is found
     """
 
-    for item in whitelist:
-        if "/" in item:
-            # handle CIDR
-        elif ipaddress.
+    # First pass check to see if it's in the raw list
+    if ip_address in whitelist:
+        return True
+
+    # IP address not present, so now iterate through subnets
+    ip_obj = ipaddress.ip_address(ip_address)
+
+    networks = {ipaddress.ip_network(item) for item in whitelist if "/" in item}
+
+    for network in networks:
+        if ip_obj in network:
+            return True
+
+    return False
