232ca5c89600f8284da7230981b6a127daa03515
r7-kszczepanskagorna <65252060+r7-kszczepanskagorna@users.noreply.github.com>
2020-05-27 00:56:45 +0200

    New action markdown to text (#485)
    * Markdown to text - New action: Markdown to TXT
    
    * Markdown to text - New action: Markdown to TXT
    
    * regenerate help.md
    
    * [SOAR-1190] Make regenerate to change checksum
    
    Co-authored-by: Mateusz Górny <mateusz.gorny@morecode.ai>
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>
    Co-authored-by: r7-mgorny <mateusz_gorny@rapid7.com>

--- markdown/setup.py
+++ markdown/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="markdown-rapid7-plugin",
-      version="3.0.0",
+      version="3.1.0",
       description="Convert markdown to and from various popular formats",
       author="rapid7",
       author_email="",
