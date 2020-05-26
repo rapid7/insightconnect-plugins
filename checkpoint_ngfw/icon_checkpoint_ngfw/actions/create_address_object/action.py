a2dca0467bd8130bb2978d3cc69a0eb638647679
Mike Rinehart <mike_rinehart@rapid7.com>
2020-05-26 13:36:54 -0500

    Progress

--- checkpoint_ngfw/icon_checkpoint_ngfw/actions/create_address_object/action.py
+++ checkpoint_ngfw/icon_checkpoint_ngfw/actions/create_address_object/action.py
@@ -1,6 +1,9 @@
 import komand
 from .schema import CreateAddressObjectInput, CreateAddressObjectOutput, Input, Output, Component
 # Custom imports below
+import ipaddress
+from komand.exceptions import PluginException
+from icon_checkpoint_ngfw.util.utils import check_if_ip_in_whitelist
 
 
 class CreateAddressObject(komand.Action):
@@ -15,16 +18,29 @@ class CreateAddressObject(komand.Action):
     def run(self, params={}):
         url = f"{self.connection.server_and_port}/web_api/add-host"
 
-        ip_address = params.get(Input.HOST_IP)
+        user_ip_address = params.get(Input.HOST_IP)
         name = params.get(Input.NAME)
         whitelist = params.get(Input.WHITELIST)
-        skip_rfc1918 = params.get(Input.SKIP_RFC1918)
+        skip_rfc1918 = params.get(Input.SKIP_RFC1918, [])
 
+        try:
+            is_private = ipaddress.ip_address(user_ip_address).is_private
+        except ValueError as e:
+            # example: `ValueError: 'hello world' does not appear to be an IPv4 or IPv6 address`
+            raise PluginException(cause=f"Invalid IP address provided: {user_ip_address}",
+                                  assistance=e)
 
+        if skip_rfc1918 and is_private:
+            self.logger.info(f"Provided IP address {user_ip_address} is private - skipping!")
+            return  # TODO: Fix me
+
+        if len(whitelist) > 0 and check_if_ip_in_whitelist(ip_address=user_ip_address, whitelist=whitelist):
+            self.logger.info(f"Provided IP address {user_ip_address} was found within the whitelist - skipping!")
+            return  # TODO: Fix me
 
         payload = {
             "name": name,
-            "ip-address": ip_address
+            "ip-address": user_ip_address
         }
 
         color = params.get(Input.COLOR)
--- checkpoint_ngfw/icon_checkpoint_ngfw/util/utils.py
+++ checkpoint_ngfw/icon_checkpoint_ngfw/util/utils.py
@@ -1,7 +1,22 @@
 from enum import Enum
+import ipaddress
 
 
 class DetailsLevel(Enum):
     uid = "uid"
     standard = "standard"
     full = "full"
+
+
+def check_if_ip_in_whitelist(ip_address: str, whitelist: [str]) -> bool:
+    """
+    Check if an IP address is in a whitelist
+    :param ip_address: IP address to check for
+    :param whitelist: List of IP addresses/CIDR IP addresses to check against
+    :return: True if the IP address is found
+    """
+
+    for item in whitelist:
+        if "/" in item:
+            # handle CIDR
+        elif ipaddress.
