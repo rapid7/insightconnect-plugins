cba0f3d0ef6d8455ffb83b02932c3e05d86f5e47
Mike Rinehart <mike_rinehart@rapid7.com>
2020-05-29 13:53:24 -0500

    Preliminary

--- checkpoint_ngfw/icon_checkpoint_ngfw/actions/check_if_address_in_group/action.py
+++ checkpoint_ngfw/icon_checkpoint_ngfw/actions/check_if_address_in_group/action.py
@@ -1,8 +1,13 @@
 import komand
+from komand.exceptions import PluginException
+
 from .schema import CheckIfAddressInGroupInput, CheckIfAddressInGroupOutput, Input, Output, Component
 # Custom imports below
 from icon_checkpoint_ngfw.util.utils import DetailsLevel
 from typing import Optional
+
+import aiohttp
+import asyncio
 
 
 class CheckIfAddressInGroup(komand.Action):
@@ -70,6 +75,26 @@ class CheckIfAddressInGroup(komand.Action):
 
         return None
 
+    async def get_group(self, session: aiohttp.ClientSession, name: str) -> dict:
+        endpoint = f"{self.connection.server_and_port}/web_api/show-group"
+        payload = {
+            "name": name
+        }
+        headers = self.connection.get_headers()
+        result = await session.post(endpoint, headers=headers, json=payload, verify=self.connection.ssl_verify)  # TODO: Verify this works
+
+        try:
+            result.raise_for_status()
+        except Exception as e:
+            raise PluginException(cause=f"Could not find group {name}.",
+                                  assistance=result.text,
+                                  data=e)
+        group = await result.json()
+        return group
+
+    async def get_group_details(self, session: aiohttp.ClientSession, group_name: str) -> list:
+        pass
+
     def _get_all_groups(self) -> [dict]:
         """
         Returns all network groups found within the Check Point instance
@@ -80,6 +105,8 @@ class CheckIfAddressInGroup(komand.Action):
         limit = 500
         current_offset = 0
 
+        groups_simplified = []
+
         while True:
             response = self.connection.get_groups(details_level=DetailsLevel.full, limit=limit, offset=current_offset)
 
@@ -87,9 +114,18 @@ class CheckIfAddressInGroup(komand.Action):
             if not objects:
                 break
 
-            for group in objects:
-                self.logger.info(f"Getting hosts for group: {group['name']}")
-                full_groups.append(self.connection.get_group(name=group["name"]))
+            groups_simplified.extend(objects)
+
+            # for group in objects:
+            #     self.logger.info(f"Getting hosts for group: {group['name']}")
+            #     full_groups.append(self.connection.get_group(name=group["name"]))
             current_offset += limit
+
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=self.connection.ssl_verify)) as session:
+            tasks = []
+            for group in groups_simplified:
+                tasks.append(asyncio.Task(self.get_group_details(session=session, group_name=group["name"])))
+                # group_full = await self.get_group_details(session=session, group_name=group["name"])
+                full_groups = asyncio.gather(*tasks)
 
         return full_groups
