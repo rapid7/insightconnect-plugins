fc9f2ff7e0f56b856b713b971fd52c31ede4d01d
Mike Rinehart <mike_rinehart@rapid7.com>
2020-06-15 12:14:28 -0500

    Update API

--- symantec_endpoint_security/icon_symantec_endpoint_security/util/api.py
+++ symantec_endpoint_security/icon_symantec_endpoint_security/util/api.py
@@ -1,13 +1,24 @@
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import json
 
 from typing import Optional
 from typing import Dict
 from typing import Any
 
+from enum import Enum
+
 from logging import Logger
+import logging
 
 Agent = Dict[str, Any]
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+class HashType(Enum):
+    md5 = "MD5"
+    sha256 = "SHA256"
 
 
 class APIException(Exception):
@@ -16,13 +27,16 @@ class APIException(Exception):
         self.status_code = status_code
         self.message = message
 
+    def __str__(self):
+        return f"\nStatus code: {self.status_code}\nMessage: {self.message}"
+
 
 class APIClient(object):
     """
     API client for Symantec Endpoint Protection
     """
 
-    STATUS_CODES = {
+    _STATUS_CODES = {
         400: APIException(status_code=400, message="The parameters are invalid."),
         401: APIException(status_code=401, message="The user that is currently logged on has insufficient rights "
                                                    "to execute the web method, or the user is unauthorized."),
@@ -41,7 +55,8 @@ class APIClient(object):
                                 "Authorization": f"Bearer {self.auth_token}"}
 
     @classmethod
-    def new_client(cls, host: str, username: str, password: str, domain: str, port: str, logger: Logger):
+    def new_client(cls, host: str, username: str, password: str, domain: str, port: str,
+                   logger: Optional[Logger] = None):
         """
         Authenticates with Symantec Endpoint Protection and returns an API client
         :param host: Console host
@@ -52,6 +67,9 @@ class APIClient(object):
         :param logger: Logger to use
         :return: API Client
         """
+
+        if not logger:
+            logger = logging.Logger("symantec_api_client", logging.DEBUG)
 
         url = f"https://{host}:{port}/sepm/api/v1"
         auth_url = f"{url}/identity/authenticate"
@@ -79,8 +97,8 @@ class APIClient(object):
             return cls(base_url=url,
                        auth_token=auth_token,
                        logger=logger)
-        elif cls.STATUS_CODES.get(response.status_code) is not None:
-            raise cls.STATUS_CODES[response.status_code]
+        elif cls._STATUS_CODES.get(response.status_code) is not None:
+            raise cls._STATUS_CODES[response.status_code]
         else:
             raise APIException(status_code=None, message=f"An unhandled response was received from Symantec Endpoint "
                                                          f"Protection: {response.text}")
@@ -110,8 +128,60 @@ class APIClient(object):
                                                              "non-JSON response!")
             except (KeyError, IndexError):
                 return None
-        elif self.STATUS_CODES.get(response.status_code) is not None:
-            raise self.STATUS_CODES[response.status_code]
+        elif self._STATUS_CODES.get(response.status_code) is not None:
+            raise self._STATUS_CODES[response.status_code]
+        else:
+            raise APIException(status_code=None, message=f"An unhandled response was received from Symantec Endpoint "
+                                                         f"Protection: {response.text}")
+
+    def blacklist_file(self,
+                       blacklist_data: [str],
+                       blacklist_description: str,
+                       domain_id: str,
+                       hash_type: HashType,
+                       name: Optional[str] = None):
+        """
+        Blacklists a file hash
+        :param blacklist_data: The blacklist file’s data
+        :param blacklist_description: The blacklist file’s description
+        :param domain_id: The domain ID to which the blacklist file is applied
+        :param hash_type: The blacklist file’s hash type, either MD5 or SHA256
+        :param name: The blacklist file's ID, only required when updating an existing blacklist file
+        :return:
+        """
+
+        url = f"{self.base_url}/policy-objects/fingerprints"
+
+        status_codes = {
+            **self._STATUS_CODES,
+            409: APIException(status_code=409,
+                              message="The request could not be completed due to a conflict with the current state "
+                                      "of the target resource. The file fingerprint already exists.")
+        }
+
+        body = {
+            "data": blacklist_data,
+            "description": blacklist_description,
+            "domainId": domain_id,
+            "hashType": hash_type.value,
+            "name": name
+        }
+
+        response = self.session.post(url=url,
+                                     verify=False,
+                                     json=body)
+
+        if response.status_code == 200:
+            try:
+                id_ = response.json()["id"]
+                return id_
+            except json.JSONDecodeError:
+                raise APIException(status_code=None, message="Symantec Endpoint Protection server returned a "
+                                                             "non-JSON response!")
+            except (KeyError, IndexError):
+                return None
+        elif status_codes.get(response.status_code) is not None:
+            raise status_codes[response.status_code]
         else:
             raise APIException(status_code=None, message=f"An unhandled response was received from Symantec Endpoint "
                                                          f"Protection: {response.text}")
