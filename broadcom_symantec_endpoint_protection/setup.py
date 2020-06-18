4fdec1392264b98a8c0fce9d52096d09fdee5dd9
Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
2020-06-17 19:44:02 -0500

    Regen (#540)
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>

--- broadcom_symantec_endpoint_protection/setup.py
+++ broadcom_symantec_endpoint_protection/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="broadcom_symantec_endpoint_protection-rapid7-plugin",
-      version="1.0.0",
+      version="1.0.1",
       description="Broadcom Symantec Endpoint Protection delivers the most complete, integrated endpoint security platform on the planet",
       author="rapid7",
       author_email="",
