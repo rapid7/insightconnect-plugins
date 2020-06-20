a8061654fbd5a429b5e1e30b99e6d9d341b82e3e
r7-kszczepanskagorna <kamila_szczepanska-gorna@rapid7.com>
2020-06-20 18:11:29 +0200

    [SOAR-964] New plugin McAfee ATD witg one action Submit File

---
+++ mcafee_atd/help.md
@@ -0,0 +1,97 @@
+# Description
+[McAfee Advanced Threat Defense](https://www.mcafee.com/enterprise/en-us/products/advanced-threat-defense.html) provides an API framework for external applications to access core McAfeeATD functions through the REST protocol
+
+# Key Features
+
+* Verify blacklisted and whitelisted hash values
+
+# Requirements
+
+* Username and password
+* Base URL for McAfee ATD
+
+# Documentation
+## Setup
+
+The connection configuration accepts the following parameters:
+
+|Name|Type|Default|Required|Description|Enum|Example|
+|----|----|-------|--------|-----------|----|-------|
+|credentials|credential_username_password|None|True|Username and password|None|{"username":"user1", "password":"mypassword"}|
+|port|integer|443|False|The port number for provided host|None|443|
+|url|string|None|True|Base URL for the McAfee Advanced Threat Defense|None|https://www.example.com|
+|verify_ssl|boolean|True|False|Check the server's SSL certificate|None|True|
+
+Example input:
+
+```
+{
+  "credentials": {
+    "username":"user1",
+    "password":"mypassword"
+  },
+  "port": 443,
+  "url": "https://www.example.com",
+  "verify_ssl": true
+}
+```
+## Technical Details
+
+### Actions
+
+#### Submit Hash
+
+This action is used to action to submit a MD5 hash and check it's status.
+
+##### Input
+
+|Name|Type|Default|Required|Description|Enum|Example|
+|----|----|-------|--------|-----------|----|-------|
+|hash|string|None|True|MD5 Hash to submit|None|9de5069c5afe602b2ea0a04b66beb2c0|
+
+Example input:
+
+```
+{
+  "hash": "9de5069c5afe602b2ea0a04b66beb2c0"
+}
+```
+
+##### Output
+
+|Name|Type|Required|Description|
+|----|----|--------|-----------|
+|results|object|True|Return information about given MD5 Hash|
+|success|boolean|True|Success status of submit Hash request|
+
+Example output:
+
+```
+{
+  "results": {
+    "9de5069c5afe602b2ea0a04b66beb2c0": "Previously submitted"
+  },
+  "success": true
+}
+```
+
+### Triggers
+
+_This plugin does not contain any triggers._
+
+### Custom Output Types
+
+_This plugin does not contain any custom output types._
+## Troubleshooting
+
+_This plugin does not contain any troubleshooting information._
+
+# Version History
+
+* 1.0.0 - Initial plugin
+
+# Links
+
+## References
+
+* [McAfee Advanced Threat Defense](https://www.mcafee.com/enterprise/en-us/products/advanced-threat-defense.html)
---
+++ mcafee_atd/icon_mcafee_atd/util/api.py
@@ -0,0 +1,76 @@
+from insightconnect_plugin_runtime.exceptions import PluginException, ConnectionTestException
+import json
+import base64
+from .mcafee_request import McAfeeRequest
+
+
+class McAfeeATDAPI:
+    def __init__(self, mc_afee_request: McAfeeRequest, username: str, password: str, logger: object):
+        self.mc_afee_request = mc_afee_request
+        self.username = username
+        self.password = password
+        self.logger = logger
+        self.STATUSES = {
+            'w': 'Whitelisted',
+            'b': 'Blacklisted',
+            '0': 'Not found',
+            'j': 'Previously submitted',
+            'Invalid input data': 'Invalid hash value'
+        }
+
+    def submit_hash(self, md5_hash: str):
+        submit_hash = self._make_login_request(
+            "POST",
+            "atdHashLookup.php",
+            {'data': json.dumps({"md5": md5_hash})}
+        )
+
+        if not submit_hash.get("success", False):
+            raise PluginException(
+                cause="Unknown error occurred. ",
+                assistance="Please contact support or try again later."
+            )
+
+        results = {}
+        statuses = submit_hash.get("results", {})
+        for submitted_hash, status in statuses.items():
+            results[submitted_hash] = self.STATUSES.get(status, status)
+
+        return results
+
+    def _get_login_headers(self):
+        session_response = self.mc_afee_request.make_json_request("POST", "session.php", headers={
+            "Accept": "application/vnd.ve.v1.0+json",
+            "Content-Type": "application/json",
+            "VE-SDK-API": base64.encodebytes(
+                f"{self.username}:{self.password}".encode()
+            ).decode("utf-8").rstrip()
+        })
+
+        if session_response.get("success", False):
+            session = session_response.get("results", {}).get("session")
+            user_id = session_response.get("results", {}).get("userId")
+            return {
+                "Accept": "application/vnd.ve.v1.0+json",
+                "VE-SDK-API": base64.encodebytes(
+                    f"{session}:{user_id}".encode()
+                ).decode("utf-8").rstrip()
+            }
+
+        raise ConnectionTestException(ConnectionTestException.Preset.USERNAME_PASSWORD)
+
+    def _make_login_request(self, method: str, path: str, json_data: dict):
+        headers = None
+        try:
+            headers = self._get_login_headers()
+            response = self.mc_afee_request.make_json_request(
+                method,
+                path,
+                data=json_data,
+                headers=headers
+            )
+            return response
+        except ConnectionTestException as e:
+            raise PluginException(cause=e.cause, assistance=e.assistance, data=e.data)
+        finally:
+            self.mc_afee_request.make_json_request("DELETE", "session.php", headers=headers)
