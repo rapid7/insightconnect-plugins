d9dbd6ef3294dea8d2cde88636fd1d1fe0ac6818
Joey McAdams <jmcadams@rapid7.com>
2020-06-24 12:36:33 -0500

    added connection test

--- firepower_import_csv/icon_cisco_firepower/connection/connection.py
+++ firepower_import_csv/icon_cisco_firepower/connection/connection.py
@@ -1,6 +1,9 @@
 import insightconnect_plugin_runtime
 from .schema import ConnectionSchema, Input
 # Custom imports below
+from insightconnect_plugin_runtime.exceptions import ConnectionTestException
+from paramiko import SSHClient
+import paramiko
 
 
 class Connection(insightconnect_plugin_runtime.Connection):
@@ -15,5 +18,37 @@ class Connection(insightconnect_plugin_runtime.Connection):
         self.host = params.get(Input.SERVER)
 
     def test(self):
-        # TODO: Implement connection test
-        pass
+        self.logger.info("Testing connection to server...")
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        # ssh connect
+        ssh.connect(self.host,
+                    username=self.username,
+                    password=self.password,
+                    look_for_keys=False,
+                    timeout=30)
+
+        # ssh and run nmimport.pl
+        test_command = f"pwd"
+        stdin, stdout, stderr = ssh.exec_command(test_command)
+
+        # This will hang if the output is excessive. Around 1000 records is the limit this will reasonablly read back from.
+        stdout_str = stdout.read(1000)
+        stderr_str = stderr.read(1000)
+
+        ssh.close()
+
+        self.logger.info("Test complete.")
+        self.logger.info(f"STDOUT: {stdout_str.decode('utf-8').strip()}")
+        self.logger.info(f"STDERR: {stderr_str.decode('utf-8').strip()}")
+
+        if not stderr_str:
+            return {"success": True}
+        else:
+            raise ConnectionTestException(cause="SSH to Firepower returned an error.",
+                                          assistance=f"{stderr_str.decode('utf-8').strip()}")
+
+
+
+
