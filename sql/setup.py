af88b9efa31d87970122de0a1b578d43422f0f01
r7-mgorny <mateusz_gorny@rapid7.com>
2020-06-28 22:44:58 +0200

    [SOAR-1696] Fix issue where connection test always success

--- sql/setup.py
+++ sql/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='sql-rapid7-plugin',
-      version='2.0.5',
-      description='The SQL plugin allows you to run SQL queries',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="sql-rapid7-plugin",
+      version="2.0.6",
+      description="The SQL plugin allows you to run SQL queries",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_sql']
