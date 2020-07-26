80c6a68b5c735f207c7129a182896e0f23d85013
Jon Schipp <jonschipp@gmail.com>
2020-07-26 12:59:14 -0500

    Remove unneccessary calls to logger

--- microsoft_atp/komand_microsoft_atp/util/api.py
+++ microsoft_atp/komand_microsoft_atp/util/api.py
@@ -167,8 +167,6 @@ class WindwosDefenderATP_API:
 
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
         except json.decoder.JSONDecodeError as e:
-            self.logger.info(f"Invalid json: {e}")
             raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=response.text)
         except requests.exceptions.HTTPError as e:
-            self.logger.info(f"Call to Any Run failed: {e}")
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
