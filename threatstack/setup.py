56c124de457dff461193e9487d0c1edd2be24e9a
Mike Rinehart <mike_rinehart@rapid7.com>
2020-07-27 13:37:09 -0500

    Regen

--- threatstack/setup.py
+++ threatstack/setup.py
@@ -9,6 +9,6 @@ setup(name="threatstack-rapid7-plugin",
       author_email="",
       url="",
       packages=find_packages(),
-      install_requires=['insightconnect_plugin_runtime'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/icon_threatstack']
       )
