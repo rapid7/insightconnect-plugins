900cfa54cfa261963d48dc2b3f3a3b31f3848424
r7-kszczepanskagorna <kamila_szczepanska-gorna@rapid7.com>
2020-08-05 20:03:42 +0200

    [ SOAR-2137] Update PluginException

--- basename/komand_basename/actions/basename/action.py
+++ basename/komand_basename/actions/basename/action.py
@@ -8,17 +8,19 @@ class Basename(insightconnect_plugin_runtime.Action):
 
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='basename',
-                description='Get the basename of a path',
-                input=BasenameInput(),
-                output=BasenameOutput())
+            name='basename',
+            description='Get the basename of a path',
+            input=BasenameInput(),
+            output=BasenameOutput())
 
     def run(self, params={}):
         path = params.get(Input.PATH)
         basename = os.path.basename(path)
         if basename is None or basename == '':
-            self.logger.error(f"Not able to retrieve basename of {path}")
-            raise PluginException('Unable to find basename')
+            raise PluginException(
+                cause='Unable to find basename',
+                assistance=f"Not able to retrieve basename of {path}"
+            )
 
         return {
             Output.BASENAME: basename
