9de71b5b97ce3433669fd4f968c08b8f29fb302a
r7-kszczepanskagorna <kamila_szczepanska-gorna@rapid7.com>
2020-08-06 17:06:11 +0200

    [SOAR-2137] basename - make regenerate

--- basename/setup.py
+++ basename/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="basename-rapid7-plugin",
-      version="2.0.0",
+      version="1.1.0",
       description="This plugin is used to get the last item of a file path or URL using Python's basename utility",
       author="rapid7",
       author_email="",
