6e3884db2276cd35229c5eb55aa4295fae11cdd9
r7-kszczepanskagorna <kamila_szczepanska-gorna@rapid7.com>
2020-08-17 19:05:15 +0200

    [SOAR-2253] Remove unnecessary comment from connection

--- file_info/icon_file_info/connection/connection.py
+++ file_info/icon_file_info/connection/connection.py
@@ -9,18 +9,7 @@ class Connection(insightconnect_plugin_runtime.Connection):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
     def connect(self, params):
-        """
-        Connection config params are supplied as a dict in
-        params or also accessible in self.parameters['key']
-
-        The following will setup the var to be accessed
-          self.blah = self.parameters['blah']
-        in the action and trigger files as:
-          blah = self.connection.blah
-        """
-        # TODO: Implement connection or 'pass' if no connection is necessary
         self.logger.info("Connect: Connecting...")
 
     def test(self):
-        # TODO: Implement connection test
         pass
