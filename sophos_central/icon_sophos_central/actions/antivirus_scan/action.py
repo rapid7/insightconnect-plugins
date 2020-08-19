b3f9ff9822b92b12ffd56026168e158dda4ec892
r7-kszczepanskagorna <65252060+r7-kszczepanskagorna@users.noreply.github.com>
2020-08-19 02:21:52 +0200

    New action Check Tamper Protection Status in Sophos Central plugin (#630)
    * [SOAR-2398] New action Check Tamper Protection Status in Sophos Central plugin
    
    * [SOAR-2398] Change output

--- sophos_central/icon_sophos_central/actions/antivirus_scan/action.py
+++ sophos_central/icon_sophos_central/actions/antivirus_scan/action.py
@@ -8,39 +8,17 @@ class AntivirusScan(insightconnect_plugin_runtime.Action):
 
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='antivirus_scan',
-                description=Component.DESCRIPTION,
-                input=AntivirusScanInput(),
-                output=AntivirusScanOutput())
+            name='antivirus_scan',
+            description=Component.DESCRIPTION,
+            input=AntivirusScanInput(),
+            output=AntivirusScanOutput())
 
     def run(self, params={}):
-        uuid = None
-        page_key = None
-        agent = params.get(Input.AGENT)
-
-        for index in range(9999):
-            endpoints = self.connection.client.get_endpoints(page_key=page_key)
-            page_key = endpoints.get("pages", {}).get("nextKey", None)
-
-            for e in endpoints.get("items", []):
-                if e.get("hostname") == agent:
-                    uuid = e.get("id")
-                elif e.get("id") == agent:
-                    uuid = e.get("id")
-                elif agent in e.get("ipv4Addresses"):
-                    uuid = e.get("id")
-                elif agent in e.get("macAddresses"):
-                    uuid = e.get("id")
-                elif agent in e.get("ipv6Addresses"):
-                    uuid = e.get("id")
-
-            if page_key is None or index > endpoints.get("pages", {}).get("total", 0):
-                break
-
-        if uuid is None:
-            raise PluginException(preset=PluginException.Preset.NOT_FOUND)
-
-        antivirus_scan = self.connection.client.antivirus_scan(uuid)
+        antivirus_scan = self.connection.client.antivirus_scan(
+            self.connection.client.get_endpoint_id(
+                params.get(Input.AGENT)
+            )
+        )
 
         return {
             Output.ID: antivirus_scan.get("id"),
