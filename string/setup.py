b5d94d815fbd0e23d1559bed9c09589fabad092b
Zac Youtz <zachary_youtz@rapid7.com>
2020-08-26 08:28:18 -0400

    Update string plugin to v4 runtime and mark as cloud ready (#639)
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>

--- string/setup.py
+++ string/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 
 setup(name="string-rapid7-plugin",
-      version="1.3.0",
+      version="1.3.1",
       description="The String plugin provides common programmatic string operations",
       author="rapid7",
       author_email="",
       url="",
       packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_string']
       )
