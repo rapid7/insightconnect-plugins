aaaef908051e13c04ca6ffc11ab248d702acd783
r7-mgorny <mateusz_gorny@rapid7.com>
2020-09-04 21:32:55 +0200

    [SOAR-1942] Add `search_timeframe` input to action Search

--- splunk/komand_splunk/actions/search/action.py
+++ splunk/komand_splunk/actions/search/action.py
@@ -4,6 +4,7 @@ from .schema import SearchInput, SearchOutput, Input, Output, Component
 import json
 from datetime import datetime
 from komand.exceptions import PluginException
+from typing import Optional
 
 
 class Search(komand.Action):
@@ -16,15 +17,15 @@ class Search(komand.Action):
             output=SearchOutput())
 
     def run(self, params={}):
-        """Run action"""
-        search_timeframe = self.parse_search_timeframe(params.get(Input.SEARCH_TIMEFRAME))
+        search_timeframe = params.get(Input.SEARCH_TIMEFRAME)
         if search_timeframe:
+            parse_search = self.parse_search_timeframe(search_timeframe)
             result = self.connection.client.jobs.oneshot(
                 params.get(Input.QUERY),
                 count=params.get(Input.COUNT),
                 output_mode="json",
-                earliest_time=search_timeframe["start_time"],
-                latest_time=search_timeframe["end_time"]
+                earliest_time=parse_search["start_time"],
+                latest_time=parse_search["end_time"]
             )
         else:
             result = self.connection.client.jobs.oneshot(
@@ -41,29 +42,26 @@ class Search(komand.Action):
         return {Output.RESULT: results, Output.COUNT: count}
 
     @staticmethod
-    def parse_search_timeframe(search_timeframe: str) -> dict:
+    def parse_search_timeframe(search_timeframe: Optional[str]) -> dict:
         datetime_format = "%Y-%m-%dT%H:%M:%S"
-        if search_timeframe:
-            split_search_timeframe = search_timeframe.split("-")
-            if not split_search_timeframe[0].isdigit():
+        split_search_timeframe = search_timeframe.split("-")
+        if not split_search_timeframe[0].isdigit():
+            raise PluginException(
+                cause="Invalid search start timestamp.",
+                assistance="Start time should only be a number."
+            )
+        start_time = datetime.fromtimestamp(int(split_search_timeframe[0])).strftime(datetime_format)
+        if len(split_search_timeframe) > 1:
+            if not split_search_timeframe[1].isdigit():
                 raise PluginException(
-                    cause="Invalid search start timestamp.",
-                    assistance="Start time should only be a number."
+                    cause="Invalid search end timestamp.",
+                    assistance="End time should only be a number."
                 )
-            start_time = datetime.fromtimestamp(int(split_search_timeframe[0])).strftime(datetime_format)
-            if len(split_search_timeframe) > 1:
-                if not split_search_timeframe[1].isdigit():
-                    raise PluginException(
-                        cause="Invalid search end timestamp.",
-                        assistance="End time should only be a number."
-                    )
-                end_time = datetime.fromtimestamp(int(split_search_timeframe[1])).strftime(datetime_format)
-            else:
-                end_time = datetime.now().strftime(datetime_format)
+            end_time = datetime.fromtimestamp(int(split_search_timeframe[1])).strftime(datetime_format)
+        else:
+            end_time = datetime.now().strftime(datetime_format)
 
-            return {
-                "start_time": start_time,
-                "end_time": end_time
-            }
-
-        return {}
+        return {
+            "start_time": start_time,
+            "end_time": end_time
+        }
