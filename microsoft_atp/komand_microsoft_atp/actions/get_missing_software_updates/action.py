e77215a5a42d12cc7e0b3448f3f486e7e56d96a3
r7-mgorny <mateusz_gorny@rapid7.com>
2020-09-14 02:12:26 +0200

    [MC-17] Add new action Get Missing Software Updates

---
+++ microsoft_atp/komand_microsoft_atp/actions/get_missing_software_updates/action.py
@@ -0,0 +1,22 @@
+import insightconnect_plugin_runtime
+from .schema import GetMissingSoftwareUpdatesInput, GetMissingSoftwareUpdatesOutput, Input, Output, Component
+# Custom imports below
+
+
+class GetMissingSoftwareUpdates(insightconnect_plugin_runtime.Action):
+
+    def __init__(self):
+        super(self.__class__, self).__init__(
+                name='get_missing_software_updates',
+                description=Component.DESCRIPTION,
+                input=GetMissingSoftwareUpdatesInput(),
+                output=GetMissingSoftwareUpdatesOutput())
+
+    def run(self, params={}):
+        machine_id = self.connection.client.find_first_machine(params.get(Input.MACHINE)).get("id")
+        self.logger.info("Attempting to isolate machine id: " + machine_id)
+        return {
+            Output.UPDATES: insightconnect_plugin_runtime.helper.clean(
+                self.connection.client.get_missing_software_updates(machine_id)
+            ).get("value")
+        }
