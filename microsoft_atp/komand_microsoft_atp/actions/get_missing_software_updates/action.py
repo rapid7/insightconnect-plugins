7b3bcc3c181073ffa9bac2a14c04805f2747d7c0
Jon Schipp <jonschipp@gmail.com>
2020-09-15 17:52:27 -0500

    Update logging message

--- microsoft_atp/komand_microsoft_atp/actions/get_missing_software_updates/action.py
+++ microsoft_atp/komand_microsoft_atp/actions/get_missing_software_updates/action.py
@@ -14,7 +14,7 @@ class GetMissingSoftwareUpdates(insightconnect_plugin_runtime.Action):
 
     def run(self, params={}):
         machine_id = self.connection.client.find_first_machine(params.get(Input.MACHINE)).get("id")
-        self.logger.info("Attempting to isolate machine id: " + machine_id)
+        self.logger.info("Attempting to find machine id: " + machine_id)
         return {
             Output.UPDATES: insightconnect_plugin_runtime.helper.clean(
                 self.connection.client.get_missing_software_updates(machine_id)
