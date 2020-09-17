46d36a55f834d54559bc981cae5aacdb2b1806f7
Dawid Mandyna <dawid_mandyna@rapid7.com>
2020-09-17 22:47:53 +0100

    Corrections.

--- cisco_firepower_management_center/icon_cisco_firepower_management_center/actions/remove_address_from_group/action.py
+++ cisco_firepower_management_center/icon_cisco_firepower_management_center/actions/remove_address_from_group/action.py
@@ -8,10 +8,10 @@ class RemoveAddressFromGroup(komand.Action):
 
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='remove_address_from_group',
-                description=Component.DESCRIPTION,
-                input=RemoveAddressFromGroupInput(),
-                output=RemoveAddressFromGroupOutput())
+            name='remove_address_from_group',
+            description=Component.DESCRIPTION,
+            input=RemoveAddressFromGroupInput(),
+            output=RemoveAddressFromGroupOutput())
 
     def run(self, params={}):
         address = params.get(Input.ADDRESS)
@@ -22,21 +22,26 @@ class RemoveAddressFromGroup(komand.Action):
         if not address_group or not address_object:
             raise PluginException(cause=f"The address {address} or group {group} does not exist in Cisco Firepower.",
                                   assistance="Please enter valid names and try again.")
- 
+
         return {
             Output.NETWORK_GROUP: self.connection.cisco_firepower_api.update_address_group(
-                self._generate_payload(address_object, address_group)
+                self._generate_payload(address, address_object, address_group)
             )
         }
 
     @staticmethod
-    def _generate_payload(address_object: dict, address_group: dict) -> dict:
-
+    def _generate_payload(address: str, address_object: dict, address_group: dict) -> dict:
+        found = False
         for address_group_object in address_group.get('objects'):
-            if (address_group_object.get('name') == 'TestAddressObjectHost1' or
-                address_group_object.get('value') == 'TestAddressObjectHost1'):
+            if (address_group_object.get('name') == address_object.get('name') or
+                    address_group_object.get('value') == address_object.get('value')):
                 address_group.get('objects').remove(address_group_object)
-        
+                found = True
+
+        if not found:
+            raise PluginException(cause=f"The address {address} does not exist in the address group.",
+                                  assistance="Please enter valid names and try again.")
+
         address_group.pop('links', None)
 
         return address_group
