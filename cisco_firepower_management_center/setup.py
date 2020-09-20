0b35d3f33a12cba93bafb8d591bb690149364168
Dawid Mandyna <66844174+dmandyna-r7@users.noreply.github.com>
2020-09-20 21:54:07 +0100

    Cisco FirePower Management Centre - New Actions - Add Address to Group, Check If Address in Group and Remove Address from Group (#679)
    * Create Address Object make regenerate
    
    * Using FMCApi, reverting back to previous state.
    
    * Action written using fmcapi
    
    * Working version - pre-review.
    
    * Match Whitelist corrections.
    
    * Both actions working.
    
    * Whitespace and unused variable correction.
    
    * 3 Actions
    
    * Initial Commit
    
    * Initial Commit fix
    
    * Both actions created.
    
    * Help updated.
    
    * Make regenerate
    
    * Working version.
    
    * 2 new actions in plugin spec
    
    * All 3 actions working.
    
    * Corrections.
    
    * replacing re with validators.
    
    * CIDR validation correction.
    
    * Check CIDR correction.
    
    * Version upgrade to 1.2.0
    
    * Example output for Create Address Object
    
    * Plugin spec corrections.
    
    * Update cisco_firepower_management_center/plugin.spec.yaml
    
    * Update cisco_firepower_management_center/help.md
    
    * Update description for Check if Address in Group action
    
    * Remove period in desc
    
    * Update plugin description in help.md
    
    * Update descx
    
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>
    Co-authored-by: Jon Schipp <jonschipp@gmail.com>

--- cisco_firepower_management_center/setup.py
+++ cisco_firepower_management_center/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="cisco_firepower_management_center-rapid7-plugin",
-      version="1.1.0",
+      version="1.2.0",
       description="This plugin utilizes Cisco Firepower Management Center to create URL block policies and manage address objects to block hosts",
       author="rapid7",
       author_email="",
