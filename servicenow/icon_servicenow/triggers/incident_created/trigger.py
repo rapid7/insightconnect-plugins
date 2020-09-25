ef11a5dbf3aec0f5eb1ed66949d26fd436dbe82a
Ned Cramer <ned_cramer@rapid7.com>
2020-09-25 16:33:03 -0400

    Change for readability and add error handling

--- servicenow/icon_servicenow/triggers/incident_created/trigger.py
+++ servicenow/icon_servicenow/triggers/incident_created/trigger.py
@@ -11,10 +11,10 @@ class IncidentCreated(insightconnect_plugin_runtime.Trigger):
 
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='incident_created',
-                description=Component.DESCRIPTION,
-                input=IncidentCreatedInput(),
-                output=IncidentCreatedOutput())
+            name='incident_created',
+            description=Component.DESCRIPTION,
+            input=IncidentCreatedInput(),
+            output=IncidentCreatedOutput())
         self.last_poll_time = (datetime.now(tz=timezone.utc) - timedelta(seconds=1)).replace(microsecond=0)
         self.most_recent_system_id = ""
         self.last_sys_id = ""
@@ -33,26 +33,39 @@ class IncidentCreated(insightconnect_plugin_runtime.Trigger):
 
         # Incidents stored from least to most recent date
         # Loop from most recent incident to least recent incident
-        for i in range(len(incidents)-1, -1, -1):
-            incident = incidents[i]
-            d = datetime.strptime(incident[0], "%Y-%m-%d %H:%M:%S")
 
-            # Turn offset naive date into offset aware date
-            date_aware = utc.localize(d)
+        most_recent_incident = True
+        for incident in sorted(incidents, reverse=True):
+            if len(incident) == 2:
+                if incident[0] != "" and incident[1] != "":
+                    d = datetime.strptime(incident[0], "%Y-%m-%d %H:%M:%S")
 
-            sys_id = incident[1]
+                    # Turn offset naive date into offset aware date
+                    date_aware = utc.localize(d)
 
-            # Since descending date list, an incident that is after last polling time can break the loop
-            # Other condition ensures no duplicate triggers
-            # if date_aware > self.last_poll_time and sys_id != self.last_sys_id:
-            if date_aware >= self.last_poll_time and sys_id != self.last_sys_id:
-                self.connection.logger.info(f"Found new incident: {sys_id}")
-                self.send({Output.SYSTEM_ID: sys_id})
+                    sys_id = incident[1]
+
+                    # Since descending date list, an incident that is after last polling time can break the loop
+                    # Other condition ensures no duplicate triggers
+                    if date_aware >= self.last_poll_time and sys_id != self.last_sys_id:
+                        self.connection.logger.info(f"Found new incident: {sys_id}")
+                        if most_recent_incident is True:
+                            self.last_sys_id = sys_id
+                            most_recent_incident = False
+                        self.send({Output.SYSTEM_ID: sys_id})
+                    else:
+                        break
+                else:
+                    if incident[0] == "":
+                        self.connection.logger.warning(f"Warning: An incident could not be read -- incident creation "
+                                                       f"date could not be found. Verify that the created date field exists.")
+                    else:
+                        self.connection.logger.warning(f"Warning: An incident could not be read -- incident system id "
+                                                       f"could not be found. Verify that the system id field exists.")
+
             else:
-                break
-
-        if incidents:
-            self.last_sys_id = incidents[len(incidents)-1][1]
+                self.connection.logger.warning(f'Warning: An incident could not be read -- the incident '
+                                               f'did not have the necessary values.')
 
         # minus ~1 second to ensure that incidents between polls are not missed
         adjusted_poll = datetime.now(tz=timezone.utc) - timedelta(seconds=1)
