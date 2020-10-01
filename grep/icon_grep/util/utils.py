9cc4ed5493d012220c3322821fad299cfb670d0b
matt hofert <mhofert@rapid7.com>
2020-10-01 14:02:49 -0500

    plugin rewrite

--- grep/icon_grep/util/utils.py
+++ grep/icon_grep/util/utils.py
@@ -1,58 +1,26 @@
-import komand
 import tempfile
-import shutil
-
-# Specify path for unix utils grep and wc:
-grep = "egrep"
-wc = "wc"
-
-FOUND = "found"
-HITS = "hits"
-MATCHES = "matches"
-TEMP_PATH = tempfile.mkdtemp() + "/"
+import subprocess
 
 
-def cat_lines(log, text, pattern, behavior):
-    temp_file_name = "tmp.txt"
-    with open(TEMP_PATH + temp_file_name, 'w') as f:
-        f.write(text)
-
-    return print_lines(log, 'cat', TEMP_PATH + temp_file_name, pattern, behavior)
-
-
-def echo_lines(log, text, pattern, behavior):
-    return print_lines(log, 'echo', text, pattern, behavior)
-
-
-# Run grep on text for pattern, return stdout
-def print_lines(log, command, text, pattern, behavior):
-    if behavior == "Default":
-        cmd = "{0} '{1}' | {2} {3} > {4}results.txt".format(command, text, grep, pattern, TEMP_PATH)
-        log.info(cmd)
-        komand.helper.exec_command(cmd)
-    elif behavior == "Only matching":
-        cmd = "{0} '{1}' | {2} -o {3} > {4}results.txt".format(command, text, grep, pattern, TEMP_PATH)
-        log.info(cmd)
-        komand.helper.exec_command(cmd)
-    results = ''
-    try:
-        with open(TEMP_PATH + "results.txt", 'r') as f:
-            results = f.read()
-    except Exception as ex:
-        log.info("No Matches found")
-        log.info(ex)
-    return results
+def run_grep(text: str, pattern: str, behavior: str) -> str:
+    with tempfile.NamedTemporaryFile() as fp:
+        fp.write(text.encode())
+        fp.seek(0)
+        if behavior == 'Default':
+            matches = subprocess.check_output(['egrep', pattern, fp.name])
+        elif behavior == 'Only matching':
+            matches = subprocess.check_output(['egrep', '-o', pattern, fp.name])
+    return matches.decode()
 
 
-def process_grep(result_lines):
+def process_grep(result_lines: str) -> dict:
     matches = str.splitlines(result_lines)
     if matches:
         found = True
         hits = len(matches)
     else:
         found = False
-        matches = ""
+        matches = ''
         hits = 0
-    shutil.rmtree(TEMP_PATH)
     matches = matches if hits != 0 else []
-    return {FOUND: found, HITS: hits, MATCHES: matches}
+    return {'found': found, 'hits': hits, 'matches': matches}
