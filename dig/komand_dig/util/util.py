fcadc76ff066ea3b641f048a8c27cb75ece0a9de
r7-kszczepanskagorna <kamila_szczepanska-gorna@rapid7.com>
2020-10-04 02:47:15 +0200

    [MC-108] Fix bug when `answers` in function `execute_command` is str

--- dig/komand_dig/util/util.py
+++ dig/komand_dig/util/util.py
@@ -28,17 +28,17 @@ def execute_command(logger, cmd, answer_section_regex, flags):
     ns = util.safe_parse(re.search('SERVER: (.+?)#', stdout))
     # Grab number of answers
     answers = util.safe_parse(re.search(r'ANSWER: ([0-9]+)', stdout))
+    answer_section = None
     if util.not_empty(answers):
         answers = int(answers)
 
-    answer_section = None
-    # We need answers to continue
-    if answers > 0:
-        # Grab resolved address section
-        if flags is None:
-            answer_section = util.safe_parse(re.search(answer_section_regex, stdout))
-        else:
-            answer_section = util.safe_parse(re.search(answer_section_regex, stdout, flags=flags))
+        # We need answers to continue
+        if answers > 0:
+            # Grab resolved address section
+            if flags is None:
+                answer_section = util.safe_parse(re.search(answer_section_regex, stdout))
+            else:
+                answer_section = util.safe_parse(re.search(answer_section_regex, stdout, flags=flags))
 
     if status != "NOERROR":
         stdout = f'Resolution failed, nameserver {ns} returned {status} status'
