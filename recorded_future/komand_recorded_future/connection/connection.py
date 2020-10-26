6fbfca62c83dddbf20375a58e7cb93b7006e4c6a
Joey McAdams <jmcadams@rapid7.com>
2020-10-26 10:34:53 -0500

    added headers

--- recorded_future/komand_recorded_future/connection/connection.py
+++ recorded_future/komand_recorded_future/connection/connection.py
@@ -1,9 +1,9 @@
 import komand
 from .schema import ConnectionSchema
-
 # Custom imports below
 from rfapi import ApiV2Client
 from komand_recorded_future.util import demo_test
+import yaml
 
 
 class Connection(komand.Connection):
@@ -14,7 +14,22 @@ class Connection(komand.Connection):
 
     def connect(self, params):
         self.token = params.get("api_key").get("secretKey")
-        self.client = ApiV2Client(auth=self.token)
+        self.app_version = self.setup_custom_header()
+        self.app_name = "rapid7_insightconnect"
+        self.headers = {
+            "User-Agent": f"{self.app_name}/{self.app_version}",
+            "X-RFToken": self.token
+        }
+
+        self.client = ApiV2Client(auth=self.token, app_name=self.app_name, app_version=self.app_version)
 
     def test(self):
         return demo_test.demo_test(self.token, self.logger)
+
+    def setup_custom_header(self):
+        with open("../plugin.spec.yaml") as f:
+            spec = yaml.load(f)
+
+        return spec.get("version")
+
+
