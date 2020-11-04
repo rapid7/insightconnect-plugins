da4213cf150aee7bdaded2bfefc62b1bb274b32e
dsliwinski-r7 <dominik_sliwinski@rapid7.com>
2020-11-04 13:29:55 +0100

    [MC-26] Datetime new action Get Future Time

--- datetime/komand_datetime/actions/__init__.py
+++ datetime/komand_datetime/actions/__init__.py
@@ -3,6 +3,7 @@ from .add_to_datetime.action import AddToDatetime
 from .date_from_epoch.action import DateFromEpoch
 from .epoch_from_date.action import EpochFromDate
 from .get_datetime.action import GetDatetime
+from .get_future_time.action import GetFutureTime
 from .subtract_from_datetime.action import SubtractFromDatetime
 from .time_elapsed.action import TimeElapsed
 from .to_localtime.action import ToLocaltime
