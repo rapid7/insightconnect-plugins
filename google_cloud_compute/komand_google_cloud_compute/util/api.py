935a2672e0477347df589f44a909ed1d161b031e
Mateusz Górny <mateusz.gorny@io-commander.ai>
2020-11-28 21:25:46 +0100

    [MC-201] Google Cloud Compute improve plugin

---
+++ google_cloud_compute/komand_google_cloud_compute/util/api.py
@@ -0,0 +1,195 @@
+from googleapiclient.errors import HttpError
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+class GoogleCloudComputeAPI:
+    def __init__(self, service, project_id: str):
+        self.service = service
+        self.project_id = project_id
+
+    def get_firewall(self, firewall: str):
+        request = self.service.firewalls().get(
+            project=self.project_id,
+            firewall=firewall
+        )
+        return self.execute(request)
+
+    def list_firewalls(self, filter: None, max_results: None, order_by: None, page_token: None):
+        if not filter:
+            filter = None
+        if not max_results:
+            max_results = None
+        if not order_by:
+            order_by = None
+        if not page_token:
+            page_token = None
+
+        request = self.service.firewalls().list(
+            project=self.project_id,
+            filter=filter,
+            maxResults=max_results,
+            orderBy=order_by,
+            pageToken=page_token
+        )
+        return self.execute(request)
+
+    def insert_firewall(self, body: map):
+        request = self.service.firewalls().insert(
+            project=self.project_id,
+            body=body
+        )
+        return self.execute(request)
+
+    def update_firewall(self, firewall: str, body: map):
+        request = self.service.firewalls().update(
+            project=self.project_id,
+            firewall=firewall,
+            body=body
+        )
+        return self.execute(request)
+
+    def delete_firewall(self, firewall: str):
+        request = self.service.firewalls().delete(
+            project=self.project_id,
+            firewall=firewall
+        )
+        return self.execute(request)
+
+    def disk_snapshot(self, zone: str, disk: str, snapshot_body: map):
+        request = self.service.disks().createSnapshot(
+            project=self.project_id,
+            zone=zone,
+            disk=disk,
+            body=snapshot_body
+        )
+        return self.execute(request)
+
+    def delete_snapshots(self, snapshot: str):
+        request = self.service.snapshots().delete(
+            project=self.project_id,
+            snapshot=snapshot
+        )
+        return self.execute(request)
+
+    def disk_attach(self, zone: str, instance: str, source: str):
+        attached_disk_body = {
+            "source": source
+        }
+
+        request = self.service.instances().attachDisk(
+            project=self.project_id,
+            zone=zone,
+            instance=instance,
+            body=attached_disk_body
+        )
+        return self.execute(request)
+
+    def disk_detach(self, zone: str, instance: str, device_name: str):
+        request = self.service.instances().detachDisk(
+            project=self.project_id,
+            zone=zone,
+            instance=instance,
+            deviceName=device_name
+        )
+        return self.execute(request)
+
+    def disk_list(self, zone: str, filter: None, max_results: None, order_by: None, page_token: None):
+        if not filter:
+            filter = None
+        if not max_results:
+            max_results = None
+        if not order_by:
+            order_by = None
+        if not page_token:
+            page_token = None
+
+        request = self.service.disks().list(
+            project=self.project_id,
+            zone=zone,
+            filter=filter,
+            maxResults=max_results,
+            orderBy=order_by,
+            pageToken=page_token
+        )
+        return self.execute(request)
+
+    def start_instance(self, zone: str, instance: str):
+        request = self.service.instances().start(
+            project=self.project_id,
+            zone=zone,
+            instance=instance
+        )
+        return self.execute(request)
+
+    def stop_instance(self, zone: str, instance: str):
+        request = self.service.instances().stop(
+            project=self.project_id,
+            zone=zone,
+            instance=instance
+        )
+        return self.execute(request)
+
+    def list_instances(self, zone: str, filter: None, max_results: None, order_by: None, page_token: None):
+        if not filter:
+            filter = None
+        if not max_results:
+            max_results = None
+        if not order_by:
+            order_by = None
+        if not page_token:
+            page_token = None
+
+        request = self.service.instances().list(
+            project=self.project_id,
+            zone=zone,
+            filter=filter,
+            maxResults=max_results,
+            orderBy=order_by,
+            pageToken=page_token
+        )
+        return self.execute(request)
+
+    def list_snapshots(self, filter: None, max_results: None, order_by: None, page_token: None):
+        if not filter:
+            filter = None
+        if not max_results:
+            max_results = None
+        if not order_by:
+            order_by = None
+        if not page_token:
+            page_token = None
+
+        request = self.service.snapshots().list(
+            project=self.project_id,
+            filter=filter,
+            maxResults=max_results,
+            orderBy=order_by,
+            pageToken=page_token
+        )
+        return self.execute(request)
+
+    def list_zones(self):
+        request = self.service.zones().list(
+            project=self.project_id
+        )
+        return self.execute(request)
+
+    @staticmethod
+    def execute(request):
+        try:
+            return request.execute()
+        except HttpError as e:
+            if e.resp.status == 401:
+                raise PluginException(preset=PluginException.Preset.USERNAME_PASSWORD, data=e)
+            if e.resp.status == 403:
+                raise PluginException(preset=PluginException.Preset.API_KEY, data=e)
+            if e.resp.status == 404:
+                raise PluginException(preset=PluginException.Preset.NOT_FOUND, data=e)
+            if 400 <= e.resp.status < 500:
+                raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
+            if e.resp.status >= 500:
+                raise PluginException(preset=PluginException.Preset.SERVER_ERROR, data=e)
+
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
+        except Exception as e:
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
