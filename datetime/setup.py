9dd2d53e65bcc7a11ef16bfc83265b8e2b6810a2
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2021-02-03 00:26:41 +0100

    Support epochs in ms, μs and ns in Date from Epoch action in Datetime plugin (#801)
    * [MC-253][MC-270] Support epochs in ms μs ns in Date from Epoch action in Datetime plugin
    
    * Change version

--- datetime/setup.py
+++ datetime/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="datetime-rapid7-plugin",
-      version="2.2.0",
+      version="3.0.0",
       description="This plugin manipulates timestamps using Python's maya library",
       author="rapid7",
       author_email="",
