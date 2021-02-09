a973cea5d3eb9c6ad6716a66db9fa2e6873f0272
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2021-02-10 00:21:39 +0100

    Google Cloud Compute improve plugin (#751)
    * [MC-201] Google Cloud Compute improve plugin
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * Update google_cloud_compute/help.md
    
    * [MC-201] Refactored and improved whole plugin by introducing google-api-python-client
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/komand_google_cloud_compute/util/api.py
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update google_cloud_compute/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * [MC-201] Renamed `filter` argument to `filter_` due to shadowing a built-in one.
    
    * [MC-201] Fixed setting default values in `api.py` methods. Added hint for `service` parameter in `api.py` constructor.
    
    * [MC-201] Fixed import in `api.py`.
    
    * [MC-201] Removed `pageToken` parameter and handled pagination internally.
    
    * [MC-201] Updated description of all actions in `plugin.spec.yaml` to align them with `help.md`.
    
    Co-authored-by: Mateusz Górny <mateusz.gorny@io-commander.ai>
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>
    Co-authored-by: tfranckiewicz-r7 <77052794+tfranckiewicz-r7@users.noreply.github.com>
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    Co-authored-by: tfranckiewicz-r7 <Tomasz_Franckiewicz@rapid7.com>

--- google_cloud_compute/setup.py
+++ google_cloud_compute/setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 
-setup(name='google_cloud_compute-rapid7-plugin',
-      version='2.0.2',
-      description='Google Compute Engine delivers virtual machines running in Google innovative data centers and worldwide fiber network',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="google_cloud_compute-rapid7-plugin",
+      version="3.0.0",
+      description="Google Compute Engine delivers virtual machines running in Google innovative data centers and worldwide fiber network",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_google_cloud_compute']
       )
