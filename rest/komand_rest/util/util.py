cc702844b25eb7892dd196067253ec158df64029
r7-kszczepanskagorna <65252060+r7-kszczepanskagorna@users.noreply.github.com>
2021-02-19 00:46:22 +0100

    Rest plugin - support multiple authentications (#808)
    * [MC-303] Rest plugin - support multiple authentications
    
    * [MC-303] Fix PluginException
    
    * Update rest/help.md
    
    * Major version bump and updated docs
    
    * Remove requirement for Custom to need a secret key
    
    * Add 2nd example input and update Pendo URL
    
    * [MC-303] Change ConnectionException to PluginException, change version in help.md, strip /
    
    * [MC-303] Fix issue with base url for pendo and opsgenie
    
    * Add Insight Platform example
    
    * Update rest/help.md
    
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>
    Co-authored-by: Jon Schipp <jonschipp@gmail.com>
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>

--- rest/komand_rest/util/util.py
+++ rest/komand_rest/util/util.py
@@ -1,6 +1,10 @@
-class Common:
+import requests
+from komand.exceptions import PluginException
 
+
+class Common:
     '''Merge 2 dictionaries'''
+
     @staticmethod
     def merge_dicts(x, y):
         """Given two dicts, merge them into a new dict as a shallow copy."""
@@ -9,9 +13,45 @@ class Common:
         return z
 
     '''Copy the case insensitive headers dict to a normal one'''
+
     @staticmethod
     def copy_dict(x):
         d = {}
         for key in x:
             d[key] = x[key]
         return d
+
+    @staticmethod
+    def call_api(url, path, headers, ssl_verify, auth=None):
+        try:
+            response = requests.request(
+                "GET",
+                f"{url}{path}",
+                headers=headers,
+                verify=ssl_verify,
+                auth=auth
+            )
+
+            if response.status_code == 401:
+                raise PluginException(
+                    preset=PluginException.Preset.USERNAME_PASSWORD,
+                    data=response.text
+                )
+            if response.status_code == 403:
+                raise PluginException(preset=PluginException.Preset.API_KEY, data=response.text)
+            if response.status_code == 404:
+                raise PluginException(preset=PluginException.Preset.NOT_FOUND, data=response.text)
+            if 400 <= response.status_code < 500:
+                raise PluginException(
+                    preset=PluginException.Preset.UNKNOWN,
+                    data=response.text
+                )
+            if response.status_code >= 500:
+                raise PluginException(preset=PluginException.Preset.SERVER_ERROR, data=response.text)
+
+            if 200 <= response.status_code < 300:
+                return response
+
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
+        except requests.exceptions.HTTPError as e:
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
