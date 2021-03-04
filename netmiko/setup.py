eec01477a05477c76bb9d76f4f8eaedbec15b9c9
r7-kszczepanskagorna <65252060+r7-kszczepanskagorna@users.noreply.github.com>
2021-03-04 22:46:41 +0100

    Netmiko plugin fix connection issue (#831)
    * [MC-192] Netmiko plugin fix connection issue
    
    * Update netmiko/help.md
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>

--- netmiko/setup.py
+++ netmiko/setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup, find_packages
 
 
-setup(name='netmiko-rapid7-plugin',
-      version='1.0.1',
-      description='Access network devices over SSH',
-      author='rapid7',
-      author_email='',
-      url='',
-      packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/komand_netmiko']
-      )
+setup(
+    name="netmiko-rapid7-plugin",
+    version="1.0.2",
+    description="Access network devices over SSH",
+    author="rapid7",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["komand"],  # Add third-party dependencies to requirements.txt, not here!
+    scripts=["bin/komand_netmiko"],
+)
