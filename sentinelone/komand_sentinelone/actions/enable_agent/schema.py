744f857e4167515d5f015315d08a1054da07c6d0
r7-kszczepanskagorna <kamila_szczepanska-gorna@rapid7.com>
2021-03-12 23:03:06 +0100

    [MC-325][MC-326] Enum of timezones

--- sentinelone/komand_sentinelone/actions/enable_agent/schema.py
+++ sentinelone/komand_sentinelone/actions/enable_agent/schema.py
@@ -11,15 +11,14 @@ class Input:
     AGENT = "agent"
     FILTER = "filter"
     REBOOT = "reboot"
-
+    
 
 class Output:
     AFFECTED = "affected"
-
+    
 
 class EnableAgentInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        """
+    schema = json.loads("""
    {
   "type": "object",
   "title": "Variables",
@@ -47,16 +46,14 @@ class EnableAgentInput(insightconnect_plugin_runtime.Input):
     "reboot"
   ]
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class EnableAgentOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        """
+    schema = json.loads("""
    {
   "type": "object",
   "title": "Variables",
@@ -72,8 +69,7 @@ class EnableAgentOutput(insightconnect_plugin_runtime.Output):
     "affected"
   ]
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
