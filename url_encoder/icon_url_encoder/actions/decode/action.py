e9dcff72788b5f0ecab4686f475ed572f06f08ad
Max Berezin <52976633+mberezin-r7@users.noreply.github.com>
2021-03-19 12:52:46 -0400

    SOAR-4698: URL encoder/decoder plugin (#849)
    * Initial plugin spec
    
    * Copy help readme from base64
    
    * icon-plugin generate python plugin.spec.yaml
    
    * Add encode all boolean input to schema
    
    * Update readme description of URL encoding
    
    * Split protocol prefix and rejoin if present, and check encode all chars or not
    
    * Set max split in case the URL contains an additional ://
    
    * Rewrite help readme, still need to verify accuracy. Most "base64" instances were just replaced with "URL"
    
    * Add unit test files
    
    * Try noqa in zoom plugin
    
    * Wrong branch
    
    * Split encode/decode logic into separate methods
    Regenerate plugin from spec
    Implement basic test cases
    
    * Add logging message and additional unit tests
    
    * Add TEMP icon just to pass validator
    
    * Add example field to encode_app input variable
    
    * Example should be a string
    
    * Update checksum and help from regeneration
    
    * Change encode_all example from string to boolean. Still false, will try making true next
    Change example input URL change output key "data" and "" to "url"
    Change example input URL itself
    
    * Set encode_all example to true, see if validator approves now
    Did not actually update value with correct example output yet
    
    * Get rid of special characters in description, but not in example
    
    * Run icon-plugin regenerate
    
    * Description changes were actually reverted earlier. Applying now.
    
    * The validator was probably complaining about plugin.spec.yaml not help.md
    
    * Regenerate plugin
    
    * Add unicode tests
    Update help
    
    * Fix examples, before regenerate
    
    * Fix examples, before regenerate, again
    
    * Regenerate
    
    * Undo regenerator-replaced & in ``` block with \u0026
    
    * Make consistent encode and decode methods return types
    
    * Add URL utils icon
    
    * Update help
    
    * Make title readable
    
    * Remove requests requirement
    
    * Reword help doc
    
    * decode now has real parameter errors
    
    * Update plugin spec with decode errors var
    
    * Regenerate plugin
    
    * remove -> ignore
    
    * Add replace and ignore decoder tests
    
    * regenerate
    
    * Rename plugin url_utils -> url_encoder
    
    * Implement PR review changes
    
    * Fix dockerfile
    
    * Move links to references section
    
    * Fix links, improve wording specificity
    
    * Regenerate plugin
    
    * Rephrase with URL encoding language
    
    * Regenerate
    
    * Add checksum

---
+++ url_encoder/icon_url_encoder/actions/decode/action.py
@@ -0,0 +1,37 @@
+import insightconnect_plugin_runtime
+from .schema import DecodeInput, DecodeOutput, Input, Output, Component
+from insightconnect_plugin_runtime.exceptions import PluginException
+from urllib.parse import unquote
+
+
+class Decode(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="decode",
+            description=Component.DESCRIPTION,
+            input=DecodeInput(),
+            output=DecodeOutput(),
+        )
+
+    def run(self, params={}):
+        input_url = params.get(Input.URL)
+        errors = params.get(Input.ERRORS)
+
+        try:
+            result = self.decode_url(input_url, errors)
+            return {Output.URL: result}
+        except Exception as e:
+            self.logger.error("An error has occurred while decoding ", e)
+            raise PluginException(
+                cause="Failed to decode because a valid encoded URL input was not provided.",
+                assistance="If you would like continue to attempt to decode the input try setting the value of the error field to ignore errors or to replace the characters.",
+                data=e,
+            )
+
+    def decode_url(self, input_url: str, errors: str) -> str:
+        self.logger.info(f"Decoding: {input_url}")
+
+        if errors.lower() in ["replace", "ignore"]:
+            return unquote(input_url, errors=errors)
+        else:
+            return unquote(input_url)
---
+++ url_encoder/unit_test/test_decode.py
@@ -0,0 +1,103 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../'))
+
+from unittest import TestCase
+from icon_url_utils.connection.connection import Connection
+from icon_url_utils.actions.decode import Decode
+import json
+import logging
+
+
+class TestDecode(TestCase):
+    def test_integration_decode(self):
+
+        log = logging.getLogger("Test")
+        test_conn = Connection()
+        test_action = Decode()
+
+        test_conn.logger = log
+        test_action.logger = log
+
+        try:
+            with open("../tests/decode.json") as file:
+                test_json = json.loads(file.read()).get("body")
+                connection_params = test_json.get("connection")
+                action_params = test_json.get("input")
+        except Exception as e:
+            message = """
+            Could not find or read sample tests from /tests directory
+            
+            An exception here likely means you didn't fill out your samples correctly in the /tests directory 
+            Please use 'icon-plugin generate samples', and fill out the resulting test files in the /tests directory
+            """
+            self.fail(message)
+
+
+        test_conn.connect(connection_params)
+        test_action.connection = test_conn
+        results = test_action.run(action_params)
+
+        # TODO: The following assert should be updated to look for data from your action
+        # For example: self.assertEquals({"success": True}, results) 
+        self.assertEquals({}, results)
+
+
+    def test_decode_default(self):
+
+        log = logging.getLogger("Test")
+        test_action = Decode()
+        test_action.logger = log
+        test_url = 'https://example.com/page?text=abc%24%25%5E-space%20here~%3C%3E%28%29%23%21123'
+        result = test_action.decode_url(test_url, errors='')
+
+        actual = 'https://example.com/page?text=abc$%^-space here~<>()#!123'
+        self.assertEqual(result, actual)
+
+
+    def test_decode_all(self):
+
+        log = logging.getLogger("Test")
+        test_action = Decode()
+        test_action.logger = log
+        test_url = 'https://example.com%2Fpage%3Ftext%3Dabc%24%25%5E-space%20here~%3C%3E%28%29%23%21123'
+        result = test_action.decode_url(test_url, errors='')
+
+        actual = 'https://example.com/page?text=abc$%^-space here~<>()#!123'
+        self.assertEqual(result, actual)
+
+
+    def test_decode_unicode(self):
+
+        log = logging.getLogger("Test")
+        test_action = Decode()
+        test_action.logger = log
+        test_url = '%C4%95%CF%87%C4%81m%C6%A5%C4%BC%C8%85.%C6%88%C8%AD%E1%B9%81'
+        result = test_action.decode_url(test_url, errors='')
+
+        actual = 'ĕχāmƥļȅ.ƈȭṁ'
+        self.assertEqual(result, actual)
+
+
+    def test_decode_replace(self):
+
+        log = logging.getLogger("Test")
+        test_action = Decode()
+        test_action.logger = log
+        test_url = 'example.com/utf8%3D%E2%9C%93%26replace%3D%99'
+        result = test_action.decode_url(test_url, errors='replace')
+
+        actual = 'example.com/utf8=✓&replace=�'
+        self.assertEqual(result, actual)
+
+
+    def test_decode_ignore(self):
+
+        log = logging.getLogger("Test")
+        test_action = Decode()
+        test_action.logger = log
+        test_url = 'example.com/utf8%3D%E2%9C%93%26replace%3D%99'
+        result = test_action.decode_url(test_url, errors='ignore')
+
+        actual = 'example.com/utf8=✓&replace='
+        self.assertEqual(result, actual)
