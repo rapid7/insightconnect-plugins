277d0a18b5f79d2d5d1399bd07ae14eae61c397a
r7-kszczepanskagorna <kamila_szczepanska-gorna@rapid7.com>
2021-03-27 01:53:03 +0100

    [MC-373] Strip trailing spaces in route

--- rest/komand_rest/util/util.py
+++ rest/komand_rest/util/util.py
@@ -44,7 +44,7 @@ class Common:
 
 def url_path_join(*parts):
     """Normalize url parts and join them with a slash."""
-    schemes, netlocs, paths, queries, fragments = zip(*(urlsplit(part) for part in parts))
+    schemes, netlocs, paths, queries, fragments = zip(*(urlsplit(part.strip()) for part in parts))
     scheme = first(schemes)
     netloc = first(netlocs)
     path = '/'.join(x.strip('/') for x in paths if x)
