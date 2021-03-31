bbfc91952c6693a72edfe9f24515e13384bd298e
r7-kszczepanskagorna <65252060+r7-kszczepanskagorna@users.noreply.github.com>
2021-03-31 17:36:55 +0200

    Rest plugin improvements (#856)
    * [MC-373] Rest plugin improvements
    
    * [MC-373] Rest plugin improvements
    
    * Update rest/komand_rest/connection/connection.py
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update rest/komand_rest/util/util.py
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * [MC-373] Strip trailing spaces in route
    
    * Update rest/komand_rest/util/util.py
    
    * Update rest/help.md
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>

--- rest/komand_rest/util/util.py
+++ rest/komand_rest/util/util.py
@@ -1,5 +1,10 @@
 import requests
-from komand.exceptions import PluginException
+from insightconnect_plugin_runtime.exceptions import PluginException
+from requests import Response
+from urllib.parse import urlparse, urlsplit, urlunsplit
+from logging import Logger
+from requests.auth import HTTPDigestAuth, HTTPBasicAuth
+import json
 
 
 class Common:
@@ -22,9 +27,100 @@ class Common:
         return d
 
     @staticmethod
-    def call_api(url, path, headers, ssl_verify, auth=None):
+    def body_object(response) -> dict:
+
+        body_object = {}
         try:
-            response = requests.request("GET", f"{url}{path}", headers=headers, verify=ssl_verify, auth=auth)
+            body_object = response.json()
+        except ValueError:
+            """ Nothing? We don't care if it fails, that could be normal """
+        # It's possible to have a successful call with no body
+        # https://stackoverflow.com/questions/32319845/python-requests-gives-none-response-where-json-data-is-expected
+        if body_object is None:
+            body_object = {}
+
+        return body_object
+
+
+def url_path_join(*parts):
+    """Normalize url parts and join them with a slash."""
+    schemes, netlocs, paths, queries, fragments = zip(*(urlsplit(part.strip()) for part in parts))
+    scheme = first(schemes)
+    netloc = first(netlocs)
+    path = '/'.join(x.strip('/') for x in paths if x)
+    query = first(queries)
+    fragment = first(fragments)
+    return urlunsplit((scheme, netloc, path, query, fragment))
+
+
+def first(sequence, default=''):
+    return next((x for x in sequence if x), default)
+
+
+class RestAPI(object):
+    CUSTOM_SECRET_INPUT = "CUSTOM_SECRET_INPUT"  # noqa: B105
+
+    def __init__(
+            self, url: str, logger: Logger, ssl_verify: bool, default_headers: dict = None
+    ):
+        self.url = url
+        self.logger = logger
+        self.ssl_verify = ssl_verify
+        self.auth = None
+        self.default_headers = default_headers
+
+    def with_credentials(
+            self, authentication_type: str, username: str = None, password: str = None, secret_key: str = None
+    ):
+        if authentication_type == "Basic Auth" or authentication_type == "Digest Auth":
+            if not username or not password:
+                raise PluginException(
+                    cause="Basic Auth authentication selected without providing username and password.",
+                    assistance="The authentication type requires a username and password."
+                               " Please complete the connection with a username and password or change the authentication type.",
+                )
+        else:
+            if not secret_key:
+                raise PluginException(
+                    cause="An authentication type was selected that requires a secret key.",
+                    assistance="Please complete the connection with a secret key or change the authentication type.",
+                )
+
+        if authentication_type == "Basic Auth":
+            self.auth = HTTPBasicAuth(username, password)
+        elif authentication_type == "Digest Auth":
+            self.auth = HTTPDigestAuth(username, password)
+        elif authentication_type == "Bearer Token":
+            self.default_headers = Common.merge_dicts(self.default_headers, {"Authorization": f"Bearer {secret_key}"})
+        elif authentication_type == "Rapid7 Insight":
+            self.default_headers = Common.merge_dicts(self.default_headers, {"X-Api-Key": secret_key})
+        elif authentication_type == "OpsGenie":
+            self.default_headers = Common.merge_dicts(self.default_headers, {"Authorization": f"GenieKey {secret_key}"})
+        elif authentication_type == "Pendo":
+            self.default_headers = Common.merge_dicts(
+                self.default_headers, {"content-type": "application/json", "x-pendo-integration-key": secret_key}
+            )
+        elif authentication_type == "Custom":
+            new_headers = {}
+            for key, value in self.default_headers.items():
+                if value == self.CUSTOM_SECRET_INPUT:
+                    new_headers[key] = secret_key
+                else:
+                    new_headers[key] = value
+            self.default_headers = new_headers
+
+    def call_api(
+            self, method: str, path: str, data: str = None, json_data: dict = None, headers: dict = None
+    ) -> Response:
+        try:
+            response = requests.request(
+                method,
+                url_path_join(self.url, path),
+                data=data,
+                json=json_data,
+                headers=Common.merge_dicts(self.default_headers, headers or {}),
+                auth=self.auth
+            )
 
             if response.status_code == 401:
                 raise PluginException(preset=PluginException.Preset.USERNAME_PASSWORD, data=response.text)
@@ -33,7 +129,10 @@ class Common:
             if response.status_code == 404:
                 raise PluginException(preset=PluginException.Preset.NOT_FOUND, data=response.text)
             if 400 <= response.status_code < 500:
-                raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
+                raise PluginException(
+                    preset=PluginException.Preset.UNKNOWN,
+                    data=response.json().get("message", response.text),
+                )
             if response.status_code >= 500:
                 raise PluginException(preset=PluginException.Preset.SERVER_ERROR, data=response.text)
 
@@ -41,5 +140,22 @@ class Common:
                 return response
 
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
+        except json.decoder.JSONDecodeError as e:
+            raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=e)
         except requests.exceptions.HTTPError as e:
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
+
+
+class TestRestAPI(RestAPI):
+    def __init__(self, rest_api: RestAPI):
+        super().__init__(
+            TestRestAPI.get_parsed_url(rest_api.url),
+            rest_api.logger,
+            rest_api.ssl_verify,
+            rest_api.default_headers
+        )
+
+    @staticmethod
+    def get_parsed_url(url: str):
+        url_parsed = urlparse(url)
+        return f"{url_parsed.scheme}://{url_parsed.netloc}"
