34cc092450c01dbeb6e33bc4580d0d7f85f44ace
Joey McAdams <jmcadams@rapid7.com>
2021-03-31 15:02:04 -0500

    adding some tests

---
+++ base64/unit_test/test_decode.py
@@ -0,0 +1,44 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../'))
+
+from unittest import TestCase
+from komand_base64.connection.connection import Connection
+from komand_base64.actions.decode import Decode
+import json
+import logging
+
+
+class TestDecode(TestCase):
+    def test_integration_decode(self):
+        log = logging.getLogger("Test")
+        test_conn = Connection()
+        test_action = Decode()
+
+        test_conn.logger = log
+        test_action.logger = log
+
+        test_json_file = {
+            "body": {
+                "action": "decode",
+                "input": {
+                    "base64": "c29tZSBjb250ZW50",
+                    "errors": "nothing"
+                },
+                "meta": {}
+            },
+            "type": "action_start",
+            "version": "v1"
+        }
+
+
+        test_json = test_json_file.get("body")
+        connection_params = test_json.get("connection")
+        action_params = test_json.get("input")
+
+        test_conn.connect(connection_params)
+        test_action.connection = test_conn
+        results = test_action.run(action_params)
+
+        expected = {'data': 'some content'}
+        self.assertEquals(expected, results)
---
+++ base64/unit_test/test_encode.py
@@ -0,0 +1,47 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../'))
+
+from unittest import TestCase
+from komand_base64.connection.connection import Connection
+from komand_base64.actions.encode import Encode
+import json
+import logging
+
+
+class TestEncode(TestCase):
+    def test_integration_encode(self):
+        log = logging.getLogger("Test")
+        test_conn = Connection()
+        test_action = Encode()
+
+        test_conn.logger = log
+        test_action.logger = log
+
+        test_json_file = {
+            "body": {
+                "action": "encode",
+                "connection": null,
+                "input": {
+                    "content": "some content"
+                },
+                "meta": {}
+            },
+            "type": "action_start",
+            "version": "v1"
+        }
+
+        test_json = test_json_file.get("body")
+        connection_params = test_json.get("connection")
+        action_params = test_json.get("input")
+
+        test_conn.connect(connection_params)
+        test_action.connection = test_conn
+        results = test_action.run(action_params)
+
+        # TODO: Remove this line
+        self.fail("Unimplemented test case")
+
+        # TODO: The following assert should be updated to look for data from your action
+        # For example: self.assertEquals({"success": True}, results)
+        self.assertEquals({}, results)
