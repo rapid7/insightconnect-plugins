f1f873ae1f9b2a5f06b287b46c0fb4dbddf964bd
r7-kszczepanskagorna <65252060+r7-kszczepanskagorna@users.noreply.github.com>
2021-04-19 19:17:19 +0200

    Netmiko plugin improvements (#871)
    * [MC-374] Netmiko plugin improvements
    
    * [MC-374] Change examle key
    
    * Update netmiko/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * Update netmiko/plugin.spec.yaml
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    
    * [MC-374] Regenerate after commit suggestions
    
    * [MC-374] Change example in secret
    
    * [MC-421] Fix bug where error occur when SSH Key is empty
    
    * Update netmiko/help.md
    
    * [MC-374] Fix typo and regenerate
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    Co-authored-by: Jon Schipp <30870727+jschipp-r7@users.noreply.github.com>

--- netmiko/komand_netmiko/actions/configuration_commands/action.py
+++ netmiko/komand_netmiko/actions/configuration_commands/action.py
@@ -1,30 +1,28 @@
-import komand
-from .schema import ConfigurationCommandsInput, ConfigurationCommandsOutput
+import insightconnect_plugin_runtime
+from .schema import ConfigurationCommandsInput, ConfigurationCommandsOutput, Component, Input, Output
 import netmiko
 
+from insightconnect_plugin_runtime.exceptions import PluginException
 
-class ConfigurationCommands(komand.Action):
+
+class ConfigurationCommands(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="configuration_commands",
-            description="Change the devices configuration",
+            description=Component.DESCRIPTION,
             input=ConfigurationCommandsInput(),
             output=ConfigurationCommandsOutput(),
         )
         self.device = None
 
     def run(self, params={}):
-        self.device = self.connection.client(params.get("host"))
-        any_config_command = params.get("command")
+        self.device = self.connection.client(params.get(Input.HOST))
         try:
-            output = self.device.send_config_set(any_config_command)
-            return {"results": output}
+            return {
+                Output.RESULTS: self.device.send_config_set(params.get(Input.COMMAND))
+            }
         except netmiko.NetMikoTimeoutException:
-            self.logger.error("Cannot connect/configure this device")
-            raise
-
-    def test(self):
-        output = self.connection.device_connect.is_alive()
-        if not output:
-            raise Exception("Could not connect")
-        return {"results": "Test passed!"}
+            raise PluginException(
+                cause="Cannot connect/configure this device.",
+                assistance="Please check provided connection data and try again."
+            )
