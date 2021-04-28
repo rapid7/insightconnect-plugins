5230c7de5644241b4ecd240dd7a355f0995599cc
Aine <aine_holland@rapid7.com>
2021-04-28 10:50:49 -0400

    asset search breakdown

--- rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/asset_search/action.py
+++ rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/asset_search/action.py
@@ -7,6 +7,39 @@ from .schema import AssetSearchInput, AssetSearchOutput, Input, Output, Componen
 # Custom imports below
 
 
+def format_response(resources: [], params={}):
+    assets = list()
+    ip = params.get(Input.IP)
+    hostname = params.get(Input.HOSTNAME)
+    for page in range(len(resources)):
+        current = str(resources[page])
+        asset = current.split("]},")
+        for num in range(len(asset)):
+            curr = asset[num]
+            if num == 0:
+                curr = curr[11:] + "]"
+            else:
+                curr = curr[2:] + "]"
+            if num == len(asset) - 1:
+                ending = curr.split("], 'metadata':")
+                curr = ending[0]
+                if curr[len(curr) - 1] == "}":
+                    curr = curr[:len(curr) - 1]
+            curr = "{" + curr + "}"
+            curr = ast.literal_eval(curr)
+            if hostname != "":
+                if "host_name" in curr:
+                    if hostname == curr["host_name"]:
+                        assets.append(curr)
+            elif ip != "":
+                if "ip" in curr:
+                    if ip == curr["ip"]:
+                        assets.append(curr)
+            elif hostname == "" and ip == "":
+                assets.append(curr)
+    return assets
+
+
 class AssetSearch(insightconnect_plugin_runtime.Action):
 
     def __init__(self):
@@ -18,8 +51,6 @@ class AssetSearch(insightconnect_plugin_runtime.Action):
 
     def run(self, params={}):
         size = params.get(Input.SIZE, 0)
-        ip = params.get(Input.IP)
-        hostname = params.get(Input.HOSTNAME)
         sort_criteria = params.get(Input.SORT_CRITERIA, dict())
         parameters = list()
 
@@ -36,32 +67,6 @@ class AssetSearch(insightconnect_plugin_runtime.Action):
             parameters.append(("size", 100))
             resources = self.connection.ivm_cloud_api.call_api_pages("assets", "POST", parameters)
 
-        assets = list()
-        for page in range(len(resources)):
-            current = str(resources[page])
-            asset = current.split("]},")
-            for num in range(len(asset)):
-                curr = asset[num]
-                if num == 0:
-                    curr = curr[11:] + "]"
-                else:
-                    curr = curr[2:] + "]"
-                if num == len(asset) - 1:
-                    ending = curr.split("], 'metadata':")
-                    curr = ending[0]
-                    if curr[len(curr) - 1] == "}":
-                        curr = curr[:len(curr) - 1]
-                curr = "{" + curr + "}"
-                curr = ast.literal_eval(curr)
-                if hostname != "":
-                    if "host_name" in curr:
-                        if hostname == curr["host_name"]:
-                            assets.append(curr)
-                elif ip != "":
-                    if "ip" in curr:
-                        if ip == curr["ip"]:
-                            assets.append(curr)
-                elif hostname == "" and ip == "":
-                    assets.append(curr)
+        assets = format_response(resources, params)
 
         return {Output.ASSETS: assets}
