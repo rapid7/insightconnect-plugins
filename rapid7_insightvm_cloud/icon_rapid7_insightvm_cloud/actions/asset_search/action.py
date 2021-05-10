4973a01b92525f5f5ef8b15ddd815638c5fdfa4c
Aine <aine_holland@rapid7.com>
2021-05-10 10:09:26 -0400

    I fixed it

--- rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/asset_search/action.py
+++ rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/asset_search/action.py
@@ -7,37 +7,6 @@ from .schema import AssetSearchInput, AssetSearchOutput, Input, Output, Componen
 # Custom imports below
 
 
-def format_response(resources: [], params={}):
-    results = list()
-    ip = params.get(Input.IPS)
-    hostname = params.get(Input.HOSTNAMES)
-
-    string_page = str(resources)
-    assets = string_page.split("]},")
-    for asset_number in range(len(assets)):
-        asset = assets[asset_number]
-        if asset_number == 0:
-            asset = asset[11:] + "]"
-        else:
-            asset = asset[2:] + "]"
-        if asset_number == len(assets) - 1:
-            ending = asset.split("], 'metadata':")
-            asset = ending[0]
-            if asset[len(asset) - 1] == "}":
-                asset = asset[: len(asset) - 1]
-        asset = "{" + asset + "}"
-        asset = ast.literal_eval(asset)
-        if hostname:
-            if asset.get("host_name") in hostname:
-                results.append(asset)
-        if ip:
-            if asset.get("ip") in ip:
-                results.append(asset)
-        if hostname == [] and ip == []:
-            results.append(asset)
-    return results
-
-
 class AssetSearch(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
@@ -45,6 +14,9 @@ class AssetSearch(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
+        results = list()
+        ips = params.get(Input.IPS)
+        hostnames = params.get(Input.HOSTNAMES)
         size = params.get(Input.SIZE, 0)
         sort_criteria = params.get(Input.SORT_CRITERIA, dict())
         parameters = list()
@@ -52,10 +24,18 @@ class AssetSearch(insightconnect_plugin_runtime.Action):
         for key, value in sort_criteria.items():
             parameters.append(("sort", f"{key},{value}"))
 
+        parameters.append(("size", size))
         resources = self.connection.ivm_cloud_api.call_api("assets", "POST", parameters)
 
-        assets = []
-        for asset in resources:
-            assets.append(asset)
+        assets = resources.get("data")
+        for asset in assets:
+            if hostnames:
+                if asset.get("host_name") in hostnames:
+                    results.append(asset)
+            if ips:
+                if asset.get("ip") in ips:
+                    results.append(asset)
+            if hostnames == [] and ips == []:
+                results.append(asset)
 
-        return {Output.ASSETS: assets}
+        return {Output.ASSETS: results}
--- rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/start_scan/action.py
+++ rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/start_scan/action.py
@@ -3,7 +3,6 @@ from .schema import StartScanInput, StartScanOutput, Input, Component
 
 # Custom imports below
 from insightconnect_plugin_runtime.exceptions import PluginException
-from ..asset_search.action import format_response
 
 
 class StartScan(insightconnect_plugin_runtime.Action):
@@ -15,8 +14,8 @@ class StartScan(insightconnect_plugin_runtime.Action):
     def run(self, params={}):
         asset_ids = params.get(Input.ASSET_IDS)
         name = params.get(Input.NAME)
-        ips = params.get(Input.IP)
-        hostnames = params.get(Input.HOSTNAME)
+        ips = params.get(Input.IPS)
+        hostnames = params.get(Input.HOSTNAMES)
         resources = self.connection.ivm_cloud_api.call_api("assets", "POST", params)
         if asset_ids == [] and ips == [] and hostnames == []:
             raise PluginException(
@@ -24,10 +23,16 @@ class StartScan(insightconnect_plugin_runtime.Action):
                 assistance="Please enter asset id, hostname, or ip.",
             )
         if hostnames != [] or ips != []:
-            extra_ids = format_response(resources, params)
+            extra_ids = resources.get("data")
             for extra_id in extra_ids:
-                if extra_id.get("id") not in asset_ids:
-                    asset_ids.append(extra_id.get("id"))
+                if hostnames:
+                    if extra_id.get("host_name") in hostnames:
+                        if extra_id.get("id") not in asset_ids:
+                            asset_ids.append(extra_id.get("id"))
+                if ips:
+                    if extra_id.get("ip") in ips:
+                        if extra_id.get("id") not in asset_ids:
+                            asset_ids.append(extra_id.get("id"))
 
         body = {"asset_ids": asset_ids, "name": name}
 
