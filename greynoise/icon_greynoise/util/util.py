f6ea21180c3776ad3c82334b3e7e2306272f919a
Brad Chiappetta <brad@greynoise.io>
2021-05-12 09:33:57 -0400

    fix black error on util file

--- greynoise/icon_greynoise/util/util.py
+++ greynoise/icon_greynoise/util/util.py
@@ -4,7 +4,7 @@ from insightconnect_plugin_runtime.exceptions import PluginException
 class GNRequestFailure(Exception):
     def __init__(self, status_code, message):
         self.status_code = status_code
-        self.message = message['message']
+        self.message = message["message"]
 
         raise PluginException(
             cause="Received HTTP %d status code from GreyNoise. Verify your input and try again." % self.status_code,
@@ -18,8 +18,7 @@ class GNValueError(Exception):
         self.message = message
 
         raise PluginException(
-            cause="Received HTTP 404 status code from GreyNoise."
-                  "Input provided was not found, please try another.",
+            cause="Received HTTP 404 status code from GreyNoise." "Input provided was not found, please try another.",
             assistance="If the issue persists please contact GreyNoise support.",
             data=f"{self.message}",
         )
