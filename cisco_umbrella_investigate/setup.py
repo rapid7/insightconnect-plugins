14c530fbb97e4a8356cd13a0684662fb7f1545b9
Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
2021-05-18 16:25:52 -0500

    Black (#915)

--- cisco_umbrella_investigate/setup.py
+++ cisco_umbrella_investigate/setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup, find_packages
 
 
-setup(name="cisco_umbrella_investigate-rapid7-plugin",
-      version="3.1.2",
-      description="This plugin utilizes Cisco Umbrella Investigate to get the most complete view of the relationships and evolution of Internet domains, IP addresses, and autonomous systems to pinpoint attackers infrastructures and predict future threats",
-      author="rapid7",
-      author_email="",
-      url="",
-      packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/komand_cisco_umbrella_investigate']
-      )
+setup(
+    name="cisco_umbrella_investigate-rapid7-plugin",
+    version="3.1.3",
+    description="This plugin utilizes Cisco Umbrella Investigate to get the most complete view of the relationships and evolution of Internet domains, IP addresses, and autonomous systems to pinpoint attackers infrastructures and predict future threats",
+    author="rapid7",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["komand"],  # Add third-party dependencies to requirements.txt, not here!
+    scripts=["bin/komand_cisco_umbrella_investigate"],
+)
