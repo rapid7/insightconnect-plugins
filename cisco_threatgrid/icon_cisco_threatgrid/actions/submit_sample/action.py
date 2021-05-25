a9ce242e2ba3af847b08e460ba0b5bef9b6a8ae8
r7-kszczepanskagorna <65252060+r7-kszczepanskagorna@users.noreply.github.com>
2021-05-25 23:00:05 +0200

    [MC-266] Cisco ThreatGrid plugin filename issue (#921)
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>

--- cisco_threatgrid/icon_cisco_threatgrid/actions/submit_sample/action.py
+++ cisco_threatgrid/icon_cisco_threatgrid/actions/submit_sample/action.py
@@ -16,10 +16,11 @@ class SubmitSample(komand.Action):
         )
 
     def run(self, params={}):
+        sample = params.get(Input.SAMPLE)
 
         # File handling
         try:
-            file_bytes = base64.b64decode(params.get(Input.SAMPLE, None)["content"])
+            file_bytes = base64.b64decode(sample["content"])
         except KeyError as e:
             raise PluginException(cause="The sample file is empty.", assistance="Please check your input.", data=e)
         except Exception as e:
@@ -29,13 +30,17 @@ class SubmitSample(komand.Action):
         if not isinstance(is_private, bool):
             is_private = True
 
+        filename = params.get(Input.SAMPLE_FILENAME)
+        if not filename:
+            filename = sample.get("filename")
+
         data = {
             "callback_url": params.get(Input.CALLBACK_URL),
             "email_notification": params.get(Input.EMAIL_NOTIFICATION),
             "network_exit": params.get(Input.NETWORK_EXIT),
             "playbook": params.get(Input.PLAYBOOK),
             "private": is_private,
-            "sample_filename": params.get(Input.SAMPLE_FILENAME),
+            "sample_filename": filename,
             "sample_password": params.get(Input.SAMPLE_PASSWORD),
             "tags": params.get(Input.TAGS),
             "vm": params.get(Input.VM),
