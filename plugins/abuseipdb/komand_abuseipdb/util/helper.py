52ef887b379e1aa3317cb1a9d3c6467c6e7e1a90
Mateusz Górny <mgorny@rapid7.com>
2021-05-29 19:25:18 +0200

    [MC-521] Fix error message in IPv4 search

--- plugins/abuseipdb/komand_abuseipdb/util/helper.py
+++ plugins/abuseipdb/komand_abuseipdb/util/helper.py
@@ -8,6 +8,9 @@ def get_json(response):
         if len(json_["errors"]) > 0 and "detail" in json_["errors"][0]:
             details = json_["errors"][0]["detail"]
 
-        raise PluginException(cause="Received an error response from AbuseIPDB.", assistance=details)
+        raise PluginException(
+            cause="Received an error response from AbuseIPDB.",
+            assistance=details
+        )
 
     return json_
