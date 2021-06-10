52887d18896265d0058d9cf2b37d6965c1eabf43
Joey McAdams <jmcadams@rapid7.com>
2021-06-10 12:43:10 -0500

    initial_commit

---
+++ plugins/base64/unit_test/test_encode.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from komand_base64.actions.encode import Encode
+import logging
+
+class TestEncode(TestCase):
+    def test_encode(self):
+        test_encoder = Encode()
+        log = logging.getLogger("Test")
+        test_encoder.logger = log
+
+        input_params = {
+            "content":"I like cheese"
+        }
+
+        results = test_encoder.run(input_params)
+        self.assertEqual("SSBsaWtlIGNoZWVzZQ==", results.get("data"))
+
