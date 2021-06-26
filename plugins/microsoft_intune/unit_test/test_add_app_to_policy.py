7c78aff7df3f3fe01dd3ac0cb18d8c0cc016ba68
tfranckiewicz-r7 <Tomasz_Franckiewicz@rapid7.com>
2021-06-26 19:25:11 +0200

    [MC-36] Fixes for GitHub validators.

--- plugins/microsoft_intune/unit_test/test_add_app_to_policy.py
+++ plugins/microsoft_intune/unit_test/test_add_app_to_policy.py
@@ -2,7 +2,7 @@ import sys
 import os
 import mock
 
-sys.path.append(os.path.abspath('../'))
+sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
 from icon_microsoft_intune.actions.add_app_to_policy import AddAppToPolicy
@@ -13,12 +13,13 @@ from unit_test.utils import MockConnection
 
 @mock.patch("requests.request", side_effect=mocked_requests_request)
 class TestAddAppToPolicy(TestCase):
-
     def test_add_app_to_policy(self, mock_post):
         action = AddAppToPolicy()
         action.connection = MockConnection()
 
-        actual = action.run({"application_name": "Comfy", "device_type": "android", "policy_name": "my-android-app-policy"})
+        actual = action.run(
+            {"application_name": "Comfy", "device_type": "android", "policy_name": "my-android-app-policy"}
+        )
         expected = {"success": True}
 
         self.assertEqual(actual, expected)
--- plugins/microsoft_intune/unit_test/test_delete_app_from_policy.py
+++ plugins/microsoft_intune/unit_test/test_delete_app_from_policy.py
@@ -2,7 +2,7 @@ import sys
 import os
 import mock
 
-sys.path.append(os.path.abspath('../'))
+sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
 from icon_microsoft_intune.actions.delete_app_from_policy import DeleteAppFromPolicy
@@ -13,12 +13,13 @@ from unit_test.utils import MockConnection
 
 @mock.patch("requests.request", side_effect=mocked_requests_request)
 class TestDeleteAppFromPolicy(TestCase):
-
     def test_delete_app_from_policy(self, mock_post):
         action = DeleteAppFromPolicy()
         action.connection = MockConnection()
 
-        actual = action.run({"application_name": "Comfy", "device_type": "android", "policy_name": "my-android-app-policy"})
+        actual = action.run(
+            {"application_name": "Comfy", "device_type": "android", "policy_name": "my-android-app-policy"}
+        )
         expected = {"success": True}
 
         self.assertEqual(actual, expected)
