eb39ee8da51286e6d7deba43657e196290035b02
Joey McAdams <44783688+jmcadams-r7@users.noreply.github.com>
2021-07-15 13:09:02 -0500

    Making this run build against cortext xdr

--- plugins/palo_alto_cortex_xdr/icon_palo_alto_cortex_xdr/connection/connection.py
+++ plugins/palo_alto_cortex_xdr/icon_palo_alto_cortex_xdr/connection/connection.py
@@ -38,5 +38,6 @@ class Connection(insightconnect_plugin_runtime.Connection):
         return fqdn
 
     def test(self):
+        # This code needs fixed. 
         api = CortexXdrAPI()
         api.test_connection()
