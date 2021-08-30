8260a2a1128df721dabf72b3d6901e61289b88e0
PJ Mara <pj_mara@rapid7.com>
2021-08-30 10:12:07 -0400

    tried to implement fix- pending new library upload to test properly:

--- plugins/rapid7_metasploit/komand_rapid7_metasploit/connection/connection.py
+++ plugins/rapid7_metasploit/komand_rapid7_metasploit/connection/connection.py
@@ -4,6 +4,7 @@ from .schema import ConnectionSchema
 # Custom imports below
 import ssl
 from metasploit.msfrpc import MsfRpcClient
+from komand_rapid7_metasploit.connection.schema import Input
 
 
 class Connection(komand.Connection):
@@ -19,6 +20,20 @@ class Connection(komand.Connection):
         else:
             ssl._create_default_https_context = _create_unverified_https_context
         self.logger.info("Connect: Connecting...")
+        self.logger.info(params)
+        password = unicode(params.get(Input.CREDENTIALS)["password"]).encode("utf-8")
+        new_params = {}
+        self.recursive_encode_params(params, new_params)
         params = {unicode(k).encode("utf-8"): unicode(v).encode("utf-8") for k, v in params.iteritems()}
-        self.client = MsfRpcClient(**params)
+        self.logger.info(new_params)
+        self.client = MsfRpcClient(password, **new_params)
         self.logger.info("Client connection established")
+
+    def recursive_encode_params(self, template_dict, building_dict):
+        for k, v in template_dict.iteritems():
+            new_key = unicode(k).encode("utf-8")
+            if isinstance(v, dict):
+                building_dict[new_key] = self.recursive_encode_params(template_dict[k],{})
+            else:
+                building_dict[new_key] = unicode(v).encode("utf-8")
+        return building_dict
