74035e3057aede1ebacc41feb1e69543cd4f3f8c
PJ Mara <pj_mara@rapid7.com>
2021-08-31 15:05:09 -0400

    black formatting

--- plugins/extractit/unit_test/test_url_extractor.py
+++ plugins/extractit/unit_test/test_url_extractor.py
@@ -168,11 +168,8 @@ class TestUrlExtractor(TestCase):
         action = UrlExtractor()
         actual = action.run(
             {
-                Input.STR: "<html><head>\r\nhttps://www.example.com/phishing\" style=\"box-sizing:border-box; line-height:inherit; margin:0px\"><a href=\"https://www.example.com/extractit\">https://www.example.com/extractit</a></p><p style=\"box-sizing:border-box; line-height:inherit; margin:0px\">Normal text inlaid with my link</p></div></div></td></tr></tbody></table></th></tr></tbody></table></div></body></html>"
+                Input.STR: '<html><head>\r\nhttps://www.example.com/phishing" style="box-sizing:border-box; line-height:inherit; margin:0px"><a href="https://www.example.com/extractit">https://www.example.com/extractit</a></p><p style="box-sizing:border-box; line-height:inherit; margin:0px">Normal text inlaid with my link</p></div></div></td></tr></tbody></table></th></tr></tbody></table></div></body></html>'
             }
         )
-        expected = {Output.URLS: [
-            "https://www.example.com/phishing",
-            "https://www.example.com/extractit"
-        ]}
+        expected = {Output.URLS: ["https://www.example.com/phishing", "https://www.example.com/extractit"]}
         self.assertEqual(actual, expected)
