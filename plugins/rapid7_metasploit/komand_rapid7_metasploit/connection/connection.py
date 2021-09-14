dc79b57b796c2f3420ea89d9f6c3fed01e30343a
PJ Mara <pj_mara@rapid7.com>
2021-09-14 15:15:15 -0400

    initial fix commit. Lots in here, sorry about that

--- plugins/rapid7_metasploit/komand_rapid7_metasploit/connection/connection.py
+++ plugins/rapid7_metasploit/komand_rapid7_metasploit/connection/connection.py
@@ -1,16 +1,17 @@
-import komand
-from .schema import ConnectionSchema
+import insightconnect_plugin_runtime
+from .schema import ConnectionSchema, Input
 
 # Custom imports below
 import ssl
 from metasploit.msfrpc import MsfRpcClient
-from komand_rapid7_metasploit.connection.schema import Input
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
         self.client = None
+        self.password = None
+        self.params = None
 
     def connect(self, params):
         try:
@@ -21,19 +22,12 @@ class Connection(komand.Connection):
             ssl._create_default_https_context = _create_unverified_https_context
         self.logger.info("Connect: Connecting...")
         self.logger.info(params)
-        password = unicode(params.get(Input.CREDENTIALS)["password"]).encode("utf-8")
-        new_params = {}
-        self.recursive_encode_params(params, new_params)
-        params = {unicode(k).encode("utf-8"): unicode(v).encode("utf-8") for k, v in params.iteritems()}
-        self.logger.info(new_params)
-        self.client = MsfRpcClient(password, **new_params)
+        password = params.get(Input.CREDENTIALS)["password"]
+        self.client = MsfRpcClient(password, username=params.get(Input.CREDENTIALS).get('username', 'msf'), **params)
+        self.password = password
+        self.params = params
         self.logger.info("Client connection established")
 
-    def recursive_encode_params(self, template_dict, building_dict):
-        for k, v in template_dict.iteritems():
-            new_key = unicode(k).encode("utf-8")
-            if isinstance(v, dict):
-                building_dict[new_key] = self.recursive_encode_params(template_dict[k],{})
-            else:
-                building_dict[new_key] = unicode(v).encode("utf-8")
-        return building_dict
+    def reconnect(self):
+        self.client = MsfRpcClient(self.password, username=self.params.get(Input.CREDENTIALS).
+                                   get('username', 'msf'), **self.params)
--- plugins/rapid7_metasploit/unit_test/test_new_modules.py
+++ plugins/rapid7_metasploit/unit_test/test_new_modules.py
@@ -2,32 +2,31 @@ import sys
 import os
 import json
 
-sys.path.append(os.path.abspath("../"))
+sys.path.append(os.path.abspath("../komand_rapid7_metasploit/"))
 
 from unittest import TestCase
-from komand_rapid7_metasploit.actions.search_for_exploit import SearchForExploit
+from komand_rapid7_metasploit.triggers.new_modules import NewModules
 from komand_rapid7_metasploit.connection import Connection
-import komand
+import logging
 
-class TestSearch(TestCase):
-    def test_integration_search(self):
+class TestNewModules(TestCase):
+    def test_integration_new_modules(self):
         log = logging.getLogger("Test")
         test_conn = Connection()
-        test_action = SearchForExploit()
+        test_trigger = NewModules()
 
         test_conn.logger = log
-        test_action.logger = log
+        test_trigger.logger = log
 
         try:
-            with open("../tests/search_for_exploit.json") as file:
+            with open("../tests/new_modules.json") as file:
                 test_json = json.loads(file.read()).get("body")
                 connection_params = test_json.get("connection")
-                action_params = test_json.get("input")
         except Exception as e:
             message = "Missing json file"
-
             self.fail(message)
 
         test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        results = test_action.run(action_params)
+        test_trigger.connection = test_conn
+        results = test_trigger.run()
+        self.assertIsNotNone(results)
