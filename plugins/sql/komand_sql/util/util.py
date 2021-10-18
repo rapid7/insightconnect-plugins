62902728ba2e2e59897b23a0f25ffbbc2a8ea7cb
Max Berezin <52976633+mberezin-r7@users.noreply.github.com>
2021-10-18 11:26:39 -0400

    Implement Pylint fixes for dict.get() and catch Exception

--- plugins/sql/komand_sql/util/util.py
+++ plugins/sql/komand_sql/util/util.py
@@ -16,7 +16,7 @@ def generate_results(conn_type, connection, query, parameters, logger):
             rows_affected = analyze_response["Plan Rows"]
             operation = analyze_response["Operation"]
         except KeyError:
-            if "Plan Rows" in analyze_response:
+            if analyze_response.get("Plan Rows", default_value=False):
                 rows_affected = analyze_response["Plan Rows"]
             else:
                 rows_affected = 0
@@ -51,7 +51,7 @@ def generate_results(conn_type, connection, query, parameters, logger):
             for j, col in enumerate(row.keys()):
                 try:
                     v = str(row[col])
-                except:
+                except Exception:
                     v = row[col]
                 result_row[header[j]] = v
             results.append(result_row)
