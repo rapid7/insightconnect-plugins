9738fac0ab7d57332a3d04500b10daccd49405ee
Mateusz Gorny <mateusz_gorny@rapid7.com>
2021-11-03 21:24:00 +0100

    [MC-771][MC-808] Fix unit_test expecteds files

--- plugins/rapid7_intsights/unit_test/test_get_cve_by_id.py
+++ plugins/rapid7_intsights/unit_test/test_get_cve_by_id.py
@@ -19,19 +19,19 @@ class TestGetCveByID(TestCase):
     @patch("requests.request", side_effect=Util.mock_request)
     def test_get_cve_by_id_empty(self, make_request):
         actual = self.action.run()
-        expected = Util.read_file_to_dict("expecteds/get_cve_by_id_all.json")
+        expected = Util.read_file_to_dict("expecteds/get_cve_by_id_all.json.resp")
         self.assertEqual(expected, actual)
 
     @patch("requests.request", side_effect=Util.mock_request)
     def test_get_cve_by_id_with_one_id(self, make_request):
         actual = self.action.run({Input.CVE_ID: ["CVE-2020-7064"]})
-        expected = Util.read_file_to_dict("expecteds/get_cve_by_id.json")
+        expected = Util.read_file_to_dict("expecteds/get_cve_by_id.json.resp")
         self.assertEqual(expected, actual)
 
     @patch("requests.request", side_effect=Util.mock_request)
     def test_get_cve_by_id_with_many_id(self, make_request):
         actual = self.action.run({Input.CVE_ID: ["CVE-2021-3739", "CVE-2020-7064"]})
-        expected = Util.read_file_to_dict("expecteds/get_cve_by_id_all.json")
+        expected = Util.read_file_to_dict("expecteds/get_cve_by_id_all.json.resp")
         self.assertEqual(expected, actual)
 
     @patch("requests.request", side_effect=Util.mock_request)
