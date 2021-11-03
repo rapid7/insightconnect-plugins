f26f12cedef86a1eced115afc1b88f76c3553354
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2021-11-03 17:03:51 +0100

    [MC-740] Fix threatscore KeyError (#1031)
    * [MC-740] Fix threatscore KeyError
    
    * Update plugins/hybrid_analysis/help.md
    
    * used refactor to change hash to hash_ in 2 files
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>
    Co-authored-by: PJ Mara <pj_mara@rapid7.com>
    Co-authored-by: PJ Mara <87435279+pmara-r7@users.noreply.github.com>

--- plugins/hybrid_analysis/icon_hybrid_analysis/connection/connection.py
+++ plugins/hybrid_analysis/icon_hybrid_analysis/connection/connection.py
@@ -9,12 +9,12 @@ class Connection(komand.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
-    def get(self, hash):
+    def get(self, hash_):
         if self.params.get("url"):
 
-            url = self.params.get("url") + "/scan/" + hash
+            url = self.params.get("url") + "/scan/" + hash_
         else:
-            url = "https://www.hybrid-analysis.com/api" + "/scan/" + hash
+            url = "https://www.hybrid-analysis.com/api" + "/scan/" + hash_
 
         return requests.get(
             url,
@@ -32,3 +32,9 @@ class Connection(komand.Connection):
         """ """
         self.logger.info("Connecting")
         self.params = params
+
+    def test(self):
+        """Test action"""
+        result = self.lookup("040c0111aef474d8b7bfa9a7caa0e06b4f1049c7ae8c66611a53fc2599f0b90f")
+        self.logger.info("Got test result: %s", result)
+        return result
