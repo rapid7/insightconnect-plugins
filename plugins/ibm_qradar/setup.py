9f81a7d55b04aa43f1c27ae5d340a850b6b38207
Hiren Patel <hirenkumar.patel@sacumen.com>
2022-01-11 11:26:52 +0530

    plugin version updated to 1.0.0

--- plugins/ibm_qradar/setup.py
+++ plugins/ibm_qradar/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ibm_qradar-rapid7-sacumen-plugin",
-    version="1.0.28",
+    version="1.0.0",
     description="IBM QRadar reduces billions of events and flows into a manageable number of actionable offenses that are prioritized by their impact on your business operations. This plugin allows you to use IBM QRadar to orchestrate and automate Ariel search queries and automate offense management.",
     author="rapid7-sacumen",
     author_email="",
