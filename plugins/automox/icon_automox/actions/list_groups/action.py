5e845db2bcbae500568aac1aea8fc13218ff34e3
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-01-19 12:40:52 -0500

    Automox Plugin: Initial release of plugin with base management functionality (#1042) (#1063)
    Enhancements to input descriptions for greater clarity (#1066)

---
+++ plugins/automox/icon_automox/actions/list_groups/action.py
@@ -0,0 +1,19 @@
+import insightconnect_plugin_runtime
+from .schema import ListGroupsInput, ListGroupsOutput, Input, Output, Component
+# Custom imports below
+
+
+class ListGroups(insightconnect_plugin_runtime.Action):
+
+    def __init__(self):
+        super(self.__class__, self).__init__(
+                name='list_groups',
+                description=Component.DESCRIPTION,
+                input=ListGroupsInput(),
+                output=ListGroupsOutput())
+
+    def run(self, params={}):
+        groups = self.connection.automox_api.get_groups(params.get(Input.ORG_ID))
+        self.logger.info(f"Returned {len(groups)} groups")
+
+        return {Output.GROUPS: groups}
