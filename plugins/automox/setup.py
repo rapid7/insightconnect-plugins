0186c3e610e10e8731960d37907b4ccbc58b0019
Zachary Youtz <zachary.youtz@automox.com>
2022-01-21 21:37:36 -0700

    Automox plugin enhancement to set Vulnerability Sync source on upload and return report source on retrieval

--- plugins/automox/setup.py
+++ plugins/automox/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="automox-automox-plugin",
-      version="1.0.0",
+      version="1.1.0",
       description="Automox is modernizing IT operations with continuous visibility, insight, and agility for your entire IT environment",
       author="automox",
       author_email="",
