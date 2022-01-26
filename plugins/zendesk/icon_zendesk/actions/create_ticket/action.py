31ee9537083fe8974e5ffd46bc7b82e4a133ce22
Maxim Berezin <maxim_berezin@rapid7.com>
2022-01-26 13:46:45 -0500

    Black reformat

--- plugins/zendesk/icon_zendesk/actions/create_ticket/action.py
+++ plugins/zendesk/icon_zendesk/actions/create_ticket/action.py
@@ -97,5 +97,5 @@ class CreateTicket(insightconnect_plugin_runtime.Action):
         try:
             test = self.connection.client.users.me().email
             return {"success": test}
-        except:     # pylint: disable=try-except-raise
+        except:  # pylint: disable=try-except-raise
             raise
