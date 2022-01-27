ffc2f637daddfd726ae17a9b2274298b51aa6dbe
lukaszlaszuk <93910865+lukaszlaszuk@users.noreply.github.com>
2022-01-27 17:48:38 +0100

    Soar 7769 ops genie [fix-validation] (#1081)
    * Fixed validation errors

--- plugins/opsgenie/icon_opsgenie/util/validators.py
+++ plugins/opsgenie/icon_opsgenie/util/validators.py
@@ -17,7 +17,7 @@ class InputDataValidator:
         "note": 25000,
     }
 
-    EXCEPTION_MESSAGE_LIMIT = 'Limit of maximum input characters for parameter "{parameter_name}" has been exceeded (maximum {type} {maximum})'
+    EXCEPTION_MESSAGE_LIMIT = 'Limit of maximum input characters for parameter "{parameter_name}" has been exceeded (maximum {parameter_type} {maximum})'
     EXCEPTION_MESSAGE_REQUIRED = "No required parameter has been entered"
     PARAMS_CHAR_REQUIRED = "message"
 
@@ -37,14 +37,17 @@ class InputDataValidator:
                 if isinstance(self.PARAMS_CHAR_LIMITING.get(key), int) and value is not None:
                     self._is_valid_input(value, self.PARAMS_CHAR_LIMITING.get(key), key, "characters")
 
-    def _is_valid_input(self, value: str, max_char: int, parameter_name: str, type: str) -> None:
+    def _is_valid_input(self, value: str, max_char: int, parameter_name: str, parameter_type: str) -> None:
         if self._check_params_limit(value, max_char):
             raise PluginException(
-                preset=PluginException.Preset.UNKNOWN, data=self._exception_message(parameter_name, type, max_char)
+                preset=PluginException.Preset.UNKNOWN,
+                data=self._exception_message(parameter_name, parameter_type, max_char),
             )
 
     def _check_params_limit(self, value: str, max_char: int) -> bool:
         return max_char < len(value)
 
-    def _exception_message(self, parameter_name: str, type: str, max_char: int) -> str:
-        return self.EXCEPTION_MESSAGE_LIMIT.format(parameter_name=parameter_name, type=type, maximum=max_char)
+    def _exception_message(self, parameter_name: str, parameter_type: str, max_char: int) -> str:
+        return self.EXCEPTION_MESSAGE_LIMIT.format(
+            parameter_name=parameter_name, parameter_type=parameter_type, maximum=max_char
+        )
