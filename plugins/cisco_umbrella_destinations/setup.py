6397301252e0aa83584bf646466820fbfa6740f6
Conor McNally <conor_mcnally@rapid7.com>
2022-01-31 17:10:47 +0000

    Others

--- plugins/cisco_umbrella_destinations/setup.py
+++ plugins/cisco_umbrella_destinations/setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup, find_packages
 
 
-setup(name="cisco_umbrella_destinations-rapid7-plugin",
-      version="1.0.0",
+setup(name="cisco_umbrella_destinations-conor-plugin",
+      version="1.0.1",
       description="Cisco Umbrella Destinations allows users to manage, block, and allow network destinations based on policies within their organization",
-      author="rapid7",
+      author="conor",
       author_email="",
       url="",
       packages=find_packages(),
