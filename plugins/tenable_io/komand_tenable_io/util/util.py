bbb5b931d7777d869675e53e80a87fc29d95010c
Maxim Berezin <maxim_berezin@rapid7.com>
2022-02-07 16:53:50 -0500

    Thanos snap Tenable into dust, see what Prospector does

--- plugins/tenable_io/komand_tenable_io/util/util.py
+++
@@ -1,7 +0,0 @@
-def folder_verify(conn, logger):
-    try:
-        folder = conn.folder_helper.create(name="test")
-        folder.delete()
-        return "Connection successful"
-    except Exception as e:
-        logger.error("Connection failed. Error: " + str(e))
