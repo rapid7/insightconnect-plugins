8db20f0afe02f23351f9dfa5eb145d64c7270a63
akarolik-r7 <97876326+akarolik-r7@users.noreply.github.com>
2022-02-11 13:47:23 +0100

    Active_directory_ldap: [MC-769] explicitly close LDAP connection. Moved communication method… (#1106)
    * [MC-769] explicitly close LDAP connection. Moved communication methods with LDAP to separate class
    
    * [MC-769] Refactoring for connection class and help
    
    * [MC-769] Update unit test after changing assistance message
    
    * [MC-769] Disable pylint error for connection test

--- plugins/active_directory_ldap/komand_active_directory_ldap/actions/unlock_user/action.py
+++ plugins/active_directory_ldap/komand_active_directory_ldap/actions/unlock_user/action.py
@@ -1,21 +1,19 @@
-import komand
-from .schema import UnlockUserInput, UnlockUserOutput, Input, Output, Component
-from komand_active_directory_ldap.util.utils import ADUtils, UserAccountFlags
+import insightconnect_plugin_runtime
 
 # Custom imports below
+from komand_active_directory_ldap.util.utils import UserAccountFlags
+from .schema import UnlockUserInput, UnlockUserOutput, Input, Output, Component
 
 
-class UnlockUser(komand.Action):
+class UnlockUser(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="unlock_user", description=Component.DESCRIPTION, input=UnlockUserInput(), output=UnlockUserOutput()
         )
 
     def run(self, params={}):
-        user_flag = UserAccountFlags.LOCKOUT
-        unlock_user = True
         return {
-            Output.SUCCESS: ADUtils.change_useraccountcontrol_property(
-                self.connection.conn, params.get(Input.DISTINGUISHED_NAME), unlock_user, user_flag, self.logger
+            Output.SUCCESS: self.connection.client.unblock_user(
+                params.get(Input.DISTINGUISHED_NAME), UserAccountFlags.LOCKOUT
             )
         }
