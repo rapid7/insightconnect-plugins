298d3123c5f57ba7a22ed8e3a0f2704dfb1bcf9c
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2022-02-11 13:54:52 +0100

    PowerShell_Python - MC-524 Add credentials and secret_key as python and powershell scripts variables (#1092)
    Add custom credentials in Execute Script and PowerShell String actions

--- plugins/powershell/komand_powershell/connection/connection.py
+++ plugins/powershell/komand_powershell/connection/connection.py
@@ -1,17 +1,23 @@
 import komand
-from .schema import ConnectionSchema
+from .schema import ConnectionSchema, Input
 
 
 class Connection(komand.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
+        self.username = None
+        self.password = None
+        self.auth_type = None
+        self.port = None
+        self.domain = None
+        self.kdc = None
 
-    def connect(self, params):
-        self.username = params.get("credentials").get("username")
-        self.password = params.get("credentials").get("password")
-        self.auth_type = params.get("auth")
-        self.port = params.get("port")
-        self.domain = params.get("kerberos").get("domain_name")
-        self.kdc = params.get("kerberos").get("kdc")
+    def connect(self, params={}):
+        self.username = params.get(Input.CREDENTIALS, {}).get("username")
+        self.password = params.get(Input.CREDENTIALS, {}).get("password")
+        self.auth_type = params.get(Input.AUTH, "None")
+        self.port = params.get(Input.PORT, 5986)
+        self.domain = params.get(Input.KERBEROS, {}).get("domain_name")
+        self.kdc = params.get(Input.KERBEROS, {}).get("kdc")
 
         self.logger.info("Connect: Connecting..")
