73cda7ea63c5671ee7101d7344a6f333399c3ec9
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2022-02-11 13:54:52 +0100

    PowerShell_Python - MC-524 Add credentials and secret_key as python and powershell scripts variables (#1092)
    Add custom credentials in Execute Script and PowerShell String actions

--- plugins/powershell/komand_powershell/util/util.py
+++ plugins/powershell/komand_powershell/util/util.py
@@ -2,51 +2,121 @@ import winrm
 import base64
 import subprocess  # noqa: B404
 
+import komand
+from komand.exceptions import PluginException
 
-def fix_run_ps(self, script):  # Fixes string bug in python 3 for NTLM connection
-    encoded_ps = base64.b64encode(script.encode("utf_16_le")).decode("ascii")
-    rs = self.run_cmd("powershell -encodedcommand {0}".format(encoded_ps))
-    if len(rs.std_err):
-        rs.std_err = self._clean_error_msg(rs.std_err.decode("utf-8"))
-    return rs
+DECODING_TYPE = "utf-8"
 
 
-winrm.Session.run_ps = fix_run_ps
+class FixWinrmSession(winrm.Session):
+    def run_ps(self, script: str) -> winrm.Response:  # Fixes string bug in python 3 for NTLM connection
+        encoded_ps = base64.b64encode(script.encode("utf_16_le")).decode("ascii")
+        rs = self.run_cmd(f"powershell -encodedcommand {encoded_ps}")
+        if len(rs.std_err):
+            rs.std_err = self._clean_error_msg(rs.std_err.decode(DECODING_TYPE))
+        return rs
 
 
-def local(action, powershell_script):
+def run_powershell_script(
+    auth: str,
+    action: komand.Action,
+    host_ip: str,
+    kdc: str,
+    domain: str,
+    host_name: str,
+    powershell_script: str,
+    password: str,
+    username: str,
+    port: int,
+) -> dict:
+    if auth == "None" or not host_ip:
+        data = run_script_locally(action=action, powershell_script=powershell_script)
+
+    elif auth == "NTLM":
+        data = run_script_using_ntlm(
+            action=action,
+            host_ip=host_ip,
+            powershell_script=powershell_script,
+            username=username,
+            password=password,
+            port=port,
+        )
+
+    elif auth == "Kerberos":
+        data = run_script_using_kerberos(
+            action=action,
+            host_ip=host_ip,
+            kdc=kdc,
+            domain=domain,
+            host_name=host_name,
+            powershell_script=powershell_script,
+            password=password,
+            username=username,
+            port=port,
+        )
+
+    elif auth == "CredSSP":
+        data = run_script_using_credssp(
+            action=action,
+            host_ip=host_ip,
+            powershell_script=powershell_script,
+            username=username,
+            password=password,
+            port=port,
+        )
+
+    else:
+        raise PluginException(
+            cause="No valid Authentication type was chosen.",
+            assistance="Make sure that you choose a correct Authentication type based on documentation.",
+        )
+
+    stdout = data.get("stdout")
+    stderr = data.get("stderr")
+
+    if stdout:
+        stdout = action.safe_encode(stdout)
+    if stderr:
+        stderr = action.safe_encode(stderr)
+
+    return {"stdout": stdout, "stderr": stderr}
+
+
+def run_script_locally(action: komand.Action, powershell_script: str) -> dict:
     action.logger.info("Running on local VM")
     action.logger.debug("PowerShell script: " + powershell_script)
-    process = subprocess.Popen(
+    with subprocess.Popen(
         powershell_script,
         shell="true",  # noqa: B602
         executable="pwsh",
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-    )
-    output, error = process.communicate()
-    try:
-        output = output.decode("utf-8")
-    except AttributeError:
-        pass
-    try:
-        error = error.decode("utf-8")
-    except AttributeError:
-        pass
+    ) as process:
+        stdout, error = process.communicate()
+        try:
+            stdout = stdout.decode(DECODING_TYPE)
+        except AttributeError:
+            pass
+        try:
+            error = error.decode(DECODING_TYPE)
+        except AttributeError:
+            pass
 
-    return {"output": output, "stderr": error}
+        return {"stdout": stdout, "stderr": error}
 
 
-def ntlm(action, host_ip, powershell_script, username, password, port):
+def run_script_using_ntlm(
+    action: komand.Action, host_ip: str, powershell_script: str, username: str, password: str, port: int
+) -> dict:
     # Adds needed https and port number to host IP
     action.logger.info("Running a NTLM connection")
-    host_connection = "https://{host_ip}:{port}/wsman".format(host_ip=host_ip, port=port)
+    host_connection = f"https://{host_ip}:{port}/wsman"
     action.logger.debug("Host Connection: " + host_connection)
     action.logger.debug("PowerShell script: " + powershell_script)
 
-    powershell_session = winrm.Session(host_connection, auth=(username, password), transport="ntlm")
+    powershell_session = FixWinrmSession(host_connection, auth=(username, password), transport="ntlm")
     # Forces the Protocol to not fail with self signed certs
-    p = winrm.Protocol(
+    powershell_session.protocol = winrm.Protocol(
         endpoint=host_connection,
         transport="ntlm",
         username=username,
@@ -54,123 +124,24 @@ def ntlm(action, host_ip, powershell_script, username, password, port):
         server_cert_validation="ignore",
         message_encryption="auto",
     )
-    powershell_session.protocol = p
-    run_script = powershell_session.run_ps(powershell_script)
-    exit_code = run_script.status_code
-    error_value = run_script.std_err
-    output = run_script.std_out
 
-    try:
-        error_value = error_value.decode("utf-8")
-    except AttributeError:
-        pass
-    output = output.decode("utf-8")
+    error_value, stdout = run_powershell_session(action, powershell_script, powershell_session)
 
-    if exit_code != 0:
-        action.logger.error(error_value)
-        raise Exception("An error occurred in the PowerShell script, see logging for more info")
-
-    return {"output": output, "stderr": error_value}
+    return {"stdout": stdout, "stderr": error_value}
 
 
-def kerberos(action, host_ip, kdc, domain, host_name, powershell_script, password, username, port):
-    action.logger.info("Running Kerberos connection")
-    # Adds needed https and port number to host IP
-    host_connection = "https://{host_ip}:{port}/wsman".format(host_ip=host_ip, port=port)
-    action.logger.debug("PowerShell script: " + powershell_script)
-
-    udomain = domain.upper()
-    # Config for krb5 file to the domain
-    krb_config = """[libdefaults]
-default_realm = {udomain}
-forwardable = true
-proxiable = true
-
-[realms]
-{udomain} = {{
-kdc = {kdc}
-admin_server = {kdc}
-default_domain = {udomain}
-}}
-
-[domain_realm]
-.{domain} = {udomain}
-{domain} = {udomain}""".format(
-        udomain=udomain, domain=domain, kdc=kdc
-    )
-    action.logger.debug(krb_config)
-    # Config for DNS
-    dns = "search %s\r\nnameserver %s" % (domain, kdc)
-    action.logger.debug(dns)
-    # Sends output from stdout on shell commands to logging. Preventing errors
-    subprocess.call("mkdir -p /var/lib/samba/private", shell="true")  # noqa: B607,B602
-    subprocess.call("systemctl enable sssd", shell="true")  # noqa: B607,B602
-    # Setup realm to join the domain
-    with open("/etc/krb5.conf", "w") as f:
-        f.write(krb_config)
-    # Creates a Kerberos ticket
-    kinit = """echo '%s' | kinit %s@%s""" % (password, username, domain.upper())
-    response = subprocess.Popen(kinit, shell="true", stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa: B602
-    (stdout, stderr) = response.communicate()
-    stdout = stdout.decode("utf-8")
-    stderr = stderr.decode("utf-8")
-    action.logger.info("Attempt to make Kerberos ticket stdout: " + stdout)
-    action.logger.info("Attempt to make Kerberos ticket stderr: " + stderr)
-    # DNS info so the plugin knows where to find the domain
-    with open("/etc/resolv.conf", "w") as f:
-        f.write(dns)
-    # Joins Komand to domain
-    realm = """echo '%s' | realm --install=/ join --user=%s %s""" % (password, username, domain)
-    response = subprocess.Popen(realm, shell="true", stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa: B602
-    (stdout, stderr) = response.communicate()
-    stdout = stdout.decode("utf-8")
-    stderr = stderr.decode("utf-8")
-    action.logger.info("Attempt to join domain stdout: " + stdout)
-    action.logger.info("Attempt to join domain stderr: " + stderr)
-    # Allows resolution if A name not set for host
-    with open("/etc/hosts", "a") as f:
-        f.write("\r\n" + host_ip + " " + host_name)
-
-    # Runs the script on the host
-    powershell_session = winrm.Session(host_connection, auth=(username, password), transport="kerberos")
-
-    # Forces the protocol to not fail with self signed certs
-    p = winrm.Protocol(
-        endpoint=host_connection,
-        transport="kerberos",
-        username=username,
-        password=password,
-        server_cert_validation="ignore",
-    )
-    powershell_session.protocol = p
-    run_script = powershell_session.run_ps(powershell_script)
-    exit_code = run_script.status_code
-    error_value = run_script.std_err
-    output = run_script.std_out
-
-    try:
-        error_value = error_value.decode("utf-8")
-    except AttributeError:
-        pass
-    output = output.decode("utf-8")
-
-    if exit_code != 0:
-        action.logger.error(error_value)
-        raise Exception("An error occurred in the PowerShell script, see logging for more info")
-
-    return {"output": output, "stderr": error_value}
-
-
-def credssp(action, host_ip, powershell_script, username, password, port):
+def run_script_using_credssp(
+    action: komand.Action, host_ip: str, powershell_script: str, username: str, password: str, port: int
+) -> dict:
     # Adds needed https and port number to host IP
     action.logger.info("Running a CredSSP connection")
-    host_connection = "https://{host_ip}:{port}/wsman".format(host_ip=host_ip, port=port)
+    host_connection = f"https://{host_ip}:{port}/wsman"
     action.logger.debug("Host Connection: " + host_connection)
     action.logger.debug("PowerShell script: " + powershell_script)
 
-    powershell_session = winrm.Session(host_connection, auth=(username, password), transport="credssp")
+    powershell_session = FixWinrmSession(host_connection, auth=(username, password), transport="credssp")
     # Forces the Protocol to not fail with self signed certs
-    p = winrm.Protocol(
+    powershell_session.protocol = winrm.Protocol(
         endpoint=host_connection,
         transport="credssp",
         username=username,
@@ -178,20 +149,134 @@ def credssp(action, host_ip, powershell_script, username, password, port):
         server_cert_validation="ignore",
         message_encryption="auto",
     )
-    powershell_session.protocol = p
+
+    error_value, stdout = run_powershell_session(action, powershell_script, powershell_session)
+
+    return {"stdout": stdout, "stderr": error_value}
+
+
+def run_script_using_kerberos(
+    action: komand.Action,
+    host_ip: str,
+    kdc: str,
+    domain: str,
+    host_name: str,
+    powershell_script: str,
+    password: str,
+    username: str,
+    port: int,
+) -> dict:
+    action.logger.info("Running Kerberos connection")
+    # Adds needed https and port number to host IP
+    host_address = f"https://{host_ip}:{port}/wsman"
+    configure_machine_for_kerberos_connection(
+        action, domain, host_ip, host_name, kdc, password, powershell_script, username
+    )
+
+    # Runs the script on the host
+    powershell_session = FixWinrmSession(host_address, auth=(username, password), transport="kerberos")
+
+    # Forces the protocol to not fail with self signed certs
+    powershell_session.protocol = winrm.Protocol(
+        endpoint=host_address,
+        transport="kerberos",
+        username=username,
+        password=password,
+        server_cert_validation="ignore",
+    )
+    error_value, stdout = run_powershell_session(action, powershell_script, powershell_session)
+
+    return {"stdout": stdout, "stderr": error_value}
+
+
+def configure_machine_for_kerberos_connection(
+    action: komand.Action,
+    domain: str,
+    host_ip: str,
+    host_name: str,
+    kdc: str,
+    password: str,
+    powershell_script: str,
+    username: str,
+):
+    action.logger.debug("PowerShell script: " + powershell_script)
+    udomain = domain.upper()
+    # Config for krb5 file to the domain
+    krb_config = (
+        f"[libdefaults]\n"
+        f"default_realm = {udomain}\n"
+        f"forwardable = true\n"
+        f"proxiable = true\n"
+        f"\n"
+        f"[realms]\n"
+        f"{udomain} = {{\n"
+        f"kdc = {kdc}\n"
+        f"admin_server = {kdc}\n"
+        f"default_domain = {udomain}\n"
+        f"}}\n"
+        f"\n"
+        f"[domain_realm]\n"
+        f".{domain} = {udomain}\n"
+        f"{domain} = {udomain}\n"
+    )
+
+    action.logger.debug(krb_config)
+    # Config for DNS
+    dns = f"search {domain}\r\nnameserver {kdc}"
+    action.logger.debug(dns)
+    # Sends output from stdout on shell commands to logging. Preventing errors
+    subprocess.call("mkdir -p /var/lib/samba/private", shell="true")  # noqa: B607,B602
+    subprocess.call("systemctl enable sssd", shell="true")  # noqa: B607,B602
+    # Setup realm to join the domain
+    with open("/etc/krb5.conf", "w", encoding="utf-8") as f:
+        f.write(krb_config)
+
+    # Creates a Kerberos ticket
+    kinit = f"""echo '{password}' | kinit {username}@{domain.upper()}"""
+    response = subprocess.Popen(kinit, shell="true", stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa: B602
+    (stdout, stderr) = response.communicate()
+    stdout = stdout.decode(DECODING_TYPE)
+    stderr = stderr.decode(DECODING_TYPE)
+    action.logger.info("Attempt to make Kerberos ticket stdout: " + stdout)
+    action.logger.info("Attempt to make Kerberos ticket stderr: " + stderr)
+    # DNS info so the plugin knows where to find the domain
+    with open("/etc/resolv.conf", "w", encoding="utf-8") as f:
+        f.write(dns)
+    # Joins Komand to domain
+    realm = f"""echo '{password}' | realm --install=/ join --user={username} {domain}"""
+    response = subprocess.Popen(realm, shell="true", stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa: B602
+    (stdout, stderr) = response.communicate()
+    stdout = stdout.decode(DECODING_TYPE)
+    stderr = stderr.decode(DECODING_TYPE)
+    action.logger.info("Attempt to join domain stdout: " + stdout)
+    action.logger.info("Attempt to join domain stderr: " + stderr)
+    # Allows resolution if A name not set for host
+    with open("/etc/hosts", "a", encoding="utf-8") as f:
+        f.write("\r\n" + host_ip + " " + host_name)
+
+
+def run_powershell_session(action: komand.Action, powershell_script: str, powershell_session: winrm.Session) -> tuple:
     run_script = powershell_session.run_ps(powershell_script)
     exit_code = run_script.status_code
     error_value = run_script.std_err
     output = run_script.std_out
-
     try:
-        error_value = error_value.decode("utf-8")
+        error_value = error_value.decode(DECODING_TYPE)
     except AttributeError:
         pass
-    output = output.decode("utf-8")
-
+    output = output.decode(DECODING_TYPE)
     if exit_code != 0:
         action.logger.error(error_value)
-        raise Exception("An error occurred in the PowerShell script, see logging for more info")
+        raise PluginException(
+            cause="An error occurred in the PowerShell script.", assistance="See logging for more info."
+        )
+    return error_value, output
 
-    return {"output": output, "stderr": error_value}
+
+def add_credentials_to_script(powershell_script: str, params: dict) -> dict:
+    powershell_credentials = (
+        f"$username = '{params.get('username_and_password', {}).get('username')}'\n"
+        f"$password = '{params.get('username_and_password', {}).get('password')}' | ConvertTo-SecureString -asPlainText -Force\n"
+        f"$secret_key = '{params.get('secret_key', {}).get('secretKey')}'\n"
+    )
+    return powershell_credentials + powershell_script
