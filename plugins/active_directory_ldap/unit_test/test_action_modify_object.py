fd7a5cbf156f002fb9e7c3b925d75eea28d7c93f
akarolik-r7 <97876326+akarolik-r7@users.noreply.github.com>
2022-02-11 13:47:23 +0100

    Active_directory_ldap: [MC-769] explicitly close LDAP connection. Moved communication method… (#1106)
    * [MC-769] explicitly close LDAP connection. Moved communication methods with LDAP to separate class
    
    * [MC-769] Refactoring for connection class and help
    
    * [MC-769] Update unit test after changing assistance message
    
    * [MC-769] Disable pylint error for connection test

--- plugins/active_directory_ldap/unit_test/test_action_modify_object.py
+++ plugins/active_directory_ldap/unit_test/test_action_modify_object.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, mock
-from komand.exceptions import PluginException
+from insightconnect_plugin_runtime.exceptions import PluginException
 from komand_active_directory_ldap.actions.modify_object import ModifyObject
 from komand_active_directory_ldap.actions.modify_object.schema import Input, Output
 from unit_test.common import MockServer
@@ -34,4 +34,7 @@ class TestActionModifyObject(TestCase):
             action.run({Input.DISTINGUISHED_NAME: "CN=empty_search,DC=example,DC=com"})
 
         self.assertEqual("The DN was not found.", context.exception.cause)
-        self.assertEqual("The DN CN=empty_search,DC=example,DC=com was not found", context.exception.assistance)
+        self.assertEqual(
+            "Please check that the specified DN CN=empty_search,DC=example,DC=com is correct and try again.",
+            context.exception.assistance,
+        )
