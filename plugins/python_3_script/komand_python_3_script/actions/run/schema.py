3db902b18a4a5cbf18f4dd93863c67b64034732b
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2022-02-11 13:54:52 +0100

    PowerShell_Python - MC-524 Add credentials and secret_key as python and powershell scripts variables (#1092)
    Add custom credentials in Execute Script and PowerShell String actions

--- plugins/python_3_script/komand_python_3_script/actions/run/schema.py
+++ plugins/python_3_script/komand_python_3_script/actions/run/schema.py
@@ -10,6 +10,8 @@ class Component:
 class Input:
     FUNCTION = "function"
     INPUT = "input"
+    SECRET_KEY = "secret_key"
+    USERNAME_AND_PASSWORD = "username_and_password"
     
 
 class Output:
@@ -37,6 +39,67 @@ class RunInput(komand.Input):
       "title": "Input",
       "description": "Input object to be passed as `params={}` to the `run` function",
       "order": 2
+    },
+    "secret_key": {
+      "$ref": "#/definitions/credential_secret_key",
+      "title": "Secret Key",
+      "description": "Credential secret key available in script as python variable (`secret_key`)",
+      "order": 3
+    },
+    "username_and_password": {
+      "$ref": "#/definitions/credential_username_password",
+      "title": "Username and Password",
+      "description": "Username and password available in script as python variables (`username`, `password`)",
+      "order": 4
+    }
+  },
+  "required": [
+    "function"
+  ],
+  "definitions": {
+    "credential_secret_key": {
+      "id": "credential_secret_key",
+      "type": "object",
+      "title": "Credential: Secret Key",
+      "description": "A shared secret key",
+      "properties": {
+        "secretKey": {
+          "type": "string",
+          "title": "Secret Key",
+          "displayType": "password",
+          "description": "The shared secret key",
+          "format": "password"
+        }
+      },
+      "required": [
+        "secretKey"
+      ]
+    },
+    "credential_username_password": {
+      "id": "credential_username_password",
+      "type": "object",
+      "title": "Credential: Username and Password",
+      "description": "A username and password combination",
+      "properties": {
+        "password": {
+          "type": "string",
+          "title": "Password",
+          "displayType": "password",
+          "description": "The password",
+          "format": "password",
+          "order": 2
+        },
+        "username": {
+          "type": "string",
+          "title": "Username",
+          "description": "The username to log in with",
+          "order": 1
+        }
+      },
+      "required": [
+        "username",
+        "password"
+      ]
     }
   }
 }
