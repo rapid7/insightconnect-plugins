68d645aaf772fdfef5bf018381a3833251000803
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2022-02-11 13:54:52 +0100

    PowerShell_Python - MC-524 Add credentials and secret_key as python and powershell scripts variables (#1092)
    Add custom credentials in Execute Script and PowerShell String actions

--- plugins/python_3_script/komand_python_3_script/actions/run/action.py
+++ plugins/python_3_script/komand_python_3_script/actions/run/action.py
@@ -1,6 +1,9 @@
-import komand
-from .schema import RunOutput, RunInput, Component
 import sys
+
+import komand
+from komand.exceptions import PluginException
+
+from .schema import RunOutput, RunInput, Component, Input
 
 sys.path.append("/var/cache/python_dependencies/lib/python3.7/site-packages")
 
@@ -12,19 +15,42 @@ class Run(komand.Action):
         )
 
     def run(self, params={}):
-        self.logger.info("Input: (below)\n\n%s\n", params["input"])
-        self.logger.info("Function: (below)\n\n%s\n", params["function"])
+        self.logger.info(f"Input: (below)\n\n{params.get(Input.INPUT)}\n")
+        func = params.get(Input.FUNCTION)
+        self.logger.info(f"Function: (below)\n\n{func}\n")
+        func = self._add_credentials_to_function(func, params)
 
         try:
-            func = params["function"]
-            exec(func)  # noqa: B102
-            funcname = func.split(" ")[1].split("(")[0]
-            out = locals()[funcname](params["input"])
+            out = self._exec_python_function(func=func, params=params)
         except Exception as e:
-            raise Exception("Could not run supplied script. Error: " + str(e))
+            raise PluginException(cause="Could not run supplied script", data=str(e))
         try:
             if out is None:
-                raise Exception("Output type was None. Ensure that output has a non-None data type")
+                raise PluginException(
+                    cause="Output type was None", assistance="Ensure that output has a non-None data type"
+                )
             return out
         except UnboundLocalError:
-            raise Exception("No output was returned. Check supplied script to ensure that it returns output")
+            raise PluginException(
+                cause="No output was returned.", assistance="Check supplied script to ensure that it returns output"
+            )
+
+    @staticmethod
+    def _exec_python_function(func, params):
+        exec(func)  # noqa: B102
+        funcname = func.split(" ")[1].split("(")[0]
+        out = locals()[funcname](params.get(Input.INPUT))
+        return out
+
+    @staticmethod
+    def _add_credentials_to_function(func: str, params: dict):
+        credentials_definition = [
+            f"\tusername='{params.get(Input.USERNAME_AND_PASSWORD).get('username')}'",
+            f"\tpassword='{params.get(Input.USERNAME_AND_PASSWORD).get('password')}'",
+            f"\tsecret_key='{params.get(Input.SECRET_KEY).get('secretKey')}'",
+        ]
+
+        func_lines = func.split("\n")
+        func_lines = [func_lines[0]] + credentials_definition + func_lines[1:]
+
+        return "\n".join(func_lines)
