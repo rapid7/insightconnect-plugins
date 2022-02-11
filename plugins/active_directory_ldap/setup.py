8791deb15ce3eed399abe37616627e0e383630ca
akarolik-r7 <97876326+akarolik-r7@users.noreply.github.com>
2022-02-11 13:47:23 +0100

    Active_directory_ldap: [MC-769] explicitly close LDAP connection. Moved communication method… (#1106)
    * [MC-769] explicitly close LDAP connection. Moved communication methods with LDAP to separate class
    
    * [MC-769] Refactoring for connection class and help
    
    * [MC-769] Update unit test after changing assistance message
    
    * [MC-769] Disable pylint error for connection test

--- plugins/active_directory_ldap/setup.py
+++ plugins/active_directory_ldap/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 
 setup(name="active_directory_ldap-rapid7-plugin",
-      version="5.3.1",
+      version="5.3.2",
       description="This plugin utilizes Microsoft's Active Directory service to create and manage domains, users, and objects within a network",
       author="rapid7",
       author_email="",
       url="",
       packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_active_directory_ldap']
       )
