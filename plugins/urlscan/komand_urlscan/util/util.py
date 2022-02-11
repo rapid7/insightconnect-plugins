24537ed192485b129956a0301f412d82003686d6
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-02-11 06:45:32 -0500

    URLScan - MC-881 fix issue with not compatible types in output for get scan res… (#1097) (#1114)
    * MC-881 fix issue with not compatible types in output for get scan result action. Prepare unit test for action get scan. Refactor existing tests
    * [MC-881] fix lint errors for plugin urlscan

---
+++ plugins/urlscan/komand_urlscan/util/util.py
@@ -0,0 +1,11 @@
+class Util:
+    @staticmethod
+    def update_properties(object_to_validate: dict, properties: list) -> dict:
+        for checked_property in properties:
+            value_of_property = object_to_validate.get(checked_property, [])
+            if isinstance(value_of_property, dict):
+                if value_of_property:
+                    object_to_validate[checked_property] = [value_of_property]
+                else:
+                    object_to_validate[checked_property] = []
+        return object_to_validate
