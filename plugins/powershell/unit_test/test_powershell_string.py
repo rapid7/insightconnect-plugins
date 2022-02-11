25bef2844847101ade3144940d5cc7285352d1cc
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2022-02-11 13:54:52 +0100

    PowerShell_Python - MC-524 Add credentials and secret_key as python and powershell scripts variables (#1092)
    Add custom credentials in Execute Script and PowerShell String actions

---
+++ plugins/powershell/unit_test/test_powershell_string.py
@@ -0,0 +1,54 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from unittest.mock import patch
+from parameterized import parameterized
+
+from unit_test.util import Util
+from komand_powershell.actions.powershell_string import PowershellString
+
+
+class TestPowershellString(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.params = {
+            "address": "10.0.1.11",
+            "username_and_password": {"password": "example_password", "username": "example_user"},
+            "host_name": "windows",
+            "script": "Get-Date",
+            "secret_key": {"secretKey": "s1e2c3r4e5t67k8e9y"},
+        }
+
+    @parameterized.expand(
+        [
+            ("ntlm", "inputs/ntlm_connection.json.resp", "expecteds/ntlm_connection.json.resp"),
+            ("cred_ssp", "inputs/credssp_connection.json.resp", "expecteds/credssp_connection.json.resp"),
+        ]
+    )
+    @patch("komand_powershell.util.util.FixWinrmSession", side_effect=Util.mock_powershell)
+    def test_powershell_string_ntlm_credssp(self, name: str, input_path: str, expected_path: str, mock_powershell):
+        params = Util.read_file_to_dict(input_path)
+        action = Util.default_connector(PowershellString(), params)
+        actual = action.run(params=self.params)
+        expected = Util.read_file_to_dict(expected_path)
+        self.assertEqual(actual, expected)
+
+    @patch("subprocess.Popen", side_effect=Util.mock_powershell)
+    def test_powershell_string_local(self, mock_powershell):
+        params = Util.read_file_to_dict("inputs/local_connection.json.resp")
+        action = Util.default_connector(PowershellString(), params)
+        actual = action.run(params=self.params)
+        expected = Util.read_file_to_dict("expecteds/local_connection.json.resp")
+        self.assertEqual(actual, expected)
+
+    @patch("komand_powershell.util.util.FixWinrmSession", side_effect=Util.mock_powershell)
+    @patch("komand_powershell.util.util.configure_machine_for_kerberos_connection")
+    def test_powershell_string_kerberos(self, mock_configure_kerberos, mock_powershell):
+        params = Util.read_file_to_dict("inputs/kerberos_connection.json.resp")
+        action = Util.default_connector(PowershellString(), params)
+        actual = action.run(params=self.params)
+        expected = Util.read_file_to_dict("expecteds/kerberos_connection.json.resp")
+        self.assertEqual(actual, expected)
--- plugins/python_3_script/setup.py
+++ plugins/python_3_script/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="python_3_script-rapid7-plugin",
-      version="2.0.4",
+      version="3.0.0",
       description="Run a Python 3 script",
       author="rapid7",
       author_email="",
