2d5bc8e6494269d27d4d26ce83a559fafdb268e2
Thomas Cheng <thomas_cheng@rapid7.com>
2022-02-16 14:58:01 -0500

    initial addition of uninstall_sensor action

--- plugins/carbon_black_response/icon_carbon_black_response/actions/uninstall_sensor/action.py
+++ plugins/carbon_black_response/icon_carbon_black_response/actions/uninstall_sensor/action.py
@@ -18,6 +18,10 @@ class UninstallSensor(insightconnect_plugin_runtime.Action):
             # Returns single sensor if ID is supplied
             sensor_data = self.connection.carbon_black.get_object("/api/v1/sensor/%s" % id)
             sensor_data["uninstall"] = True
+            put_response = self.connection.carbon_black.put_object("/api/v1/sensor/%s" % id, sensor_data)
+            sensor_data = self.connection.carbon_black.get_object("/api/v1/sensor/%s" % id)
+            if not sensor_data["uninstall"]:
+                return {"success": False}
         except Exception as ex:
             self.logger.error("Failed to uninstall sensor: %s", ex)
             raise ex
