700db8a08efda9c8cf36569ab881a8a100ba092c
Thomas Cheng <87387242+tcheng-r7@users.noreply.github.com>
2022-02-18 14:11:13 -0500

    SOAR-7681 encoded url fix for ExtractIt (#1118)
    Squashed all commits, major commits:
    
    * fix url extract behavior with encoded urls
    
    * revert and sanitize test url
    
    * add keep_original_urls argument to extract function in extractIt plugin
    
    * regenerate checksums
    
    * update keep_original_urls input description
    
    * moved utf-8 to constant, update description of keep_original_urls, other minor changes
    
    * update keep_original_urls description
    
    * update to extractor.py to satisfy black lint

--- plugins/extractit/unit_test/test_url_extractor.py
+++ plugins/extractit/unit_test/test_url_extractor.py
@@ -9,8 +9,8 @@ parameters = Util.load_parameters("url_extractor").get("parameters")
 
 class TestUrlExtractor(TestCase):
     @parameterized.expand(parameters)
-    def test_extract_urls(self, name, string, file, expected):
+    def test_extract_urls(self, name, string, file, keep_original_urls, expected):
         action = UrlExtractor()
-        actual = action.run({Input.STR: string, Input.FILE: file})
+        actual = action.run({Input.STR: string, Input.FILE: file, Input.KEEP_ORIGINAL_URLS: keep_original_urls})
         expected = {Output.URLS: expected}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
