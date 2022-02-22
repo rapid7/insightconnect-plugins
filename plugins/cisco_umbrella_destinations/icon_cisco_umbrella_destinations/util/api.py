3ceb090c2d0cba682a976532235f35f8fee353f3
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2022-02-22 15:42:26 +0000

    Updated api.py based on PR review
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>

--- plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/util/api.py
+++ plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/util/api.py
@@ -5,7 +5,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 from insightconnect_plugin_runtime.exceptions import PluginException
 
-ERROR_MSG = "Invalid input data, ensure the information you are inputting is correct"
+ERROR_MSG = "Invalid input data, ensure the input is correct"
 
 
 class CiscoUmbrellaManagementAPI:
