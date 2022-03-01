4c8d0bedce9390757a4f05061ed88db4227f0f2b
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2022-03-01 14:38:14 +0100

    GetURL_Update decoding for replacing bytes not in UTF-8 (#1048)
    * MC-755 init branch. Update decoding for replacing bates which is not present in utf8
    
    * [MC-755] Black reformat
    
    * Update plugins/get_url/komand_get_url/triggers/poll_file/trigger.py
    
    * [MC-755] Fix file decoding error after file download
    
    * [MC-755] Fix file decoding error after file download
    
    * [MC-755] Update exception messages. Mock cache in unit tests
    
    * [MC-755] Remove unused variable after changing assistance message
    
    * [MC-755] Create methods for handling exceptions while reading and writing contents to cache files
    
    * [MC-755] Add type-hint to function arguments and return. Add doc sting for function in trigger

--- plugins/get_url/komand_get_url/triggers/poll_file/trigger.py
+++ plugins/get_url/komand_get_url/triggers/poll_file/trigger.py
@@ -1,9 +1,12 @@
-import komand
 import time
-from .schema import PollFileInput, PollFileOutput, Input, Output, Component
+from http.client import HTTPResponse
+
+import komand
 
 # Custom imports below
+from komand_get_url.util import constants
 from komand_get_url.util.utils import Utils
+from .schema import PollFileInput, PollFileOutput, Input, Output, Component
 
 
 class PollFile(komand.Trigger):
@@ -14,65 +17,47 @@ class PollFile(komand.Trigger):
             input=PollFileInput(),
             output=PollFileOutput(),
         )
+        self.is_modified = True
+        self.utils = Utils(action=self)
 
     def run(self, params={}):
-        utils = Utils(action=self)
+        poll = params.get(Input.POLL, constants.DEFAULT_TIMEOUT)
         url = params.get(Input.URL)
         is_verify = params.get(Input.IS_VERIFY, True)
-        poll = params.get(Input.POLL, 60)
-
+        user_agent = params.get(Input.USER_AGENT, constants.DEFAULT_USER_AGENT)
         while True:
-            """Check for supported url prefix"""
-            utils.validate_url(url)
-
-            is_modified = True
-            meta = utils.hash_url(url)
-            cache_file = "/var/cache/" + meta["file"]
-
-            """Attempt to retrieve headers from past request"""
-            headers = {}
-            if komand.helper.check_cachefile(meta["metafile"]):
-                headers = utils.check_url_meta_file(meta)
-
-            """Download file"""
-            url_object = komand.helper.open_url(
-                url,
-                verify=is_verify,
-                If_None_Match=headers.get("etag", ""),
-                If_Modified_Since=headers.get("last-modified", ""),
-                User_Agent=params.get(Input.USER_AGENT, "Mozilla/5.0"),
-            )
-
-            """File modified"""
+            url_object, meta = self.utils.check_prefix_and_download(url, is_verify, user_agent)
+            # File modified
             if url_object:
-                contents = url_object.read().decode("utf-8")
-
-                """Write etag and last modified to cache"""
-                utils.create_url_meta_file(meta, url_object)
-
-                """We can't guarantee server supports lastmodified/etag, compare contents"""
-                if komand.helper.check_cachefile(cache_file):
-                    old = komand.helper.open_cachefile(cache_file)
-                    old_contents = old.read()
-                    old.close()
-                    if old_contents == contents:
-                        is_modified = False
-                        self.logger.info("GetUrl: File not updated")
-
-                """Write new URL file contents to cache"""
-                if is_modified:
-                    f = komand.helper.open_cachefile(cache_file)
-                    f.write(contents)
-                    f.close()
-
-                    """Check URL status code and return file contents"""
-                    if url_object.code >= 200 or url_object.code <= 299:
-                        f = komand.helper.encode_string(contents)
-                        if f:
-                            output = {
-                                Output.BYTES: f.decode("utf-8"),
-                                Output.STATUS_CODE: url_object.code or 200,
-                            }
-                            self.send(output)
-
+                self._save_to_cache_and_send(url_object, meta)
             time.sleep(poll)
+
+    def _save_to_cache_and_send(self, url_object: HTTPResponse, meta: dict):
+        """The function checks whether changes have been made to the file the user is monitoring using trigger.
+        If the file under the given URL does not match the cache,
+        the function refreshes the cache and returns the new file to the user.
+        """
+        cache_file = constants.DEFAULT_CACHE_FOLDER + meta.get("file")
+        contents = url_object.read().decode(constants.DEFAULT_ENCODING, "replace")
+
+        # Write etag and last modified to cache
+        self.utils.create_url_meta_file(meta, url_object)
+
+        # We can't guarantee server supports lastmodified/etag, compare contents
+        if komand.helper.check_cachefile(cache_file):
+            old_contents = self.utils.read_contents_from_cache(cache_file)
+            if old_contents == contents:
+                self.is_modified = False
+                self.logger.debug("GetUrl: File not updated")
+
+        if self.is_modified:
+            self.utils.write_contents_to_cache(cache_file, contents)
+
+            # Check URL status code and return file contents
+            if 200 <= url_object.code <= 299:
+                self.send(
+                    {
+                        Output.BYTES: komand.helper.encode_string(contents).decode(constants.DEFAULT_ENCODING),
+                        Output.STATUS_CODE: url_object.code or 200,
+                    }
+                )
