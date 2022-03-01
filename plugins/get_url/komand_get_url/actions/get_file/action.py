1f59c64eb447182cc06682267b835b9b4a2f308f
r7-mgorny <65253008+r7-mgorny@users.noreply.github.com>
2022-03-01 14:38:14 +0100

    GetURL_Update decoding for replacing bytes not in UTF-8 (#1048)
    * MC-755 init branch. Update decoding for replacing bates which is not present in utf8
    
    * [MC-755] Black reformat
    
    * Update plugins/get_url/komand_get_url/triggers/poll_file/trigger.py
    
    * [MC-755] Fix file decoding error after file download
    
    * [MC-755] Fix file decoding error after file download
    
    * [MC-755] Update exception messages. Mock cache in unit tests
    
    * [MC-755] Remove unused variable after changing assistance message
    
    * [MC-755] Create methods for handling exceptions while reading and writing contents to cache files
    
    * [MC-755] Add type-hint to function arguments and return. Add doc sting for function in trigger

--- plugins/get_url/komand_get_url/actions/get_file/action.py
+++ plugins/get_url/komand_get_url/actions/get_file/action.py
@@ -1,10 +1,9 @@
 import komand
-from .schema import GetFileInput, GetFileOutput, Input, Output, Component
-
-# Custom imports below
-from komand_get_url.util.utils import Utils
 from komand.exceptions import PluginException
-import http
+
+from komand_get_url.util import constants
+from komand_get_url.util.utils import Utils
+from .schema import GetFileInput, GetFileOutput, Input, Output, Component
 
 
 class GetFile(komand.Action):
@@ -15,69 +14,49 @@ class GetFile(komand.Action):
             input=GetFileInput(),
             output=GetFileOutput(),
         )
+        self.utils = Utils(action=self)
 
     def run(self, params={}):
-        utils = Utils(action=self)
         url = params.get(Input.URL)
         checksum = params.get(Input.CHECKSUM)
-        tout = params.get(Input.TIMEOUT, 60)
+        timeout = params.get(Input.TIMEOUT, constants.DEFAULT_TIMEOUT)
         is_verify = params.get(Input.IS_VERIFY, True)
-
-        # Check for supported url prefix
-        utils.validate_url(url)
-
-        meta = utils.hash_url(url)
-        cache_file = "/var/cache/" + meta["file"]
-
-        # Attempt to retrieve headers from past request
-        headers = {}
-        if komand.helper.check_cachefile(meta["metafile"]):
-            headers = utils.check_url_meta_file(meta)
-
-        # Download file
-        url_object = komand.helper.open_url(
-            url,
-            timeout=tout,
-            verify=is_verify,
-            If_None_Match=headers.get("etag", ""),
-            If_Modified_Since=headers.get("last-modified", ""),
-            User_Agent=params.get(Input.USER_AGENT, "Mozilla/5.0"),
-        )
-
+        user_agent = params.get(Input.USER_AGENT, constants.DEFAULT_USER_AGENT)
+        url_object, meta = self.utils.check_prefix_and_download(url, is_verify, user_agent, timeout)
+        cache_file = constants.DEFAULT_CACHE_FOLDER + meta.get("file")
         if url_object:
-            contents = url_object.read(8388608).decode("utf-8")
-
+            contents = url_object.read().decode(constants.DEFAULT_ENCODING, "replace")
             # Optional integrity check of file
-            if checksum:
-                if not komand.helper.check_hashes(contents, checksum):
-                    self.logger.error("GetFile: File Checksum Failed")
-                    raise PluginException(cause="GetURL Failed", assistance="File Checksum Failed")
+            if checksum and not komand.helper.check_hashes(contents, checksum):
+                self.logger.error("GetFile: File Checksum Failed")
+                raise PluginException(
+                    cause="Checksums between the downloaded file and provided checksum did not match.",
+                    assistance="Verify the file you meant to download and the checksum you provided are correct.",
+                )
 
             # Write etag and last modified to cache
-            utils.create_url_meta_file(meta, url_object)
+            self.utils.create_url_meta_file(meta, url_object)
 
             # Write URL file contents to cache
-            f = komand.helper.open_cachefile(cache_file)
-            f.write(contents)
-            f.close()
+            self.utils.write_contents_to_cache(cache_file, contents)
 
             # Check URL status code and return file contents
-            if url_object.code is None or url_object.code >= 200 or url_object.code <= 299:
-                f = komand.helper.encode_string(contents)
-                if f:
-                    return {
-                        Output.BYTES: f.decode("utf-8"),
-                        Output.STATUS_CODE: url_object.code or 200,
-                    }
+            if not url_object.code or 200 <= url_object.code <= 299:
+                return {
+                    Output.BYTES: komand.helper.encode_string(contents).decode(constants.DEFAULT_ENCODING),
+                    Output.STATUS_CODE: url_object.code or 200,
+                }
 
         # When the download fails or file is not modified
-        if url_object is None:
+        else:
             # Attempt to return file from cache if available
             self.logger.info(f"GetURL: File not modified: {url}")
             if komand.helper.check_cachefile(cache_file):
-                f = komand.helper.encode_file(cache_file)
                 self.logger.info(f"GetURL: File returned from cache: {cache_file}")
-                return {Output.BYTES: f.decode("utf-8"), Output.STATUS_CODE: 200}
+                return {
+                    Output.BYTES: komand.helper.encode_file(cache_file).decode(constants.DEFAULT_ENCODING),
+                    Output.STATUS_CODE: 200,
+                }
 
         # If file hasn't been returned then we fail
         self.logger.info(f"GetURL: Download failed for {url}")
--- plugins/get_url/komand_get_url/util/utils.py
+++ plugins/get_url/komand_get_url/util/utils.py
@@ -1,14 +1,17 @@
-import komand
-import json
 import hashlib
+import json
+from http.client import HTTPResponse
+
+import komand
 from komand.exceptions import PluginException
+from typing import AnyStr
 
 
 class Utils(object):
     def __init__(self, action):
         self.logger = action.logger
 
-    def validate_url(self, url):
+    def validate_url(self, url: str) -> bool:
         """Check for supported URL prefixes from url string"""
         if url.startswith("http://") or url.startswith("https://") or url.startswith("ftp://"):
             return True
@@ -18,16 +21,13 @@ class Utils(object):
             assistance=f"GetURL: Unsupported URL prefix: {url}",
         )
 
-    def get_headers(self, url_object):
+    def __get_headers(self, url_object: HTTPResponse) -> dict:
         """Return cache related headers from urllib2 headers dictonary"""
-        if "etag" or "last-modified" in url_object.headers.dict:
-            etag = url_object.headers.get("etag")
-            lm = url_object.headers.get("last-modified")
-            return {"etag": etag, "last-modified": lm}
+        etag = url_object.headers.get("etag")
+        lm = url_object.headers.get("last-modified")
+        return {"etag": etag, "last-modified": lm}
 
-        self.logger.error("GetHeaders: Error occurred while obtaining etag and last-modified headers")
-
-    def hash_url(self, url):
+    def hash_url(self, url: str) -> dict:
         """Creates a dictionary containing hashes from a url of type string"""
         try:
             self.logger.info(f"url: {url}")
@@ -40,29 +40,77 @@ class Utils(object):
             self.logger.error(f"HashUrl: Error hashing url {e}")
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
 
-    def create_url_meta_file(self, meta, url_object):
+    def create_url_meta_file(self, meta: dict, url_object: HTTPResponse):
         """Create metadata file from meta info information"""
-        headers = self.get_headers(url_object)
+        headers = self.__get_headers(url_object)
         data = {
             "url": meta.get("url"),
             "last-modified": headers.get("last-modified"),
             "etag": headers.get("etag"),
             "file": meta.get("file"),
         }
-        with komand.helper.open_cachefile(meta["metafile"]) as f:
-            json.dump(data, f)
+        with komand.helper.open_cachefile(meta.get("metafile")) as loaded_file:
+            json.dump(data, loaded_file)
         self.logger.info(f"CreateUrlMetaFile: MetaFile created: {str(data)}")
 
-    def check_url_meta_file(self, meta):
+    def check_url_meta_file(self, meta: dict):
         """Check caching headers from meta info dictionary"""
         try:
-            with komand.helper.open_cachefile(meta["metafile"]) as f:
-                data = json.load(f)
+            with komand.helper.open_cachefile(meta.get("metafile")) as loaded_file:
+                data = json.load(loaded_file)
             return data
         except Exception as e:
-            self.logger.error(f"CheckUrlMetaFile: Error while retreving meta file, error {e}")
+            self.logger.error(f"CheckUrlMetaFile: Error while retrieving meta file, error {e}")
             raise PluginException(
                 preset=PluginException.Preset.UNKNOWN,
-                assistance="Error while retreving meta file",
+                assistance="Error while retrieving meta file",
                 data=e,
             )
+
+    def check_prefix_and_download(
+        self, url: str, is_verify: bool, user_agent: str, timeout: int = None
+    ) -> (HTTPResponse, dict):
+        """Check for supported url prefix"""
+        self.validate_url(url)
+        meta = self.hash_url(url)
+
+        # Attempt to retrieve headers from past request
+        headers = {}
+        if komand.helper.check_cachefile(meta.get("metafile")):
+            headers = self.check_url_meta_file(meta)
+
+        # Download file
+        url_object = komand.helper.open_url(
+            url,
+            timeout=timeout,
+            verify=is_verify,
+            If_None_Match=headers.get("etag", ""),
+            If_Modified_Since=headers.get("last-modified", ""),
+            User_Agent=user_agent,
+        )
+        return url_object, meta
+
+    def write_contents_to_cache(self, cache_file: str, contents: bytes):
+        try:
+            old_cache_file = komand.helper.open_cachefile(cache_file)
+            old_cache_file.write(contents)
+            old_cache_file.close()
+        except IOError as error:
+            raise PluginException(
+                cause="Error appear while saving data to cache file.",
+                assistance="Please contact support for assistance.",
+                data=error,
+            )
+
+    def read_contents_from_cache(self, cache_file: str) -> AnyStr:
+        try:
+            old_cache_file = komand.helper.open_cachefile(cache_file)
+            old_contents = old_cache_file.read()
+            old_cache_file.close()
+        except Exception as error:
+            raise PluginException(
+                cause="Error appear while reading contents from cache file.",
+                assistance="Please contact support for assistance.",
+                data=error,
+            )
+        return old_contents
