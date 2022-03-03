8e8bf4d77ff86a8388a9de692fee0bbcb79d7c19
Dawid Mandyna <66844174+dmandyna-r7@users.noreply.github.com>
2022-03-03 17:38:22 +0000

    [MC-642][MC-699][MC-767][MC-914][MC-934] - SentinelOne Bugfixes and New Actions (#1091)

--- plugins/sentinelone/unit_test/test_agents_action.py
+++ plugins/sentinelone/unit_test/test_agents_action.py
@@ -3,122 +3,43 @@ import os
 
 sys.path.append(os.path.abspath("../"))
 
-from unittest import TestCase
-from komand_sentinelone.connection.connection import Connection
+from unittest.mock import patch
 from komand_sentinelone.actions.agents_action import AgentsAction
-import json
-import logging
+from komand_sentinelone.actions.agents_action.schema import Input
 from insightconnect_plugin_runtime.exceptions import PluginException
+from unit_test.util import Util
+from unittest import TestCase
 
 
 class TestAgentsAction(TestCase):
-    def setup(self) -> None:
-        log = logging.getLogger("Test")
-        test_conn = Connection()
-        test_action = AgentsAction()
-        test_conn.logger = log
-        test_action.logger = log
-        try:
-            with open("../tests/agents_action.json") as file:
-                test_json = json.loads(file.read()).get("body")
-                connection_params = test_json.get("connection")
-                action_params = test_json.get("input")
-        except Exception as e:
-            self.fail("Likely could not find tests in test directory. Generate and fill out samples to fix this.")
-        return action_params, connection_params, test_action, test_conn, log
+    @classmethod
+    @patch("requests.post", side_effect=Util.mocked_requests_get)
+    def setUpClass(cls, mock_request) -> None:
+        cls.action = Util.default_connector(AgentsAction())
+        Util.mock_response_params = {}
 
-    def test_agents_action_abort_scan(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "abort-scan"
+    @patch("requests.request", side_effect=Util.mocked_requests_get)
+    def test_should_success_with_filter(self, mock_request):
+        expected = {"affected": 1}
+        for test in [
+            "abort-scan",
+            "connect",
+            "decommission",
+            "disconnect",
+            "fetch-logs",
+            "initiate-scan",
+            "restart-machine",
+            "shutdown",
+            "uninstall",
+        ]:
+            with self.subTest(f"Running agent with action: {test}"):
+                actual = self.action.run({Input.FILTER: '{"ids": ["1000000000000000000"]}', Input.ACTION: test})
+                self.assertEqual(expected, actual)
 
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
+    @patch("requests.request", side_effect=Util.mocked_requests_get)
+    def test_fail_when_wrong_action(self, mock_request):
+        with self.assertRaises(PluginException) as error:
+            self.action.run({Input.FILTER: '{"ids": ["1000000000000000000"]}', Input.ACTION: "wrong_action"})
 
-    def test_all_agents_action_connect(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "connect"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-    def test_all_agents_action_decommission(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "decommission"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-        action_params["filter"] = {}
-        self.assertRaises(PluginException, test_action.run, action_params)
-
-    def test_all_agents_action_disconnect(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "disconnect"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-        action_params["filter"] = {}
-        self.assertRaises(PluginException, test_action.run, action_params)
-
-    def test_all_agents_action_fetch_logs(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "fetch-logs"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-    def test_all_agents_action_initiate_scan(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "initiate-scan"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-    def test_all_agents_action_restart_machine(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "restart-machine"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-        action_params["filter"] = {}
-        self.assertRaises(PluginException, test_action.run, action_params)
-
-    def test_all_agents_action_shutdown(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "shutdown"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-        action_params["filter"] = {}
-        self.assertRaises(PluginException, test_action.run, action_params)
-
-    def test_all_agents_action_uninstall(self):
-        action_params, connection_params, test_action, test_conn, log = self.setup()
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        action_params["action"] = "uninstall"
-
-        results = test_action.run(action_params)
-        self.assertTrue("affected" in results.keys())
-
-        action_params["filter"] = {}
-        self.assertRaises(PluginException, test_action.run, action_params)
+        self.assertEqual(error.exception.cause, "API call failed: This is some error text")
+        self.assertEqual(error.exception.assistance, "")
