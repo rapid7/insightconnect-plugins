ef80133a04c78189467b046df4a2c1ea28b8e9aa
Dawid Mandyna <66844174+dmandyna-r7@users.noreply.github.com>
2022-03-03 17:38:22 +0000

    [MC-642][MC-699][MC-767][MC-914][MC-934] - SentinelOne Bugfixes and New Actions (#1091)

--- plugins/sentinelone/setup.py
+++ plugins/sentinelone/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="sentinelone-rapid7-plugin",
-      version="6.2.0",
+      version="6.3.0",
       description="The SentinelOne plugin allows you to manage and mitigate all your security operations through SentinelOne",
       author="rapid7",
       author_email="",
