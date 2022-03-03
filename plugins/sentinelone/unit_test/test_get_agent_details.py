ada8add11aa73d0fdb4aa195c2cc47acfab3e157
Dawid Mandyna <66844174+dmandyna-r7@users.noreply.github.com>
2022-03-03 17:38:22 +0000

    [MC-642][MC-699][MC-767][MC-914][MC-934] - SentinelOne Bugfixes and New Actions (#1091)

--- plugins/sentinelone/unit_test/test_get_agent_details.py
+++ plugins/sentinelone/unit_test/test_get_agent_details.py
@@ -3,49 +3,127 @@ import os
 
 sys.path.append(os.path.abspath("../"))
 
-from unittest import TestCase
-from komand_sentinelone.connection.connection import Connection
+from unittest.mock import patch
 from komand_sentinelone.actions.get_agent_details import GetAgentDetails
-import json
-import logging
+from komand_sentinelone.actions.get_agent_details.schema import Input
+from unit_test.util import Util
+from unittest import TestCase
+
+expected = {
+    "agent": {
+        "accountName": "account",
+        "agentVersion": "1.1.1",
+        "allowRemoteShell": False,
+        "computerName": "computer",
+        "coreCount": "1",
+        "cpuCount": "1",
+        "cpuId": "11",
+        "createdAt": "2021-11-11",
+        "domain": "domain",
+        "externalId": "10.10.10.10",
+        "externalIp": "10.10.10.10",
+        "firewallEnabled": True,
+        "groupIp": "1.0.0.0",
+        "groupUpdatedAt": "2021-11-11",
+        "id": "ffff-ffff",
+        "inRemoteShellSession": True,
+        "infected": False,
+        "isActive": True,
+        "isDecommissioned": False,
+        "isPendingUninstall": True,
+        "isUninstalled": False,
+        "isUpToDate": "2021-11-11",
+        "lastActiveDate": "2021-11-11",
+        "lastIpToMgmt": "1.1.1.1",
+        "lastLoggedInUserName": "username",
+        "licenseKey": "ffff-ffff-ffffffff-ffff",
+        "locationEnabled": True,
+        "networkQuarantineEnabled": True,
+        "networkStatus": "active",
+        "operationalState": "active",
+        "operationalStateExpiration": "active",
+        "osArch": "x64",
+        "osName": "windows 7",
+        "osType": "win",
+        "osUsername": "username",
+        "remoteProfilingState": "active",
+        "remoteProfilingStateExpiration": "active",
+        "scanFinishedAt": "2021-11-11",
+        "threatRebootRequired": False,
+        "totalMemory": 8000,
+        "updatedAt": "2021-11-11",
+        "uuid": "ffff-ffff-ffffffff-ffff",
+    }
+}
 
 
 class TestGetAgentDetails(TestCase):
-    def test_integration_get_agent_details(self):
-        """
-        This is an integration test that will connect to the services your plugin uses. It should be used
-        as the basis for tests below that can run independent of a "live" connection.
+    @classmethod
+    @patch("requests.post", side_effect=Util.mocked_requests_get)
+    def setUpClass(cls, mock_request) -> None:
+        cls.action = Util.default_connector(GetAgentDetails())
 
-        This test assumes a normal plugin structure with a /tests directory. In that /tests directory should
-        be json samples that contain all the data needed to run this test. To generate samples run:
+    @patch("requests.get", side_effect=Util.mocked_requests_get)
+    def test_should_return_response_when_any_status(self, mock_request):
+        actual = self.action.run(
+            {Input.AGENT: "hostname123", Input.CASE_SENSITIVE: True, Input.OPERATIONAL_STATE: "Any"}
+        )
+        self.assertEqual(expected, actual)
 
-        icon-plugin generate samples
+    @patch("requests.get", side_effect=Util.mocked_requests_get)
+    def test_should_return_response_when_na_status(self, mock_request):
+        expected["agent"]["operationalState"] = "na"
+        actual = self.action.run(
+            {Input.AGENT: "hostname_na", Input.CASE_SENSITIVE: True, Input.OPERATIONAL_STATE: "na"}
+        )
+        self.assertEqual(expected, actual)
 
-        """
+    @patch("requests.get", side_effect=Util.mocked_requests_get)
+    def test_should_return_response_when_fully_disabled_status(self, mock_request):
+        expected["agent"]["operationalState"] = "fully_disabled"
+        actual = self.action.run(
+            {
+                Input.AGENT: "hostname_fully_disabled",
+                Input.CASE_SENSITIVE: True,
+                Input.OPERATIONAL_STATE: "fully_disabled",
+            }
+        )
+        self.assertEqual(expected, actual)
 
-        log = logging.getLogger("Test")
-        test_conn = Connection()
-        test_action = GetAgentDetails()
+    @patch("requests.get", side_effect=Util.mocked_requests_get)
+    def test_should_return_response_when_partially_disabled_status(self, mock_request):
+        expected["agent"]["operationalState"] = "partially_disabled"
+        actual = self.action.run(
+            {
+                Input.AGENT: "hostname_partially_disabled",
+                Input.CASE_SENSITIVE: True,
+                Input.OPERATIONAL_STATE: "partially_disabled",
+            }
+        )
+        self.assertEqual(expected, actual)
 
-        test_conn.logger = log
-        test_action.logger = log
+    @patch("requests.get", side_effect=Util.mocked_requests_get)
+    def test_should_return_response_when_disabled_error_status(self, mock_request):
+        expected["agent"]["operationalState"] = "disabled_error"
+        actual = self.action.run(
+            {
+                Input.AGENT: "hostname_disabled_error",
+                Input.CASE_SENSITIVE: True,
+                Input.OPERATIONAL_STATE: "disabled_error",
+            }
+        )
+        self.assertEqual(expected, actual)
 
-        try:
-            with open("../tests/get_agent_details.json") as file:
-                test_json = json.loads(file.read()).get("body")
-                connection_params = test_json.get("connection")
-                action_params = test_json.get("input")
-        except Exception as e:
-            message = """
-            Could not find or read sample tests from /tests directory
-            
-            An exception here likely means you didn't fill out your samples correctly in the /tests directory 
-            Please use 'icon-plugin generate samples', and fill out the resulting test files in the /tests directory
-            """
-            self.fail(message)
-
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        results = test_action.run(action_params)
-
-        self.assertTrue(len(results.get("agent")) >= 0)
+    @patch("requests.get", side_effect=Util.mocked_requests_get)
+    def test_should_return_empty_when_different_status(self, mock_request):
+        for test in [
+            "na",
+            "fully_disabled",
+            "partially_disabled",
+            "disabled_error",
+        ]:
+            with self.subTest(f"Running agent with action: {test}"):
+                actual = self.action.run(
+                    {Input.AGENT: "hostname123", Input.CASE_SENSITIVE: True, Input.OPERATIONAL_STATE: test}
+                )
+                self.assertEqual({"agent": {}}, actual)
