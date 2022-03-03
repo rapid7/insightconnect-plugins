19887cf8f3a61afb1bb6bd3871c8f8d7f2b654de
dsliwinski-r7 <dominik_sliwinski@rapid7.com>
2022-03-03 17:17:18 +0100

    [MC-650][MC-651][MC-652] Regenerate

--- plugins/fireeye_hx/icon_fireeye_hx/actions/check_host_quarantine_status/action.py
+++ plugins/fireeye_hx/icon_fireeye_hx/actions/check_host_quarantine_status/action.py
@@ -1,10 +1,10 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import CheckHostQuarantineStatusInput, CheckHostQuarantineStatusOutput, Input, Output, Component
 
 # Custom imports below
 
 
-class CheckHostQuarantineStatus(komand.Action):
+class CheckHostQuarantineStatus(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="check_host_quarantine_status",
--- plugins/fireeye_hx/icon_fireeye_hx/actions/unquarantine_host/action.py
+++ plugins/fireeye_hx/icon_fireeye_hx/actions/unquarantine_host/action.py
@@ -1,10 +1,10 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import UnquarantineHostInput, UnquarantineHostOutput, Input, Output, Component
 
 # Custom imports below
 
 
-class UnquarantineHost(komand.Action):
+class UnquarantineHost(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="unquarantine_host",
