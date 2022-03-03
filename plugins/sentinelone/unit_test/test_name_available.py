7c8d225b9902494a9ebb91ea84b1ba4d3eadd2ae
Dawid Mandyna <66844174+dmandyna-r7@users.noreply.github.com>
2022-03-03 17:38:22 +0000

    [MC-642][MC-699][MC-767][MC-914][MC-934] - SentinelOne Bugfixes and New Actions (#1091)

--- plugins/sentinelone/unit_test/test_name_available.py
+++ plugins/sentinelone/unit_test/test_name_available.py
@@ -3,49 +3,21 @@ import os
 
 sys.path.append(os.path.abspath("../"))
 
-from unittest import TestCase
-from komand_sentinelone.connection.connection import Connection
+from unittest.mock import patch
 from komand_sentinelone.actions.name_available import NameAvailable
-import json
-import logging
+from komand_sentinelone.actions.name_available.schema import Input
+from unit_test.util import Util
+from unittest import TestCase
 
 
 class TestNameAvailable(TestCase):
-    def test_integration_name_available(self):
-        """
-        This is an integration test that will connect to the services your plugin uses. It should be used
-        as the basis for tests below that can run independent of a "live" connection.
+    @classmethod
+    @patch("requests.post", side_effect=Util.mocked_requests_get)
+    def setUpClass(cls, mock_request) -> None:
+        cls.action = Util.default_connector(NameAvailable())
 
-        This test assumes a normal plugin structure with a /tests directory. In that /tests directory should
-        be json samples that contain all the data needed to run this test. To generate samples run:
-
-        icon-plugin generate samples
-
-        """
-
-        log = logging.getLogger("Test")
-        test_conn = Connection()
-        test_action = NameAvailable()
-
-        test_conn.logger = log
-        test_action.logger = log
-
-        try:
-            with open("../tests/name_available.json") as file:
-                test_json = json.loads(file.read()).get("body")
-                connection_params = test_json.get("connection")
-                action_params = test_json.get("input")
-        except Exception as e:
-            message = """
-            Could not find or read sample tests from /tests directory
-            
-            An exception here likely means you didn't fill out your samples correctly in the /tests directory 
-            Please use 'icon-plugin generate samples', and fill out the resulting test files in the /tests directory
-            """
-            self.fail(message)
-
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        results = test_action.run(action_params)
-
-        self.assertTrue("available" in results.keys())
+    @patch("requests.request", side_effect=Util.mocked_requests_get)
+    def test_should_success(self, mock_request):
+        expected = {"available": True}
+        actual = self.action.run({Input.NAME: "account_name"})
+        self.assertEqual(expected, actual)
