e79bbeb6f8a8bcdceabc2454d5fc0c5909379d62
Conor McNally <conor_mcnally@rapid7.com>
2022-03-04 14:44:20 +0000

    Fixed dGet on None for comment

--- plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/actions/dGet/action.py
+++ plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/actions/dGet/action.py
@@ -3,6 +3,7 @@ from .schema import DGetInput, DGetOutput, Input, Output, Component
 
 
 # Custom imports below
+from icon_cisco_umbrella_destinations.util.api import return_non_empty
 
 
 class DGet(insightconnect_plugin_runtime.Action):
@@ -17,4 +18,5 @@ class DGet(insightconnect_plugin_runtime.Action):
     def run(self, params={}):
         destination_list_id = params.get(Input.DESTINATIONLISTID)
         result = self.connection.client.get_destinations(destination_list_id=destination_list_id).get('data', [])
+        result = [return_non_empty(element) for element in result]
         return {Output.SUCCESS: result}
