a4b28d6fb6972ecb6bda3a0ad6beb2468df2c981
jrose-r7 <87443773+jrose-r7@users.noreply.github.com>
2022-03-08 07:16:01 -0500

    Carbon Black - [SOAR-7132] Update Find Event and Get Details for a Specific Event Actions (#1034)
    * Update plugins/carbon_black_defense/komand_carbon_black_defense/connection/connection.py
    * plugin spec cleanup
    * Reformat for successful black
    * Update plugin.spec.yaml find_event input time_range to string
    * Update EVENTIDS and EVENTINFO
    * Incorporate time_range find_events action.py
    * Update parameter retrieval find_events action.py
    * Add unit tests for the updated actions
    * Update help.md and plugin.spec.yaml version history and supported versions | Remove unused requirement and file

--- plugins/carbon_black_defense/komand_carbon_black_defense/actions/find_event/action.py
+++ plugins/carbon_black_defense/komand_carbon_black_defense/actions/find_event/action.py
@@ -1,68 +1,64 @@
-import komand
-from .schema import FindEventInput, FindEventOutput, Output
+import insightconnect_plugin_runtime
+from insightconnect_plugin_runtime.exceptions import PluginException
 
-# Custom imports below
-import requests
+from komand_carbon_black_defense.util.util import Util
+
+from .schema import FindEventInput, FindEventOutput, Input, Output
 
 
-class FindEvent(komand.Action):
-
-    # URI for Find Event
-    _URI = "/integrationServices/v3/event/"
-
+class FindEvent(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="find_event",
-            description="Retrieves all events matching the input search criteria. Response is a list of events in JSON format. Resulting events are sorted in descending order of time",
+            description="Retrieves all events matching the input search criteria. "
+            "Response is a list of events in JSON format."
+            "Resulting events are sorted in descending order of time",
             input=FindEventInput(),
             output=FindEventOutput(),
         )
 
-    def run(self, params={}):
-        host = self.connection.host
-        token = self.connection.token
-        connector = self.connection.connector
-        payload = dict()
-        for param in params:
-            if params[param]:
-                payload[param] = params[param]
-        self.logger.info(payload)
-        headers = {"X-Auth-Token": f"{token}/{connector}"}
-        url = host + FindEvent._URI
+    @Util.retry(tries=6, timeout=60, exceptions=PluginException, backoff_seconds=1)
+    def get_enriched_event_status(self, id_):
+        enriched_event_search_status = self.connection.get_enriched_event_status(id_)
+        if not enriched_event_search_status:
+            raise PluginException
+        return enriched_event_search_status
 
-        if payload:
-            result = requests.get(url, headers=headers, params=payload)
-        else:
-            result = requests.get(url, headers=headers)
-        try:
-            data = komand.helper.clean(result.json())
-        except ValueError:
-            self.logger.error(result.text)
-            raise Exception(
-                f"Error: Received an unexpected response"
-                f" (non-JSON or no response was received). Raw response in logs."
+    def run(self, params={}):
+        device_external_ip = params.get(Input.DEVICE_EXTERNAL_IP)
+        process_name = params.get(Input.PROCESS_NAME)
+        enriched_event_type = params.get(Input.ENRICHED_EVENT_TYPE)
+        process_hash = params.get(Input.PROCESS_HASH)
+        device_name = params.get(Input.DEVICE_NAME)
+        time_range = params.get(Input.TIME_RANGE)
+
+        criteria = {}
+
+        if device_external_ip:
+            criteria["device_external_ip"] = device_external_ip
+        if process_name:
+            criteria["process_name"] = process_name
+        if enriched_event_type:
+            criteria["enriched_event_type"] = enriched_event_type
+        if process_hash:
+            criteria["process_hash"] = process_hash
+        if device_name:
+            criteria["device_name"] = device_name
+        if not criteria:
+            raise PluginException(
+                cause="No inputs were provided.",
+                assistance="At least one input must be provided while configuring this action.",
             )
-        if result.status_code == 200:
-            return {
-                Output.SUCCESS: data["success"],
-                Output.LATESTTIME: data["latestTime"],
-                Output.RESULTS: data["results"],
-                Output.ELAPSED: data["elapsed"],
-                Output.MESSAGE: data["message"],
-                Output.TOTALRESULTS: data["totalResults"],
-            }
-        if result.status_code in range(400, 499):
-            raise Exception(
-                f"Carbon Black returned a {result.status_code} code."
-                f" Verify the token and host configuration in the connection. Response was: {result.text}"
-            )
-        if result.status_code in range(500, 599):
-            raise Exception(
-                f"Carbon Black returned a {result.status_code} code."
-                f" If the problem persists please contact support for help. Response was: {result.text}"
-            )
-        self.logger.error(result.text)
-        raise Exception(
-            f"An unknown error occurred."
-            f" Carbon Black returned a {result.status_code} code. Contact support for help. Raw response in logs."
-        )
+        id_ = self.connection.get_job_id_for_enriched_event(criteria, None, time_range)
+
+        self.logger.info(f"Got enriched event job ID: {id_}")
+        if id_ is None:
+            return {Output.EVENTINFO: None, Output.SUCCESS: False}
+        self.get_enriched_event_status(id_)
+        response = self.connection.retrieve_results_for_enriched_event(job_id=id_)
+        data = insightconnect_plugin_runtime.helper.clean(response)
+
+        return {
+            Output.SUCCESS: True,
+            Output.EVENTINFO: data,
+        }
