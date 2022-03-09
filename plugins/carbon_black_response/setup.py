2ed00a6a0df324b28f486d37b7eab667afc357e4
Thomas Cheng <thomas_cheng@rapid7.com>
2022-03-09 14:47:54 -0500

    test regenerate

--- plugins/carbon_black_response/setup.py
+++ plugins/carbon_black_response/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="carbon_black_response-rapid7-plugin",
-      version="3.1.11",
+      version="3.2.0",
       description="Automate information collection, endpoint isolation and hash blacklisting",
       author="rapid7",
       author_email="",
