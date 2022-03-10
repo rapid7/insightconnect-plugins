365ef4c18fc207234b6484081d50f6681f7d6896
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-03-10 08:07:48 -0500

    [MC-891] Fix Hybrid Analysis Connection Test (#1089) (#1168)
    * [MC-804] update Api version. Adding test for actions. Update documentation
    
    * [MC-804] Fix code formatting. Fix prospector messages
    
    * [MC-804] Specify docker image version. Update plugin spec. Update unit test.
    
    * [MC-804] Update plugin spec. Update unit tests
    
    * [MC-804] Update private method annotation
    
    * [MC-804] Update help and plugin spec

---
+++ plugins/hybrid_analysis/icon_hybrid_analysis/util/api.py
@@ -0,0 +1,58 @@
+import json
+import urllib.parse
+from logging import Logger
+
+import requests
+
+from icon_hybrid_analysis.util import constants
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+class HybridAnalysisAPI:
+    def __init__(self, url, api_key, logger: Logger):
+        self.api_version = "v2"
+        self.base_url = f"{url}/api/{self.api_version}"
+        self.headers = {"User-Agent": constants.DEFAULT_USER_AGENT, "api-key": api_key}
+        self.logger = logger
+
+    def lookup_by_hash(self, analyzed_hash: str):
+        return self._send_request(method="POST", path="/search/hash", data={"hash": analyzed_hash})
+
+    def lookup_by_terms(self, data: dict):
+        return self._send_request(method="POST", path="/search/terms", data=data)
+
+    def report(self, analyzed_hash: str):
+        return self._send_request(method="GET", path=f"/report/{urllib.parse.quote(analyzed_hash)}/state")
+
+    def submit(self, files: dict, data: dict):
+        return self._send_request(method="POST", path="/submit/file", files=files, data=data)
+
+    def _send_request(self, method: str, path: str, data: dict = None, files: dict = None) -> dict:
+        try:
+            response = requests.request(
+                method.upper(),
+                f"{self.base_url}{path}",
+                data=data,
+                files=files,
+                headers=self.headers,
+            )
+
+            if not response.status_code or 200 <= response.status_code <= 299:
+                return response.json()
+
+            if response.status_code == 403:
+                raise PluginException(preset=PluginException.Preset.API_KEY, data=response.text)
+            elif response.status_code == 404:
+                raise PluginException(preset=PluginException.Preset.NOT_FOUND, data=response.text)
+            elif response.status_code >= 500:
+                raise PluginException(preset=PluginException.Preset.SERVER_ERROR, data=response.text)
+            else:
+                raise PluginException(
+                    cause=response.json().get("message", "Unknown"),
+                    assistance=f"validation_errors: {response.json().get('validation_errors')}",
+                )
+
+        except json.decoder.JSONDecodeError as e:
+            raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=e)
+        except requests.exceptions.HTTPError as e:
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
