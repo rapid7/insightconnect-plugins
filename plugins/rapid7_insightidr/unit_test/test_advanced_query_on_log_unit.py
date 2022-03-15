15169cb6147b039b2e7cc710d5b9d703ce42f848
Maxim Berezin <maxim_berezin@rapid7.com>
2022-03-15 14:32:19 -0400

    Copy from MoreSOAR branch unit tests for changed actions, test payloads, utils. Unit test fail with output useful for debugging.

---
+++ plugins/rapid7_insightidr/unit_test/test_advanced_query_on_log_unit.py
@@ -0,0 +1,62 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_rapid7_insightidr.actions.advanced_query_on_log import AdvancedQueryOnLog
+from komand_rapid7_insightidr.actions.advanced_query_on_log.schema import Input, Output
+from unit_test.util import Util
+from unittest.mock import patch
+
+
+@patch("requests.Session.get", side_effect=Util.mocked_requests)
+@patch("aiohttp.ClientSession.get", side_effect=Util.mocked_async_requests)
+class TestAdvancedQueryOnLog(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(AdvancedQueryOnLog())
+
+    def test_advanced_query_on_log_one_label(self, mock_get, mock_async_get):
+        actual = self.action.run({Input.QUERY: "", Input.LOG: "example_log1"})
+        self.maxDiff = None
+        expected = ["Out of order entry"]
+
+        self.assertEqual(actual.get(Output.COUNT), 1)
+        self.assertEqual(actual.get(Output.RESULTS)[0].get("labels"), expected)
+
+    def test_advanced_query_on_log_two_labels(self, mock_get, mock_async_get):
+        actual = self.action.run(
+            {
+                Input.QUERY: "",
+                Input.LOG: "example_log2",
+            }
+        )
+        expected = ["Out of order entry", "Out of events"]
+
+        self.assertEqual(actual.get(Output.COUNT), 1)
+        self.assertEqual(actual.get(Output.RESULTS)[0].get("labels"), expected)
+
+    def test_advanced_query_on_log_without_label(self, mock_get, mock_async_get):
+        actual = self.action.run(
+            {
+                Input.QUERY: "",
+                Input.LOG: "example_log3",
+            }
+        )
+        expected = []
+
+        self.assertEqual(actual.get(Output.COUNT), 1)
+        self.assertEqual(actual.get(Output.RESULTS)[0].get("labels"), expected)
+
+    def test_advanced_query_on_log_wrong_label(self, mock_get, mock_async_get):
+        actual = self.action.run(
+            {
+                Input.QUERY: "",
+                Input.LOG: "example_log4",
+            }
+        )
+        expected = []
+
+        self.assertEqual(actual.get(Output.COUNT), 1)
+        self.assertEqual(actual.get(Output.RESULTS)[0].get("labels"), expected)
---
+++ plugins/rapid7_insightidr/unit_test/util.py
@@ -0,0 +1,121 @@
+import asyncio
+import json
+import logging
+import os
+
+from komand.exceptions import PluginException
+from komand_rapid7_insightidr.connection import Connection
+from komand_rapid7_insightidr.connection.schema import Input
+from requests.models import HTTPError
+
+
+class Meta:
+    version = "0.0.0"
+
+
+class Util:
+    @staticmethod
+    def default_connector(action, connect_params: object = None):
+        default_connection = Connection()
+        default_connection.meta = Meta()
+        default_connection.logger = logging.getLogger("connection logger")
+        if connect_params:
+            params = connect_params
+        else:
+            params = {
+                Input.URL: "https://rapid7.com",
+                Input.API_KEY: {"secretKey": "4472f2g7-991z-4w70-li11-7552w8qm0266"},
+            }
+        default_connection.connect(params)
+        action.connection = default_connection
+        action.logger = logging.getLogger("action logger")
+        return action
+
+    @staticmethod
+    def read_file_to_string(filename):
+        with open(filename) as my_file:
+            return my_file.read()
+
+    @staticmethod
+    async def mocked_async_requests(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, filename, status_code):
+                self.filename = filename
+                self.status = status_code
+                self.text = "This is some error text"
+
+            def raise_for_status(self):
+                if self.status == 404:
+                    raise HTTPError("Not found", response=self)
+
+            async def json(self):
+                if self.filename == "error":
+                    raise PluginException(preset=PluginException.Preset.SERVER_ERROR)
+                if self.filename == "empty":
+                    return {}
+
+                return json.loads(
+                    Util.read_file_to_string(
+                        os.path.join(os.path.dirname(os.path.realpath(__file__)), f"payloads/{self.filename}.json.resp")
+                    )
+                )
+
+        if args[0] == "https://rapid7.com/log_search/management/labels/00000000-0000-0000-0000-000000000006":
+            return MockResponse("label_006", 200)
+        elif args[0] == "https://rapid7.com/log_search/management/labels/00000000-0000-0000-0000-000000000007":
+            return MockResponse("label_007", 200)
+        elif args[0] == "https://rapid7.com/log_search/management/labels/not exist label - 404":
+            return MockResponse("label_404", 404)
+
+        raise Exception("Not implemented")
+
+    @staticmethod
+    def mocked_requests(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, filename, status_code):
+                self.filename = filename
+                self.status_code = status_code
+                self.text = "This is some error text"
+
+            def raise_for_status(self):
+                if self.status_code == 404:
+                    raise HTTPError("Not found", response=self)
+
+            def json(self):
+                if self.filename == "error":
+                    raise PluginException(preset=PluginException.Preset.SERVER_ERROR)
+                if self.filename == "empty":
+                    return {}
+
+                return json.loads(
+                    Util.read_file_to_string(
+                        os.path.join(os.path.dirname(os.path.realpath(__file__)), f"payloads/{self.filename}.json.resp")
+                    )
+                )
+
+        if args[0] == "https://rapid7.com/log_search/management/logs":
+            return MockResponse("logs", 200)
+        elif (
+            args[0] == "https://rapid7.com/log_search/query/logs/log_id"
+            or args[0] == "https://rapid7.com/log_search/query/logsets/log_id"
+        ):
+            return MockResponse("log_id", 200)
+        elif (
+            args[0] == "https://rapid7.com/log_search/query/logs/log_id2"
+            or args[0] == "https://rapid7.com/log_search/query/logsets/log_id2"
+        ):
+            return MockResponse("log_id2", 200)
+        elif (
+            args[0] == "https://rapid7.com/log_search/query/logs/log_id3"
+            or args[0] == "https://rapid7.com/log_search/query/logsets/log_id3"
+        ):
+            return MockResponse("log_id3", 200)
+        elif (
+            args[0] == "https://rapid7.com/log_search/query/logs/log_id4"
+            or args[0] == "https://rapid7.com/log_search/query/logsets/log_id4"
+        ):
+            return MockResponse("log_id4", 200)
+        elif args[0] == "https://rapid7.com/log_search/management/logsets":
+            return MockResponse("logsets", 200)
+
+        raise Exception("Not implemented")
