d73f2535819327bc88dca3c7ace59b21f6e14989
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-03-15 14:15:30 -0400

    [MC-973] Update sdk to version 4. Update unit tests after update SDK (#1171)
    * [MC-973] Update sdk to version 4. Update unit tests after update SDK
    
    * Removed unnecessary parameter params from the test in connection

--- plugins/get_url/komand_get_url/connection/connection.py
+++ plugins/get_url/komand_get_url/connection/connection.py
@@ -1,17 +1,17 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import ConnectionSchema
 
 # Custom imports below
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
     def connect(self, params):
         pass
 
-    def test(self, params={}):
+    def test(self):
         url = "https://www.google.com"
-        komand.helper.check_url(url)
+        insightconnect_plugin_runtime.helper.check_url(url)
         return {}
