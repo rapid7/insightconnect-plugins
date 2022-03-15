9931d0fbf4c2cfd0a5a05768770a0dc82b2d2d97
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-03-15 14:15:30 -0400

    [MC-973] Update sdk to version 4. Update unit tests after update SDK (#1171)
    * [MC-973] Update sdk to version 4. Update unit tests after update SDK
    
    * Removed unnecessary parameter params from the test in connection

--- plugins/get_url/komand_get_url/triggers/poll_file/trigger.py
+++ plugins/get_url/komand_get_url/triggers/poll_file/trigger.py
@@ -1,7 +1,7 @@
 import time
 from http.client import HTTPResponse
 
-import komand
+import insightconnect_plugin_runtime
 
 # Custom imports below
 from komand_get_url.util import constants
@@ -9,7 +9,7 @@ from komand_get_url.util.utils import Utils
 from .schema import PollFileInput, PollFileOutput, Input, Output, Component
 
 
-class PollFile(komand.Trigger):
+class PollFile(insightconnect_plugin_runtime.Trigger):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="poll_file",
@@ -44,7 +44,7 @@ class PollFile(komand.Trigger):
         self.utils.create_url_meta_file(meta, url_object)
 
         # We can't guarantee server supports lastmodified/etag, compare contents
-        if komand.helper.check_cachefile(cache_file):
+        if insightconnect_plugin_runtime.helper.check_cachefile(cache_file):
             old_contents = self.utils.read_contents_from_cache(cache_file)
             if old_contents == contents:
                 self.is_modified = False
@@ -57,7 +57,9 @@ class PollFile(komand.Trigger):
             if 200 <= url_object.code <= 299:
                 self.send(
                     {
-                        Output.BYTES: komand.helper.encode_string(contents).decode(constants.DEFAULT_ENCODING),
+                        Output.BYTES: insightconnect_plugin_runtime.helper.encode_string(contents).decode(
+                            constants.DEFAULT_ENCODING
+                        ),
                         Output.STATUS_CODE: url_object.code or 200,
                     }
                 )
