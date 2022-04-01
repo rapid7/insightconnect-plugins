beda119dc138f5176d0355eb003c51d0089b1e57
Conor McNally <conor_mcnally@rapid7.com>
2022-04-01 11:37:43 +0100

    Updated connection

--- plugins/manage_engine_service_desk/icon_manage_engine_service_desk/connection/connection.py
+++ plugins/manage_engine_service_desk/icon_manage_engine_service_desk/connection/connection.py
@@ -1,5 +1,6 @@
 import insightconnect_plugin_runtime
 from .schema import ConnectionSchema, Input
+from ..util.api import ManageEngineAPI
 
 # Custom imports below
 
@@ -7,10 +8,13 @@ from .schema import ConnectionSchema, Input
 class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
+        self.client = None
 
     def connect(self, params):
         self.logger.info("Connect: Connecting...")
         apiKey = params.get(Input.API_KEY).get("secretKey")
+
+        self.client = ManageEngineAPI(api_key=apiKey)
 
     def test(self):
         # TODO: Implement connection test
