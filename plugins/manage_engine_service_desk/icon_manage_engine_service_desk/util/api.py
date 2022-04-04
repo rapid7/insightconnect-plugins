7973d432453097730c53f36aa94a92d4d73b617c
Conor McNally <conor_mcnally@rapid7.com>
2022-04-04 11:48:58 +0100

    All calls mapped

--- plugins/manage_engine_service_desk/icon_manage_engine_service_desk/util/api.py
+++ plugins/manage_engine_service_desk/icon_manage_engine_service_desk/util/api.py
@@ -11,6 +11,26 @@ class ManageEngineAPI:
         self.api_key = api_key
         self.logger = logger
 
+    # Add request
+    # https://www.manageengine.com/products/service-desk/sdpod-v3-api/requests/request.html#add-request
+    def add_request(self, data: dict) -> dict:
+        return self._call_api("POST", "api/v3/requests", data=data)
+
+    # Edit request
+    # https://www.manageengine.com/products/service-desk/sdpod-v3-api/requests/request.html#edit-request
+    def edit_request(self, data: dict, request_id: int) -> dict:
+        return self._call_api("PUT", f"api/v3/requests/{request_id}", data=data)
+
+    # Delete request
+    # https://www.manageengine.com/products/service-desk/sdpod-v3-api/requests/request.html#delete-request
+    def delete_request(self, request_id: int):
+        return self._call_api("DELETE", f"api/v3/requests/{request_id}", None)
+
+    # Get request
+    # https://www.manageengine.com/products/service-desk/sdpod-v3-api/requests/request.html#get-request
+    def get_request(self, request_id: int):
+        return self._call_api("GET", f"api/v3/requests/{request_id}", None)
+
     # Get ALL requests
     # https://www.manageengine.com/products/service-desk/sdpod-v3-api/requests/request.html#get-list-request
     def get_list_request(self) -> dict:
@@ -31,15 +51,15 @@ class ManageEngineAPI:
     ) -> dict:
 
         headers = {
+            "Accept": "application/v3+json",
+            "Authorization": f"Zoho-oauthtoken {self.api_key}",
             "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "application/vnd.manageengine.sdp.v3+json",
         }
         response = requests.request(
             method,
             self.url + path,
             data=data,
             headers=headers,
-            # auth=auth
         )
         if response.status_code == 400:
             raise PluginException(cause="Theres a fair few reasons why this isn't working. Good luck figuring it out")
@@ -51,3 +71,8 @@ class ManageEngineAPI:
             raise PluginException(preset=PluginException.Preset.SERVER_ERROR)
         if 200 <= response.status_code < 300:
             return response.json()
+
+    def _extract_status_code(self, input_dict: dict):
+        # TODO: Implement a method to extract the status code from the error message
+
+        pass
