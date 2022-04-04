24fd0963ffec3f0c3d7e6c6d5a53b3d5e36be363
Maxim Berezin <maxim_berezin@rapid7.com>
2022-04-04 15:11:24 -0400

    Instantiate mocked request connector for action unit tests. TODO: delete commented original args for mocked_request, if unneeded.

--- plugins/cortex_v2/unit_test/util.py
+++ plugins/cortex_v2/unit_test/util.py
@@ -1,18 +1,6 @@
 import logging
 from icon_cortex_v2.connection import Connection
-
-
-class MockClient:
-    def __init__(self):
-        self.service = "test"
-
-    def execute(self):
-        pass
-
-
-class MockConnection:
-    def __init__(self):
-        self.service = MockClient()
+from icon_cortex_v2.connection.schema import Input
 
 
 class Util:
@@ -20,6 +8,28 @@ class Util:
     def default_connector(action):
         default_connection = Connection()
         default_connection.logger = logging.getLogger("connection logger")
-        action.connection = MockConnection()
+        params = {
+            Input.API_KEY: {"secretKey": ""},
+            Input.PROTOCOL: "HTTP",
+            Input.HOST: "0.0.0.0",
+            Input.PORT: 9999,
+            Input.VERIFY: False,
+        }
+        default_connection.connect(params)
+        action.connection = default_connection
         action.logger = logging.getLogger("action logger")
         return action
+
+    # (self, method: str, path: str, params, **kwargs)
+    @staticmethod
+    def mocked_request(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, status_code=None):
+                self.status_code = status_code
+
+        if args[1] == "one test case action full URL":
+            return MockResponse(status_code=200)
+        elif args[1] == "another test case but 404":
+            return MockResponse(status_code=404)
+        else:
+            raise Exception("Not implemented")
