8dc60973f417afdafb0dce7359ac4c8f8d7a306b
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2022-04-08 23:21:18 +0200

    [MC-1005] Update Google Directory plugin (#1193)
    * [MC-1005] Update Google Directory plugin
    
    * [MC-1005] Update plugin spec
    
    * [MC-1005] Update unit tests
    
    * [MC-1005] Update timestamp for supported versions in plugin.spec

--- plugins/google_directory/setup.py
+++ plugins/google_directory/setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 
-setup(name='google_directory-rapid7-plugin',
-      version='2.0.1',
-      description='Uses the Google Directory plugin to manage members, users, and user aliases',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="google_directory-rapid7-plugin",
+      version="2.0.2",
+      description="Uses the Google Directory plugin to manage members, users, and user aliases",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/icon_google_directory']
       )
