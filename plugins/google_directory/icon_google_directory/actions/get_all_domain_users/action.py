124bd49bf0f5a57857cd0eea61edcc4c8cb846bd
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-04-11 09:51:53 -0400

    [MC-1005] Update Google Directory plugin (#1193) (#1212)
    * [MC-1005] Update Google Directory plugin
    
    * [MC-1005] Update plugin spec
    
    * [MC-1005] Update unit tests
    
    * [MC-1005] Update timestamp for supported versions in plugin.spec

--- plugins/google_directory/icon_google_directory/actions/get_all_domain_users/action.py
+++ plugins/google_directory/icon_google_directory/actions/get_all_domain_users/action.py
@@ -1,45 +1,34 @@
-import komand
-from .schema import GetAllDomainUsersInput, GetAllDomainUsersOutput, Input
+import insightconnect_plugin_runtime
+from .schema import GetAllDomainUsersInput, GetAllDomainUsersOutput, Input, Output, Component
 
 # Custom imports below
 
 
-class GetAllDomainUsers(komand.Action):
+class GetAllDomainUsers(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="get_all_domain_users",
-            description="Get all domain users",
+            description=Component.DESCRIPTION,
             input=GetAllDomainUsersInput(),
             output=GetAllDomainUsersOutput(),
         )
 
     def run(self, params={}):
-        domain = params.get(Input.DOMAIN)
         service = self.connection.service
-
-        # Call the Admin SDK Directory API
         self.logger.info("Fetching users...")
-        request = service.users().list(domain=domain, orderBy="email")
+        request = service.users().list(domain=params.get(Input.DOMAIN), orderBy="email")
 
-        # Get all users
         users = []
-        while request is not None:
+        while request:
             result = request.execute()
-            users += result.get("users") or []
+            users += result.get("users", [])
             request = service.users().list_next(request, result)
 
-        if len(users) == 0:
-            return {"users": []}
-
-        # Format output
-        formatted_users = []
-        for user in users:
-            email = user.get("primaryEmail", "")
-            if "name" in user:
-                name = user["name"].get("fullName", "")
-            else:
-                name = ""
-
-            formatted_users.append({"email": email, "name": name})
-
-        return {"users": formatted_users}
+        if users:
+            formatted_users = []
+            for user in users:
+                formatted_users.append(
+                    {"email": user.get("primaryEmail", ""), "name": user.get("name", {}).get("fullName", "")}
+                )
+            return {Output.USERS: formatted_users}
+        return {Output.USERS: users}
