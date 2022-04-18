94d3b8bdb256811b903f93591b83ea6e7e893811
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-04-18 12:32:39 -0400

    [MC-890] ATT Cybersecurity Alien Vault OTX plugin update (#1200) (#1216)
    * [MC-890] ATT Cybersecurity AlienVault OTX plugin update
    
    * [MC-890] Resolve comments

--- plugins/att_cybersecurity_alienvault_otx/komand_att_cybersecurity_alienvault_otx/util/utils.py
+++ plugins/att_cybersecurity_alienvault_otx/komand_att_cybersecurity_alienvault_otx/util/utils.py
@@ -1,5 +1,6 @@
 import IndicatorTypes
-
+from insightconnect_plugin_runtime.exceptions import PluginException
+from OTXv2 import InvalidAPIKey, NotFound, BadRequest
 
 indicator_types = {
     "IPv4": IndicatorTypes.IPv4,
@@ -9,7 +10,26 @@ indicator_types = {
 }
 
 
-def get_indicatortypes(indicator_type):
-    if indicator_type not in indicator_types:
-        raise Exception(f"Indicator type: {indicator_type} is not a supported indicator type")
-    return indicator_types.get(indicator_type)
+def get_indicator_type(indicator_type):
+    if indicator_type in indicator_types:
+        return indicator_types.get(indicator_type)
+    raise PluginException(
+        cause=f"Indicator type {indicator_type} is not a supported indicator type.",
+        assistance="Please provide a valid indicator type.",
+    )
+
+
+def raise_exception(error):
+    error_type = type(error)
+    if error_type == InvalidAPIKey:
+        raise PluginException(preset=PluginException.Preset.API_KEY)
+    if error_type == NotFound:
+        raise PluginException(preset=PluginException.Preset.NOT_FOUND)
+    if error_type == BadRequest:
+        raise PluginException(
+            cause="The server is unable to process the request.",
+            assistance="Verify that your input is correct and try again. If the issue persists, please contact "
+            "support.",
+            data=error,
+        )
+    raise PluginException(preset=PluginException.Preset.UNKNOWN, data=error)
