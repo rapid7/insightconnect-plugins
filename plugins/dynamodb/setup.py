3b67a3baecc34b857397200bbe3d4627f80dfbeb
akarolik-r7 <97876326+akarolik-r7@users.noreply.github.com>
2022-05-11 22:13:38 +0200

    [MC-547] DynamoDB major update (#1204)

--- plugins/dynamodb/setup.py
+++ plugins/dynamodb/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 
 setup(name="dynamodb-rapid7-plugin",
-      version="2.0.0",
+      version="3.0.0",
       description="Dynamo DB is a key/value store powered by Amazon available for members of AWS. Users can manage their data using the DynamoDB plugin for Rapid7 InsightConnect",
       author="rapid7",
       author_email="",
       url="",
       packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_dynamodb']
       )
