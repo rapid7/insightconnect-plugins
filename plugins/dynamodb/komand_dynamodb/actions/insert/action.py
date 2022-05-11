17ec63dd7ef3ffbb30b8aecdefbaed4ceef014f1
akarolik-r7 <97876326+akarolik-r7@users.noreply.github.com>
2022-05-11 22:13:38 +0200

    [MC-547] DynamoDB major update (#1204)

--- plugins/dynamodb/komand_dynamodb/actions/insert/action.py
+++ plugins/dynamodb/komand_dynamodb/actions/insert/action.py
@@ -1,29 +1,28 @@
-import komand
-from .schema import InsertOutput, InsertInput
+import copy
+
+import insightconnect_plugin_runtime
+from komand_dynamodb.util.constants import AWS_NONE_VALUE
+from .schema import InsertOutput, InsertInput, Component, Input, Output
 
 # Custom imports below
+from ...util.utils import Utils
 
 
-class Insert(komand.Action):
+class Insert(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="insert",
-            description="Store an object into Dynamo",
+            description=Component.DESCRIPTION,
             input=InsertInput(),
             output=InsertOutput(),
         )
 
     def run(self, params={}):
-        table_name = params.get("table")
-        expr = params.get("condition_expression")
-        t = self.connection.dynamodb.Table(table_name)
-        data = params.get("data")
-        kwargs = {"Item": data}
-        if (expr is not None) and (len(expr) > 0):
-            kwargs["ConditionExpression"] = expr
-        t.put_item(**kwargs)
-        return {"success": True}
-
-    def test(self):
-        """TODO: Test action"""
-        return {}
+        mapped_params = copy.deepcopy(params)
+        mapped_params[Input.RETURN_ITEM_COLLECTION_METRICS] = Utils.map_return_item_collection_metrics(
+            params.get(Input.RETURN_ITEM_COLLECTION_METRICS, False)
+        )
+        mapped_params[Input.RETURN_VALUES] = Utils.map_return_values(params.get(Input.RETURN_VALUES, False))
+        mapped_params[Input.RETURN_CONSUMED_CAPACITY] = params.get(Input.RETURN_CONSUMED_CAPACITY, AWS_NONE_VALUE)
+        self.connection.client.insert_data(input_schema=self.input.schema, params=mapped_params)
+        return {Output.SUCCESS: True}
