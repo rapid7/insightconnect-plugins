58d0c4dd606ba1a9d789a065bd96e16f498bcb5d
Maxim Berezin <maxim_berezin@rapid7.com>
2022-05-25 17:35:49 -0400

    Replace komand -> insightconnect_plugin_runtime

--- plugins/thehive/icon_thehive/actions/create_case_task/action.py
+++ plugins/thehive/icon_thehive/actions/create_case_task/action.py
@@ -1,4 +1,4 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import CreateCaseTaskInput, CreateCaseTaskOutput, Component
 
 # Custom imports below
@@ -7,7 +7,7 @@ import time
 import requests
 
 
-class CreateCaseTask(komand.Action):
+class CreateCaseTask(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="create_case_task",
