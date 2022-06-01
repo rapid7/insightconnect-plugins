ad35267a6aec29d0ea1a920f802fe1b10342f404
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-06-01 13:54:49 -0400

    [MC-868] Recorded Future Handle 404 Not Found Error (#1230) (#1277)
    * MC-868 Handle 404 Not Found Error in actions:
    
    - Lookup IP, Lookup Domain, Lookup Hash, Lookup Alert,
    - Lookup Vulnerability, Lookup URL, Lookup Malware
    
    - Create unit_tests for above actions
    - Update some example outputs in help.md
    - Update keywords, key features
    
    * MC-868 Black formatting
    
    * MC-868 PR changes
    
    * MC-868 Update Major version
    
    * MC-868 Update version description

---
+++ plugins/recorded_future/unit_test/util.py
@@ -0,0 +1,102 @@
+import json
+import logging
+import sys
+import os
+
+import insightconnect_plugin_runtime
+
+sys.path.append(os.path.abspath("../"))
+
+from komand_recorded_future.connection import Connection
+from komand_recorded_future.connection.schema import Input
+
+
+class Util:
+    @staticmethod
+    def default_connector(action: insightconnect_plugin_runtime.Action, params=None):
+        if not params:
+            params = {Input.API_KEY: {"secretKey": "gf844hgja77cff5417f9df14165c74e1538"}}
+        action.connection = Connection()
+        action.connection.meta = "{}"
+        action.connection.logger = logging.getLogger("connection logger")
+        action.connection.connect(params)
+        action.logger = logging.getLogger("action logger")
+        return action
+
+    @staticmethod
+    def read_file_to_string(filename):
+        with open(filename, "r", encoding="utf-8") as file_reader:
+            return file_reader.read()
+
+    @staticmethod
+    def read_file_to_dict(filename):
+        return json.loads(Util.read_file_to_string(os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)))
+
+    @staticmethod
+    def mock_request(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, status_code: int, filename: str = None):
+                self.status_code = status_code
+                if filename:
+                    self.text = Util.read_file_to_string(
+                        os.path.join(os.path.dirname(os.path.realpath(__file__)), f"responses/{filename}.json.resp")
+                    )
+                else:
+                    self.text = ""
+
+            def json(self):
+                return json.loads(self.text)
+
+        if kwargs.get("headers", {}).get("X-RFToken") == "bad_api_key":
+            return MockResponse(401)
+        if kwargs.get("headers", {}).get("X-RFToken") == "unauthorized":
+            return MockResponse(403)
+        if kwargs.get("headers", {}).get("X-RFToken") == "unknown":
+            return MockResponse(407)
+        if kwargs.get("headers", {}).get("X-RFToken") == "server_error":
+            return MockResponse(700)
+        if kwargs.get("headers", {}).get("X-RFToken") == "invalid_json":
+            return MockResponse(200, "invalid_json")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/ip/151.10.1.8":
+            return MockResponse(200, "lookup_ip_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/ip/151.14.112.855":
+            return MockResponse(404, "lookup_ip_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/ip/151.1014.112.855":
+            return MockResponse(404, "lookup_ip_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/domain/coffee-break.pl":
+            return MockResponse(200, "lookup_domain_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/domain/valid_not_found_domain.com":
+            return MockResponse(404, "lookup_domain_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/domain/not_valid_not_found_domain.com":
+            return MockResponse(404, "lookup_domain_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/url/https%3A%2F%2Fbbc.com":
+            return MockResponse(200, "lookup_url_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/url/https%3A%2F%2Fsome_url_not_found":
+            return MockResponse(404, "lookup_url_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/url/https%3A%2F%2Fsome_invalid_url_not_found":
+            return MockResponse(404, "lookup_url_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/hash/44d88612fea8a8f36de82e1278abb02f":
+            return MockResponse(200, "lookup_hash_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/hash/5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8":
+            return MockResponse(404, "lookup_hash_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/hash/44d88612fea8a8f36devxcvfxcd82e1278abb02f":
+            return MockResponse(404, "lookup_hash_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/alert/fhS1El":
+            return MockResponse(200, "lookup_alert_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/alert/fhS2El":
+            return MockResponse(404, "lookup_alert_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/alert/fhSghfn5431El":
+            return MockResponse(404, "lookup_alert_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/vulnerability/CVE-2009-0001":
+            return MockResponse(200, "lookup_vulnerability_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/vulnerability/CVE-2009-0002":
+            return MockResponse(404, "lookup_vulnerability_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/vulnerability/CVE-209-001":
+            return MockResponse(404, "lookup_vulnerability_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/malware/ShciZX":
+            return MockResponse(200, "lookup_malware_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/malware/ShciYX":
+            return MockResponse(404, "lookup_malware_not_found")
+        if kwargs.get("url") == "https://api.recordedfuture.com/v2/malware/Shcfg345iZX":
+            return MockResponse(404, "lookup_malware_not_found")
+        raise NotImplementedError("Not implemented", kwargs)
