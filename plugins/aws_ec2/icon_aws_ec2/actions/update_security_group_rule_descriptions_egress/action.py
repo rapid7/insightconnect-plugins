be252d1c68448b56641b00b0999f7199f36fea80
Max Berezin <52976633+mberezin-r7@users.noreply.github.com>
2022-06-07 15:42:36 -0400

    [SOAR-9449] AWS EC2 - Bump Python to 3 and update to insightconnect runtime (#1299)
    Co-authored-by: Jakub Kaluza <jakub_kaluza@rapid7.com>

---
+++ plugins/aws_ec2/icon_aws_ec2/actions/update_security_group_rule_descriptions_egress/action.py
@@ -0,0 +1,21 @@
+from .schema import (
+    UpdateSecurityGroupRuleDescriptionsEgressInput,
+    UpdateSecurityGroupRuleDescriptionsEgressOutput,
+    Component,
+)
+
+# Custom imports below
+from icon_aws_ec2.util.common import AWSAction
+
+
+class UpdateSecurityGroupRuleDescriptionsEgress(AWSAction):
+    def __init__(self):
+        super().__init__(
+            name="update_security_group_rule_descriptions_egress",
+            description=Component.DESCRIPTION,
+            input_=UpdateSecurityGroupRuleDescriptionsEgressInput(),
+            output=UpdateSecurityGroupRuleDescriptionsEgressOutput(),
+            aws_service="ec2",
+            aws_command="update_security_group_rule_descriptions_egress",
+            pagination_helper=None,
+        )
