777622f9a8bbfb69ae3692fbeefad69abcf23063
Max Berezin <52976633+mberezin-r7@users.noreply.github.com>
2022-06-07 15:42:36 -0400

    [SOAR-9449] AWS EC2 - Bump Python to 3 and update to insightconnect runtime (#1299)
    Co-authored-by: Jakub Kaluza <jakub_kaluza@rapid7.com>

---
+++ plugins/aws_ec2/icon_aws_ec2/actions/describe_security_groups/action.py
@@ -0,0 +1,22 @@
+from .schema import DescribeSecurityGroupsInput, DescribeSecurityGroupsOutput, Component
+
+# Custom imports below
+from icon_aws_ec2.util.common import AWSAction, PaginationHelper
+
+
+class DescribeSecurityGroups(AWSAction):
+    def __init__(self):
+        super().__init__(
+            name="describe_security_groups",
+            description=Component.DESCRIPTION,
+            input_=DescribeSecurityGroupsInput(),
+            output=DescribeSecurityGroupsOutput(),
+            aws_service="ec2",
+            aws_command="describe_security_groups",
+            pagination_helper=PaginationHelper(
+                input_token=["next_token"],
+                output_token=["next_token"],
+                result_key=["SecurityGroups"],
+                limit_key="max_results",
+            ),
+        )
