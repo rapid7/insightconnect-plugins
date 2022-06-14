2cbec3b005b0e7f20195796c435bbea19647d6d7
Conor McNally <conor_mcnally@rapid7.com>
2022-06-14 15:50:52 +0100

    Working solution / Code complete

--- plugins/rest/komand_rest/actions/post/action.py
+++ plugins/rest/komand_rest/actions/post/action.py
@@ -1,6 +1,6 @@
 import insightconnect_plugin_runtime
 from .schema import PostInput, PostOutput, Component, Input, Output
-import json
+import logging
 
 # Custom imports below
 from komand_rest.util.util import Common
@@ -17,16 +17,11 @@ class Post(insightconnect_plugin_runtime.Action):
         headers = params.get(Input.HEADERS, {})
         body = params.get(Input.BODY, {})
 
-        # Determine if headers / content-type == x-www-form-urlencoded
-        # If so, convert body to string and urlencode ('&', '=')
-        # Else, proceed with regular body
+        # Support encoding for x-www-form-urlencoded
         for key, value in headers.items():
             if "content-type" in key.lower():
                 if "x-www-form-urlencoded" in value:
                     body = convert_dict_body_to_string(body)
-                    return body
-                elif "json" in value:
-                    return body
 
         response = self.connection.api.call_api(
             method="POST",
