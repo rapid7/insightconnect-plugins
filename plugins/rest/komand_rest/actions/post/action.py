13dd8eb4b396758de4ea2223f9902852a5b05e24
Conor McNally <conor_mcnally@rapid7.com>
2022-06-15 13:51:47 +0100

    Move function from action to util

--- plugins/rest/komand_rest/actions/post/action.py
+++ plugins/rest/komand_rest/actions/post/action.py
@@ -3,23 +3,8 @@ from .schema import PostInput, PostOutput, Component, Input, Output
 
 # Custom imports below
 from komand_rest.util.util import Common
-from komand_rest.util.util import convert_dict_body_to_string
-from typing import Dict, Any
-
-
-def convert_body_for_urlencoded(headers: Dict[str, str], body: Dict[str, Any]) -> str:
-    """
-    This method will encode the body if the headers == x-www-form-urlencoded
-    :param headers: Headers dict to read for conditional
-    :param body: Body dict to convert to string with encoding
-    :return: Body as an encoded string value
-    """
-    for key, value in headers.items():
-        if "content-type" in key.lower():
-            if "x-www-form-urlencoded" in value:
-                body = convert_dict_body_to_string(body)
-                break
-    return body
+from komand_rest.util.util import convert_dict_body_to_string, convert_body_for_urlencoded
+from typing import Dict, Any, Union
 
 
 class Post(insightconnect_plugin_runtime.Action):
@@ -32,7 +17,7 @@ class Post(insightconnect_plugin_runtime.Action):
         headers = params.get(Input.HEADERS, {})
         body = params.get(Input.BODY, {})
 
-        convert_body_for_urlencoded(headers, body)
+        body = convert_body_for_urlencoded(headers, body)
 
         response = self.connection.api.call_api(
             method="POST",
