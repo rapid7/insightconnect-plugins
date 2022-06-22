1510bc01b35834211b4e36a0145374d979278469
Maxim Berezin <maxim_berezin@rapid7.com>
2022-06-22 18:29:09 -0400

    Remove unused imports

--- plugins/cortex_v2/icon_cortex_v2/actions/run_analyzer/action.py
+++ plugins/cortex_v2/icon_cortex_v2/actions/run_analyzer/action.py
@@ -1,5 +1,4 @@
 import insightconnect_plugin_runtime
-from insightconnect_plugin_runtime.exceptions import PluginException
 from .schema import RunAnalyzerInput, RunAnalyzerOutput, Input, Output, Component
 
 # Custom imports below
--- plugins/cortex_v2/icon_cortex_v2/actions/run_file_analyzer/action.py
+++ plugins/cortex_v2/icon_cortex_v2/actions/run_file_analyzer/action.py
@@ -1,5 +1,4 @@
 import insightconnect_plugin_runtime
-from insightconnect_plugin_runtime.exceptions import PluginException
 from .schema import RunFileAnalyzerInput, RunFileAnalyzerOutput, Input, Output, Component
 
 # Custom imports below
