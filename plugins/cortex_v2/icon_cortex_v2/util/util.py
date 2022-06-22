0ffddfdb2bf086d1ad8688d982a2d70742d3807e
Maxim Berezin <maxim_berezin@rapid7.com>
2022-06-22 16:32:49 -0400

    Separate relevant keys into constant variables

--- plugins/cortex_v2/icon_cortex_v2/util/util.py
+++ plugins/cortex_v2/icon_cortex_v2/util/util.py
@@ -1,16 +1,17 @@
 from typing import List, Dict, Any
 
 
+RELEVANT_ANALYZER_KEYS = {"id", "version", "dataTypeList", "name", "description", "license", "author", "url"}
+RELEVANT_JOB_KEYS = {"status", "date", "id", "analyzerId"}
+RELEVANT_JOB_ARTIFACT_KEYS = {"dataType", "data"}
+
+
 def filter_analyzers(analyzers: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     return [filter_analyzer(analyzer) for analyzer in analyzers]
 
 
 def filter_analyzer(analyzer: Dict[str, Any]) -> Dict[str, Any]:
-    return {
-        key: value
-        for key, value in analyzer.items()
-        if key in {"id", "version", "dataTypeList", "name", "description", "license", "author", "url"}
-    }
+    return {key: value for key, value in analyzer.items() if key in RELEVANT_ANALYZER_KEYS}
 
 
 def filter_jobs(jobs: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
@@ -18,7 +19,7 @@ def filter_jobs(jobs: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
 
 
 def filter_job(job: Dict[str, Any]) -> Dict[str, Any]:
-    return {key: value for key, value in job.items() if key in {"status", "date", "id", "analyzerId"}}
+    return {key: value for key, value in job.items() if key in RELEVANT_JOB_KEYS}
 
 
 def filter_job_artifacts(artifacts: List[Dict]) -> List[Dict[str, Any]]:
@@ -26,7 +27,7 @@ def filter_job_artifacts(artifacts: List[Dict]) -> List[Dict[str, Any]]:
 
 
 def filter_job_artifact(artifact: Dict[str, Any]) -> Dict[str, Any]:
-    return {key: value for key, value in artifact.items() if key in {"dataType", "data"}}
+    return {key: value for key, value in artifact.items() if key in RELEVANT_JOB_ARTIFACT_KEYS}
 
 
 def eq_(field: Any, value: Any) -> Dict[str, Any]:
