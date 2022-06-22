c143d5d05eb43fa2dca05ba431df79ba373fd84b
Maxim Berezin <maxim_berezin@rapid7.com>
2022-06-22 16:08:46 -0400

    Add type hints to util methods. Refactor single letter named variables.

--- plugins/cortex_v2/icon_cortex_v2/util/util.py
+++ plugins/cortex_v2/icon_cortex_v2/util/util.py
@@ -1,39 +1,39 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 
 
-def filter_analyzers(analyzers: List[Dict]):
+def filter_analyzers(analyzers: List[Dict]) -> List[Dict]:
     return [filter_analyzer(analyzer) for analyzer in analyzers]
 
 
-def filter_analyzer(analyzer: Dict):
+def filter_analyzer(analyzer: Dict) -> Dict:
     return {
-        k: v
-        for k, v in analyzer.items()
-        if k in {"id", "version", "dataTypeList", "name", "description", "license", "author", "url"}
+        key: value
+        for key, value in analyzer.items()
+        if key in {"id", "version", "dataTypeList", "name", "description", "license", "author", "url"}
     }
 
 
-def filter_jobs(jobs: List[Dict]):
+def filter_jobs(jobs: List[Dict]) -> List[Dict]:
     return [filter_job(job) for job in jobs]
 
 
-def filter_job(job: Dict):
-    return {k: v for k, v in job.items() if k in {"status", "date", "id", "analyzerId"}}
+def filter_job(job: Dict) -> Dict:
+    return {key: value for key, value in job.items() if key in {"status", "date", "id", "analyzerId"}}
 
 
-def filter_job_artifacts(artifacts: List[Dict]):
+def filter_job_artifacts(artifacts: List[Dict]) -> List[Dict]:
     return [filter_job_artifact(artifact) for artifact in artifacts]
 
 
-def filter_job_artifact(artifact: Dict):
-    return {k: v for k, v in artifact.items() if k in {"dataType", "data"}}
+def filter_job_artifact(artifact: Dict) -> Dict:
+    return {key: value for key, value in artifact.items() if key in {"dataType", "data"}}
 
 
-def eq_(field, value):
+def eq_(field: Any, value: Any) -> Dict:
     # Based on https://github.com/TheHive-Project/Cortex4py/blob/2.1.0/cortex4py/query.py#L2
     return {"_field": field, "_value": value}
 
 
-def and_(criteria):
+def and_(criteria: Any) -> Dict:
     # Based on https://github.com/TheHive-Project/Cortex4py/blob/2.1.0/cortex4py/query.py#L22
     return {"_and": criteria}
