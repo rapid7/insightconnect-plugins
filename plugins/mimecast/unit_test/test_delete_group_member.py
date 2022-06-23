cecefc0ccd263ebfe67e54a4e4bdca3e52d0a22f
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-06-23 11:12:39 -0400

    [MC-929] [MC-932] [MC-882] Update Mimecast plugin (#1296) (#1325)
    * [MC-929] Update SDK version | Add new action Get Audit Events | Add unit tests for all actions and error handling | Create separate class for API communication | Add base URL of API for plugin
    * [MC-929] Code reformat. Fix prospector errors
    * [MC-929] Code reformate
    * [MC-929] Fix pylint error
    * [MC-929] Update example in help
    * [MC-929] Resolve PR comments

---
+++ plugins/mimecast/unit_test/test_delete_group_member.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from unittest import TestCase
+from unittest.mock import patch
+
+sys.path.append(os.path.abspath("../"))
+from insightconnect_plugin_runtime.exceptions import PluginException
+from komand_mimecast.util.constants import BASIC_ASSISTANCE_MESSAGE, FOLDER_EMAIL_NOT_FOUND_ERROR, ERROR_CASES
+from unit_test.util import Util
+from komand_mimecast.actions import DeleteGroupMember
+
+
+@patch("requests.request", side_effect=Util.mocked_request)
+class TestDeleteGroupMember(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(DeleteGroupMember())
+
+    def test_delete_group_member(self, mocked_request):
+        actual = self.action.run(Util.load_json("inputs/delete_group_member.json.exp"))
+        expect = Util.load_json("expected/delete_group_member.json.exp")
+        self.assertEqual(expect, actual)
+
+    def test_bad_delete_group_member(self, mocked_request):
+        with self.assertRaises(PluginException) as exception:
+            self.action.run(Util.load_json("inputs/delete_group_member_bad.json.exp"))
+        self.assertEqual(exception.exception.cause, ERROR_CASES.get(FOLDER_EMAIL_NOT_FOUND_ERROR))
+        self.assertEqual(exception.exception.assistance, BASIC_ASSISTANCE_MESSAGE)
