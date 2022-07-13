c885c4affcecce200229a1f828276ef26c8d7940
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-07-13 10:40:25 -0400

    SOAR-9823 fix issue with incorrect data validation (#1322) (#1348)

---
+++ plugins/azure_ad_admin/unit_test/mock.py
@@ -0,0 +1,24 @@
+from typing import Dict
+
+
+class MockRequest:
+    def __init__(self, status_code):
+        self.status_code = status_code
+
+    def json(self) -> Dict:
+        return {
+            "accountEnabled": True,
+            "mobilePhone": None,
+            "manager": {
+                "@odata.type": "#microsoft.graph.user",
+                "city": None,
+                "companyName": None,
+            },
+        }
+
+    def text(self) -> Dict:
+        return {"text": "sample response"}
+
+    def raise_for_status(self):
+        if 400 <= self.status_code:
+            raise Exception("HTTPError")
