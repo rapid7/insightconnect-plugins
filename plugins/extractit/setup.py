40f17f00af843658515588079d737f8411480d3a
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2022-07-19 15:54:46 +0200

    ExtractIt - 9478 - Fix issue with extracting URLs from link query parameters (#1339)

--- plugins/extractit/setup.py
+++ plugins/extractit/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="extractit-rapid7-plugin",
-      version="3.0.3",
+      version="3.0.4",
       description="The ExtractIt plugin has a collection of actions used to extract various information from text. These include URLs, domains, emails, IPs, and more",
       author="rapid7",
       author_email="",
--- plugins/extractit/unit_test/test_url_extractor.py
+++ plugins/extractit/unit_test/test_url_extractor.py
@@ -1,10 +1,16 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
 from unittest import TestCase
 from icon_extractit.actions.url_extractor import UrlExtractor
 from icon_extractit.actions.url_extractor.schema import Input, Output
 from unit_test.util import Util
+from icon_extractit.util.extractor import remove_extracted_urls_from_links
 from parameterized import parameterized
 
-parameters = Util.load_parameters("url_extractor").get("parameters")
+parameters = Util.load_parameters("url_extractor").get("parameters", {})
 
 
 class TestUrlExtractor(TestCase):
@@ -14,3 +20,24 @@ class TestUrlExtractor(TestCase):
         actual = action.run({Input.STR: string, Input.FILE: file, Input.KEEP_ORIGINAL_URLS: keep_original_urls})
         expected = {Output.URLS: expected}
         self.assertEqual(expected, actual)
+
+    @parameterized.expand(
+        [
+            (
+                [
+                    "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital",
+                    "http://example.com/?url=http://example.com",
+                    "http://example.com",
+                    "https://example.com/api?url=http://example2.com/QWekqjwekj23$!@412kj4?q=Test+test==&amp",
+                    "http://example2.com/QWekqjwekj23$!@412kj4?q=Test+test==&amp",
+                ],
+                [
+                    "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital",
+                    "http://example.com/?url=http://example.com",
+                    "https://example.com/api?url=http://example2.com/QWekqjwekj23$!@412kj4?q=Test+test==&amp",
+                ],
+            )
+        ]
+    )
+    def test_remove_extracted_urls_from_links(self, input_list_of_urls, expected):
+        self.assertEqual(expected, remove_extracted_urls_from_links(input_list_of_urls))
