a0f0a5fd86ed91606b4d733791e15d9b8dac6089
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2022-07-19 18:26:14 +0200

    [SOAR-9944] Insight IDR Update (#1332)
    * Insight IDR - 9944 - Added new and updated existing actions
    
    * Updated region_code connection input, added more sort enum values for list investigations action, unittests

---
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/actions/create_investigation/action.py
@@ -0,0 +1,60 @@
+import insightconnect_plugin_runtime
+from .schema import CreateInvestigationInput, CreateInvestigationOutput, Input, Output, Component
+from insightconnect_plugin_runtime.exceptions import PluginException
+from insightconnect_plugin_runtime.helper import clean
+from komand_rapid7_insightidr.util.endpoints import Investigations
+from komand_rapid7_insightidr.util.resource_helper import ResourceHelper
+
+# Custom imports below
+import json
+
+
+class CreateInvestigation(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="create_investigation",
+            description=Component.DESCRIPTION,
+            input=CreateInvestigationInput(),
+            output=CreateInvestigationOutput(),
+        )
+
+    def run(self, params={}):
+        title = params.get(Input.TITLE)
+        status = params.get(Input.STATUS)
+        priority = params.get(Input.PRIORITY)
+        disposition = params.get(Input.DISPOSITION)
+        email = params.get(Input.EMAIL)
+
+        data = clean(
+            {
+                Input.TITLE: title,
+                Input.STATUS: status,
+                Input.PRIORITY: priority,
+                Input.DISPOSITION: disposition,
+            }
+        )
+
+        if email:
+            data["assignee"][Input.EMAIL] = email
+
+        request = ResourceHelper(self.connection.session, self.logger)
+
+        endpoint = Investigations.create_investigation(self.connection.url)
+        response = request.resource_request(endpoint, "post", payload=data)
+
+        try:
+            result = json.loads(response.get("resource"))
+        except json.decoder.JSONDecodeError:
+            self.logger.error(f"InsightIDR response: {response}")
+            raise PluginException(
+                cause="The response from InsightIDR was not in the correct format.",
+                assistance="Contact support for help. See log for more details",
+            )
+        try:
+            return {Output.INVESTIGATION: clean(result)}
+        except KeyError:
+            self.logger.error(result)
+            raise PluginException(
+                cause="The response from InsightIDR was not in the correct format.",
+                assistance="Contact support for help. See log for more details",
+            )
