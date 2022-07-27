09e0bdad26f0c92fd5bd7149d09eac4d866ddb33
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2022-07-27 16:34:57 +0100

    Generate unit tests | Add mock and test utils files

---
+++ plugins/rapid7_insightvm_cloud/unit_test/mock.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+from unit_test.utils import Utils
+
+STUB_ASSET_ID = "5058b0b4-701a-414e-9630-430d2cddbf4d"
+STUB_SEARCH_ID = "86a8abc0-95f3-4353-adf5-abb631c1f824"
+STUB_REGION = "us"
+SUB_BAD_REGION = "us_invalid"
+REQUEST_GET = "GET"
+REQUEST_POST = "POST"
+
+
+# Define and return mock API responses based on request type and endpoint
+def mock_request(*args, **_kwarg):
+    return mock_request_selection(args[0], args[1])
+
+
+class MockResponse:
+    def __init__(self, filename: str, status_code: int) -> None:
+        self.status_code = status_code
+        if filename:
+            self.text = Utils.read_file_to_string(
+                os.path.join(os.path.dirname(os.path.realpath(__file__)), f"payloads/{filename}.json.resp")
+            )
+        else:
+            self.text = ""
+
+    def json(self):
+        data = json.loads(self.text)
+        return data.get("actual", data)
+
+
+def mock_request_post(url):
+    if url == f"https://{STUB_REGION}.api.insight.rapid7.com/vm/v4/integration/assets/{STUB_ASSET_ID}":
+        return MockResponse("get_asset", 200)
+    raise Exception("Response has been not implemented")
+
+
+def mock_request_get(url):
+    if url == f"https://{STUB_REGION}.api.insight.rapid7.com/vm/v4/integration/assets/{STUB_ASSET_ID}":
+        return MockResponse("get_asset", 200)
+    raise Exception("Response has been not implemented")
+
+
+def mock_request_selection(method, url):
+    # Check reqeust type and endpoint. Return appropriate file name to be loaded and response code
+    if method == REQUEST_POST:
+        return mock_request_post(url)
+    elif method == REQUEST_GET:
+        return mock_request_get(url)
+    raise Exception("Response has been not implemented")
---
+++ plugins/rapid7_insightvm_cloud/unit_test/utils.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import json
+
+from icon_rapid7_insightvm_cloud.connection.connection import Connection
+from icon_rapid7_insightvm_cloud.connection.schema import Input
+
+
+class Utils:
+    @staticmethod
+    def read_file_to_dict(filename):
+        with open(filename, "rt", encoding="utf8"):
+            return json.loads(
+                Utils.read_file_to_string(os.path.join(os.path.dirname(os.path.realpath(__file__)), filename))
+            )
+
+    @staticmethod
+    def read_file_to_string(filename):
+        with open(filename, "rt", encoding="utf8") as my_file:
+            return my_file.read()
+
+    @staticmethod
+    def default_connector(action, connect_params: object = None):
+        default_connection = Connection()
+        default_connection.logger = logging.getLogger("connection logger")
+        if connect_params:
+            params = connect_params
+        else:
+            params = {
+                Input.REGION: "us",
+                Input.CREDENTIALS: {
+                    "secretKey": "secret_key"
+                },
+            }
+        default_connection.connect(params)
+        action.connection = default_connection
+        action.logger = logging.getLogger("action logger")
+        return default_connection, action
