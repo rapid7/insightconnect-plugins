98ddba264b802adb5102ba30d62ef8689ede979a
Conor McNally <conor_mcnally@rapid7.com>
2022-08-02 13:56:13 +0100

    Undoing int_to_bool change

--- plugins/type_converter/icon_type_converter/actions/integer_to_boolean/action.py
+++ plugins/type_converter/icon_type_converter/actions/integer_to_boolean/action.py
@@ -15,14 +15,7 @@ class IntegerToBoolean(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        value = params.get(Input.INPUT)
-        if value == 0:
-            return {Output.OUTPUT: False}
-        elif value == 1:
-            return {Output.OUTPUT: True}
-        else:
-            raise PluginException(cause="Invalid input", assistance="Value must be 0 or 1")
-        # try:
-        #     return {Output.OUTPUT: params.get(Input.INPUT) != 0}
-        # except Exception as error:
-        #     raise PluginException(cause="Converting error.", assistance="Check input", data=error)
+        try:
+            return {Output.OUTPUT: params.get(Input.INPUT) != 0}
+        except Exception as error:
+            raise PluginException(cause="Converting error.", assistance="Check input", data=error)
