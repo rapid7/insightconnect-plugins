75fde9cbb96bd8609fd3a90ad463a617d3868645
Aine <aine_holland@rapid7.com>
2022-08-10 09:15:06 -0400

    no broken parenthesis

--- plugins/extractit/icon_extractit/actions/url_extractor/action.py
+++ plugins/extractit/icon_extractit/actions/url_extractor/action.py
@@ -25,39 +25,28 @@ class UrlExtractor(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        urls = clear_urls(
-            remove_extracted_urls_from_links(
-                extract(Regex.URL, params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS))
-            )
-        )
+        clear_urls = extract(Regex.URL, params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS))
+        clear_urls = self.balance_parentheses(clear_urls)
 
-        input_str = self.get_input_string(
-            params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS)
-        )
+        return {
+            Output.URLS: clear_urls
+        }
 
-        for i, url in enumerate(urls):
-            if url[len(url) - 1] == ")":
-                if input_str[(input_str.find(url) - 1)] == "(":
-                    urls[i] = url[: len(url) - 1]
-
-        return {Output.URLS: urls}
-
-    def get_input_string(self, provided_string: str, provided_file: str, keep_original_url: bool = False):
-        if provided_string:
-            if keep_original_url:
-                return provided_string
-            else:
-                provided_string = urllib.parse.unquote(provided_string)
-                return provided_string
-        elif provided_file:
-            try:
-                if keep_original_url:
-                    provided_file = base64.b64decode(provided_file.encode(DEFAULT_ENCODING)).decode(DEFAULT_ENCODING)
-                else:
-                    provided_file = urllib.parse.unquote(
-                        base64.b64decode(provided_file.encode(DEFAULT_ENCODING)).decode(DEFAULT_ENCODING)
-                    )
-                return provided_file
-            except UnicodeDecodeError:
-                file_content = extract_content_from_file(base64.b64decode(provided_file))
-                return file_content
+    def balance_parentheses(self, clear_urls):
+        check = []
+        for url in clear_urls:
+            for i in range(len(url)):
+                if url[i] in ('(', '[', '{'):
+                    check.append(i)
+                elif url[i] in (')', ']', '{'):
+                    if len(check) != 0:
+                        check.pop()
+                    else:
+                        new_url = url[:i] + url[i+1:]
+                        clear_urls = list(map(lambda x: x.replace(url, new_url), clear_urls))
+            if len(check) != 0:
+                for i in check:
+                    new_url = url[:i] + url[i+1:]
+                    clear_urls = list(map(lambda x: x.replace(url, new_url), clear_urls))
+            check.clear()
+        return clear_urls
