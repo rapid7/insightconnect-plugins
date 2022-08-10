15cf9e06f01bd83e58d4d0e78b4c1bdb119d686a
Aine <aine_holland@rapid7.com>
2022-08-10 09:15:06 -0400

    no broken parenthesis

--- plugins/extractit/icon_extractit/actions/url_extractor/action.py
+++ plugins/extractit/icon_extractit/actions/url_extractor/action.py
@@ -16,12 +16,28 @@ class UrlExtractor(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
+        clear_urls = extract(Regex.URL, params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS))
+        clear_urls = self.balance_parentheses(clear_urls)
+
         return {
-            Output.URLS: clear_urls(
-                remove_extracted_urls_from_links(
-                    extract(
-                        Regex.URL, params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS)
-                    )
-                )
-            )
+            Output.URLS: clear_urls
         }
+
+    def balance_parentheses(self, clear_urls):
+        check = []
+        for url in clear_urls:
+            for i in range(len(url)):
+                if url[i] in ('(', '[', '{'):
+                    check.append(i)
+                elif url[i] in (')', ']', '{'):
+                    if len(check) != 0:
+                        check.pop()
+                    else:
+                        new_url = url[:i] + url[i+1:]
+                        clear_urls = list(map(lambda x: x.replace(url, new_url), clear_urls))
+            if len(check) != 0:
+                for i in check:
+                    new_url = url[:i] + url[i+1:]
+                    clear_urls = list(map(lambda x: x.replace(url, new_url), clear_urls))
+            check.clear()
+        return clear_urls
