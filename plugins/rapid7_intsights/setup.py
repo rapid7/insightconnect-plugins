8684a713c3790a6451990bd93eae85e3b3ab918f
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2022-08-11 16:03:13 +0100

    Regenerate

--- plugins/rapid7_intsights/setup.py
+++ plugins/rapid7_intsights/setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup, find_packages
 
 
-setup(name="rapid7_intsights-rapid7-plugin",
+setup(name="rapid7_intsights-rapid7_ab-plugin",
       version="3.2.1",
       description="Threat Command by Rapid7 is disrupting external threat intelligence with a combination of human and automated collection, intelligent analysis, and strategic threat hunting that turns the clear, deep, and dark webs into an intelligence resource that any company can deploy",
-      author="rapid7",
+      author="rapid7_ab",
       author_email="",
       url="",
       packages=find_packages(),
