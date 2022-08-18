79a1bbfa0fd9a0ee4ad44781936eefe699b896be
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2022-08-18 18:38:06 +0100

    Rapid7 InsightVM Cloud 3.1.0 Release (#1393)
    * [SOAR-9972] [SOAR-7617] InsightVM Cloud Update Error Handling | Cloud Enablement (#1370)
    
    * Update error handling | Update supported versions | Regenerate | Improve Stop Scan output
    
    * Generate unit tests | Add mock and test utils files
    
    * Generate unit tests | Update mock and API request | Clean output Get Asset
    
    * Add additional tests Get Asset
    
    * Stop failing unimplemented tests
    
    * Move utils methods to test file
    
    * Undo move utils
    
    * Add tests for Get Asset and Asset Search | Generated test file for Get scan
    
    * Add tests for Get Scan | Update Get Scan handle empty response | Update call_api empty response
    
    * Add all unit tests | Update handling of Get Scan and Start Scan action error handling and clean output | Update call api function
    
    * Fix failing tests | Reversion | Regenerate
    
    * Black formatting
    
    * Refactor static code analysis
    
    * Update test file path | Regenerate
    
    * Fix empty body values in request
    
    * Update help.md
    
    * Update for cloud enablement | update versioning | regenerate
    
    * Fix connection test (#1391)

--- plugins/rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/get_scan/action.py
+++ plugins/rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/get_scan/action.py
@@ -16,15 +16,14 @@ class GetScan(insightconnect_plugin_runtime.Action):
 
     def run(self, params={}):
         scan_id = params.get(Input.SCAN_ID)
-
         response = self.connection.ivm_cloud_api.call_api("scan/" + scan_id, "GET")
+        response = insightconnect_plugin_runtime.helper.clean(response)
+        del response["status_code"]
         if response.get("id") is None:
             raise PluginException(
                 cause=f"Failed to get a valid response from InsightVM with given scan ID '{scan_id}'.",
                 assistance="Please try a different scan ID.",
-                data=400,
+                data=response,
             )
         else:
-            if response.get("finished") is None:
-                del response["finished"]
             return response
