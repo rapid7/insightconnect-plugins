8eb79af3a7d8465e32210659bfca23ee7a88e6c7
Aine <69211380+aholland-r7@users.noreply.github.com>
2022-08-24 10:31:40 -0400

    Soar 9910 ExtractIt 3 Issues Fix (#1384)
    Co-authored-by: Conor McNally <conor_mcnally@rapid7.com>
    Co-authored-by: igorski-r7 <igor_gorski@rapid7.com>

--- plugins/extractit/icon_extractit/actions/url_extractor/action.py
+++ plugins/extractit/icon_extractit/actions/url_extractor/action.py
@@ -2,8 +2,17 @@ import insightconnect_plugin_runtime
 from .schema import UrlExtractorInput, UrlExtractorOutput, Input, Output, Component
 
 # Custom imports below
+import base64
+import urllib.parse
 from icon_extractit.util.util import Regex
-from icon_extractit.util.extractor import extract, clear_urls, remove_extracted_urls_from_links
+from icon_extractit.util.extractor import (
+    extract,
+    clear_urls,
+    remove_extracted_urls_from_links,
+    extract_content_from_file,
+)
+
+DEFAULT_ENCODING = "utf-8"
 
 
 class UrlExtractor(insightconnect_plugin_runtime.Action):
@@ -16,12 +25,39 @@ class UrlExtractor(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        return {
-            Output.URLS: clear_urls(
-                remove_extracted_urls_from_links(
-                    extract(
-                        Regex.URL, params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS)
-                    )
-                )
+        urls = clear_urls(
+            remove_extracted_urls_from_links(
+                extract(Regex.URL, params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS))
             )
-        }
+        )
+
+        input_str = self.get_input_string(
+            params.get(Input.STR), params.get(Input.FILE), params.get(Input.KEEP_ORIGINAL_URLS)
+        )
+
+        for i, url in enumerate(urls):
+            if url[len(url) - 1] == ")":
+                if input_str[(input_str.find(url) - 1)] == "(":
+                    urls[i] = url[: len(url) - 1]
+
+        return {Output.URLS: urls}
+
+    def get_input_string(self, provided_string: str, provided_file: str, keep_original_url: bool = False):
+        if provided_string:
+            if keep_original_url:
+                return provided_string
+            else:
+                provided_string = urllib.parse.unquote(provided_string)
+                return provided_string
+        elif provided_file:
+            try:
+                if keep_original_url:
+                    provided_file = base64.b64decode(provided_file.encode(DEFAULT_ENCODING)).decode(DEFAULT_ENCODING)
+                else:
+                    provided_file = urllib.parse.unquote(
+                        base64.b64decode(provided_file.encode(DEFAULT_ENCODING)).decode(DEFAULT_ENCODING)
+                    )
+                return provided_file
+            except UnicodeDecodeError:
+                file_content = extract_content_from_file(base64.b64decode(provided_file))
+                return file_content
