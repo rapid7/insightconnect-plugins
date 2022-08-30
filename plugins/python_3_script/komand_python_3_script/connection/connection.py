e8222763f3d5a628716d84d8c9e1fe96f816a155
Aine <aine_holland@rapid7.com>
2022-08-30 13:42:18 -0400

    stop using komand

--- plugins/python_3_script/komand_python_3_script/connection/connection.py
+++ plugins/python_3_script/komand_python_3_script/connection/connection.py
@@ -1,13 +1,13 @@
 import os
-import komand
+import insightconnect_plugin_runtime
 
 from subprocess import run, check_output, TimeoutExpired, CalledProcessError  # noqa: B404
-from komand.exceptions import ConnectionTestException
+from insightconnect_plugin_runtime.exceptions import ConnectionTestException
 
 from .schema import ConnectionSchema, Input
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
         self.dependencies, self.timeout = None, None
