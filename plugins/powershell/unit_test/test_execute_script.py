6c683a0bf6af4668753bcc2ca2f4299ebbfb9a1c
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-08-31 13:57:53 -0400

    Soar 11068 powershell fix exposed credentials (#1408)
    * Fixing the help.md conflict
    
    * [SOAR-11068] - PowerShell - Migrate to insighconnect_plugin_runtime
    
    Co-authored-by: Bindu Laxminarayan <bindu.laxminarayan@rapid7.com>
    Co-authored-by: Jakub Kaluza <jakub_kaluza@rapid7.com>

--- plugins/powershell/unit_test/test_execute_script.py
+++ plugins/powershell/unit_test/test_execute_script.py
@@ -8,19 +8,13 @@ from unittest.mock import patch
 from parameterized import parameterized
 
 from unit_test.util import Util
-from komand_powershell.actions.execute_script import ExecuteScript
+from icon_powershell.actions.execute_script import ExecuteScript
 
 
 class TestExecuteScript(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.params = {
-            "address": "10.0.1.11",
-            "username_and_password": {"password": "example_password", "username": "example_user"},
-            "host_name": "windows",
-            "script": "R2V0LURhdGU=",
-            "secret_key": {"secretKey": "s1e2c3r4e5t67k8e9y"},
-        }
+        cls.params = {"address": "10.0.1.11", "host_name": "windows", "script": "R2V0LURhdGU="}
 
     @parameterized.expand(
         [
@@ -28,7 +22,7 @@ class TestExecuteScript(TestCase):
             ("cred_ssp", "inputs/credssp_connection.json.resp", "expecteds/credssp_connection.json.resp"),
         ]
     )
-    @patch("komand_powershell.util.util.FixWinrmSession", side_effect=Util.mock_powershell)
+    @patch("icon_powershell.util.util.FixWinrmSession", side_effect=Util.mock_powershell)
     def test_powershell_string_ntlm_credssp(self, name: str, input_path: str, expected_path: str, mock_powershell):
         params = Util.read_file_to_dict(input_path)
         action = Util.default_connector(ExecuteScript(), params)
@@ -44,8 +38,8 @@ class TestExecuteScript(TestCase):
         expected = Util.read_file_to_dict("expecteds/local_connection.json.resp")
         self.assertEqual(actual, expected)
 
-    @patch("komand_powershell.util.util.FixWinrmSession", side_effect=Util.mock_powershell)
-    @patch("komand_powershell.util.util.configure_machine_for_kerberos_connection")
+    @patch("icon_powershell.util.util.FixWinrmSession", side_effect=Util.mock_powershell)
+    @patch("icon_powershell.util.util.configure_machine_for_kerberos_connection")
     def test_powershell_string_kerberos(self, mock_configure_kerberos, mock_powershell):
         params = Util.read_file_to_dict("inputs/kerberos_connection.json.resp")
         action = Util.default_connector(ExecuteScript(), params)
