fd6afb25dc4e650b4192f534b9a110990a65a433
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2022-09-13 13:18:46 +0100

    Add url encoded body transformation

--- plugins/rest/komand_rest/actions/get/action.py
+++ plugins/rest/komand_rest/actions/get/action.py
@@ -3,7 +3,7 @@ from .schema import GetInput, GetOutput, Component, Input, Output
 
 # Custom imports below
 from komand_rest.util.util import Common
-
+from komand_rest.util.util import convert_body_for_urlencoded, check_headers_for_urlencoded
 
 class Get(insightconnect_plugin_runtime.Action):
     def __init__(self):
@@ -12,12 +12,16 @@ class Get(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        response = self.connection.api.call_api(
-            method="GET",
-            path=params.get(Input.ROUTE),
-            headers=params.get(Input.HEADERS, {}),
-            data=params.get(Input.BODY)
-        )
+        headers = params.get(Input.HEADERS, {})
+        body = params.get(Input.BODY)
+
+        if body and check_headers_for_urlencoded(headers):
+            body = convert_body_for_urlencoded(headers, body)
+            kwargs = {"method": "GET", "path": params.get(Input.ROUTE), "data": body, "headers": headers}
+        else:
+            kwargs = {"method": "GET", "path": params.get(Input.ROUTE), "json_data": body, "headers": headers}
+
+        response = self.connection.api.call_api(**kwargs)
 
         return {
             Output.BODY_OBJECT: Common.body_object(response),
