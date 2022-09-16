1fb7b6114cda5f9cbb3f3890f08ec3548e408811
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2022-09-16 15:21:05 +0100

    Regenerate

--- plugins/rest/setup.py
+++ plugins/rest/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="rest-rapid7-plugin",
-      version="5.1.1",
+      version="5.1.0",
       description="The HTTP Requests plugin to make it easy to integrate with RESTful services",
       author="rapid7",
       author_email="",
