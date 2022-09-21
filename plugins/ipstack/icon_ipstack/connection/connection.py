58dfe9cc42509c54a609bc99c4b550a9cfc51a1d
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2022-09-21 14:47:40 +0100

    [SOAR-9664] IPStack - Cloud Enabled (#1420)

--- plugins/ipstack/icon_ipstack/connection/connection.py
+++ plugins/ipstack/icon_ipstack/connection/connection.py
@@ -1,7 +1,9 @@
 import insightconnect_plugin_runtime
-from .schema import ConnectionSchema
+from .schema import ConnectionSchema, Input
+from insightconnect_plugin_runtime.exceptions import ConnectionTestException
 
 # Custom imports below
+import json
 
 
 class Connection(insightconnect_plugin_runtime.Connection):
@@ -10,4 +12,17 @@ class Connection(insightconnect_plugin_runtime.Connection):
         self.token = None
 
     def connect(self, params):
-        self.token = params["cred_token"]["secretKey"]
+        self.token = params.get(Input.CRED_TOKEN).get("secretKey")
+
+    def test(self):
+        url = "http://api.ipstack.com/" + "check" + "?access_key=" + self.token + "&output=json"
+        resp = insightconnect_plugin_runtime.helper.open_url(url)
+        dic = json.loads(resp.read())
+        if "error" in dic:
+            code = dic["error"].get("code")
+            if code != 200:
+                raise ConnectionTestException(
+                    cause="Connection test failed", assistance="Check the API key and try again"
+                )
+        else:
+            return {"success": True}
