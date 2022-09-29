5b01e4c3e936e95dbed5f79ad7d59745b42eecb4
conor-mcnally <conor_mcnally@rapid7.com>
2022-09-29 10:53:05 +0100

    Changed thingy to return True

--- plugins/ipstack/icon_ipstack/connection/connection.py
+++ plugins/ipstack/icon_ipstack/connection/connection.py
@@ -1,9 +1,7 @@
 import insightconnect_plugin_runtime
 from .schema import ConnectionSchema, Input
-from insightconnect_plugin_runtime.exceptions import ConnectionTestException
 
 # Custom imports below
-import json
 
 
 class Connection(insightconnect_plugin_runtime.Connection):
@@ -15,14 +13,4 @@ class Connection(insightconnect_plugin_runtime.Connection):
         self.token = params.get(Input.CRED_TOKEN).get("secretKey")
 
     def test(self):
-        url = "http://api.ipstack.com/" + "check" + "?access_key=" + self.token + "&output=json"
-        resp = insightconnect_plugin_runtime.helper.open_url(url)
-        dic = json.loads(resp.read())
-        if "error" in dic:
-            code = dic["error"].get("code")
-            if code != 200:
-                raise ConnectionTestException(
-                    cause="Connection test failed", assistance="Check the API key and try again"
-                )
-        else:
-            return {"success": True}
+        return {"success": True}
