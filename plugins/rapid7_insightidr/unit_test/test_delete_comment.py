4ad5447bbc0068de074d1c13030bd40300721b8e
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2022-10-03 15:48:30 +0200

    [MC-1262] InsightIDR plugin new actions (#1431)
    * [MC-1262] InsightIDR plugin new actions
    
    * [MC-1262] Reformat, fix prospector
    
    * [MC-1262] Update unit tests

---
+++ plugins/rapid7_insightidr/unit_test/test_delete_comment.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_rapid7_insightidr.actions.delete_comment import DeleteComment
+from komand_rapid7_insightidr.actions.delete_comment.schema import Input
+from unit_test.util import Util
+from unittest.mock import patch
+from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+@patch("requests.Session.request", side_effect=Util.mocked_requests)
+class TestDeleteComment(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(DeleteComment())
+
+    @parameterized.expand(Util.load_parameters("delete_comment").get("parameters"))
+    def test_delete_comment(self, mock_request, name, comment_rrn, expected):
+        actual = self.action.run({Input.COMMENT_RRN: comment_rrn})
+        self.assertEqual(actual, expected)
+
+    @parameterized.expand(Util.load_parameters("delete_comment_bad").get("parameters"))
+    def test_delete_comment_bad(self, mock_request, name, comment_rrn, cause, assistance):
+        with self.assertRaises(PluginException) as error:
+            self.action.run({Input.COMMENT_RRN: comment_rrn})
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
