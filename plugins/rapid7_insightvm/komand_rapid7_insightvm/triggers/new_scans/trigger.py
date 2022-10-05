e544e899c313285e9a8ebd7d6efd97ee5c3bafa5
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2022-10-05 15:04:44 +0100

    Remove leftover code

--- plugins/rapid7_insightvm/komand_rapid7_insightvm/triggers/new_scans/trigger.py
+++ plugins/rapid7_insightvm/komand_rapid7_insightvm/triggers/new_scans/trigger.py
@@ -72,7 +72,6 @@ class NewScans(insightconnect_plugin_runtime.Trigger):
                     self.send({Output.SCAN: scan_details})
 
                     # Update cache
-                    cache_site_scans[site_id] = cache_site_scans.get(site_id, [])
                     if cache_site_scans.get(site_id) is None:
                         cache_site_scans[site_id] = []
                     cache_site_scans[site_id].append(scan.get("scan_id"))
