9f50a84d1f9b6dce365de40c449725d5af5e9487
conor-mcnally <conor_mcnally@rapid7.com>
2022-10-07 12:24:01 +0100

    Added unit test for markdown_to_txt

---
+++ plugins/markdown/unit_test/test_markdown_to_txt.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath('../'))
+from parameterized import parameterized
+from unittest import TestCase
+from komand_markdown.actions.markdown_to_txt import MarkdownToTxt
+import logging
+
+
+class TestMarkdownToTxt(TestCase):
+
+    # @parameterized.expand([
+    #     ({"markdown": "IyBSYXBpZDcgSW5zaWdodENvbm5lY3Q="}),
+    #     ({"markdown_string": "# Rapid7 InsightConnect"})
+    # ])
+    def test_markdown_to_txt_valid_markdown(self):
+        action = MarkdownToTxt()
+        input_params = {"markdown": "IyBSYXBpZDcgSW5zaWdodENvbm5lY3Q="}
+        results = action.run(input_params)
+
+        log = logging.getLogger("Test")
+        action.logger = log
+        expected = {'txt_string': 'Rapid7 InsightConnect\n', 'txt': 'UmFwaWQ3IEluc2lnaHRDb25uZWN0Cg=='}
+        self.assertEqual(results, expected)
+
+    def test_markdown_to_txt_valid_markdown_string(self):
+        action = MarkdownToTxt()
+        input_params = {"markdown_string": "# Rapid7 InsightConnect"}
+        results = action.run(input_params)
+
+        log = logging.getLogger("Test")
+        action.logger = log
+        expected = {'txt_string': 'Rapid7 InsightConnect\n', 'txt': 'UmFwaWQ3IEluc2lnaHRDb25uZWN0Cg=='}
+        self.assertEqual(results, expected)
