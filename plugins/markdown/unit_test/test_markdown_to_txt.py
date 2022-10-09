26366f53785e33a79ce700553f09574be4076591
conor-mcnally <conor_mcnally@rapid7.com>
2022-10-09 22:56:54 +0100

    Reformatted to_txt with parameterized

--- plugins/markdown/unit_test/test_markdown_to_txt.py
+++ plugins/markdown/unit_test/test_markdown_to_txt.py
@@ -5,43 +5,36 @@ sys.path.append(os.path.abspath("../"))
 from parameterized import parameterized
 from unittest import TestCase
 from komand_markdown.actions.markdown_to_txt import MarkdownToTxt
-import logging
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 
 class TestMarkdownToTxt(TestCase):
+    expected_result = {"txt_string": "Rapid7 InsightConnect\n", "txt": "UmFwaWQ3IEluc2lnaHRDb25uZWN0Cg=="}
+    expected_error = "Input Error"
 
-    # @parameterized.expand([
-    #     ({"markdown": "IyBSYXBpZDcgSW5zaWdodENvbm5lY3Q="}),
-    #     ({"markdown_string": "# Rapid7 InsightConnect"})
-    # ])
-    def test_markdown_to_txt_valid_markdown(self):
-        action = MarkdownToTxt()
-        input_params = {"markdown": "IyBSYXBpZDcgSW5zaWdodENvbm5lY3Q="}
-        results = action.run(input_params)
+    def setUp(self) -> None:
+        self.action = MarkdownToTxt()
 
-        log = logging.getLogger("Test")
-        action.logger = log
-        expected = {"txt_string": "Rapid7 InsightConnect\n", "txt": "UmFwaWQ3IEluc2lnaHRDb25uZWN0Cg=="}
+    @parameterized.expand(
+        [
+            ({"markdown": "IyBSYXBpZDcgSW5zaWdodENvbm5lY3Q="}, expected_result),
+            ({"markdown_string": "# Rapid7 InsightConnect"}, expected_result),
+        ]
+    )
+    def test_markdown_to_txt_valid_markdown(self, input_params, expected):
+        results = self.action.run(input_params)
         self.assertEqual(results, expected)
 
-    def test_markdown_to_txt_valid_markdown_string(self):
-        action = MarkdownToTxt()
-        input_params = {"markdown_string": "# Rapid7 InsightConnect"}
-        results = action.run(input_params)
-
-        log = logging.getLogger("Test")
-        action.logger = log
-        expected = {"txt_string": "Rapid7 InsightConnect\n", "txt": "UmFwaWQ3IEluc2lnaHRDb25uZWN0Cg=="}
-        self.assertEqual(results, expected)
-
-    def test_markdown_to_txt_invalid_both(self):
-        action = MarkdownToTxt()
-        input_params = {"markdown": "IyBSYXBpZDcgSW5zaWdodENvbm5lY3Q=", "markdown_string": "# Rapid7 InsightConnect"}
-        results = action.run(input_params)
-
-        log = logging.getLogger("Test")
-        action.logger = log
-
-        expected = "Input Error Only one of Markdown or Markdown String can be defined"
-
-        self.assertRaises()
+    @parameterized.expand(
+        [
+            (
+                {"markdown": "IyBSYXBpZDcgSW5zaWdodENvbm5lY3Q=", "markdown_string": "# Rapid7 InsightConnect"},
+                expected_error,
+            ),
+            ({"markdown": "", "markdown_string": ""}, expected_error),
+        ]
+    )
+    def test_markdown_to_txt_invalid_both(self, input_params, exception):
+        with self.assertRaises(PluginException) as context:
+            self.action.run(input_params)
+        self.assertEqual(context.exception.cause, exception)
