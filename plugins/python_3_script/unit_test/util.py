137f1a98e68c98db0a82ab82dbbeccdde6dc3b55
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2022-10-21 15:52:35 +0200

    Python3 Script - 11285 - Fix issue with installing python modules | Fix issue with indentation (#1467)

--- plugins/python_3_script/unit_test/util.py
+++ plugins/python_3_script/unit_test/util.py
@@ -3,39 +3,41 @@ import logging
 import os
 
 from icon_python_3_script.connection import Connection
-from icon_python_3_script.actions.run.schema import Input
+from insightconnect_plugin_runtime.action import Action
+from typing import Union, Dict, Any
 
 
 class Util:
     @staticmethod
-    def default_connector(action, connect_params: object = None):
+    def default_connector(action: Action, connect_params: object = None) -> Action:
         default_connection = Connection()
         default_connection.logger = logging.getLogger("connection logger")
         if connect_params:
             params = connect_params
         else:
             params = {"modules": [], "timeout": 60}
-
         default_connection.connect(params)
         action.connection = default_connection
         action.logger = logging.getLogger("action logger")
         return action
 
     @staticmethod
-    def read_file_to_string(filename, directory_path=os.path.dirname(os.path.realpath(__file__))):
+    def read_file_to_string(filename: str, directory_path: str = os.path.dirname(os.path.realpath(__file__))) -> str:
         with open(os.path.join(directory_path, filename)) as my_file:
             return my_file.read()
 
     @staticmethod
-    def read_file_to_dict(filename, directory_path=os.path.dirname(os.path.realpath(__file__))):
+    def read_file_to_dict(
+        filename: str, directory_path: str = os.path.dirname(os.path.realpath(__file__))
+    ) -> Dict[str, Any]:
         return json.loads(Util.read_file_to_string(filename, directory_path))
 
     @staticmethod
-    def mock_exec_python_function(*args, **kwargs):
+    def mock_exec_python_function(*args, **kwargs) -> Union[Dict[str, Any], None]:
         if kwargs.get("params", {}).get("input") == {"some_input": "bad"}:
             return None
-        if "username" in kwargs.get("func", ""):
+        if "username" in kwargs.get("function_", ""):
             return Util.read_file_to_dict(f"payloads/run_with_credentials.json.exp")
-        if "username" not in kwargs.get("func", ""):
+        if "username" not in kwargs.get("function_", ""):
             return Util.read_file_to_dict(f"payloads/run_no_credentials.json.exp")
         raise NotImplemented
