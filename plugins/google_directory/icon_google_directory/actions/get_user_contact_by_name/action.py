faefaa4937dc57de76435b7bf27492b3808639ef
Hephzi <hephzibah_wilson@rapid7.com>
2022-11-23 15:24:10 +0000

    Got nicer error, still not right one though

--- plugins/google_directory/icon_google_directory/actions/get_user_contact_by_name/action.py
+++ plugins/google_directory/icon_google_directory/actions/get_user_contact_by_name/action.py
@@ -17,47 +17,43 @@ class GetUserContactByName(insightconnect_plugin_runtime.Action):
             output=GetUserContactByNameOutput(),
         )
 
-    # def run(self, params={}):
-    #     full_name = params.get(Input.FULL_NAME)
-    #     try:
-    #         response = (
-    #             self.connection.service.users().list(customer="my_customer", query=f"name:'{full_name}'").execute()
-    #         )
-    #         return {Output.CONTACT: return_contact_information_name(response)}
-    #     except Exception as exception:
-    #         error = handle_service_error(exception)
-    #         raise PluginException(cause=error.get("cause"), assistance=error.get("assistance"),
-    #                               data=error.get("data"))
-
     def run(self, params={}):
         full_name = params.get(Input.FULL_NAME)
         try:
+
             # Generate response with full_name input
+            print('1')
             response = (
                 self.connection.service.users().list(customer="my_customer", query=f"name:'{full_name}'").execute()
             )
-            print(f"RESPONSE: {response}")
-            # If name exists in the response, then run return_contact_information_name on it
+            # Print Response so we can see what's going on
+            #   print(f"RESPONSE: {response}")
+            # If 'users' exists in the response, then run return_contact_information_name on it
+            #   if 'users' in response.keys():
+            print('2')
             if 'users' in response.keys():
+                print('3')
                 return {Output.CONTACT: return_contact_information_name(response)}
-
             # Otherwise, if "name" is NOT found, then throw the user not found messages
+            # else:
+            #     raise PluginException(cause=Message.USER_CONTACT_CAUSE_USER_NOT_FOUND,
+            #                           assistance=Message.USER_CONTACT_ASSISTANCE_USER_NOT_FOUND)
             else:
-                # raise PluginException(cause=Message.USER_CONTACT_CAUSE_USER_NOT_FOUND,
-                #                       assistance=Message.USER_CONTACT_ASSISTANCE_USER_NOT_FOUND)
-                raise Exception()
+                print(response)
+                raise PluginException(data=response)
 
         # Handles all errors
         except Exception as error:
+            print('5')
+            print("THIS IS THE SECOND EXCEPT EXCEPTION AS ERROR")
             print(error)
             print(type(error))
             print(len(str(error)))
             # If error is 400
-            if '400' in str(error):
-                raise PluginException(cause=Message.USER_CONTACT_CAUSE_USER_NOT_FOUND,
-                                      assistance=Message.USER_CONTACT_ASSISTANCE_USER_NOT_FOUND)
+            # if '400' in str(error):
+            raise PluginException(cause=Message.USER_CONTACT_CAUSE_USER_NOT_FOUND,
+                                  assistance=Message.USER_CONTACT_ASSISTANCE_USER_NOT_FOUND, data=error)
 
         # except Exception as exception:
         #     logging.info("YO HEPHZI, THIS IS THE 'except Exception as exception' SECTION")
-        #     error = handle_service_error(exception)
-        #     raise PluginException(cause=error.get("cause"), assistance=error.get("assistance"), data=exception)
+        #     print(exception)
