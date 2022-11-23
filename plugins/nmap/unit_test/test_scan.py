d03165f2df98abe1b76691c809df3f303f539ab5
TKuzas-R7 <tkuzas@rapid7.com>
2022-11-23 14:48:29 +0000

    refactored test_scan.py for lint, removed redundant requirement

--- plugins/nmap/unit_test/test_scan.py
+++ plugins/nmap/unit_test/test_scan.py
@@ -5,11 +5,9 @@ sys.path.append(os.path.abspath("../"))
 import logging
 from typing import Dict, List
 from unittest import TestCase
-
 from komand_nmap.actions.scan import Scan
 from komand_nmap.connection.connection import Connection
 from parameterized import parameterized
-
 from unit_test.mock import mocked_port_scanner
 
 
@@ -22,11 +20,10 @@ class TestScan(TestCase):
         self.action = Scan()
         self.action.connection = self.connection
         self.action.logger = logging.getLogger("action logger")
-
         self.payload = {"arguments": "-A", "hosts": "socutil01", "ports": "", "sudo": True}
 
-    @parameterized.expand([(["test"], {"test": "192.168.0.1"}, {'result': ['192.168.0.1']})])
+    @parameterized.expand([(["test"], {"test": "192.168.0.1"}, {"result": ["192.168.0.1"]})])
     def test_scan(self, hosts: List[str], scanned_hosts: Dict[str, str], result: str) -> None:
-            mocked_port_scanner(hosts, scanned_hosts)
-            response = self.action.run(self.payload)
-            assert response == result
+        mocked_port_scanner(hosts, scanned_hosts)
+        response = self.action.run(self.payload)
+        assert response == result
