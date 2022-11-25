0189e5411228e4992576894ed0521049d3c76602
Hephzi <hephzibah_wilson@rapid7.com>
2022-11-25 10:55:30 +0000

    Tidied up

--- plugins/google_directory/icon_google_directory/actions/get_user_contact_by_name/action.py
+++ plugins/google_directory/icon_google_directory/actions/get_user_contact_by_name/action.py
@@ -1,5 +1,3 @@
-import logging
-
 import insightconnect_plugin_runtime
 from insightconnect_plugin_runtime.exceptions import PluginException
 from .schema import GetUserContactByNameInput, GetUserContactByNameOutput, Input, Output, Component
@@ -20,42 +18,16 @@ class GetUserContactByName(insightconnect_plugin_runtime.Action):
     def run(self, params={}):
         full_name = params.get(Input.FULL_NAME)
         try:
-
             # Generate response with full_name input
-            print('1')
             response = (
                 self.connection.service.users().list(customer="my_customer", query=f"name:'{full_name}'").execute()
             )
-            # Print Response so we can see what's going on
-            #   print(f"RESPONSE: {response}")
-            # If 'users' exists in the response, then run return_contact_information_name on it
-            #   if 'users' in response.keys():
-            print('2')
             if 'users' in response.keys():
-                print('3')
                 return {Output.CONTACT: return_contact_information_name(response)}
-            # Otherwise, if "name" is NOT found, then throw the user not found messages
-            # else:
-            #     raise PluginException(cause=Message.USER_CONTACT_CAUSE_USER_NOT_FOUND,
-            #                           assistance=Message.USER_CONTACT_ASSISTANCE_USER_NOT_FOUND)
             else:
-                print('2.1')
-                print(response)
                 raise Exception('User not found')
 
         # Handles all errors
         except Exception as error:
-            print('5')
-            print("THIS IS THE SECOND EXCEPT EXCEPTION AS ERROR")
-            print(f"ERROR: {error}")
-            print(f"TYPE OF ERROR: {type(error)}")
-            print(f"LEN STR ERROR: {len(str(error))}")
-            # If error is 400
-            # if '400' in str(error):
-            print(f"Plugin exception hit, here is the error {error}")
-            raise PluginException(cause=Message.USER_CONTACT_CAUSE_USER_NOT_FOUND,
-                                  assistance=Message.USER_CONTACT_ASSISTANCE_USER_NOT_FOUND, data=error)
-
-        # except Exception as exception:
-        #     logging.info("YO HEPHZI, THIS IS THE 'except Exception as exception' SECTION")
-        #     print(exception)
+            raise PluginException(cause=Message.USER_CONTACT_CAUSE,
+                                  assistance=Message.USER_CONTACT_ASSISTANCE, data=error)
--- plugins/google_directory/icon_google_directory/util/tools.py
+++ plugins/google_directory/icon_google_directory/util/tools.py
@@ -1,12 +1,6 @@
-import logging
-
-
 class Message:
-    USER_CONTACT_CAUSE_USER_NOT_FOUND = "Contact information was not found in server's response."
-    USER_CONTACT_ASSISTANCE_USER_NOT_FOUND = "Please check and verify if the user exists."
-    USER_CONTACT_CAUSE = "Something went wrong"
-    USER_CONTACT_ASSISTANCE = "Please check the logs for further information and contact the Insight Connect team if " \
-                              "the error persists "
+    USER_CONTACT_CAUSE = "Contact information was not found in server's response."
+    USER_CONTACT_ASSISTANCE = "Please check and verify if the user exists."
 
 
 def return_contact_information(input_dict: dict) -> dict:
@@ -28,20 +22,6 @@ def return_contact_information(input_dict: dict) -> dict:
         if "postalCode" in address:
             address["postal_code"] = address.pop("postalCode")
     return {"addresses": addresses_list, "phone_numbers": phones_list, "email_addresses": emails_list}
-
-    # def return_contact_information_name(users: list) -> list:
-    #     """return_contact_information. Function allows to map data that contains contact informations retrieved from
-    #     Google API users().list()
-    #
-    #     :param input_dict: Input dict as a response got from Google API
-    #     :type input_dict: dict
-    #
-    #     :returns: Mapped dictionary that contains keys such as 'addresses', 'phones', and 'emails'
-    #     :rtype: dict
-    #     """
-    # if input_dict.get("users") is None:
-    #     return {"addresses": [], "phone_numbers": [], "email_addresses": []}
-    # return list(map(return_contact_information, users))
 
 
 def return_contact_information_name(input_dict: dict) -> dict:
@@ -67,19 +47,3 @@ def return_contact_information_name(input_dict: dict) -> dict:
             if "postalCode" in address:
                 address["postal_code"] = address.pop("postalCode")
         return {"name": name, "addresses": addresses_list, "phone_numbers": phones_list, "email_addresses": emails_list}
-
-#
-# def handle_service_error(error) -> dict:
-#     print(type(error))
-#     message = {}
-#     if error:
-#         logging.info("HI HEPHZI, THIS IS THE BIT WHERE THE THINGY IS MADE" + str(error))
-#         message = {"cause": "User was not found",
-#                    "assistance": "A user was not found with the passed search parameters",
-#                    "data": error}
-#     # else:
-#     #     message = {"cause": "Something went wrong",
-#     #                "assistance": "Please check the logs for further information and contact the Insight Connect team "
-#     #                              "if the error persists",
-#     #                "data": error}
-#     return message
