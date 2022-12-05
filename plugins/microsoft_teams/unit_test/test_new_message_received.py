0f368a1b0725350331c9becb2f5f3db968f809e9
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2022-12-05 16:18:28 +0100

    Microsoft Teams - 57 - New Message Received: Fixed issue where font-size value appeared in the urls, and domains output fields (#1511)

--- plugins/microsoft_teams/unit_test/test_new_message_received.py
+++ plugins/microsoft_teams/unit_test/test_new_message_received.py
@@ -1,15 +1,27 @@
-from unittest import TestCase, mock
 import json
 import logging
-import requests
-import sys
 import os
+import sys
+from unittest import TestCase, mock
+
+import requests
 
 sys.path.append(os.path.abspath("../"))
 
 from icon_microsoft_teams.triggers.new_message_received import NewMessageReceived
 from insightconnect_plugin_runtime.exceptions import PluginException
+from parameterized import parameterized
 
+GET_INDICATORS_RESPONSE = {
+    "domains": [],
+    "urls": [],
+    "email_addresses": [],
+    "hashes": {"md5_hashes": [], "sha1_hashes": [], "sha256_hashes": []},
+    "ip_addresses": {"ipv4_addresses": [], "ipv6_addresses": []},
+    "mac_addresses": [],
+    "cves": [],
+    "uuids": [],
+}
 
 # Get a real payload from file
 def read_file_to_string(filename):
@@ -46,51 +58,60 @@ def mocked_requests_get(*args, **kwargs):
 
 
 class TestNewMessageReceived(TestCase):
+    def setUp(self) -> None:
+        self.log = logging.getLogger("Test")
+        self.nmr = NewMessageReceived()
+        self.nmr.logger = self.log
+        self.nmr.connection = MockConnection()
+
     def test_sort_messages_from_request(self):
-        with open(os.path.join(os.path.dirname(__file__), "./payloads/get_messages.json")) as f:
-            text = f.read()
+        with open(os.path.join(os.path.dirname(__file__), "./payloads/get_messages.json")) as file:
+            text = file.read()
             json_payload = json.loads(text)
-
-        log = logging.getLogger("Test")
-
-        nmr = NewMessageReceived()
-        nmr.logger = log
-
-        result = nmr.sort_messages_from_request(json_payload)
-
-        newest = result[0]
-        oldest = result[-1]
-
-        self.assertEqual(newest.get("body").get("content"), "This should be first")
-        self.assertEqual(oldest.get("body").get("content"), "This is very old")
+        result = self.nmr.sort_messages_from_request(json_payload)
+        self.assertEqual(result[0].get("body").get("content"), "This should be first")
+        self.assertEqual(result[1].get("body").get("content"), "This is very old")
 
     def test_compile_message_content(self):
-        nmr = NewMessageReceived()
-        regex = nmr.compile_message_content(".")
+        regex = self.nmr.compile_message_content(".")
         self.assertTrue(regex.search("stuff"))
-
         with self.assertRaises(PluginException):
-            nmr.compile_message_content("[")
+            self.nmr.compile_message_content("[")
 
     @mock.patch("requests.get", side_effect=mocked_requests_get)
     def test_get_sorted_messages(self, mockGet):
-        log = logging.getLogger("Test")
-        nmr = NewMessageReceived()
-        nmr.logger = log
-        nmr.connection = MockConnection()
-
-        messages = nmr.get_sorted_messages("http://somefakeendpoint.com")
-
+        messages = self.nmr.get_sorted_messages("http://somefakeendpoint.com")
         self.assertIsNotNone(messages)
         self.assertEqual(messages[0].get("body").get("content"), "This should be first")
-        self.assertEqual(messages[-1].get("body").get("content"), "This is very old")
+        self.assertEqual(messages[1].get("body").get("content"), "This is very old")
+
+    @parameterized.expand(
+        [
+            (0, GET_INDICATORS_RESPONSE),
+            (1, GET_INDICATORS_RESPONSE),
+            (
+                2,
+                dict(
+                    GET_INDICATORS_RESPONSE,
+                    **{
+                        "domains": ["example.com/s/?domain=test", "example.com?domain=test", "example.com"],
+                        "urls": [
+                            "http://example.com/s/?domain=test",
+                            "https://example.com?domain=test",
+                            "https://example.com",
+                        ],
+                    },
+                ),
+            ),
+        ]
+    )
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_indicators(self, message_number, expected_result, mockGet):
+        messages = self.nmr.get_sorted_messages("http://somefakeendpoint.com")
+        indicators = [self.nmr.get_indicators(message.get("body", {}).get("content")) for message in messages]
+        self.assertEqual(indicators[message_number], expected_result)
 
     def test_setup_endpoint(self):
-        log = logging.getLogger("Test")
-        nmr = NewMessageReceived()
-        nmr.logger = log
-        nmr.connection = MockConnection()
-
         # This was a mess to figure out...because I'm importing with from I have to refer to the class it's being
         #  called from and not the actual function that's being imported
         with mock.patch(
@@ -101,6 +122,5 @@ class TestNewMessageReceived(TestCase):
                 "icon_microsoft_teams.triggers.new_message_received.trigger.get_channels_from_microsoft",
                 return_value=[{"id": "channel"}],
             ):
-                endpoint = nmr.setup_endpoint("channel", "team")
-
+                endpoint = self.nmr.setup_endpoint("channel", "team")
         self.assertEqual("https://graph.microsoft.com/beta/teams/team/channels/channel/messages", endpoint)
