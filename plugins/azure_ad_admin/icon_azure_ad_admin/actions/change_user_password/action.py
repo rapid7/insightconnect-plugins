968ab9f95218f695b7dac8c1f2b6915986ba107c
Aine <aine_holland@rapid7.com>
2022-12-06 11:06:54 -0500

    azure ad admin inclusion fix change back

--- plugins/azure_ad_admin/icon_azure_ad_admin/actions/change_user_password/action.py
+++ plugins/azure_ad_admin/icon_azure_ad_admin/actions/change_user_password/action.py
@@ -3,7 +3,7 @@ import requests
 from insightconnect_plugin_runtime.exceptions import PluginException
 
 from .schema import ChangeUserPasswordInput, ChangeUserPasswordOutput, Input, Output, Component
-from ...util.constants import Endpoint
+from ...util.constans import Endpoint
 
 
 class ChangeUserPassword(insightconnect_plugin_runtime.Action):
