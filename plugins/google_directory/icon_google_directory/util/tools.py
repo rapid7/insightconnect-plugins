be6e42c5b78736f20616f98a06766fba7d073efc
hpwilson-r7 <107193020+hpwilson-r7@users.noreply.github.com>
2022-12-06 16:09:33 +0000

    [PLGN-18][SOAR-9356] - Add ability to search user by Full Name - Google Directory (#1469)

--- plugins/google_directory/icon_google_directory/util/tools.py
+++ plugins/google_directory/icon_google_directory/util/tools.py
@@ -3,8 +3,9 @@ class Message:
     USER_CONTACT_ASSISTANCE = "Please check and verify if the user exists."
 
 
-def return_contact_informations(input_dict: dict) -> dict:
-    """return_contact_informations. Function allows to map data that contains contact informations retrieved from Google API users().get()
+def return_contact_information(input_dict: dict) -> dict:
+    """return_contact_information. Function allows to map data that contains contact informations retrieved from
+    Google API users().get()
 
     :param input_dict: Input dict as a response got from Google API
     :type input_dict: dict
@@ -12,7 +13,6 @@ def return_contact_informations(input_dict: dict) -> dict:
     :returns: Mapped dictionary that contains keys such as 'addresses', 'phones', and 'emails'
     :rtype: dict
     """
-
     addresses_list = input_dict.get("addresses", [])
     phones_list = [element.get("value") for element in input_dict.get("phones", [])]
     emails_list = [element.get("address") for element in input_dict.get("emails", [])]
@@ -22,3 +22,28 @@ def return_contact_informations(input_dict: dict) -> dict:
         if "postalCode" in address:
             address["postal_code"] = address.pop("postalCode")
     return {"addresses": addresses_list, "phone_numbers": phones_list, "email_addresses": emails_list}
+
+
+def return_contact_information_name(input_dict: dict) -> dict:
+    """return_contact_informations. Function allows to map data that contains contact information retrieved from
+    Google API users().list()
+
+    :param input_dict: Input dict as a response got from Google API
+    :type input_dict: dict
+
+    :returns: Mapped dictionary that contains keys such as 'addresses', 'phones', and 'emails'
+    :rtype: dict
+    """
+    if input_dict.get("users") is None:
+        return {"name": None}
+    for user in input_dict.get("users"):
+        name = user.get("name").get("fullName")
+        addresses_list = user.get("addresses", [])
+        phones_list = [element.get("value") for element in user.get("phones", [])]
+        emails_list = [element.get("address") for element in user.get("emails", [])]
+        for address in addresses_list:
+            if "streetAddress" in address:
+                address["street"] = address.pop("streetAddress")
+            if "postalCode" in address:
+                address["postal_code"] = address.pop("postalCode")
+        return {"name": name, "addresses": addresses_list, "phone_numbers": phones_list, "email_addresses": emails_list}
