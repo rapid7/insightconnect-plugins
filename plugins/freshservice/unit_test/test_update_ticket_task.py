53d0dca80524f90528827a0363ba5666e04e5c1d
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2022-12-08 15:49:11 +0100

    [MC-1329] FreshService plugin (#1512)
    * [MC-1329] Add FreshService plugin
    
    * [MC-1329] Fix prospector
    
    * [MC-1329] Add icon.png and extension.png files

---
+++ plugins/freshservice/unit_test/test_update_ticket_task.py
@@ -0,0 +1,80 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from icon_freshservice.actions.update_ticket_task import UpdateTicketTask
+from icon_freshservice.actions.update_ticket_task.schema import Input
+from unit_test.util import Util
+from unittest.mock import patch
+from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+@patch("requests.request", side_effect=Util.mocked_requests)
+class TestUpdateTicketTask(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(UpdateTicketTask())
+
+    @parameterized.expand(Util.load_parameters("update_ticket_task").get("parameters"))
+    def test_update_ticket_task(
+        self,
+        mock_request,
+        name,
+        ticked_id,
+        task_id,
+        title,
+        description,
+        status,
+        due_date,
+        notify_before,
+        group_id,
+        expected,
+    ):
+        actual = self.action.run(
+            {
+                Input.TICKETID: ticked_id,
+                Input.TASKID: task_id,
+                Input.TITLE: title,
+                Input.DESCRIPTION: description,
+                Input.STATUS: status,
+                Input.DUEDATE: due_date,
+                Input.NOTIFYBEFORE: notify_before,
+                Input.GROUPID: group_id,
+            }
+        )
+        self.assertEqual(actual, expected)
+
+    @parameterized.expand(Util.load_parameters("update_ticket_task_bad").get("parameters"))
+    def test_update_ticket_task_bad(
+        self,
+        mock_request,
+        name,
+        ticked_id,
+        task_id,
+        title,
+        description,
+        status,
+        due_date,
+        notify_before,
+        group_id,
+        cause,
+        assistance,
+    ):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(
+                {
+                    Input.TICKETID: ticked_id,
+                    Input.TASKID: task_id,
+                    Input.TITLE: title,
+                    Input.DESCRIPTION: description,
+                    Input.STATUS: status,
+                    Input.DUEDATE: due_date,
+                    Input.NOTIFYBEFORE: notify_before,
+                    Input.GROUPID: group_id,
+                }
+            )
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
