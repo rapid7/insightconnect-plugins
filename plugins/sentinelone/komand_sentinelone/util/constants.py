2bfcdf1668a15c290a185c1f16dc7199654ed9f4
Dympna Laverty <dympna_laverty@rapid7.com>
2022-12-09 10:16:22 +0000

    [PLGN-71] Add Service role for SentinelOne. Remove basic authentication

--- plugins/sentinelone/komand_sentinelone/util/constants.py
+++ plugins/sentinelone/komand_sentinelone/util/constants.py
@@ -1,6 +1,4 @@
-BASIC_AUTH = "Basic Auth"
-API_TOKEN_AUTH = "API Token Auth"  # nosec bandit B105
-USERNAME_FIELD = "username"
-PASSWORD_FIELD = "password"  # nosec bandit B105
 DATA_FIELD = "data"
 API_TOKEN_FIELD = "apiToken"  # nosec bandit B105
+USER_ROLE_HEADER_TOKEN_FIELD = "Token"  # nosec bandit B105
+SERVICE_ROLE_HEADER_TOKEN_FIELD = "ApiToken"  # nosec bandit B105
