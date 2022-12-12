cf900d40f6fc88073dd1155d0d4e6df74b332f70
conor-mcnally <conor_mcnally@rapid7.com>
2022-12-12 11:48:48 +0000

    refactored

--- plugins/urlscan/komand_urlscan/actions/search/action.py
+++ plugins/urlscan/komand_urlscan/actions/search/action.py
@@ -22,11 +22,11 @@ def format_query(query: str, input_type: str) -> str:
     :rtype: str
     """
 
-    # If input_type is Custom, handle prefixes
+    # If input_type is Custom
     if input_type == "Custom":
         search_query = query
 
-    # If input_type is URL, determine if the query is a valid URL
+    # If input_type is URL, determine if the query is a valid URL,
     # then append page.url: to the query
     elif input_type == "URL":
         if not validators.url(query):
@@ -36,7 +36,7 @@ def format_query(query: str, input_type: str) -> str:
             )
         search_query = f'page.url: "{query}"'
 
-    # Handle other types, check if it is a valid URL.
+    # If input_type is domain, check if it is a valid domain,
     # then append page.domain: to the query
     else:
         if not validators.domain(query):
@@ -49,6 +49,29 @@ def format_query(query: str, input_type: str) -> str:
     return search_query
 
 
+def get_response(url: str, headers: dict) -> requests.models.Response:
+    """
+    This method runs a GET request and returns a response.
+    It also handles all exceptions, and specifically handles 400s
+    to prevent the plugin from returning a successful but empty
+    response.
+    :param url: Input URL for the request
+    :param headers:
+    :return response: The full HTTP response
+    """
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        status_code = error.response.status_code
+        if status_code == 400:
+            raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.text)
+    except Exception as error:
+        raise PluginException(cause="Something went wrong during the request.", assistance=error)
+
+    return response
+
+
 class Search(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
@@ -58,15 +81,14 @@ class Search(insightconnect_plugin_runtime.Action):
             output=SearchOutput(),
         )
 
-    def get_results(self, search_query: str, sort: str):
-        """
-        This function is used to make the requests needed to return the results
-        from the urlscan API.
+    def run(self, params={}):
 
-        :param search_query: Query to be search / appended to URL
-        :param sort: The users inputted sort method
-        :return:
-        """
+        input_type = params.get(Input.INPUT_TYPE, "Custom")
+        query = params.get(Input.Q)
+        sort = params.get(Input.SORT, "_score")
+
+        search_query = format_query(query=query, input_type=input_type)
+
         search_after = None
         results = []
         size = 10000
@@ -82,16 +104,10 @@ class Search(insightconnect_plugin_runtime.Action):
 
             url = f'{self.connection.server}/search/?{"&".join(query_params)}'
             self.logger.info(url)
+            headers = self.connection.headers
+            search_query = format_query(query=query, input_type=input_type)
 
-            try:
-                response = requests.get(url, headers=self.connection.headers)
-                response.raise_for_status()
-            except requests.exceptions.HTTPError as error:
-                status_code = error.response.status_code
-                if status_code == 400:
-                    raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.text)
-            except Exception as error:
-                raise PluginException(cause="Something went wrong during the request.", assistance=error)
+            response = get_response(url=url, headers=headers)
 
             try:
                 responses = response.json()
@@ -115,16 +131,5 @@ class Search(insightconnect_plugin_runtime.Action):
                     cause="Received an unexpected response from the Urlscan API. ",
                     assistance=f"(non-JSON or no response was received). Response was: {response.text}",
                 )
-
-            return results, has_more
-
-    def run(self, params={}):
-        input_type = params.get(Input.INPUT_TYPE, "Custom")
-        query = params.get(Input.Q)
-        sort = params.get(Input.SORT, "_score")
-
-        search_query = format_query(query=query, input_type=input_type)
-
-        results, has_more = self.get_results(search_query=search_query, sort=sort)
 
         return {Output.RESULTS: results, Output.TOTAL: len(results), Output.HAS_MORE: has_more}
