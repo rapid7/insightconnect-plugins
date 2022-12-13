a17035198a1d37e426f4abac42e80f3a639be4a8
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2022-12-13 11:33:18 +0000

    [PLGN-67] Threat Command | New Action 'Get IOCs By Filter' (#1527)
    * Add action | Add unit tests | Update help.md and plugin.spec.yaml examples and version log | Fix for validation
    
    * Add action
    
    * Reformat
    
    * Fix unused f string
    
    * Use Parameterized
    
    * Reformat
    
    * Change example website

---
+++ plugins/rapid7_intsights/icon_rapid7_intsights/actions/get_iocs_by_filter/action.py
@@ -0,0 +1,35 @@
+import insightconnect_plugin_runtime
+from .schema import GetIocsByFilterInput, GetIocsByFilterOutput, Input, Output, Component
+
+# Custom imports below
+from icon_rapid7_intsights.util.api import IOCParams
+from insightconnect_plugin_runtime.helper import clean
+
+
+class GetIocsByFilter(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="get_iocs_by_filter",
+            description=Component.DESCRIPTION,
+            input=GetIocsByFilterInput(),
+            output=GetIocsByFilterOutput(),
+        )
+
+    def run(self, params={}):
+        ioc_params = IOCParams(
+            last_updated_from=params.get(Input.LAST_UPDATED_FROM),
+            last_updated_to=params.get(Input.LAST_UPDATED_TO),
+            last_seen_from=params.get(Input.LAST_SEEN_FROM),
+            last_seen_to=params.get(Input.LAST_SEEN_TO),
+            first_seen_from=params.get(Input.FIRST_SEEN_FROM),
+            first_seen_to=params.get(Input.FIRST_SEEN_TO),
+            status=params.get(Input.STATUS),
+            type=params.get(Input.TYPE),
+            severity=params.get(Input.SEVERITY),
+            source_ids=params.get(Input.SOURCE_IDS),
+            kill_chain_phases=params.get(Input.KILL_CHAIN_PHASES),
+            limit=params.get(Input.LIMIT),
+            offset=params.get(Input.OFFSET),
+        )
+        response = self.connection.client.get_indicators_by_filter(ioc_params)
+        return clean({Output.CONTENT: response.get("content"), Output.NEXTOFFSET: response.get("nextOffset")})
---
+++ plugins/rapid7_intsights/unit_test/test_get_iocs_by_filter.py
@@ -0,0 +1,75 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from unittest.mock import patch
+from unit_test.util import Util
+from icon_rapid7_intsights.actions.get_iocs_by_filter import GetIocsByFilter
+from icon_rapid7_intsights.actions.get_iocs_by_filter.schema import Input
+from parameterized import parameterized
+
+
+class TestGetIocsByFilter(TestCase):
+    @classmethod
+    @patch("requests.request", side_effect=Util.mock_request)
+    def setUpClass(cls, mock_request) -> None:
+        cls.expected = {
+            "content": [
+                {
+                    "firstSeen": "2020-01-01T20:01:27.344Z",
+                    "geoLocation": "US",
+                    "lastSeen": "2020-01-30T16:18:51.148Z",
+                    "lastUpdateDate": "2020-02-21T23:00:51.268Z",
+                    "relatedCampaigns": ["SolarWinds"],
+                    "relatedMalware": ["doppeldridex", "dridex"],
+                    "relatedThreatActors": ["doppelspider"],
+                    "score": 100,
+                    "severity": "High",
+                    "sources": [{"ConfidenceLevel": 3, "Name": "AlienVault OTX"}],
+                    "tags": ["MyTag_1"],
+                    "type": "Domains",
+                    "value": "rapid7.com",
+                    "whitelisted": "false",
+                    "reportedFeeds": [{"ID": "SampleID", "ConfidenceLevel": 3, "Name": "AlienVault OTX"}],
+                }
+            ],
+            "nextOffset": "2022-11-18T16:59:01.626Z",
+        }
+        cls.action = Util.default_connector(GetIocsByFilter())
+
+    @parameterized.expand(
+        [
+            ["lower_limit", {Input.LAST_UPDATED_FROM: "2000-12-31T00:00:00Z", Input.LIMIT: 1}, None],
+            ["below_limit", {Input.LAST_UPDATED_FROM: "2000-12-31T00:00:00Z", Input.LIMIT: 0}, None],
+            ["above_limit", {Input.LAST_UPDATED_FROM: "2000-12-31T00:00:00Z", Input.LIMIT: 1001}, None],
+            ["single_input", {Input.LAST_UPDATED_FROM: "2000-12-31T00:00:00Z", Input.LIMIT: None}, None],
+            [
+                "all_inputs",
+                {
+                    Input.LAST_UPDATED_FROM: "2000-12-31T00:00:00Z",
+                    Input.LIMIT: 1000,
+                    Input.TYPE: ["Domains"],
+                    Input.LAST_UPDATED_TO: "2000-12-31T00:00:00Z",
+                    Input.LAST_SEEN_FROM: "2000-12-31T00:00:00Z",
+                    Input.LAST_SEEN_TO: "2000-12-31T00:00:00Z",
+                    Input.FIRST_SEEN_FROM: "2000-12-31T00:00:00Z",
+                    Input.FIRST_SEEN_TO: "2000-12-31T00:00:00Z",
+                    Input.STATUS: "Active",
+                    Input.SEVERITY: ["High", "Low"],
+                    Input.SOURCE_IDS: ["123450000012345000001233"],
+                    Input.KILL_CHAIN_PHASES: ["Exploitation", "Installation"],
+                    Input.OFFSET: "2022-11-18T16:59:01.626Z",
+                },
+                None,
+            ],
+            ["no_results", {Input.LAST_UPDATED_FROM: "2000-12-30T00:00:00Z", Input.LIMIT: None}, {"content": []}],
+        ]
+    )
+    @patch("requests.request", side_effect=Util.mock_request)
+    def test_get_indicator_by_filter_limit(self, test_name, input, expected, make_request):
+        if expected is None:
+            expected = self.expected
+        actual = self.action.run(input)
+        self.assertEqual(expected, actual)
