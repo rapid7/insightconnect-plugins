05d54bd018bd1c7e7b3089b7c30ff03c41994dc4
Aine <69211380+aholland-r7@users.noreply.github.com>
2022-12-19 09:55:01 -0500

    Plgn-60 InsightVM Cloud New Action Vuln Search (#1537)

--- plugins/rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/__init__.py
+++ plugins/rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/__init__.py
@@ -4,3 +4,4 @@ from .get_asset.action import GetAsset
 from .get_scan.action import GetScan
 from .start_scan.action import StartScan
 from .stop_scan.action import StopScan
+from .vuln_search.action import VulnSearch
