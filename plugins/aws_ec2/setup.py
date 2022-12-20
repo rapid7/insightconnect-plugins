175e348485b3819cd6cbabef2b4a374535e6aa6a
llaszuk <lukasz_laszuk@rapid7.com>
2022-12-20 13:43:35 +0100

    PLGN-62 add change log version

--- plugins/aws_ec2/setup.py
+++ plugins/aws_ec2/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="aws_ec2-rapid7-plugin",
-      version="1.2.0",
+      version="1.2.1",
       description="Amazon Elastic Compute Cloud (Amazon EC2) provides resizable computing capacity in the Amazon Web Services (AWS) cloud. This plugin allows you to manage your EC2 environment",
       author="rapid7",
       author_email="",
