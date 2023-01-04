a104a7c4a87cd48124b01c2cf29b6965b5d9be2b
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-01-04 10:06:37 +0000

    Added body as an array (#1544)
    * Added body as an array
    
    * Refactored actions
    
    * Get and Post done
    
    * Updated help.md
    
    * Regen
    
    * Update help.md
    
    * Change type to isinstance
    
    * Update helpmd
    
    * Regen
    
    * Update get in help.md
    
    * Hashes
    
    * Restored removed part
    
    * Updated help.md
    
    * fixed 2 lines in help.md

--- plugins/rest/komand_rest/actions/post/action.py
+++ plugins/rest/komand_rest/actions/post/action.py
@@ -3,7 +3,7 @@ from .schema import PostInput, PostOutput, Component, Input, Output
 
 # Custom imports below
 from komand_rest.util.util import Common
-from komand_rest.util.util import convert_body_for_urlencoded, check_headers_for_urlencoded
+from komand_rest.util.util import convert_body_for_urlencoded, check_headers_for_urlencoded, determine_body_type
 
 
 class Post(insightconnect_plugin_runtime.Action):
@@ -14,14 +14,16 @@ class Post(insightconnect_plugin_runtime.Action):
 
     def run(self, params={}):
         headers = params.get(Input.HEADERS, {})
+        body_non_array = params.get(Input.BODY, {})
+        body_as_an_array = params.get(Input.BODY_AS_AN_ARRAY, [])
 
-        body = params.get(Input.BODY, {})
+        data = determine_body_type(body_non_array, body_as_an_array)
 
-        if check_headers_for_urlencoded(headers):
-            body = convert_body_for_urlencoded(headers, body)
+        if isinstance(data, dict) and check_headers_for_urlencoded(headers):
+            body = convert_body_for_urlencoded(headers, data)
             kwargs = {"method": "POST", "path": params.get(Input.ROUTE), "data": body, "headers": headers}
         else:
-            kwargs = {"method": "POST", "path": params.get(Input.ROUTE), "json_data": body, "headers": headers}
+            kwargs = {"method": "POST", "path": params.get(Input.ROUTE), "json_data": data, "headers": headers}
 
         response = self.connection.api.call_api(**kwargs)
 
