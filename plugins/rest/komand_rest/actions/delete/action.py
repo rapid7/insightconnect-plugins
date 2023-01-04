524b363e3941f7ffc3adfb7e2a7eac1b66edcefa
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-01-04 10:06:37 +0000

    Added body as an array (#1544)
    * Added body as an array
    
    * Refactored actions
    
    * Get and Post done
    
    * Updated help.md
    
    * Regen
    
    * Update help.md
    
    * Change type to isinstance
    
    * Update helpmd
    
    * Regen
    
    * Update get in help.md
    
    * Hashes
    
    * Restored removed part
    
    * Updated help.md
    
    * fixed 2 lines in help.md

--- plugins/rest/komand_rest/actions/delete/action.py
+++ plugins/rest/komand_rest/actions/delete/action.py
@@ -3,6 +3,7 @@ from .schema import DeleteInput, DeleteOutput, Component, Input, Output
 
 # Custom imports below
 from komand_rest.util.util import Common
+from komand_rest.util.util import determine_body_type
 
 
 class Delete(insightconnect_plugin_runtime.Action):
@@ -15,10 +16,15 @@ class Delete(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
+        body_non_array = params.get(Input.BODY, {})
+        body_array = params.get(Input.BODY_AS_AN_ARRAY, [])
+
+        data = determine_body_type(body_non_array, body_array)
+
         response = self.connection.api.call_api(
             method="DELETE",
             path=params.get(Input.ROUTE),
-            data=params.get(Input.BODY, {}),
+            data=data,
             headers=params.get(Input.HEADERS, {}),
         )
 
