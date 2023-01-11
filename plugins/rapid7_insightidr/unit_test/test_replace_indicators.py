9022b1ee2b06ed168938643f42e807300a477be5
TKuzas-R7 <107193006+TKuzas-R7@users.noreply.github.com>
2023-01-11 11:18:04 +0000

    [PLGN-94] - Rapid7 insightidr 4.2.0 release (#1577)
    * [PLGN-94] - Rapid7 InsightIDR - Add Replace Indicators Action (#1536)
    
    * added action to spec file and regenerated, added endpoint
    
    * renamed endpoint, added action,schema
    
    * fixed checksum
    
    * updated spec file added action
    
    * added unit test
    
    * 100% coverage in unit tests
    
    * lint fixes
    
    * update help.md updated version number
    
    * updated spec and help.md for validator
    
    * updated help.md
    
    * removed fullstops in desc
    
    * updated to 4.2.0
    
    * removed junk in checksum
    
    * added example output in help.md and type hints in endpoints.py
    
    * more type hints in endpoints.py
    
    * added .get and fixed typo
    
    * removed print statements
    
    * removed logging
    
    * InsightIDR Replace Indicators quick fix (#1570)
    
    * fixing booboo
    
    * fail to invalid

---
+++ plugins/rapid7_insightidr/unit_test/test_replace_indicators.py
@@ -0,0 +1,70 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_rapid7_insightidr.actions.replace_indicators import ReplaceIndicators
+from komand_rapid7_insightidr.actions.replace_indicators.schema import Input
+from unit_test.util import Util
+from unittest.mock import patch
+from parameterized import parameterized
+from unit_test.mock import mock_post_request
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+@patch("requests.Session.post", side_effect=mock_post_request)
+class TestReplaceIndicators(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(ReplaceIndicators())
+
+    @parameterized.expand(
+        [
+            ["dcdba462-fcf5-4021-8707-98b14232239b", "rapid7.com", "", "", ""],
+            ["dcdba462-fcf5-4021-8707-98b14232239b", "", "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3", "", ""],
+            ["dcdba462-fcf5-4021-8707-98b14232239b", "", "", "192.0.2.0/24", ""],
+            ["dcdba462-fcf5-4021-8707-98b14232239b", "", "", "", "https://example.com"],
+        ]
+    )
+    def test_replace_indicators(self, mock_request, key, domain_names, hashes, ips, urls):
+        actual = self.action.run(
+            {Input.KEY: key, Input.DOMAIN_NAMES: domain_names, Input.HASHES: hashes, Input.IPS: ips, Input.URLS: urls}
+        )
+        expected = {
+            "threat": {
+                "name": "Threat Command Alert",
+                "note": "This threat will be automatically managed by an InsightConnect workflow",
+                "published": False,
+                "indicator_count": 1,
+            },
+            "rejected_indicators": [],
+        }
+        self.assertEqual(actual, expected)
+
+    @parameterized.expand(
+        [
+            [
+                "dcdba462",
+                "",
+                "",
+                "",
+                "",
+                "The response from InsightIDR was not in the correct format.",
+                "Contact support for help. See log for more details",
+            ],
+        ]
+    )
+    def test_replace_indicators_invalid(self, mock_request, key, domain_names, hashes, ips, urls, cause, assistance):
+        with self.assertRaises(PluginException) as error:
+            actual = self.action.run(
+                {
+                    Input.KEY: key,
+                    Input.DOMAIN_NAMES: domain_names,
+                    Input.HASHES: hashes,
+                    Input.IPS: ips,
+                    Input.URLS: urls,
+                }
+            )
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
