5a7f0c9b9453809d6991afd5f7b8b3c6d726afc6
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-01-11 10:28:21 +0100

    PLGN-106 aws ec2 add region (#1563)

--- plugins/aws_ec2/icon_aws_ec2/connection/connection.py
+++ plugins/aws_ec2/icon_aws_ec2/connection/connection.py
@@ -17,14 +17,21 @@ class Connection(insightconnect_plugin_runtime.Connection):
 
     def connect(self, params={}):
         service_name = "ec2"
-        self.region = params.get(Input.REGION)
         self.auth_params = {
             "aws_access_key_id": params.get(Input.AWS_ACCESS_KEY_ID).get("secretKey"),
             "aws_secret_access_key": params.get(Input.AWS_SECRET_ACCESS_KEY).get("secretKey"),
         }
+        self.assume_role_params = {
+            "region": params.get(Input.REGION, ""),
+            "role_arn": params.get(Input.ROLE_ARN, ""),
+            "external_id": params.get(Input.EXTERNAL_ID, ""),
+        }
 
         session = botocore.session.Session()
-        self.client = session.create_client(service_name, region_name=self.region, **self.auth_params)
+        self.client = session.create_client(
+            service_name, region_name=self.assume_role_params.get("region"), **self.auth_params
+        )
         self.logger.info("Client connection object created...")
 
-        self.client = AWSAction.try_to_assume_role(service_name, params, self.auth_params, self.region, self.client)
+        if self.assume_role_params.get(Input.ROLE_ARN):
+            self.client = AWSAction.try_to_assume_role(service_name, self.assume_role_params, self.auth_params)
