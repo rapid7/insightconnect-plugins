32cd18a989efd193c15a546d9cf39cb381ff0bc5
Hephzi <hephzibah_wilson@rapid7.com>
2023-02-01 15:00:14 +0000

    Igor's Changes

--- plugins/rapid7_insightidr/unit_test/mock.py
+++ plugins/rapid7_insightidr/unit_test/mock.py
@@ -10,6 +10,7 @@ REQUEST_PUT = "put"
 
 STUB_ID = "1b1a111d-d1fb-1a12-1651-eb1ff61a651a"
 STUB_ID_NOT_FOUND = "22b2b22b-222b-222b-2222-b2bb2bbb2b2b"
+STUB_ID_202 = "33b3b3b-333b-333b-3333-b3bb3bbb3b3b"
 STUB_INVESTIGATION_IDENTIFIER = "rrn:investigation:example:11111111-1111-1111-1111-111111111111:investigation:11111111"
 STUB_QUERY_ID = "00000000-0000-1eec-0000-000000000000"
 STUB_QUERY_ID_NOT_FOUND = "00000000-0000-8eec-0000-000000000000"
@@ -71,6 +72,8 @@ def mock_request_post(url: str) -> MockResponse:
 def mock_request_get(url: str) -> MockResponse:
     if url == f"{Util.STUB_URL_REST}/query/logs/{STUB_ID}":
         return MockResponse("query", 200)
+    if url == f"{Util.STUB_URL_REST}/query/logs/{STUB_ID_202}":
+        return MockResponse("query_202", 202)
     if url == f"{Util.STUB_URL_REST}/query/logs/{STUB_ID_NOT_FOUND}":
         return MockResponse("query_404", 404)
     if url == f"{Util.STUB_URL_REST}/query/saved_queries":
@@ -98,8 +101,8 @@ def mock_request_put(url: str) -> MockResponse:
     if url == f"{Util.STUB_URL_API}/idr/v2/investigations/{STUB_INVESTIGATION_IDENTIFIER}/status/{STUB_STATUS}":
         return MockResponse("update_investigation", 200)
     if (
-        url
-        == f"{Util.STUB_URL_API}/idr/v2/investigations/{STUB_INVESTIGATION_IDENTIFIER}/disposition/{STUB_DISPOSITION}"
+            url
+            == f"{Util.STUB_URL_API}/idr/v2/investigations/{STUB_INVESTIGATION_IDENTIFIER}/disposition/{STUB_DISPOSITION}"
     ):
         return MockResponse("update_investigation", 200)
     if url == f"{Util.STUB_URL_API}/idr/v2/investigations/{STUB_INVESTIGATION_IDENTIFIER}/assignee":
--- plugins/rapid7_insightidr/unit_test/test_query.py
+++ plugins/rapid7_insightidr/unit_test/test_query.py
@@ -15,14 +15,14 @@ from unit_test.mock import mock_get_request
 
 class TestQuery(TestCase):
     @classmethod
-    def setUpClass(self) -> None:
-        self.params = {
+    def setUpClass(cls) -> None:
+        cls.params = {
             "id": "1b1a111d-d1fb-1a12-1651-eb1ff61a651a",
             "not_found_id": "22b2b22b-222b-222b-2222-b2bb2bbb2b2b",
             "most_recent_first_true": True,
             "most_recent_first_false": False,
         }
-        self.connection_params = {
+        cls.connection_params = {
             ConnectionInput.REGION: "United States 1",
             ConnectionInput.API_KEY: {"secretKey": "api_key"},
         }
@@ -64,6 +64,54 @@ class TestQuery(TestCase):
             ]
         }
         self.assertEqual(actual, expected)
+
+    @patch("requests.Session.get", side_effect=mock_get_request)
+    def test_query_true(self, _mock_req):
+        actual = self.action.run(
+            {Input.ID: self.params.get("id"), Input.MOST_RECENT_FIRST: self.params.get("most_recent_first_true")}
+        )
+        expected = {
+            "events": [
+                {
+                    "labels": [],
+                    "log_id": "1b1a111d-d1fb-1a12-1651-eb1ff61a651a",
+                    "message": {
+                        "computerName": "iagent1-win10",
+                        "eventCode": 1111,
+                        "eventData": {
+                            "data": [],
+                            "engineVersion": "1.1.17300.4",
+                            "platformVersion": "4.18.2007.8",
+                            "productName": "%827",
+                            "signatureVersion": "1.321.836.0",
+                            "unused": "null",
+                        },
+                        "isDomainController": "false",
+                        "sid": "S-1-5-18",
+                        "sourceName": "Microsoft-Windows-Windows Defender",
+                        "timeWritten": "2020-08-07T21:44:12.335999900Z",
+                    },
+                    "sequence_number": 1211198512587571200,
+                    "sequence_number_str": "1211198512587571200",
+                    "timestamp": 1596836653511,
+                }
+            ]
+        }
+        self.assertEqual(actual, expected)
+
+    # @patch("requests.Session.get", side_effect=mock_get_request)
+    # def test_query_202(self, _mock_req):
+    #     pass
+
+    @patch("requests.Session.get", side_effect=mock_get_request)
+    def test_query_202_error(self, _mock_req):
+        with self.assertRaises(PluginException) as exception:
+            self.action.run(
+                {Input.ID: self.params.get("id_202"),
+                 Input.MOST_RECENT_FIRST: self.params.get("most_recent_first_false")}
+            )
+        cause = "The response from InsightIDR was not in the correct format."
+        self.assertEqual(exception.exception.cause, cause)
 
     @patch("requests.Session.get", side_effect=mock_get_request)
     def test_query_not_found(self, _mock_req):
