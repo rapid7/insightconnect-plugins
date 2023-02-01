a9963be81d39239c7e69b8b64d235e6a784553cf
Dympna Laverty <dympna_laverty@rapid7.com>
2023-02-01 12:45:10 +0000

    Updated to latest SDK to avoid SSL cert error. Added examples for all actions

--- plugins/samanage/komand_samanage/actions/assign_incident/action.py
+++ plugins/samanage/komand_samanage/actions/assign_incident/action.py
@@ -1,10 +1,10 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import AssignIncidentInput, AssignIncidentOutput
 
 # Custom imports below
 
 
-class AssignIncident(komand.Action):
+class AssignIncident(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="assign_incident",
