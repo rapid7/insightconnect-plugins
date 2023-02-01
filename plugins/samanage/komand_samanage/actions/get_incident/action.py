7b2d1db50c59045bf030c230abdcbac2e368986f
Dympna Laverty <dympna_laverty@rapid7.com>
2023-02-01 12:44:10 +0000

    Updated to latest SDK to avoid SSL cert error. Added examples for all actions

--- plugins/samanage/komand_samanage/actions/get_incident/action.py
+++ plugins/samanage/komand_samanage/actions/get_incident/action.py
@@ -1,10 +1,10 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import GetIncidentInput, GetIncidentOutput
 
 # Custom imports below
 
 
-class GetIncident(komand.Action):
+class GetIncident(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="get_incident",
