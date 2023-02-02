94b748ec991e237d9cbcbf5cfe9f2b83162b5151
Hephzi <hephzibah_wilson@rapid7.com>
2023-02-02 14:17:49 +0000

    Changed doc string

--- plugins/rapid7_insightidr/komand_rapid7_insightidr/util/formatting.py
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/util/formatting.py
@@ -4,12 +4,9 @@ from typing import Dict
 
 def refactor_message(message: Dict) -> Dict:
     """
-    Taking message and returning it as formatted json
-    Args:
-        message: event["message"] from query's result_response
-
-    Returns:
-        new_message: Formatted json
+    Takes message and returning it as formatted json
+    :param message: event["message"] from query's result_response
+    :returns: new_message: Formatted json
     """
     new_message = json.loads(str(message).replace("\n", "\\n").replace("'", '"'))
     return new_message
