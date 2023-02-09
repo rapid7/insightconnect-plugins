a197e3d3720374db2c18d15c99acfab97bf70b4d
Dympna Laverty <dympna_laverty@rapid7.com>
2023-02-09 09:34:46 +0000

    Updated layout of delete incident and api files

--- plugins/samanage/komand_samanage/actions/delete_incident/action.py
+++ plugins/samanage/komand_samanage/actions/delete_incident/action.py
@@ -1,16 +1,17 @@
 import insightconnect_plugin_runtime
 from .schema import DeleteIncidentInput, DeleteIncidentOutput, Input, Output, Component
+
 # Custom imports below
 
 
 class DeleteIncident(insightconnect_plugin_runtime.Action):
-
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='delete_incident',
-                description=Component.DESCRIPTION,
-                input=DeleteIncidentInput(),
-                output=DeleteIncidentOutput())
+            name="delete_incident",
+            description=Component.DESCRIPTION,
+            input=DeleteIncidentInput(),
+            output=DeleteIncidentOutput(),
+        )
 
     def run(self, params={}):
         incident_id = params.get("incident_id")
