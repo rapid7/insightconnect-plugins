158ad5255d23bc6d51eb1a235b148417a437d6b8
Hephzi <hephzibah_wilson@rapid7.com>
2023-02-20 16:14:12 +0000

    Ran black

--- plugins/ivanti_service_manager/icon_ivanti_service_manager/actions/search_incidents/action.py
+++ plugins/ivanti_service_manager/icon_ivanti_service_manager/actions/search_incidents/action.py
@@ -6,20 +6,20 @@ from .schema import SearchIncidentsInput, SearchIncidentsOutput, Input, Output, 
 
 # Not Tested Yet
 class SearchIncidents(insightconnect_plugin_runtime.Action):
-
     def __init__(self):
         super(self.__class__, self).__init__(
-            name='search_incidents',
+            name="search_incidents",
             description=Component.DESCRIPTION,
             input=SearchIncidentsInput(),
-            output=SearchIncidentsOutput())
+            output=SearchIncidentsOutput(),
+        )
 
     def run(self, params={}):
         payload = {
             "Text": params.get(Input.TEXT),
             "ObjectType": params.get(Input.OBJECT_TYPE),
             "Top": params.get(Input.TOP),
-            "Skip": params.get(Input.SKIP)
+            "Skip": params.get(Input.SKIP),
         }
 
         data = self.connection.ivanti_service_manager_api.search_incident(params.get(Input.TEXT), payload)
