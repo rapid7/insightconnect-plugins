c4402940877a31cafdff4d6f5c9e0cafce0c3bee
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-02-23 10:35:09 +0000

    [PLGN-131] Cisco Enforcement API Key fix (#1629)
    * Add ssl verify to false
    
    * SSL added + bug fixed + api refactor
    
    * Update api to client
    
    * Updated get domains
    
    * Updated spec and get domains action
    
    * removed redundant code
    
    * Deletes working but json error
    
    * Tided actions and removed old delete api calls
    
    * Deletes refactored
    
    * updated yaml names and removed most of add events
    
    * Finished add event and removed fluff from checksum
    
    * Updated help.md
    
    * Updated some plugin spec values
    
    * Updates
    
    * Unit test skeleton
    
    * Updated skeleton for all tests
    
    * domains test sucess and responses
    
    * Add event test
    
    * Changed add event input
    
    * Updated add unit test and black linting
    
    * Fixed checksum
    
    * Added parameterized
    
    * Action tests done
    
    * Connection test done
    
    * Black formatting
    
    * Deleted .coverage
    
    * Fix typos
    
    * Update delete returns | remove ssl | validator fix
    
    * Validator
    
    * Update unit tests for delete
    
    * Updated changelog
    
    * Update meta data description

---
+++ plugins/cisco_umbrella_enforcement/unit_test/test_add_event.py
@@ -0,0 +1,56 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
+from komand_cisco_umbrella_enforcement.connection.connection import Connection
+from komand_cisco_umbrella_enforcement.actions.add_event import AddEvent
+from insightconnect_plugin_runtime.exceptions import PluginException
+from parameterized import parameterized
+import logging
+from unit_test.mock import (
+    STUB_CONNECTION,
+    mock_request_202,
+    mock_request_400,
+    mock_request_401,
+    mock_request_403,
+    mock_request_404,
+    mock_request_500,
+    mocked_request,
+    STUB_EVENT_INPUT,
+)
+
+
+class TestAddEvent(TestCase):
+    def setUp(self) -> None:
+        self.connection = Connection()
+        self.connection.logger = logging.getLogger("Connection logger")
+        self.connection.connect(STUB_CONNECTION)
+
+        self.action = AddEvent()
+        self.action.connection = self.connection
+        self.action.logger = logging.getLogger("Action logger")
+
+    @mock.patch("requests.request", side_effect=mock_request_202)
+    def test_add_event(self, mock_post):
+        response = self.action.run(STUB_EVENT_INPUT)
+        expected_response = {"ID": {"ID": {"id": "12345678,1234,1234,1234-123456789123"}}}
+
+        self.assertEqual(response, expected_response)
+
+    @parameterized.expand(
+        [
+            (mock_request_400, "Invalid request."),
+            (mock_request_401, PluginException.causes[PluginException.Preset.USERNAME_PASSWORD]),
+            (mock_request_403, PluginException.causes[PluginException.Preset.UNAUTHORIZED]),
+            (mock_request_404, PluginException.causes[PluginException.Preset.NOT_FOUND]),
+            (mock_request_500, PluginException.causes[PluginException.Preset.SERVER_ERROR]),
+        ],
+    )
+    def test_not_ok(self, mock_request, exception):
+        mocked_request(mock_request)
+
+        with self.assertRaises(PluginException) as context:
+            self.action.run(STUB_EVENT_INPUT)
+        self.assertEqual(context.exception.cause, exception)
