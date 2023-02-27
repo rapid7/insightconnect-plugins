d9d0699f38d3a0ce04946e61a869ca9fffea9290
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-02-27 13:00:38 +0100

    plgn-148 add unit test, update sdk, fix issue in lookup action (#1645)

--- plugins/google_web_risk/icon_google_web_risk/actions/lookup/action.py
+++ plugins/google_web_risk/icon_google_web_risk/actions/lookup/action.py
@@ -1,11 +1,13 @@
-import komand
+import insightconnect_plugin_runtime
+from typing import List, Dict
+
 from .schema import LookupInput, LookupOutput, Input, Component, Output
-from komand.exceptions import PluginException
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 import requests
 
 
-class Lookup(komand.Action):
+class Lookup(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="lookup",
@@ -14,24 +16,22 @@ class Lookup(komand.Action):
             output=LookupOutput(),
         )
 
-    @staticmethod
-    def _check_threat_type(threat_types: [str], params: dict, threat_type: str, name: str) -> None:
+    def _check_threat_type(self, threat_types: List[str], params: Dict[str, str], threat_type: str, name: str) -> None:
         if params.get(threat_type):
             threat_types.append(name)
 
-    @staticmethod
-    def _build_threat_types(params: dict) -> [str]:
+    def _build_threat_types(self, params: Dict[str, str]) -> List[str]:
         threat_types = []
-        Lookup._check_threat_type(threat_types, params, Input.THREAT_TYPE_MALWARE, "MALWARE")
-        Lookup._check_threat_type(threat_types, params, Input.THREAT_TYPE_UNWANTED, "UNWANTED_SOFTWARE")
-        Lookup._check_threat_type(threat_types, params, Input.THREAT_TYPE_SOCIAL, "SOCIAL_ENGINEERING")
+        self._check_threat_type(threat_types, params, Input.THREAT_TYPE_MALWARE, "MALWARE")
+        self._check_threat_type(threat_types, params, Input.THREAT_TYPE_UNWANTED, "UNWANTED_SOFTWARE")
+        self._check_threat_type(threat_types, params, Input.THREAT_TYPE_SOCIAL, "SOCIAL_ENGINEERING")
         return threat_types
 
-    def run(self, params={}):
+    def run(self, params={}) -> Dict[str, str]:
         endpoint = f"{self.connection.base}uris:search"
         parameters = {
             "key": self.connection.key,
-            "threatTypes": Lookup.build_threat_types(params),
+            "threatTypes": self._build_threat_types(params),
             "uri": params.get(Input.URL),
         }
         result = requests.get(endpoint, params=parameters)
