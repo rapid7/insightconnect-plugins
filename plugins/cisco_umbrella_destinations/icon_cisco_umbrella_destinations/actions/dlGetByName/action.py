f704a97608e7e3c80444e7d83a60685acefa7900
Hephzi <hephzibah_wilson@rapid7.com>
2023-03-06 09:37:11 +0000

    action

--- plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/actions/dlGetByName/action.py
+++ plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/actions/dlGetByName/action.py
@@ -1,16 +1,14 @@
 import insightconnect_plugin_runtime
 from .schema import DlGetByNameInput, DlGetByNameOutput, Input, Output, Component
+
 # Custom imports below
 
 
 class DlGetByName(insightconnect_plugin_runtime.Action):
-
     def __init__(self):
         super(self.__class__, self).__init__(
-                name='dlGetByName',
-                description=Component.DESCRIPTION,
-                input=DlGetByNameInput(),
-                output=DlGetByNameOutput())
+            name="dlGetByName", description=Component.DESCRIPTION, input=DlGetByNameInput(), output=DlGetByNameOutput()
+        )
 
     def run(self, params={}):
         destination_list_name = params.get(Input.NAME)
