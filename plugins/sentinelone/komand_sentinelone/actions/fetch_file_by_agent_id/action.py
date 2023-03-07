5529a3bf5f2219c1fd8874f6c3152b9c7bcf8719
Dympna Laverty <118898375+dlaverty-r7@users.noreply.github.com>
2023-03-07 19:06:20 +0000

    [PLGN-153] SentinelOne run remote script and fetch file actions (#1662)

---
+++ plugins/sentinelone/komand_sentinelone/actions/fetch_file_by_agent_id/action.py
@@ -0,0 +1,28 @@
+import insightconnect_plugin_runtime
+from .schema import FetchFileByAgentIdInput, FetchFileByAgentIdOutput, Input, Output, Component
+from insightconnect_plugin_runtime.exceptions import PluginException
+from komand_sentinelone.util.helper import check_password_meets_requirements
+
+# Custom imports below
+
+
+class FetchFileByAgentId(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="fetch_file_by_agent_id",
+            description=Component.DESCRIPTION,
+            input=FetchFileByAgentIdInput(),
+            output=FetchFileByAgentIdOutput(),
+        )
+
+    def run(self, params={}):
+        agent_id = params.get(Input.AGENT_ID)
+        password = params.get(Input.PASSWORD, "")
+        file_path = params.get(Input.FILE_PATH, "")
+
+        if password:
+            check_password_meets_requirements(password)
+
+        response = self.connection.fetch_file_by_agent_id(agent_id, file_path, password)
+
+        return {Output.SUCCESS: response}
---
+++ plugins/sentinelone/komand_sentinelone/actions/run_remote_script/action.py
@@ -0,0 +1,61 @@
+import insightconnect_plugin_runtime
+from insightconnect_plugin_runtime.exceptions import PluginException
+from .schema import RunRemoteScriptInput, RunRemoteScriptOutput, Input, Output, Component
+from komand_sentinelone.util.constants import (
+    SCRIPT_TIMEOUT_LOWER_LIMIT,
+    SCRIPT_TIMEOUT_UPPER_LIMIT,
+    SCRIPT_TIMEOUT_DEFAULT,
+)
+from komand_sentinelone.util.helper import check_password_meets_requirements
+
+# Custom imports below
+
+
+class RunRemoteScript(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="run_remote_script",
+            description=Component.DESCRIPTION,
+            input=RunRemoteScriptInput(),
+            output=RunRemoteScriptOutput(),
+        )
+
+    def run(self, params={}):
+        agent_ids = params.get(Input.IDS)
+        script_id = params.get(Input.SCRIPT_ID, "")
+        task_description = params.get(Input.TASK_DESCRIPTION, "")
+        script_timeout = params.get(Input.TIMEOUT, SCRIPT_TIMEOUT_DEFAULT)
+        input_params = params.get(Input.INPUT_PARAMETERS, "")
+        password = params.get(Input.PASSWORD, "")
+
+        if script_timeout < SCRIPT_TIMEOUT_LOWER_LIMIT or script_timeout > SCRIPT_TIMEOUT_UPPER_LIMIT:
+            # Timeout outside of allowed parameters - use default value instead
+            script_timeout = SCRIPT_TIMEOUT_DEFAULT
+
+        if password:
+            check_password_meets_requirements(password)
+
+        output_dest = params.get(Input.OUTPUT_DESTINATION, "")
+        output_dir = params.get(Input.OUTPUT_DIRECTORY, "")
+        if output_dest == "Local" and not output_dir:
+            raise PluginException(
+                cause="Local output destination selected but no output directory provided.",
+                assistance="Please provide an output directory.",
+            )
+
+        user_filter = {}
+        if agent_ids:
+            user_filter = {"ids": agent_ids}
+        data = {
+            "scriptRuntimeTimeoutSeconds": script_timeout,
+            "scriptId": script_id,
+            "outputDestination": output_dest,
+            "taskDescription": task_description,
+            "inputParams": input_params,
+            "outputDirectory": output_dir,
+            "password": password,
+        }
+
+        affected = self.connection.run_remote_script(user_filter, data)
+
+        return {Output.AFFECTED: affected}
