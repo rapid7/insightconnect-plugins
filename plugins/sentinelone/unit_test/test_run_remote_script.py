d2823760217fc6c7d32cad7a15b5462a16cf0c0e
Dympna Laverty <118898375+dlaverty-r7@users.noreply.github.com>
2023-03-07 19:06:20 +0000

    [PLGN-153] SentinelOne run remote script and fetch file actions (#1662)

---
+++ plugins/sentinelone/unit_test/test_run_remote_script.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest.mock import patch
+from unit_test.util import Util
+from unittest import TestCase
+from komand_sentinelone.connection.connection import Connection
+from komand_sentinelone.actions.run_remote_script import RunRemoteScript
+from komand_sentinelone.actions.run_remote_script.schema import Input
+import json
+import logging
+
+
+class TestRunRemoteScript(TestCase):
+    @classmethod
+    @patch("requests.post", side_effect=Util.mocked_requests_get)
+    def setUpClass(cls, mock_request) -> None:
+        cls.action = Util.default_connector(RunRemoteScript())
+
+    @patch("requests.request", side_effect=Util.mocked_requests_get)
+    def test_should_success(self, mock_request):
+        expected = {"affected": 1}
+        actual = self.action.run(
+            {
+                Input.IDS: ["1470609440131178177"],
+                Input.SCRIPT_ID: "100000000000",
+                Input.TASK_DESCRIPTION: "Test task description",
+            }
+        )
+        self.assertEqual(expected, actual)
