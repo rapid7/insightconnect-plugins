10240e152a4ad2bf73fba0aeddbe0bb84b06cb06
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-03-07 11:58:29 +0100

    Cisco Umbrella Investigate - 163 - Updated the documentation and connection test (#1657)

--- plugins/cisco_umbrella_investigate/komand_cisco_umbrella_investigate/actions/latest_domains/action.py
+++ plugins/cisco_umbrella_investigate/komand_cisco_umbrella_investigate/actions/latest_domains/action.py
@@ -1,12 +1,12 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import LatestDomainsInput, LatestDomainsOutput, Input, Output
 
 # Custom imports below
-from komand.exceptions import PluginException
+from insightconnect_plugin_runtime.exceptions import PluginException
 from IPy import IP as IP_Validate
 
 
-class LatestDomains(komand.Action):
+class LatestDomains(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="latest_domains",
@@ -19,15 +19,15 @@ class LatestDomains(komand.Action):
         ip_address = params.get(Input.IP)
         try:
             IP_Validate(ip_address)
-        except Exception as e:
+        except Exception as error:
             raise PluginException(
                 cause="Invalid IP provided by user.",
                 assistance="Please try again by submitting a valid IP address.",
-                data=e,
+                data=error,
             )
 
         try:
             latest_domains = self.connection.investigate.latest_domains(ip_address)
-        except Exception as e:
-            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=e)
+        except Exception as error:
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=error)
         return {Output.DOMAINS: latest_domains}
