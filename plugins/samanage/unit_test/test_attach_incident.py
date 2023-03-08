920cb6ea3ce0002494eb8b8b6db03e58e096b442
Dympna Laverty <dympna_laverty@rapid7.com>
2023-03-08 16:37:49 +0000

    Reverted accidental overwrite

--- plugins/samanage/unit_test/test_attach_incident.py
+++ plugins/samanage/unit_test/test_attach_incident.py
@@ -7,18 +7,37 @@ sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
 from komand_samanage.connection.connection import Connection
-from komand_samanage.actions.assign_incident import AssignIncident
+from komand_samanage.actions.attach_incident import AttachIncident
 from unit_test.util import Util, mock_request_200
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 
 
-@patch("komand_samanage.util.api.request", side_effect=mock_request_200)
-class TestAssignIncident(TestCase):
-    def setUp(self) -> None:
-        self.action = Util.default_connector(AssignIncident())
+att = {
+    "id": 27211951,
+    "content_type": "text/plain",
+    "size": 12,
+    "filename": "Hello.txt",
+    "url": "https://example.com",
+    "shared_attachment": False,
+    "attachable_id": 31851783,
+    "attachable_type": "Incident",
+    "attachment_type": "attachment",
+}
+test_attachment = {"rcode": 0, "stdout": json.dumps(att)}
 
-    @parameterized.expand(Util.load_parameters("assign_incident").get("parameters"))
-    def test_assign_incident(self, mock_request, incident_id, assignee, expected):
-        actual = self.action.run({"incident_id": incident_id, "assignee": assignee})
-        self.assertEqual(actual, expected)
+
+class TestAttachIncident(TestCase):
+    def setUp(self) -> None:
+        self.action = Util.default_connector(AttachIncident())
+
+    @patch("insightconnect_plugin_runtime.helper.exec_command", return_value=test_attachment)
+    @patch("shutil.rmtree")
+    def test_attach_incident(self, plugin_mock, rm_mock):
+        response = self.action.run(
+            {"incident_id": 2134, "attachment_bytes": "ABCD", "attachment_name": "Example attachment name"}
+        )
+        plugin_mock.assert_called()
+        rm_mock.assert_called()
+        expected = {"attachment": att}
+        self.assertEqual(response, expected)
