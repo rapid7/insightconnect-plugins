be185cdf41e4b55c79f0ac5a77464d745719740d
cdankowski-r7 <121542609+cdankowski-r7@users.noreply.github.com>
2023-03-16 13:19:34 +0100

    [MC-1411] Zscaler v1.5.0 (#1639)
    * [MC-1411] Zscaler v1.5.0
    
    * [MC-1411] Regenerate
    
    * [MC-1411] Updated helpers and added new line at the end of files
    
    * [MC-1411] Prospector check fix
    
    ---------
    
    Co-authored-by: dsliwinski-r7 <dominik_sliwinski@rapid7.com>

---
+++ plugins/zscaler/unit_test/test_create_user.py
@@ -0,0 +1,60 @@
+import sys
+import os
+from unittest import TestCase
+from unittest.mock import patch
+from parameterized import parameterized
+
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+from unit_test.util import Util
+from icon_zscaler.actions.create_user import CreateUser
+from icon_zscaler.util.constants import Assistance, Cause
+
+sys.path.append(os.path.abspath("../"))
+
+
+@patch("requests.request", side_effect=Util.mock_request)
+class TestCreateUser(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(CreateUser())
+
+    @parameterized.expand(
+        [
+            [
+                "valid_user",
+                Util.read_file_to_dict("inputs/create_user.json.inp"),
+                Util.read_file_to_dict("expected/create_user.json.exp"),
+            ],
+            [
+                "mandatory_fields",
+                Util.read_file_to_dict("inputs/create_user_mandatory_fields.json.inp"),
+                Util.read_file_to_dict("expected/create_user_mandatory_fields.json.exp"),
+            ],
+        ]
+    )
+    def test_create_user(self, mock_request, test_name, input_params, expected):
+        actual = self.action.run(input_params)
+        self.assertEqual(actual, expected)
+
+    @parameterized.expand(
+        [
+            [
+                "bad_department",
+                Util.read_file_to_dict("inputs/create_user_bad_department.json.inp"),
+                Cause.DEPARTMENT_NOT_FOUND,
+                Assistance.VERIFY_INPUT,
+            ],
+            [
+                "bad_group",
+                Util.read_file_to_dict("inputs/create_user_bad_group.json.inp"),
+                Cause.GROUP_NOT_FOUND,
+                Assistance.VERIFY_INPUT,
+            ],
+        ]
+    )
+    def test_create_user_raise_exception(self, mock_request, test_name, input_parameters, cause, assistance):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(input_parameters)
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
