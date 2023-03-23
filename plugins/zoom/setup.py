6b778cba479e41d44fcb24df6c3aa05e59011c96
conor-mcnally <conor_mcnally@rapid7.com>
2023-03-23 13:49:02 +0000

    Updated version to 2.1.0

--- plugins/zoom/setup.py
+++ plugins/zoom/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="zoom-rapid7-plugin",
-      version="2.0.0",
+      version="2.0.1",
       description="Trigger workflows on user activity while also managing your users with the Zoom plugin",
       author="rapid7",
       author_email="",
