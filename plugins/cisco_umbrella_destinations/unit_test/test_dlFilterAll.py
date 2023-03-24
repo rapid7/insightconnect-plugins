bed93acb6058045481915201db0a6a4b344497b7
Hephzi <hephzibah_wilson@rapid7.com>
2023-03-24 16:38:08 +0000

    Added extra categories

--- plugins/cisco_umbrella_destinations/unit_test/test_dlFilterAll.py
+++ plugins/cisco_umbrella_destinations/unit_test/test_dlFilterAll.py
@@ -10,7 +10,6 @@ from icon_cisco_umbrella_destinations.connection.connection import Connection
 from icon_cisco_umbrella_destinations.actions.dlFilterAll import DlFilterAll
 from icon_cisco_umbrella_destinations.actions.dlFilterAll.schema import Input
 from insightconnect_plugin_runtime.exceptions import PluginException
-
 import logging
 from unit_test.mock import (
     Util,
