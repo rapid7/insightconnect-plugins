34530b79b1d25978543ade4a4e0d4c9729c96d80
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-03-29 16:14:32 +0200

    plgn-195 fix when inconsistent api (#1686)

--- plugins/rapid7_insightidr/komand_rapid7_insightidr/actions/list_alerts_for_investigation/action.py
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/actions/list_alerts_for_investigation/action.py
@@ -1,14 +1,13 @@
+import json
+from typing import Dict, Any
+
 import insightconnect_plugin_runtime
-from .schema import ListAlertsForInvestigationInput, ListAlertsForInvestigationOutput, Input, Output, Component
 from insightconnect_plugin_runtime.exceptions import PluginException
 from insightconnect_plugin_runtime.helper import clean
 
-# Custom imports below
 from komand_rapid7_insightidr.util.endpoints import Investigations
 from komand_rapid7_insightidr.util.resource_helper import ResourceHelper
-
-# Custom imports below
-import json
+from .schema import ListAlertsForInvestigationInput, ListAlertsForInvestigationOutput, Input, Output, Component
 
 
 class ListAlertsForInvestigation(insightconnect_plugin_runtime.Action):
@@ -31,7 +30,6 @@ class ListAlertsForInvestigation(insightconnect_plugin_runtime.Action):
 
         endpoint = Investigations.list_alerts_for_investigation(self.connection.url, identifier)
         response = request.resource_request(endpoint, "get", params=parameters)
-
         try:
             result = json.loads(response.get("resource"))
         except json.decoder.JSONDecodeError:
@@ -40,6 +38,7 @@ class ListAlertsForInvestigation(insightconnect_plugin_runtime.Action):
                 cause="The response from InsightIDR was not in the correct format.",
                 assistance="Contact support for help. See log for more details",
             )
+        self._get_rule_rrn(result)
         try:
             alerts = clean(result.get("data", {}))
             metadata = result.get("metadata", {})
@@ -50,3 +49,12 @@ class ListAlertsForInvestigation(insightconnect_plugin_runtime.Action):
                 cause="The response from InsightIDR was not in the correct format.",
                 assistance="Contact support for help. See log for more details",
             )
+
+    def _get_rule_rrn(self, result: Dict[str, Any]) -> None:
+        """
+        Retrieve rule rrn from all records,
+        due to inconsistent API from insightIDR
+        """
+        for data in result.get("data"):
+            if isinstance(data.get("detection_rule_rrn"), dict):
+                data["detection_rule_rrn"] = data.get("detection_rule_rrn", {}).get("rule_rrn", "")
