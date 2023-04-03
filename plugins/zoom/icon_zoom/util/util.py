ab5fd14a784b022595fb796e1ccd9870028c071d
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-04-03 15:14:36 +0100

    [PLGN-190] Zoom - Remove redundant enum (#1683)
    * Removed redundant enum
    
    * Updated version to 2.1.0
    
    * Unit test skeleton
    
    * More unit test filler
    
    * oauth successful
    
    * Successful first test
    
    * More success tests
    
    * Checkpoint #1
    
    * Fixed the unit tests for oauth handling
    
    * Unit tests GET finished
    
    * Create user tests
    
    * Checkpoint #2
    
    * Added parameterized to unit tests
    
    * Minus trigger test
    
    * Delete test
    
    * Refresh
    
    * Newlines in resp, update v to 2.1.0
    
    * Update 409 in api.py
    
    ---------
    
    Co-authored-by: Dympna Laverty <dympna_laverty@rapid7.com>

--- plugins/zoom/icon_zoom/util/util.py
+++ plugins/zoom/icon_zoom/util/util.py
@@ -4,4 +4,4 @@ DEFAULT_TASK_TYPE = "CMEF"
 class UserType:
     @staticmethod
     def value_of(user_type: str) -> int:
-        return {"Basic": 1, "Licensed": 2, "On-Prem": 3}.get(user_type)
+        return {"Basic": 1, "Licensed": 2}.get(user_type)
---
+++ plugins/zoom/unit_test/test_get_user.py
@@ -0,0 +1,92 @@
+import sys
+import os
+from parameterized import parameterized
+import logging
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
+from icon_zoom.actions.get_user import GetUser
+from icon_zoom.actions.get_user.schema import Input
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+from unit_test.mock import (
+    Util,
+    STUB_USER_ID,
+    mock_request_201,
+    mock_request_400,
+    mock_request_404,
+    mocked_request,
+)
+
+
+class TestGetUser(TestCase):
+    @mock.patch("requests.request", side_effect=mock_request_201)
+    def setUp(self, mock_request) -> None:
+        mocked_request(mock_request)
+        self.action = Util.default_connector(GetUser())
+        self.params = {Input.USER_ID: STUB_USER_ID}
+
+    @mock.patch("requests.request", side_effect=mock_request_201)
+    def test_get_user_success(self, mock_get):
+        response = self.action.run(self.params)
+        expected_response = {
+            "user": {
+                "id": "zJKyaiAyTNC-MWjiWC18KQ",
+                "dept": "Developers",
+                "email": "jchill@example.com",
+                "first_name": "Jill",
+                "last_client_version": "5.9.6.4993(mac)",
+                "last_login_time": "2021-05-05T20:40:30Z",
+                "last_name": "Chill",
+                "pmi": 3542471135,
+                "role_name": "Admin",
+                "timezone": "Asia/Shanghai",
+                "type": 1,
+                "use_pmi": False,
+                "display_name": "Jill Chill",
+                "account_id": "q6gBJVO5TzexKYTb_I2rpg",
+                "account_number": 10009239,
+                "cms_user_id": "KDcuGIm1QgePTO8WbOqwIQ",
+                "company": "Jill",
+                "user_created_at": "2018-10-31T04:32:37Z",
+                "custom_attributes": {"key": "cbf_cywdkexrtqc73f97gd4w6g", "name": "A1", "value": "1"},
+                "employee_unique_id": "HqDyI037Qjili1kNsSIrIg",
+                "group_ids": ["RSMaSp8sTEGK0_oamiA2_w"],
+                "im_group_ids": ["t-_-d56CSWG-7BF15LLrOw"],
+                "jid": "jchill@example.com",
+                "job_title": "API Developer",
+                "language": "en-US",
+                "location": "Paris",
+                "login_types": [101],
+                "manager": "thill@example.com",
+                "personal_meeting_url": "example.com",
+                "phone_numbers": [
+                    {"code": "+1", "country": "US", "label": "Mobile", "number": "800000000", "verified": True}
+                ],
+                "pic_url": "example.com",
+                "plan_united_type": "1",
+                "pronouns": "3123",
+                "pronouns_option": 1,
+                "role_id": "0",
+                "status": "pending",
+                "vanity_url": "example.com",
+                "verified": 1,
+                "cluster": "us04",
+                "zoom_one_type": 4,
+            }
+        }
+        self.assertEqual(response, expected_response)
+
+    @parameterized.expand(
+        [
+            (mock_request_400, PluginException.causes[PluginException.Preset.BAD_REQUEST]),
+            (mock_request_404, PluginException.causes[PluginException.Preset.NOT_FOUND]),
+        ],
+    )
+    @mock.patch("icon_zoom.util.api.ZoomAPI.refresh_oauth_token_if_needed", return_value=None)
+    def test_not_ok(self, mock_request, exception, mock_refresh):
+        mocked_request(mock_request)
+        with self.assertRaises(PluginException) as context:
+            self.action.run(self.params)
+        self.assertEqual(context.exception.cause, exception)
