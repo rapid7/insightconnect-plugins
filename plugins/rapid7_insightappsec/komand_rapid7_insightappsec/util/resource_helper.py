63512a1605a3793cf188d3f41349d352f97d567a
dsliwinski-r7 <dominik_sliwinski@rapid7.com>
2023-04-03 16:53:35 +0200

    [MC-1434] Fix prospector

--- plugins/rapid7_insightappsec/komand_rapid7_insightappsec/util/resource_helper.py
+++ plugins/rapid7_insightappsec/komand_rapid7_insightappsec/util/resource_helper.py
@@ -1,4 +1,5 @@
 import requests
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 
 class ResourceHelper(object):
@@ -58,11 +59,11 @@ class ResourceHelper(object):
             try:
                 error = response.json()["message"]
             except KeyError:
-                self.logger.error(f"Code: {response.status_code}, message: {error}")
                 error = "Unknown error occurred. Please contact support or try again later."
 
             status_code_message = self._ERRORS.get(response.status_code, self._ERRORS[000])
             self.logger.error(f"{status_code_message} ({response.status_code}): {error}")
-            raise Exception(
-                f"Insight AppSec returned a status code of {response.status_code}: {status_code_message}. {error}"
+            raise PluginException(
+                cause=f"Insight AppSec returned a status code of {response.status_code}: {status_code_message}.",
+                assistance=error,
             )
