b35d5a5aa29faf434cca8d2cede07029079cbf67
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-04-03 15:14:36 +0100

    [PLGN-190] Zoom - Remove redundant enum (#1683)
    * Removed redundant enum
    
    * Updated version to 2.1.0
    
    * Unit test skeleton
    
    * More unit test filler
    
    * oauth successful
    
    * Successful first test
    
    * More success tests
    
    * Checkpoint #1
    
    * Fixed the unit tests for oauth handling
    
    * Unit tests GET finished
    
    * Create user tests
    
    * Checkpoint #2
    
    * Added parameterized to unit tests
    
    * Minus trigger test
    
    * Delete test
    
    * Refresh
    
    * Newlines in resp, update v to 2.1.0
    
    * Update 409 in api.py
    
    ---------
    
    Co-authored-by: Dympna Laverty <dympna_laverty@rapid7.com>

--- plugins/zoom/icon_zoom/util/api.py
+++ plugins/zoom/icon_zoom/util/api.py
@@ -140,7 +140,7 @@ class ZoomAPI:
             response = requests.request(method, url, json=json_data, params=params, auth=auth)
             self.logger.info(f"Got response status code: {response.status_code}")
 
-            if response.status_code in [401, 404, 429, 204] or (200 <= response.status_code < 300):
+            if response.status_code in [400, 401, 404, 409, 429, 204] or (200 <= response.status_code < 300):
                 return self._handle_response(response=response, allow_404=allow_404)
 
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
@@ -152,27 +152,39 @@ class ZoomAPI:
             raise PluginException(preset=PluginException.Preset.UNKNOWN)
 
     def _handle_response(self, response: Response, allow_404: bool):
+        """
+        Helper function to process the response based on the status code returned.
+        :param response: Response object
+        :param allow_404: Boolean value to indicate whether to allow 404 status code to be ignored
+        """
+
+        if response.status_code == 400:
+            raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.json())
+
         if response.status_code == 401:
             resp = json.loads(response.text)
             raise PluginException(
                 cause=resp.get("message"),
                 assistance="Verify that the credentials configured within the Zoom plugin connection are correct.",
             )
+
         if response.status_code == 404:
-            resp = json.loads(response.text)
             if allow_404:
                 return None
             else:
-                raise PluginException(
-                    cause=resp.get("message"),
-                    assistance=f"The object at {response.url} does not exist. Verify the ID and fields "
-                    f"used are valid.",
-                )
+                raise PluginException(preset=PluginException.Preset.NOT_FOUND, data=response.json())
+
+        if response.status_code == 409:
+            raise PluginException(
+                cause="User already exists.", assistance="Please check your input and try again.", data=response.json()
+            )
+
         if response.status_code == 429:
             self._handle_rate_limit_error(response=response)
         # Success; no content
         if response.status_code == 204:
             return None
+
         if 200 <= response.status_code < 300:
             return response.json()
 
