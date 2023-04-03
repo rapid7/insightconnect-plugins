aaecc3305823e1ef8d6daa18de7880284f4fedc1
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-04-03 15:14:36 +0100

    [PLGN-190] Zoom - Remove redundant enum (#1683)
    * Removed redundant enum
    
    * Updated version to 2.1.0
    
    * Unit test skeleton
    
    * More unit test filler
    
    * oauth successful
    
    * Successful first test
    
    * More success tests
    
    * Checkpoint #1
    
    * Fixed the unit tests for oauth handling
    
    * Unit tests GET finished
    
    * Create user tests
    
    * Checkpoint #2
    
    * Added parameterized to unit tests
    
    * Minus trigger test
    
    * Delete test
    
    * Refresh
    
    * Newlines in resp, update v to 2.1.0
    
    * Update 409 in api.py
    
    ---------
    
    Co-authored-by: Dympna Laverty <dympna_laverty@rapid7.com>

---
+++ plugins/zoom/unit_test/test_delete_user.py
@@ -0,0 +1,56 @@
+import sys
+import os
+from parameterized import parameterized
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
+from icon_zoom.actions.delete_user import DeleteUser
+from icon_zoom.actions.delete_user.schema import Input
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+from unit_test.mock import (
+    Util,
+    mock_request_204,
+    mock_request_400,
+    mock_request_404,
+    mocked_request,
+)
+
+STUB_DELETE_USER_QUERY_PARAMS = {
+    Input.ID: "12345",
+    Input.ACTION: "delete",
+    Input.TRANSFER_EMAIL: "user@example.com",
+    Input.TRANSFER_WEBINARS: "",
+    Input.TRANSFER_RECORDINGS: "",
+    Input.TRANSFER_MEETINGS: "",
+}
+
+
+class TestDeleteUser(TestCase):
+    @mock.patch("requests.Session.request", side_effect=mock_request_204)
+    def setUp(self, mock_delete) -> None:
+        self.action = Util.default_connector(DeleteUser())
+        self.params = STUB_DELETE_USER_QUERY_PARAMS
+
+    @mock.patch("requests.request", side_effect=mock_request_204)
+    def delete_user_success(self, mock_delete):
+        # mocked_request(mock_request_204)
+        response = self.action.run(self.params)
+        expected_response = None
+
+        self.assertEqual(response, expected_response)
+
+    @parameterized.expand(
+        [
+            (mock_request_400, PluginException.causes[PluginException.Preset.BAD_REQUEST]),
+            (mock_request_404, PluginException.causes[PluginException.Preset.NOT_FOUND]),
+        ]
+    )
+    @mock.patch("icon_zoom.util.api.ZoomAPI.refresh_oauth_token_if_needed", return_value=None)
+    def test_not_ok(self, mock_request, exception, mock_refresh):
+        mocked_request(mock_request)
+
+        with self.assertRaises(PluginException) as context:
+            self.action.run(self.params)
+        self.assertEqual(context.exception.cause, exception)
