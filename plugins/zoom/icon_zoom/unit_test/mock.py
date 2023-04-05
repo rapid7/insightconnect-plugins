04564d50bce2023d7e57add0246b71b28a558fa5
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-04-05 14:50:06 +0100

    Zoom 2.1.0 release (#1701)
    * [PLGN-190] Zoom - Remove redundant enum (#1683)
    
    * Removed redundant enum
    
    * Updated version to 2.1.0
    
    * Unit test skeleton
    
    * More unit test filler
    
    * oauth successful
    
    * Successful first test
    
    * More success tests
    
    * Checkpoint #1
    
    * Fixed the unit tests for oauth handling
    
    * Unit tests GET finished
    
    * Create user tests
    
    * Checkpoint #2
    
    * Added parameterized to unit tests
    
    * Minus trigger test
    
    * Delete test
    
    * Refresh
    
    * Newlines in resp, update v to 2.1.0
    
    * Update 409 in api.py
    
    ---------
    
    Co-authored-by: Dympna Laverty <dympna_laverty@rapid7.com>
    
    * remove venv dependencies from checksum (#1700)
    
    * SOAR-13594: Zoom auth update (#1704)
    
    ---------
    
    Co-authored-by: Dympna Laverty <dympna_laverty@rapid7.com>
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>

--- plugins/zoom/icon_zoom/unit_test/mock.py
+++
@@ -1,5 +0,0 @@
-STUB_CONNECTION = {
-    "client_id": {"secretKey": "asdf"},
-    "client_secret": {"secretKey": "asdf"},
-    "account_id": {"secretKey": "asdf"},
-}
---
+++ plugins/zoom/unit_test/test_create_user.py
@@ -0,0 +1,55 @@
+import sys
+import os
+from parameterized import parameterized
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
+from icon_zoom.actions.create_user import CreateUser
+from icon_zoom.actions.create_user.schema import Input
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+from unit_test.mock import (
+    Util,
+    STUB_CREATE_USER,
+    mock_request_201,
+    mock_request_400,
+    mock_request_404,
+    mock_request_409,
+    mock_request_429,
+    mocked_request,
+)
+
+
+class TestCreateUser(TestCase):
+    @mock.patch("requests.request", side_effect=mock_request_201)
+    def setUp(self, mock_request) -> None:
+        mocked_request(mock_request)
+        self.action = Util.default_connector(CreateUser())
+        self.params = STUB_CREATE_USER
+
+    @mock.patch("requests.request", side_effect=mock_request_201)
+    def test_create_user_success(self, mock_post):
+        response = self.action.run(self.params)
+        expected_response = {
+            "email": "jchill@example.com",
+            "first_name": "Jill",
+            "id": "KDcuGIm1QgePTO8WbOqwIQ",
+            "last_name": "Chill",
+            "type": 1,
+        }
+
+        self.assertEqual(response, expected_response)
+
+    @parameterized.expand(
+        [
+            (mock_request_400, PluginException.causes[PluginException.Preset.BAD_REQUEST]),
+            (mock_request_404, PluginException.causes[PluginException.Preset.NOT_FOUND]),
+        ],
+    )
+    @mock.patch("icon_zoom.util.api.ZoomAPI.refresh_oauth_token", return_value=None)
+    def test_not_ok(self, mock_request, exception, mock_refresh):
+        mocked_request(mock_request)
+        with self.assertRaises(PluginException) as context:
+            self.action.run(self.params)
+        self.assertEqual(context.exception.cause, exception)
