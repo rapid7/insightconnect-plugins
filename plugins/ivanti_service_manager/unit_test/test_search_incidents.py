1081e18d9050497acd0b07416928117848ba3c2c
hpwilson-r7 <107193020+hpwilson-r7@users.noreply.github.com>
2023-04-05 14:25:07 +0100

    [PLGN-141] Ivanti Service Manager Update (#1702)
    * Added files
    
    * Removed unneccessary files
    
    * Conor's change
    
    * Non-Unit test Igor Changes
    
    * Non-Unit test Igor Changes - Part 2
    
    * Parameterised create incident
    
    * Igor's changes Unit Tests
    
    * Ran black
    
    * Added parameterized to requirements.txt
    
    * Added parameterized to requirements.txt 2
    
    * Igor's changes, Indentation in help.md
    
    * Igor change pretty print help.md

---
+++ plugins/ivanti_service_manager/unit_test/test_search_incidents.py
@@ -0,0 +1,42 @@
+import sys
+import os
+from parameterized import parameterized
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from unittest.mock import patch
+from icon_ivanti_service_manager.actions.search_incidents import SearchIncidents
+from insightconnect_plugin_runtime.exceptions import PluginException
+from unit_test.util import Util
+from unit_test.mock import mock_request
+from unit_test.payload_stubs import STUB_SEARCH_INCIDENTS_PARAMETERS
+
+
+@patch("requests.Session.request", side_effect=mock_request)
+class TestSearchIncidents(TestCase):
+    def setUp(self) -> None:
+        self.action = Util.default_connector(SearchIncidents())
+        self.connection = self.action.connection
+
+    @parameterized.expand(
+        [
+            ["text"],
+        ]
+    )
+    def test_search_incidents(self, mock_request, keyword):
+        STUB_SEARCH_INCIDENTS_PARAMETERS["keyword"] = keyword
+        actual = self.action.run(STUB_SEARCH_INCIDENTS_PARAMETERS)
+        expected = {"data": "this is good"}
+        self.assertEqual(actual, expected)
+
+    @parameterized.expand(
+        [
+            ["bad text", "No incidents found."],
+        ]
+    )
+    def test_search_incidents_fail(self, mock_request, keyword, cause):
+        with self.assertRaises(PluginException) as exception:
+            STUB_SEARCH_INCIDENTS_PARAMETERS["keyword"] = keyword
+            self.action.run(STUB_SEARCH_INCIDENTS_PARAMETERS)
+        self.assertEqual(exception.exception.cause, cause)
