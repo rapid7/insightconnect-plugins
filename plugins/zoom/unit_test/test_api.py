cccb2d725648bdf3ef7e38420ce0d0c902a3c627
Mike Rinehart <mike_rinehart@rapid7.com>
2023-04-14 10:15:52 -0500

    Add 50 attempt unit test

--- plugins/zoom/unit_test/test_api.py
+++ plugins/zoom/unit_test/test_api.py
@@ -33,13 +33,27 @@ class TestAPI(TestCase):
 
     @patch(REFRESH_OAUTH_TOKEN_PATH)
     @patch(REQUESTS_PATH)
-    def test_unauthenticated_first_run_oauth_retry_limit_met(self, mock_request, mock_refresh):
+    def test_unauthenticated_first_run_oauth_retry_limit_met_1_attempt(self, mock_request, mock_refresh):
         mock_refresh.return_value = "blah"
         api = ZoomAPI(
             account_id="blah", client_id="blah", client_secret="blah", oauth_retry_limit=1, logger=logging.getLogger()
         )
 
         mock_request.side_effect = [MockResponse(status_code=401), MockResponse(status_code=200)]
+
+        with self.assertRaises(PluginException):
+            api._call_api(method="POST", url="http://example.com")
+
+    @patch(REFRESH_OAUTH_TOKEN_PATH)
+    @patch(REQUESTS_PATH)
+    def test_unauthenticated_first_run_oauth_retry_limit_met_50_attempts(self, mock_request, mock_refresh):
+        num_retries = 50
+        mock_refresh.return_value = "blah"
+        api = ZoomAPI(
+            account_id="blah", client_id="blah", client_secret="blah", oauth_retry_limit=num_retries, logger=logging.getLogger()
+        )
+
+        mock_request.side_effect = [MockResponse(status_code=401) for _ in range(0, num_retries)]
 
         with self.assertRaises(PluginException):
             api._call_api(method="POST", url="http://example.com")
