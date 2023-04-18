ab5ff27ab10a958ff120f5deec2e833c51a68bb3
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-04-18 13:41:51 +0100

    [PLGN-193] Insight Agent | Quarantine array of hosts (#1710)
    * New action skeleton
    
    * Update version to 1.2.0
    
    * API function and action run
    
    * Black formatting
    
    * Handle lists
    
    * Action completed
    
    * Action completed
    
    * Small update to support product version in plugin spec
    
    * Remove  checksum fluff
    
    * Add new error for not found
    
    * Updated exception again
    
    * Black formatting
    
    * Add init for unit test
    
    * Add empty dict default
    
    * Removed check for agent in unquarantine
    
    * Unit test so nearly finished
    
    * Unit tests complete
    
    * Newlines
    
    * default values in gets

---
+++ plugins/rapid7_insight_agent/icon_rapid7_insight_agent/actions/quarantine_multiple/action.py
@@ -0,0 +1,33 @@
+import insightconnect_plugin_runtime
+from .schema import QuarantineMultipleInput, QuarantineMultipleOutput, Input, Output, Component
+
+# Custom imports below
+
+
+class QuarantineMultiple(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="quarantine_multiple",
+            description=Component.DESCRIPTION,
+            input=QuarantineMultipleInput(),
+            output=QuarantineMultipleOutput(),
+        )
+
+    def run(self, params={}):
+        agent_array = params.get(Input.AGENT_ARRAY)
+        quarantine_state = params.get(Input.QUARANTINE_STATE)
+        interval = params.get(Input.INTERVAL)
+
+        if quarantine_state:
+            successful_quarantine, unsuccessful_quarantine = self.connection.api.quarantine_list(
+                agent_hostname_list=agent_array, advertisement_period=interval
+            )
+        else:
+            successful_quarantine, unsuccessful_quarantine = self.connection.api.unquarantine_list(
+                agent_hostname_list=agent_array
+            )
+
+        return {
+            Output.SUCCESS: successful_quarantine,
+            Output.FAILURE: unsuccessful_quarantine,
+        }
