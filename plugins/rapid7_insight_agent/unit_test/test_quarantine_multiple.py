94697030f4880dad2319749b18376fda67efa103
conor-mcnally <conor_mcnally@rapid7.com>
2023-04-25 11:46:25 +0100

    Add unit test for empty array

--- plugins/rapid7_insight_agent/unit_test/test_quarantine_multiple.py
+++ plugins/rapid7_insight_agent/unit_test/test_quarantine_multiple.py
@@ -9,6 +9,7 @@ sys.path.append(os.path.abspath("../"))
 from icon_rapid7_insight_agent.actions.quarantine_multiple.action import QuarantineMultiple
 from icon_rapid7_insight_agent.actions.quarantine_multiple.schema import Input
 from unit_test.util import Util
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 
 @patch("requests.sessions.Session.post", side_effect=Util.mocked_request)
@@ -21,3 +22,11 @@ class TestQuarantineMultiple(TestCase):
             {Input.AGENT_ARRAY: agent_id_array, Input.INTERVAL: interval, Input.QUARANTINE_STATE: quarantine_state}
         )
         self.assertEqual(actual, expected)
+
+    def test_empty_array(self, mock_post):
+        action = Util.default_connector(QuarantineMultiple())
+        with self.assertRaises(PluginException) as context:
+            action.run(
+                {Input.AGENT_ARRAY: [], Input.INTERVAL: 1000, Input.QUARANTINE_STATE: True}
+            )
+        self.assertEqual(context.exception.cause, "Empty list provided.")
