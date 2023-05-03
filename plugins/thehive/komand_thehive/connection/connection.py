4b830d254180dd5055d29edca5eea8d8d069a684
conor-mcnally <conor_mcnally@rapid7.com>
2023-05-03 22:52:57 +0100

    Updated connection and regen with insight-plugin

--- plugins/thehive/komand_thehive/connection/connection.py
+++ plugins/thehive/komand_thehive/connection/connection.py
@@ -1,36 +1,36 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import ConnectionSchema, Input
 
 # Custom imports below
-from thehive4py.api import TheHiveApi
 import requests
 from ..util.api import HiveAPI
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
+        self.api_key = None
         self.password = None
         self.username = None
         self.verify = None
         self.client = None
         self.proxy = None
 
-    def connect(self, params):
+    def connect(self, params={}):
         protocol = params.get(Input.PROTOCOL)
         host = params.get(Input.HOST)
         port = params.get(Input.PORT)
-        self.api_key = params.get(Input.API_KEY)
-        # .format(params.get("protocol"), params.get("host").rstrip("/"), params.get("port"))
+
+        self.api_key = params.get(Input.API_KEY).get("secretKey")
         self.password = params.get(Input.CREDENTIALS).get("password")
         self.username = params.get(Input.CREDENTIALS).get("username")
         self.verify = params.get(Input.VERIFY, True)
 
-        url = f"{protocol}://{host}:{port}"
+        url = f"{protocol}://{host}:{port}/"
 
         self.logger.info(f"URL: {url}")
 
-        if not params.get("proxy"):
+        if not params.get(Input.PROXY):
             self.proxy = {}
         else:
             self.proxy = params.get(Input.PROXY)
@@ -38,6 +38,7 @@ class Connection(komand.Connection):
 
         self.logger.info("Connect: Connecting...")
         self.logger.info(f"SSL Verify: {str(self.verify)}")
+
         self.client = HiveAPI(
             url=url,
             username=self.username,
