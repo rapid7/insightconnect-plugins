9a3d27bd3737e2bbf3c698d4cdf994deae8987b0
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2023-05-09 14:59:23 +0200

    [MC-1452] Update Okta plugin (#1733)
    * [MC-1452] Update Okta plugin
    
    * [MC-1452] Fixed test
    
    * [MC-1452] Fix prospector
    
    * [MC-1452] Update help.md
    
    * [MC-1452] Update SDK image to latest version
    
    * [MC-1452] Update unit tests for task
    
    ---------
    
    Co-authored-by: Cyprian Dankowski <cyprian_dankowski@rapid7.com>

--- plugins/okta/komand_okta/actions/suspend_user/action.py
+++ plugins/okta/komand_okta/actions/suspend_user/action.py
@@ -1,13 +1,10 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import SuspendUserInput, SuspendUserOutput, Input, Output, Component
 
 # Custom imports below
-import requests
-from komand_okta.util import helpers
-from komand.exceptions import PluginException
 
 
-class SuspendUser(komand.Action):
+class SuspendUser(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="suspend_user",
@@ -17,28 +14,12 @@ class SuspendUser(komand.Action):
         )
 
     def run(self, params={}):
-        """Get the user by email"""
-        email = params.get(Input.EMAIL)
-        user_id = helpers.get_user_id(email, self.connection, self.logger)
+        login = params.get(Input.LOGIN)
+        user_id = self.connection.api_client.get_user_id(login)
+        self.connection.api_client.suspend_user(user_id)
 
-        if user_id is None:
-            return {Output.SUCCESS: False}
-
-        """ Deactivate the user by id """
-        url = requests.compat.urljoin(self.connection.okta_url, f"/api/v1/users/{user_id}/lifecycle/suspend")
-        response = self.connection.session.post(url)
-
-        if response.status_code == 404:
-            raise PluginException(
-                cause="Suspend User failed",
-                assistance="Okta: Suspend User failed with status code 404: User not found by ID after retrieving ID via email",
-            )
-        elif response.status_code == 400:
-            raise PluginException(
-                cause="Suspend User failed",
-                assistance="Okta: Suspend User failed with status code 400: User was already suspended or in a state where they could not be suspended",
-            )
-        elif response.status_code == 401:
-            raise PluginException(PluginException.Preset.API_KEY)
-
-        return {Output.EMAIL: email, Output.USER_ID: user_id, Output.SUCCESS: True}
+        return {
+            Output.LOGIN: login,
+            Output.USERID: user_id,
+            Output.SUCCESS: True,
+        }
