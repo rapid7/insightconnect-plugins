24b3d411140194768fac75f24167d98d92f1cdaf
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2023-05-09 14:59:23 +0200

    [MC-1452] Update Okta plugin (#1733)
    * [MC-1452] Update Okta plugin
    
    * [MC-1452] Fixed test
    
    * [MC-1452] Fix prospector
    
    * [MC-1452] Update help.md
    
    * [MC-1452] Update SDK image to latest version
    
    * [MC-1452] Update unit tests for task
    
    ---------
    
    Co-authored-by: Cyprian Dankowski <cyprian_dankowski@rapid7.com>

--- plugins/okta/setup.py
+++ plugins/okta/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 
 setup(name="okta-rapid7-plugin",
-      version="3.6.3",
+      version="4.0.0",
       description="Secure identity management and single sign-on to any application",
       author="rapid7",
       author_email="",
       url="",
       packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_okta']
       )
---
+++ plugins/okta/unit_test/test_deactivate_user.py
@@ -0,0 +1,47 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_okta.actions.deactivate_user import DeactivateUser
+from unit_test.util import Util
+from unittest.mock import patch
+from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+@patch("requests.request", side_effect=Util.mock_request)
+class TestDeactivateUser(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(DeactivateUser())
+
+    @parameterized.expand(
+        [
+            [
+                "success",
+                Util.read_file_to_dict("inputs/deactivate_user_success.json.inp"),
+                Util.read_file_to_dict("expected/deactivate_user_success.json.exp"),
+            ],
+        ]
+    )
+    def test_deactivate_user(self, mock_request, test_name, input_params, expected):
+        actual = self.action.run(input_params)
+        self.assertDictEqual(actual, expected)
+
+    @parameterized.expand(
+        [
+            [
+                "active_user_not_found",
+                Util.read_file_to_dict("inputs/deactivate_user_not_found.json.inp"),
+                "Resource not found.",
+                "Verify your input is correct and not malformed and try again. If the issue persists, please contact support.",
+            ],
+        ]
+    )
+    def test_deactivate_user_raise_exception(self, mock_request, test_name, input_parameters, cause, assistance):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(input_parameters)
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
