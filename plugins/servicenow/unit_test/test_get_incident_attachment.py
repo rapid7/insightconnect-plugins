cec60c8964ddf67a0e95a832117473555486bd2c
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-05-10 13:45:40 +0200

    Servicenow - 222 - Added new actions: Create Vulnerability, Get Vulnerability, Update Vulnerability, Delete Vulnerability | Added new trigger: Vulnerability Updated (#1744)

--- plugins/servicenow/unit_test/test_get_incident_attachment.py
+++ plugins/servicenow/unit_test/test_get_incident_attachment.py
@@ -1,11 +1,16 @@
-import sys
 import os
+import sys
+
+sys.path.append("../")
 
 from unittest import TestCase
 from unittest.mock import patch
-from unit_test.util import Util
+
 from icon_servicenow.actions.get_incident_attachment import GetIncidentAttachment
-from icon_servicenow.actions.get_incident_attachment.schema import Input
+from icon_servicenow.actions.get_incident_attachment.schema import Input, Output
+
+from unit_test.util import Util
+from unittest.mock import Mock
 
 sys.path.append(os.path.abspath("../"))
 
@@ -16,8 +21,7 @@ class TestGetIncidentAttachment(TestCase):
         cls.action = Util.default_connector(GetIncidentAttachment())
 
     @patch("requests.sessions.Session.get", side_effect=Util.mocked_requests)
-    def test_manage_threat_remediate(self, mock_post):
+    def test_manage_threat_remediate(self, mock_post: Mock) -> None:
         actual = self.action.run({Input.ATTACHMENT_ID: "b259f4062d9f78f9ffdd6efd05c492c7"})
-
-        expected = {"attachment_contents": "ImNtRndhV1EzWVhSMFlXTm9iV1Z1ZEhSbGN6ZzNOalF6TWpKMCI="}
+        expected = {Output.ATTACHMENT_CONTENTS: "ImNtRndhV1EzWVhSMFlXTm9iV1Z1ZEhSbGN6ZzNOalF6TWpKMCI="}
         self.assertEqual(actual, expected)
