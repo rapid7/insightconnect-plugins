33fdc5bdfa9467ab91e284a22b0499d7fcb07c79
conor-mcnally <conor_mcnally@rapid7.com>
2023-05-15 17:53:20 +0100

    Unit test outline

--- plugins/thehive/unit_test/mock.py
+++ plugins/thehive/unit_test/mock.py
@@ -5,7 +5,10 @@ from typing import Callable
 from unittest import mock
 from unittest.mock import MagicMock
 from komand_thehive.connection.connection import Connection
-from komand_thehive.connection.schema import Input
+from komand_thehive.connection.schema import Input as ConnectionInput
+from komand_thehive.actions.create_case.schema import Input as CaseInput
+from komand_thehive.actions.create_case_observable.schema import Input as CaseObservableInput
+from komand_thehive.actions.create_case_task.schema import Input as CaseTaskInput
 from insightconnect_plugin_runtime.action import Action
 
 import requests
@@ -14,15 +17,23 @@ STUB_API_KEY = "9de5069c5afe602b2ea0a04b66beb2c0"
 STUB_USERNAME = "username"
 STUB_PASSWORD = "password"
 STUB_CONNECTION_API_KEY = {
-    Input.API_KEY: {"secretKey": STUB_API_KEY},
+    ConnectionInput.API_KEY: {"secretKey": STUB_API_KEY},
 }
 STUB_CONNECTION_USERNAME_PASSWORD = {
-    Input.CREDENTIALS.get("username"): STUB_USERNAME,
-    Input.CREDENTIALS.get("password"): STUB_PASSWORD
+    ConnectionInput.CREDENTIALS.get("username"): STUB_USERNAME,
+    ConnectionInput.CREDENTIALS.get("password"): STUB_PASSWORD,
 }
 STUB_BASE_URL = "http://10.10.10.10/9000"
 STUB_CASE_ID = "abcdef123"
 STUB_USER_ID = "stubuserid"
+STUB_CASE = {
+            CaseInput.TITLE: "title",
+            CaseInput.DESCRIPTION: "description",
+            CaseInput.TLP: 2,
+
+        }
+STUB_OBSERVABLE = {CaseObservableInput.ID: STUB_CASE_ID}
+STUB_TASK = {CaseTaskInput.ID: STUB_CASE_ID}
 
 # KEEP THIS FOR REFERENCE - DELETE WHEN DONE
 STUB_RESPONSE = {
--- plugins/thehive/unit_test/test_get_cases.py
+++ plugins/thehive/unit_test/test_get_cases.py
@@ -1,21 +1,49 @@
 import sys
 import os
-sys.path.append(os.path.abspath('../'))
 
-from unittest import TestCase
-from komand_thehive.connection.connection import Connection
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
+from unittest.mock import Mock
 from komand_thehive.actions.get_cases import GetCases
-import json
-import logging
-from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
 
+from parameterized import parameterized
+from unit_test.mock import (
+    Util,
+    mocked_request,
+    mock_request_200,
+    mock_request_400,
+    mock_request_401,
+    mock_request_403,
+    mock_request_404,
+    mock_request_500,
+)
 
 class TestGetCases(TestCase):
-    def test_get_cases(self):
-        """
-        DO NOT USE PRODUCTION/SENSITIVE DATA FOR UNIT TESTS
+    @mock.patch("requests.Session.request", side_effect=mock_request_200)
+    def setUp(self, mock_post: Mock) -> None:
+        self.action = Util.default_connector(GetCases())
 
-        TODO: Implement test cases here
-        """
+    @mock.patch("requests.request", side_effect=mock_request_200)
+    def test_get_cases(self, mock_get):
+        mocked_request(mock_get)
+        response = self.action.run()
+        expected = {}
+        self.assertEqual(response, expected)
 
-        self.fail("Unimplemented Test Case")
+    @parameterized.expand(
+        [
+            (mock_request_400, PluginException.causes[PluginException.Preset.BAD_REQUEST]),
+            (mock_request_401, PluginException.causes[PluginException.Preset.USERNAME_PASSWORD]),
+            (mock_request_403, PluginException.causes[PluginException.Preset.UNAUTHORIZED]),
+            (mock_request_404, PluginException.causes[PluginException.Preset.NOT_FOUND]),
+            (mock_request_500, PluginException.causes[PluginException.Preset.SERVER_ERROR]),
+        ],
+    )
+    def test_not_ok(self, mock_request, exception):
+        mocked_request(mock_request)
+
+        with self.assertRaises(PluginException) as context:
+            self.action.run(self.params)
+        self.assertEqual(context.exception.cause, exception)
