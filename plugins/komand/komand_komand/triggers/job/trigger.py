79c77ccb73e0c56b73580bb297fad78e2dcde3cd
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2023-05-17 14:16:52 +0100

    [SOAR-9680] Komand Snyk Updates (#1772)
    * Update komand -> insight_plugin_runtime
    
    * Update log
    
    * Update exceptions | String formatting | examples
    
    * Black reformat | regen
    
    * Ignore Bandit warning
    
    * Black reformat
    
    * JSON formatting help.md
    
    * Regen
    
    * Update help.md

--- plugins/komand/komand_komand/triggers/job/trigger.py
+++ plugins/komand/komand_komand/triggers/job/trigger.py
@@ -1,9 +1,9 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import JobInput, JobOutput
 import time
 
 
-class Job(komand.Trigger):
+class Job(insightconnect_plugin_runtime.Trigger):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="job", description="Trigger on new jobs", input=JobInput(), output=JobOutput()
@@ -32,7 +32,7 @@ class Job(komand.Trigger):
 
         last_job_id = ""
 
-        self.logger.info("Looking for messages from id=%s with status=%s", self.last_job_id, self.status)
+        self.logger.info(f"Looking for messages from id={self.last_job_id} with status={self.status}")
 
         for job in jobs:
 
