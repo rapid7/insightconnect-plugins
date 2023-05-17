b41cf2d4ffd16a430985af60534b75ad335ec740
hpwilson-r7 <107193020+hpwilson-r7@users.noreply.github.com>
2023-05-17 10:20:54 +0100

    [SOAR-9678] SYNK cuckoo/requirements.txt  (#1770)
    * Changed requests version
    
    * Fixing validation issues
    
    * More validation
    
    * Adding examples to inputs
    
    * Example validation and supported versions
    
    * Fixing help.md
    
    * Fixing validators
    
    * Title Validator fix
    
    * Updating plugin version
    
    * Fixing input
    
    * Adding output examples
    
    * Fixing help.md
    
    * Fixed HelpInputOutputValidator
    
    * Fixed validation issue with input
    
    * Changed version
    
    * Undid title change
    
    Rebasing
    
    * Regenerated

--- plugins/cuckoo/setup.py
+++ plugins/cuckoo/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name='cuckoo-rapid7-plugin',
-      version='1.0.2',
-      description='Cuckoo Sandbox is an open source automated malware analysis system. Using the Cuckoo Sandbox plugin for Rapid7 InsightConnect, users can analyze files and URLs, manage tasks, and more',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="cuckoo-rapid7-plugin",
+      version="1.0.3",
+      description="Cuckoo Sandbox is an open source automated malware analysis system. Using the Cuckoo Sandbox plugin for Rapid7 InsightConnect, users can analyze files and URLs, manage tasks, and more",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
       install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_cuckoo']
