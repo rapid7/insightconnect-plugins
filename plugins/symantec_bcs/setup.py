711a5eae531403ffb113324e3e839402795c33a7
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2023-05-17 15:16:58 +0100

    [SOAR-9697] Symantec BCS Snyk Update (#1774)
    * Update formatting | Exception handling
    
    * Fix static code analysis
    
    * Regen | Sync felp.md
    
    * Black format
    
    * Regen
    
    * Regen
    
    * Fix HTML
    
    * Fix HTML
    
    * Fix validator index error
    
    * Fix validator index error
    
    * Fix validator index error
    
    * Fix validator index error
    
    * Regen
    
    * Regen

--- plugins/symantec_bcs/setup.py
+++ plugins/symantec_bcs/setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 
-setup(name='symantec_bcs-rapid7-plugin',
-      version='1.0.1',
-      description='The Symantec Business Critical Services plugin allows you to submit a file to Symantec Security Response',
-      author='rapid7',
-      author_email='',
-      url='',
+setup(name="symantec_bcs-rapid7-plugin",
+      version="1.0.2",
+      description="The Symantec Business Critical Services plugin allows you to submit a file to Symantec Security Response",
+      author="rapid7",
+      author_email="",
+      url="",
       packages=find_packages(),
-      install_requires=['komand'],  # Add third-party dependencies to requirements.txt, not here!
+      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
       scripts=['bin/komand_symantec_bcs']
       )
