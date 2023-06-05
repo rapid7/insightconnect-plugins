e81e40daed3f13c6503136de63865148434ee3bf
Dympna Laverty <118898375+dlaverty-r7@users.noreply.github.com>
2023-06-05 15:50:57 +0100

    [SOAR-9699] Updated to use latest runtime and fix requests synk vuln (#1833)
    * Updated to use latest runtime and fix requests synk vuln
    
    * Updated Dockerfile to use correct folder name
    
    ---------
    
    Co-authored-by: Dympna Laverty <dympna_laverty@rapid7.com>

--- plugins/threat_connect/komand_threat_connect/actions/bulk_indicator_download/action.py
+++ plugins/threat_connect/komand_threat_connect/actions/bulk_indicator_download/action.py
@@ -1,11 +1,11 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import BulkIndicatorDownloadInput, BulkIndicatorDownloadOutput
 
 # Custom imports below
 import datetime
 
 
-class BulkIndicatorDownload(komand.Action):
+class BulkIndicatorDownload(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="bulk_indicator_download",
