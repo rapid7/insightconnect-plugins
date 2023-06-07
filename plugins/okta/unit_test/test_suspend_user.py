707b7ebcf95bbbb15e7444492c348f7aecfb6a4c
dsliwinski-r7 <dominik_sliwinski@rapid7.com>
2023-06-07 13:42:34 +0200

    [MC-1452] Update unit tests

--- plugins/okta/unit_test/test_suspend_user.py
+++ plugins/okta/unit_test/test_suspend_user.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
 from komand_okta.actions.suspend_user import SuspendUser
-from unit_test.util import Util
+from util import Util
 from unittest.mock import patch
 from parameterized import parameterized
 from insightconnect_plugin_runtime.exceptions import PluginException
