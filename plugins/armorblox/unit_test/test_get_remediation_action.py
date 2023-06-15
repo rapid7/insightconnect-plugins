935ac149475102f85336e24489d9da8b95b0cf7f
SamhithaTatipalli <57620888+SamhithaTatipalli@users.noreply.github.com>
2023-06-15 19:17:58 +0530

    Updated armorblox plugin with the review comments. (#1721)
    * Initial commit for armorblox plugin
    
    * Fix validate errors
    
    * Timestamp changes
    
    * Updated armorblox-sdk 0.1.4 version in requirements.txt
    
    * Updated the suggestions for plugin.spec.yaml
    
    * Update plugins/armorblox/help.md
    
    * Update plugins/armorblox/help.md
    
    * Update plugins/armorblox/help.md
    
    * Update plugins/armorblox/help.md
    
    * Update plugins/armorblox/help.md
    
    * Update plugins/armorblox/help.md
    
    * Update plugins/armorblox/help.md
    
    * Updated review comments
    
    * Update plugins/armorblox/icon_armorblox/triggers/get_incidents/schema.py
    
    * Update plugins/armorblox/unit_test/payloads/get_remediation_action.json
    
    * Update plugins/armorblox/unit_test/payloads/get_remediation_action.json
    
    * Updated armorblox plugin with the required fixes
    
    * Updated plugin with latest fixes
    
    * Updated plugin with parameterized on test cases
    
    * "Fixed review comments"
    
    * Updated support field to community in plugin.spec.yaml
    
    ---------
    
    Co-authored-by: Ankita Sharma <ankita.sharma@xoriant.com>
    Co-authored-by: Rajat Upadhyaya <45485+urajat@users.noreply.github.com>

---
+++ plugins/armorblox/unit_test/test_get_remediation_action.py
@@ -0,0 +1,24 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../'))
+
+from unittest import TestCase
+from icon_armorblox.connection.connection import Connection
+from icon_armorblox.actions.get_remediation_action import GetRemediationAction
+from icon_armorblox.actions.get_remediation_action.schema import Input, Output
+import json
+import logging
+from unit_test.util import Util
+from unittest.mock import patch
+from parameterized import parameterized
+
+
+@patch("requests.get", side_effect=Util.mocked_requests)
+class TestGetIndicatorDetails(TestCase):
+    def setUp(self) -> None:
+        self.action = Util.default_connector(GetRemediationAction())
+    @parameterized.expand([("10597"),("11081"),("11063")])
+    def test_get_remediation_action(self, mock_post: Mock, incident_id: str) -> None:
+        actual = self.action.run({Input.INCIDENT_ID: incident_id})
+        expected = {'remediation_details': 'WILL_AUTO_REMEDIATE'}
+        self.assertEqual(actual, expected)
