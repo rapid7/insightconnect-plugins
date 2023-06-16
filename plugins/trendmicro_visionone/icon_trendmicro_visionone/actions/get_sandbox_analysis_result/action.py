0311e96f783dbf98eb92be7e817921d5be793c7c
pedro-securo <131287358+pedro-securo@users.noreply.github.com>
2023-06-16 10:16:08 +0200

    Trend Micro Vision One - Rapid7 InsightConnect Plugin (#1728)
    * initial commit
    
    * v1.0.0 after make regen
    
    * specfile missing examples added
    
    * after make regen
    
    * list examples and help regen
    
    * after make regen
    
    * make validate resolved
    
    * new examples generate help.md
    
    * output examples added to help.md
    
    * unit tests & plugin exceptions added
    
    * added tests
    
    * Docker USER to nobody
    
    * minor fixes
    
    * after make regenerate
    
    * array without enum
    
    * after make regenerate
    
    * pytmv1 version bump
    
    * untrack tests folder
    
    * sdk version bump
    
    * pytmv1 version bump
    
    * replace bad dummy key
    
    * after make regenerate
    
    * pytmv1 version bump
    
    * requested changes 18.05.2023
    
    * after make regenerate
    
    * all requested changes as of 25.5.2023
    
    * delete unnecessary comments
    
    * requested changes as of 31.5.2023
    
    * after make regen
    
    * avoid trigger duplicates
    
    * after make regen
    
    * add guid param usability
    
    * after make regen
    
    * clean help.md
    
    * minor type-o fix
    
    * after make regen
    
    * requested changes as of 15.6.2023

---
+++ plugins/trendmicro_visionone/icon_trendmicro_visionone/actions/get_sandbox_analysis_result/action.py
@@ -0,0 +1,57 @@
+import insightconnect_plugin_runtime
+from .schema import (
+    GetSandboxAnalysisResultInput,
+    GetSandboxAnalysisResultOutput,
+    Input,
+    Output,
+    Component,
+)
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+# Custom imports below
+
+
+class GetSandboxAnalysisResult(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="get_sandbox_analysis_result",
+            description=Component.DESCRIPTION,
+            input=GetSandboxAnalysisResultInput(),
+            output=GetSandboxAnalysisResultOutput(),
+        )
+
+    def run(self, params={}):
+        # Get Connection Client
+        client = self.connection.client
+        # Get Action Parameters
+        report_id = params.get(Input.REPORT_ID)
+        poll = params.get(Input.POLL)
+        poll_time_sec = params.get(Input.POLL_TIME_SEC)
+        # Make Action API Call
+        self.logger.info("Making API Call...")
+        response = client.get_sandbox_analysis_result(
+            submit_id=report_id,
+            poll=poll,
+            poll_time_sec=poll_time_sec,
+        )
+        if "error" in response.result_code.lower():
+            raise PluginException(
+                cause="An error occurred while getting the sandbox analysis result.",
+                assistance="Please check the report ID and try again.",
+                data=response,
+            )
+        # Return results
+        self.logger.info("Returning Results...")
+        return {
+            Output.ID: response.response.dict().get("id", ""),
+            Output.TYPE: response.response.dict().get("type", ""),
+            Output.DIGEST: response.response.dict().get("digest", {}),
+            Output.ANALYSIS_COMPLETION_DATE_TIME: response.response.dict().get(
+                "analysis_completion_date_time", ""
+            ),
+            Output.ARGUMENTS: response.response.dict().get("arguments", ""),
+            Output.DETECTION_NAMES: response.response.dict().get("detection_names", []),
+            Output.RISK_LEVEL: response.response.dict().get("risk_level", ""),
+            Output.THREAT_TYPES: response.response.dict().get("threat_types", []),
+            Output.TRUE_FILE_TYPE: response.response.dict().get("true_file_type", ""),
+        }
