a894ab635f6307f5c2cbf246c2204b52e5797192
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-06-21 14:05:11 +0200

    Servicenow - 222 - Updated the trigger Vulnerability Updated (#1819)

--- plugins/servicenow/unit_test/test_get_attachments_for_an_incident.py
+++ plugins/servicenow/unit_test/test_get_attachments_for_an_incident.py
@@ -10,7 +10,7 @@ from icon_servicenow.actions.get_attachments_for_an_incident import GetAttachmen
 from icon_servicenow.actions.get_attachments_for_an_incident.schema import GetAttachmentsForAnIncidentOutput, Input
 from jsonschema import validate
 
-from unit_test.util import Util
+from util import Util
 
 
 class TestGetAttachmentsForAnIncident(TestCase):
