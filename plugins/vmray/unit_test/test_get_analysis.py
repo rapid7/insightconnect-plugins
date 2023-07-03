43e74791c92d3831bf2f4a535d1e74a58c15cc34
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2023-07-03 12:05:32 +0100

    [PLGN-217] VMRay Analysis VTI Built In Rules Version Type Change (#1851)
    * Update to InsightConnect Plugin runtime. Update schema results | Update help.md examples and plugin.spec.yaml examples
    
    * Begin updating examples plugin spec
    
    * Regen | Update Examples | Update connection errors
    
    * Fix Exceptions | Begin Unit Test adding
    
    * Add unit tests get analysis
    
    * Add unit tests | Add default values for input {}
    
    * Remove unused param | Updated outputs | Added requirement parameterized
    
    * Remove logging | Update example input
    
    * New line
    
    * Update requirements | Update constant use | Raise exception on filetype
    
    * Update retrieval
    
    * Black submit file action

---
+++ plugins/vmray/unit_test/test_get_analysis.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_vmray.actions.get_analysis import GetAnalysis
+from util import Util
+from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
+from unittest import mock
+import logging
+
+
+class TestGetAnalysis(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(GetAnalysis())
+
+    @parameterized.expand(Util.load_data("get_analysis", "expected").get("parameters"))
+    @mock.patch("requests.Session.send", side_effect=Util.mocked_requests)
+    def test_get_analysis_unit(self, name, _input, expected, mock_request):
+        result = self.action.run(_input)
+        self.assertEqual(expected, result)
+
+    @parameterized.expand(Util.load_data("get_analysis_errors", "expected").get("parameters"))
+    @mock.patch("requests.Session.send", side_effect=Util.mocked_requests)
+    def test_get_analysis_unit_errors(self, name, _input, expected, mock_request):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(_input)
+        self.assertEqual(expected["cause"], error.exception.cause)
+        self.assertEqual(expected["assistance"], error.exception.assistance)
+        self.assertEqual(expected["data"], error.exception.data)
---
+++ plugins/vmray/unit_test/test_get_samples.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_vmray.actions.get_samples import GetSamples
+from util import Util
+from parameterized import parameterized
+from unittest import mock
+import logging
+
+
+class TestGetSamples(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(GetSamples())
+
+    @parameterized.expand(Util.load_data("get_samples", "expected").get("parameters"))
+    @mock.patch("requests.Session.send", side_effect=Util.mocked_requests)
+    def test_get_samples_unit(self, name, _input, expected, mock_request):
+        result = self.action.run(_input)
+        self.assertEqual(expected, result)
