392096d5a9e4e82234289c7814a08afa47270ae7
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-07-05 14:33:02 +0200

    run black linter, fix validators problem, fix tests (#1858)

--- plugins/trendmicro_visionone/unit_test/test_delete_email_message.py
+++ plugins/trendmicro_visionone/unit_test/test_delete_email_message.py
@@ -1,14 +1,17 @@
 from unittest import TestCase
 from unittest.mock import MagicMock
-from .mock import mock_connection, mock_action, mock_params
+
 from insightconnect_plugin_runtime.exceptions import PluginException
+
+from icon_trendmicro_visionone.actions import DeleteEmailMessage
+from mock import mock_connection, mock_params
 
 
 class TestDeleteEmailMessage(TestCase):
     def setUp(self):
-        self.action_name = "DeleteEmailMessage"
+        self.action = DeleteEmailMessage()
         self.connection = mock_connection()
-        self.action = mock_action(self.connection, self.action_name)
+        self.action.connection = self.connection
         self.mock_params = mock_params("delete_email_message")
 
     def test_integration_delete_email_message(self):
@@ -24,8 +27,6 @@ class TestDeleteEmailMessage(TestCase):
             self.assertIn(key, str(expected_result.keys()))
 
     def test_delete_email_message_failure(self):
-        self.action.connection.client.delete_email_message = MagicMock(
-            side_effect=PluginException
-        )
+        self.action.connection.client.delete_email_message = MagicMock(side_effect=PluginException)
         with self.assertRaises(PluginException):
             self.action.run(self.mock_params["input"])
--- plugins/trendmicro_visionone/unit_test/test_submit_urls_to_sandbox.py
+++ plugins/trendmicro_visionone/unit_test/test_submit_urls_to_sandbox.py
@@ -1,14 +1,17 @@
 from unittest import TestCase
 from unittest.mock import MagicMock
-from .mock import mock_connection, mock_action, mock_params
+
 from insightconnect_plugin_runtime.exceptions import PluginException
+
+from icon_trendmicro_visionone.actions import SubmitUrlsToSandbox
+from mock import mock_connection, mock_params
 
 
 class TestSubmitUrlsToSandbox(TestCase):
     def setUp(self):
-        self.action_name = "SubmitUrlsToSandbox"
+        self.action = SubmitUrlsToSandbox()
         self.connection = mock_connection()
-        self.action = mock_action(self.connection, self.action_name)
+        self.action.connection = self.connection
         self.mock_params = mock_params("submit_urls_to_sandbox")
 
     def test_integration_submit_urls_to_sandbox(self):
@@ -24,8 +27,6 @@ class TestSubmitUrlsToSandbox(TestCase):
             self.assertIn(key, str(expected_result.keys()))
 
     def test_submit_urls_to_sandbox_failure(self):
-        self.action.connection.client.submit_urls_to_sandbox = MagicMock(
-            side_effect=PluginException
-        )
+        self.action.connection.client.submit_urls_to_sandbox = MagicMock(side_effect=PluginException)
         with self.assertRaises(PluginException):
             self.action.run(self.mock_params["input"])
