aac71875a7f9bc0575c78d666c773dcd6cbf5bb5
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-07-05 14:59:01 +0200

    run black linter, fix unit tests and validators (#1857)

--- plugins/armorblox/unit_test/util.py
+++ plugins/armorblox/unit_test/util.py
@@ -12,13 +12,12 @@ class Util:
         default_connection.logger = logging.getLogger("connection logger")
         params = {
             Input.API_KEY: {"api_key": {"secretKey": ""}},
-            Input.TENANT_NAME : "tenant_name",
+            Input.TENANT_NAME: "tenant_name",
         }
         default_connection.connect(params)
         action.connection = default_connection
         action.logger = logging.getLogger("action logger")
         return action
-
 
     @staticmethod
     def mocked_requests(*args, **kwargs):
@@ -28,12 +27,11 @@ class Util:
                 self.status_code = status_code
 
             def json(self):
-                f = open(os.path.join(
-                            os.path.dirname(os.path.realpath(__file__)), f"payloads/{self.filename}.json"
-                        ))
+                f = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), f"payloads/{self.filename}.json"))
                 result = json.load(f)
                 f.close()
                 return result
+
         if args[0] == "https://tenant_name.armorblox.io/api/v1beta1/organizations/tenant_name/incidents/10597":
             return MockResponse("get_remediation_action", 200)
         elif args[0] == "https://tenant_name.armorblox.io/api/v1beta1/organizations/tenant_name/incidents/11081":
