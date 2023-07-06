7eba1ec78f76411598fb08d620aaca59744b3e6b
Dympna Laverty <118898375+dlaverty-r7@users.noreply.github.com>
2023-07-06 15:50:05 +0100

    [MC-1453] Update Duo Admin plugin (#1818) (#1862)
    * [PLGN-133] Samanage/Solarwinds updates (#1623)
    
    * Updated docker file to include curl for adding attachment
    
    * Updated examples for help.md to include strings for ids
    
    * Updated output ids to strings for consistency
    
    * Updated attach incident example
    
    * Removed print statement
    
    * Updated formatting with black check
    
    ---------
    
    
    
    * [PLGN-133] Solarwinds - updated id outputs as integers to line up with expected inputs (#1624)
    
    * Updated to output ids as integers to line up with expected inputs
    
    * Updated example fields
    
    * Updated example fields
    
    ---------
    
    
    
    * [PLGN-46] - Cybereason - Archive Sensor Functionality (#1566)
    
    * added get_sensor action
    
    * fixed checksum
    
    * sensor stats added acc
    
    * finished spec file, regenerated
    
    * generated archive sensor action
    
    * added example output for archive sensor
    
    * refactoring for lint
    
    * updated descriptions in spec file for validator
    
    * created and tested 2 new actions, need to add unit tests
    
    * update spec file
    
    * refactored quarantine action, added unit tests for most of the actions
    
    * added unit tests 95 percent coverage
    
    * fix for plugin validator
    
    * fix lint
    
    * prospector fix
    
    * unit test fix
    
    * dympna change
    
    * igor updates
    
    * lukasz changes
    
    * formatting help.md to use 2 spaces instead of 4
    
    * updated version history note
    
    * Cybereason output fix (#1727)
    
    * updated output
    
    * updated output for cybereaosn
    
    * regen
    
    * version bump
    
    * specific fake email
    
    * fixed schema issue
    
    * changing back to rapid7 vendor
    
    * version bump in helpmd
    
    * [MC-1452] Update Okta plugin (#1733)
    
    * [MC-1452] Update Okta plugin
    
    * [MC-1452] Fixed test
    
    * [MC-1452] Fix prospector
    
    * [MC-1452] Update help.md
    
    * [MC-1452] Update SDK image to latest version
    
    * [MC-1452] Update unit tests for task
    
    ---------
    
    
    
    * Servicenow - 222 - Added new actions: Create Vulnerability, Get Vulnerability, Update Vulnerability, Delete Vulnerability | Added new trigger: Vulnerability Updated (#1744)
    
    * Servicenow - 222 - Updated the vulnerability source from Application Vulnerability Item to Vulnerability Item (#1775)
    
    * Servicenow - 222 - Updated the input fields for Create Vulnerability and Update Vulnerability (#1785)
    
    * Servicenow - 222 - Updated the state input field for Update Vulnerability action (#1786)
    
    * IC fixes (#1792)
    
    * [MC-1452] Return status code in Monitor Logs task in Okta plugin (#1764)
    
    * [MC-1452] Return status code in Monitor Logs task
    
    * [MC-1452] Update exceptions in api.py
    
    * [MC-1452] Fix prospector
    
    * [MC-1452] Use logger instance
    
    * Change port input back to string (#1809)
    
    * [MC-1452] Set Okta plugin as cloud enabled (#1816)
    
    * [MC-1452] Set Okta plugin as cloud enabled
    
    * [MC-1452] Set enable_cache to false in plugin spec
    
    * [MC-1452] Update help.md
    
    * [MC-1452] Update pagination in Monitor Logs task in Okta plugin (#1838)
    
    * [MC-1452] Update pagination in Monitor Logs task in Okta plugin
    
    * [MC-1452] Update unit tests
    
    * [MC-1452] Add connection_version field in plugin spec (#1841)
    
    * [MC-1453] Update Duo Admin plugin
    
    * [MC-1453] Fix prospector
    
    * [MC-1453] Update unit tests
    
    * [MC-1453] Update pagination in Monitor Logs task
    
    * [MC-1453] Update unit tests
    
    * [MC-1453] Update exceptions
    
    * [MC-1453] Update output examples in help.md
    
    ---------
    
    Co-authored-by: dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
    Co-authored-by: Dympna Laverty <dympna_laverty@rapid7.com>
    Co-authored-by: TKuzas-R7 <107193006+TKuzas-R7@users.noreply.github.com>
    Co-authored-by: Cyprian Dankowski <cyprian_dankowski@rapid7.com>
    Co-authored-by: igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
    Co-authored-by: Conor <93926445+cmcnally-r7@users.noreply.github.com>

---
+++ plugins/duo_admin/unit_test/test_get_user_status.py
@@ -0,0 +1,48 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_duo_admin.actions.get_user_status import GetUserStatus
+from util import Util
+from unittest.mock import patch
+from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
+from komand_duo_admin.util.constants import Cause, Assistance
+
+
+@patch("requests.request", side_effect=Util.mock_request)
+class TestGetUserStatus(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(GetUserStatus())
+
+    @parameterized.expand(
+        [
+            [
+                "user_found",
+                Util.read_file_to_dict("inputs/get_user_status.json.inp"),
+                Util.read_file_to_dict("expected/get_user_status.json.exp"),
+            ],
+        ]
+    )
+    def test_get_user_status(self, mock_request, test_name, input_params, expected):
+        actual = self.action.run(input_params)
+        self.assertDictEqual(actual, expected)
+
+    @parameterized.expand(
+        [
+            [
+                "user_not_found",
+                Util.read_file_to_dict("inputs/get_user_status_invalid_username.json.inp"),
+                Cause.USER_NOT_FOUND,
+                Assistance.USER_NOT_FOUND,
+            ]
+        ]
+    )
+    def test_get_user_status_raise_plugin_exception(self, mock_request, test_name, input_parameters, cause, assistance):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(input_parameters)
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
