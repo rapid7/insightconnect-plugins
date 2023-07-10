31ca6055b6762e48added2a08bb0c8b66e6fa696
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-07-10 12:48:28 +0200

    SentinelOne - 243 - Added new action: Quarantine Multiple (#1864)

---
+++ plugins/sentinelone/unit_test/test_quarantine_multiple.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from komand_sentinelone.actions.quarantine_multiple import QuarantineMultiple
+from komand_sentinelone.actions.quarantine_multiple.schema import Input, Output
+
+from util import Util
+
+
+class TestQuarantineMultiple(TestCase):
+    @classmethod
+    @patch("requests.post", side_effect=Util.mocked_requests_get)
+    def setUpClass(cls, mock_request: Mock) -> None:
+        cls.action = Util.default_connector(QuarantineMultiple())
+
+    @patch("requests.request", side_effect=Util.mocked_requests_get)
+    @patch("requests.get", side_effect=Util.mocked_requests_get)
+    def test_should_succeed(self, mock_request: Mock, mock_get: Mock) -> None:
+        expected = {
+            Output.COMPLETED: ["hostname123", "hostname456", "hostname789"],
+            Output.FAILURES: [],
+        }
+        actual = self.action.run(
+            {Input.AGENTS: ["hostname123", "hostname456", "hostname789"], Input.QUARANTINE_STATE: True}
+        )
+        self.assertEqual(expected, actual)
