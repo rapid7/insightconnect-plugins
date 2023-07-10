ac1f2f2c7c11693cde29dc9c5b495a40afcb7b69
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-07-10 12:48:28 +0200

    SentinelOne - 243 - Added new action: Quarantine Multiple (#1864)

--- plugins/sentinelone/unit_test/test_mitigate_threat.py
+++ plugins/sentinelone/unit_test/test_mitigate_threat.py
@@ -1,13 +1,15 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.path.abspath("../"))
 
+from unittest import TestCase
 from unittest.mock import patch
+
 from komand_sentinelone.actions.mitigate_threat import MitigateThreat
 from komand_sentinelone.actions.mitigate_threat.schema import Input
-from unit_test.util import Util
-from unittest import TestCase
+
+from util import Util
 
 
 class TestMitigateThreat(TestCase):
