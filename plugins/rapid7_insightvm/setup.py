b5446dbaef9533a307ac9a6e8fb79b4625e46b3d
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2023-07-24 13:27:24 +0200

    [MC-1498] Add new input in Scan action in InsightVM plugin (#1884)
    * [MC-1498] Add new input in Scan action in InsightVM plugin
    
    * [MC-1498] Update description

--- plugins/rapid7_insightvm/setup.py
+++ plugins/rapid7_insightvm/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="rapid7_insightvm-rapid7-plugin",
-      version="6.0.0",
+      version="6.1.0",
       description="InsightVM is a powerful vulnerability management tool which finds, prioritizes, and remediates vulnerabilities. This plugin uses an orchestrator to get top remediations, scan results and start scans",
       author="rapid7",
       author_email="",
