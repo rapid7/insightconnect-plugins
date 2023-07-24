01b1c2105aebdb63e26d513984f81546434b549d
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2023-07-24 13:27:24 +0200

    [MC-1498] Add new input in Scan action in InsightVM plugin (#1884)
    * [MC-1498] Add new input in Scan action in InsightVM plugin
    
    * [MC-1498] Update description

--- plugins/rapid7_insightvm/komand_rapid7_insightvm/actions/scan/action.py
+++ plugins/rapid7_insightvm/komand_rapid7_insightvm/actions/scan/action.py
@@ -1,5 +1,5 @@
 import insightconnect_plugin_runtime
-from .schema import ScanInput, ScanOutput
+from .schema import ScanInput, ScanOutput, Input, Output, Component
 
 # Custom imports below
 from komand_rapid7_insightvm.util import endpoints
@@ -10,7 +10,7 @@ class Scan(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="scan",
-            description="Start a scan on a site",
+            description=Component.DESCRIPTION,
             input=ScanInput(),
             output=ScanOutput(),
         )
@@ -18,16 +18,16 @@ class Scan(insightconnect_plugin_runtime.Action):
     def run(self, params={}):
         resource_helper = ResourceRequests(self.connection.session, self.logger)
 
-        site_id = params.get("site_id")
-        hosts = params.get("hosts")
-        endpoint = endpoints.Scan.site_scans(self.connection.console_url, site_id)
+        hosts = params.get(Input.HOSTS)
+        endpoint = endpoints.Scan.site_scans(self.connection.console_url, params.get(Input.SITE_ID))
 
         self.logger.info(f"Using {endpoint}")
 
-        if hosts:
-            payload = {"hosts": hosts}
-            response = resource_helper.resource_request(endpoint=endpoint, method="post", payload=payload)
-        else:
-            response = resource_helper.resource_request(endpoint=endpoint, method="post")
+        response = resource_helper.resource_request(
+            endpoint=endpoint,
+            method="post",
+            payload={"hosts": hosts} if hosts else {},
+            params={"overrideBlackout": params.get(Input.OVERRIDE_BLACKOUT)},
+        )
 
-        return response
+        return {Output.ID: response.get("id"), Output.LINKS: response.get("links", [])}
