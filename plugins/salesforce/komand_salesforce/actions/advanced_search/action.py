6ae070355fc7ac345ca3cd55b765de4f7ef77842
dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>
2023-07-31 10:24:46 +0200

    [MC-1441] Update Salesforce plugin (#1865)
    * [MC-1441] Update Salesforce plugin
    
    * [MC-1441] Add Monitor Users task
    
    * [MC-1441] Fix prospector
    
    * [MC-1441] Add additional try/except in Monitor Users task

--- plugins/salesforce/komand_salesforce/actions/advanced_search/action.py
+++ plugins/salesforce/komand_salesforce/actions/advanced_search/action.py
@@ -1,33 +1,31 @@
-import komand
-from .schema import AdvancedSearchInput, AdvancedSearchOutput
+import insightconnect_plugin_runtime
+from .schema import AdvancedSearchInput, AdvancedSearchOutput, Input, Output, Component
 
 # Custom imports below
-from komand.helper import clean
+from komand_salesforce.util.helpers import clean
 
 
-class AdvancedSearch(komand.Action):
+class AdvancedSearch(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="advanced_search",
-            description="Execute a SOQL (Salesforce Object Query Language) query",
+            description=Component.DESCRIPTION,
             input=AdvancedSearchInput(),
             output=AdvancedSearchOutput(),
         )
 
     def run(self, params={}):
-        query = params.get("query")
-        results = self.connection.api.advanced_search(query)
-
+        results = self.connection.api.advanced_search(params.get(Input.QUERY))
         flat_results = []
 
         for result in results:
-            flat_result = {
-                "type": result.get("attributes", {}).get("type", ""),
-                "url": result.get("attributes", {}).get("url", ""),
-                "name": result.get("Name"),
-                "id": result.get("Id"),
-            }
-            flat_result = clean(flat_result)
-            flat_results.append(flat_result)
+            flat_results.append(
+                {
+                    "type": result.get("attributes", {}).get("type", ""),
+                    "url": result.get("attributes", {}).get("url", ""),
+                    "name": result.get("Name"),
+                    "id": result.get("Id"),
+                }
+            )
 
-        return {"search_results": flat_results}
+        return {Output.SEARCHRESULTS: clean(flat_results)}
--- plugins/salesforce/komand_salesforce/actions/get_blob_data/action.py
+++ plugins/salesforce/komand_salesforce/actions/get_blob_data/action.py
@@ -1,38 +1,34 @@
-import komand
-from .schema import GetBlobDataInput, GetBlobDataOutput
+import insightconnect_plugin_runtime
+from .schema import GetBlobDataInput, GetBlobDataOutput, Input, Output, Component
 
 # Custom imports below
 from base64 import b64encode
 from binascii import Error as B64EncodingError
-from komand.exceptions import ConnectionTestException
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 
-class GetBlobData(komand.Action):
+class GetBlobData(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="get_blob_data",
-            description="Retrieve blob data for a given record",
+            description=Component.DESCRIPTION,
             input=GetBlobDataInput(),
             output=GetBlobDataOutput(),
         )
 
     def run(self, params={}):
-        record_id = params.get("record_id")
-        object_name = params.get("object_name", "Attachment")
-        field_name = params.get("field_name", "body")
-
-        api_data = self.connection.api.get_blob_data(record_id, object_name, field_name)
-
+        api_data = self.connection.api.get_blob_data(
+            params.get(Input.RECORDID), params.get(Input.OBJECTNAME), params.get(Input.FIELDNAME)
+        )
         try:
             data = b64encode(api_data).decode()
         except (B64EncodingError, UnicodeDecodeError):
-            message = "Incorrect data format received from API: {}".format(api_data)
-            self.logger.error("Get Blob Data: " + message)
-            raise ConnectionTestException(
+            message = f"Incorrect data format received from API: {api_data}"
+            self.logger.error(f"Get Blob Data: {message}")
+            raise PluginException(
                 cause=message,
-                assistance="Please make sure that you are using a correct "
-                + "object type (not all of them allow for binary data). Also, "
-                + "check if the binary data has been set for a given record",
+                assistance="Please make sure that you are using a correct object type (not all of them allow for binary"
+                " data). Also, check if the binary data has been set for a given record",
             )
 
-        return {"data": data}
+        return {Output.DATA: data}
