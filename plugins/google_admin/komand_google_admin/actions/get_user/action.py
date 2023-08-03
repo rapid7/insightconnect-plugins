71df116e085c26843dbc54ea27dc1e1636583ee9
TKuzas-R7 <tkuzas@rapid7.com>
2023-08-03 10:24:02 +0100

    revert lint changes

--- plugins/google_admin/komand_google_admin/actions/get_user/action.py
+++ plugins/google_admin/komand_google_admin/actions/get_user/action.py
@@ -1,6 +1,5 @@
 import komand
-from .schema import GetUserInput, GetUserOutput, Input, Output, Component
-
+from .schema import GetUserInput, GetUserOutput
 
 # Custom imports below
 
@@ -8,12 +7,13 @@ from .schema import GetUserInput, GetUserOutput, Input, Output, Component
 class GetUser(komand.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
-            name="get_user",
-            description=Component.DESCRIPTION,
-            input=GetUserInput(),
-            output=GetUserOutput()
+            name="get_user", description="Get a User", input=GetUserInput(), output=GetUserOutput()
         )
 
     def run(self, params={}):
-        user = self.connection.service.users().get(userKey=params.get(Input.USER)).execute()
-        return {Output.USER: user, Output.FOUND: (not not user)}
+        user = self.connection.service.users().get(userKey=params["user"]).execute()
+        return {"user": user, "found": (not not user)}
+
+    def test(self):
+        # TODO: Implement test function
+        return {}
--- plugins/google_admin/komand_google_admin/actions/suspend_user/action.py
+++ plugins/google_admin/komand_google_admin/actions/suspend_user/action.py
@@ -1,6 +1,5 @@
 import komand
-from .schema import SuspendUserInput, SuspendUserOutput, Input, Output, Component
-
+from .schema import SuspendUserInput, SuspendUserOutput
 
 # Custom imports below
 
@@ -9,12 +8,16 @@ class SuspendUser(komand.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="suspend_user",
-            description=Component.DESCRIPTION,
+            description="Suspend a User",
             input=SuspendUserInput(),
             output=SuspendUserOutput(),
         )
 
     def run(self, params={}):
         body = {"suspended": True}
-        user = self.connection.service.users().update(userKey=params.get(Input.USER), body=body).execute()
-        return {Output.USER: user}
+        user = self.connection.service.users().update(userKey=params["user"], body=body).execute()
+        return {"user": user}
+
+    def test(self):
+        # TODO: Implement test function
+        return {}
