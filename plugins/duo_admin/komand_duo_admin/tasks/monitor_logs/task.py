28a6a73b6ded09676ff0c7596fad48aa2ae7a0af
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-08-09 12:07:10 +0200

    Duo 4.1.0 release (#1914)
    * plgn-307 duo admin update sdk (#1888)
    
    * plgn-307 duo admin update sdk, remove rate limiting for tasks (#1900)

--- plugins/duo_admin/komand_duo_admin/tasks/monitor_logs/task.py
+++ plugins/duo_admin/komand_duo_admin/tasks/monitor_logs/task.py
@@ -1,4 +1,6 @@
 import insightconnect_plugin_runtime
+from insightconnect_plugin_runtime.exceptions import PluginException
+
 from .schema import MonitorLogsInput, MonitorLogsOutput, MonitorLogsState, Component
 
 # Custom imports below
@@ -30,102 +32,106 @@ class MonitorLogs(insightconnect_plugin_runtime.Task):
 
     # pylint: disable=unused-argument
     def run(self, params={}, state={}):  # noqa: C901
-        now = self.get_current_time()
-        last_minute = now - timedelta(minutes=1)
-        last_collection_timestamp = state.get(self.LAST_COLLECTION_TIMESTAMP)
-
+        self.connection.admin_api.toggle_rate_limiting = False
         has_more_pages = False
         get_next_page = False
-        trust_monitor_next_page_params = state.get(self.TRUST_MONITOR_NEXT_PAGE_PARAMS)
-        auth_logs_next_page_params = state.get(self.AUTH_LOGS_NEXT_PAGE_PARAMS)
-        admin_logs_next_page_params = state.get(self.ADMIN_LOGS_NEXT_PAGE_PARAMS)
-
-        if not last_collection_timestamp:
-            self.logger.info("First run")
-            last_24_hours = now - timedelta(hours=24)
-            mintime_in_milliseconds = self.convert_to_milliseconds(last_24_hours)
-            maxtime_in_milliseconds = self.convert_to_milliseconds(last_minute)
-            mintime_in_seconds = self.convert_to_seconds(last_24_hours)
-            maxtime_in_seconds = self.convert_to_seconds(last_minute)
-        else:
-            if trust_monitor_next_page_params or auth_logs_next_page_params or admin_logs_next_page_params:
-                self.logger.info("Getting the next page of results...")
-                get_next_page = True
-            else:
-                self.logger.info("Subsequent run")
-            mintime_in_milliseconds = last_collection_timestamp
-            maxtime_in_milliseconds = self.convert_to_milliseconds(last_minute)
-            mintime_in_seconds = int(mintime_in_milliseconds / 1000)
-            maxtime_in_seconds = int(maxtime_in_milliseconds / 1000)
         try:
-            new_logs = []
-            if not get_next_page:
-                state[self.LAST_COLLECTION_TIMESTAMP] = maxtime_in_milliseconds
+            now = self.get_current_time()
+            last_minute = now - timedelta(minutes=1)
+            last_collection_timestamp = state.get(self.LAST_COLLECTION_TIMESTAMP)
+            trust_monitor_next_page_params = state.get(self.TRUST_MONITOR_NEXT_PAGE_PARAMS)
+            auth_logs_next_page_params = state.get(self.AUTH_LOGS_NEXT_PAGE_PARAMS)
+            admin_logs_next_page_params = state.get(self.ADMIN_LOGS_NEXT_PAGE_PARAMS)
 
-            previous_trust_monitor_event_hashes = state.get(self.PREVIOUS_TRUST_MONITOR_EVENT_HASHES, [])
-            previous_admin_log_hashes = state.get(self.PREVIOUS_ADMIN_LOG_HASHES, [])
-            previous_auth_log_hashes = state.get(self.PREVIOUS_AUTH_LOG_HASHES, [])
-            new_trust_monitor_event_hashes = []
-            new_admin_log_hashes = []
-            new_auth_log_hashes = []
+            if not last_collection_timestamp:
+                self.logger.info("First run")
+                last_24_hours = now - timedelta(hours=24)
+                mintime_in_milliseconds = self.convert_to_milliseconds(last_24_hours)
+                maxtime_in_milliseconds = self.convert_to_milliseconds(last_minute)
+                mintime_in_seconds = self.convert_to_seconds(last_24_hours)
+                maxtime_in_seconds = self.convert_to_seconds(last_minute)
+            else:
+                if trust_monitor_next_page_params or auth_logs_next_page_params or admin_logs_next_page_params:
+                    self.logger.info("Getting the next page of results...")
+                    get_next_page = True
+                else:
+                    self.logger.info("Subsequent run")
+                mintime_in_milliseconds = last_collection_timestamp
+                maxtime_in_milliseconds = self.convert_to_milliseconds(last_minute)
+                mintime_in_seconds = int(mintime_in_milliseconds / 1000)
+                maxtime_in_seconds = int(maxtime_in_milliseconds / 1000)
+            try:
+                new_logs = []
+                if not get_next_page:
+                    state[self.LAST_COLLECTION_TIMESTAMP] = maxtime_in_milliseconds
 
-            if (get_next_page and trust_monitor_next_page_params) or not get_next_page:
-                trust_monitor_events, trust_monitor_next_page_params = self.get_trust_monitor_event(
-                    mintime_in_milliseconds, maxtime_in_milliseconds, trust_monitor_next_page_params
+                previous_trust_monitor_event_hashes = state.get(self.PREVIOUS_TRUST_MONITOR_EVENT_HASHES, [])
+                previous_admin_log_hashes = state.get(self.PREVIOUS_ADMIN_LOG_HASHES, [])
+                previous_auth_log_hashes = state.get(self.PREVIOUS_AUTH_LOG_HASHES, [])
+                new_trust_monitor_event_hashes = []
+                new_admin_log_hashes = []
+                new_auth_log_hashes = []
+
+                if (get_next_page and trust_monitor_next_page_params) or not get_next_page:
+                    trust_monitor_events, trust_monitor_next_page_params = self.get_trust_monitor_event(
+                        mintime_in_milliseconds, maxtime_in_milliseconds, trust_monitor_next_page_params
+                    )
+                    new_trust_monitor_events, new_trust_monitor_event_hashes = self.compare_hashes(
+                        previous_trust_monitor_event_hashes, trust_monitor_events
+                    )
+                    new_logs.extend(new_trust_monitor_events)
+                if (get_next_page and admin_logs_next_page_params) or not get_next_page:
+                    admin_logs, admin_logs_next_page_params = self.get_admin_logs(
+                        mintime_in_seconds, maxtime_in_seconds, admin_logs_next_page_params
+                    )
+                    new_admin_logs, new_admin_log_hashes = self.compare_hashes(previous_admin_log_hashes, admin_logs)
+                    new_logs.extend(new_admin_logs)
+                if (get_next_page and auth_logs_next_page_params) or not get_next_page:
+                    auth_logs, auth_logs_next_page_params = self.get_auth_logs(
+                        mintime_in_milliseconds, maxtime_in_milliseconds, auth_logs_next_page_params
+                    )
+                    new_auth_logs, new_auth_log_hashes = self.compare_hashes(previous_auth_log_hashes, auth_logs)
+                    new_logs.extend(new_auth_logs)
+
+                state[self.PREVIOUS_TRUST_MONITOR_EVENT_HASHES] = (
+                    previous_trust_monitor_event_hashes
+                    if not new_trust_monitor_event_hashes and get_next_page
+                    else new_trust_monitor_event_hashes
                 )
-                new_trust_monitor_events, new_trust_monitor_event_hashes = self.compare_hashes(
-                    previous_trust_monitor_event_hashes, trust_monitor_events
+                state[self.PREVIOUS_ADMIN_LOG_HASHES] = (
+                    previous_admin_log_hashes if not new_admin_log_hashes and get_next_page else new_admin_log_hashes
                 )
-                new_logs.extend(new_trust_monitor_events)
-            if (get_next_page and admin_logs_next_page_params) or not get_next_page:
-                admin_logs, admin_logs_next_page_params = self.get_admin_logs(
-                    mintime_in_seconds, maxtime_in_seconds, admin_logs_next_page_params
+                state[self.PREVIOUS_AUTH_LOG_HASHES] = (
+                    previous_auth_log_hashes if not new_auth_log_hashes and get_next_page else new_auth_log_hashes
                 )
-                new_admin_logs, new_admin_log_hashes = self.compare_hashes(previous_admin_log_hashes, admin_logs)
-                new_logs.extend(new_admin_logs)
-            if (get_next_page and auth_logs_next_page_params) or not get_next_page:
-                auth_logs, auth_logs_next_page_params = self.get_auth_logs(
-                    mintime_in_milliseconds, maxtime_in_milliseconds, auth_logs_next_page_params
-                )
-                new_auth_logs, new_auth_log_hashes = self.compare_hashes(previous_auth_log_hashes, auth_logs)
-                new_logs.extend(new_auth_logs)
 
-            state[self.PREVIOUS_TRUST_MONITOR_EVENT_HASHES] = (
-                previous_trust_monitor_event_hashes
-                if not new_trust_monitor_event_hashes and get_next_page
-                else new_trust_monitor_event_hashes
-            )
-            state[self.PREVIOUS_ADMIN_LOG_HASHES] = (
-                previous_admin_log_hashes if not new_admin_log_hashes and get_next_page else new_admin_log_hashes
-            )
-            state[self.PREVIOUS_AUTH_LOG_HASHES] = (
-                previous_auth_log_hashes if not new_auth_log_hashes and get_next_page else new_auth_log_hashes
-            )
-            state[self.STATUS_CODE] = 200
+                if trust_monitor_next_page_params:
+                    state[self.TRUST_MONITOR_NEXT_PAGE_PARAMS] = trust_monitor_next_page_params
+                    has_more_pages = True
+                elif state.get(self.TRUST_MONITOR_NEXT_PAGE_PARAMS):
+                    state.pop(self.TRUST_MONITOR_NEXT_PAGE_PARAMS)
+                if auth_logs_next_page_params:
+                    state[self.AUTH_LOGS_NEXT_PAGE_PARAMS] = auth_logs_next_page_params
+                    has_more_pages = True
+                elif state.get(self.AUTH_LOGS_NEXT_PAGE_PARAMS):
+                    state.pop(self.AUTH_LOGS_NEXT_PAGE_PARAMS)
+                if admin_logs_next_page_params:
+                    state[self.ADMIN_LOGS_NEXT_PAGE_PARAMS] = admin_logs_next_page_params
+                    has_more_pages = True
+                elif state.get(self.ADMIN_LOGS_NEXT_PAGE_PARAMS):
+                    state.pop(self.ADMIN_LOGS_NEXT_PAGE_PARAMS)
 
-            if trust_monitor_next_page_params:
-                state[self.TRUST_MONITOR_NEXT_PAGE_PARAMS] = trust_monitor_next_page_params
-                has_more_pages = True
-            elif state.get(self.TRUST_MONITOR_NEXT_PAGE_PARAMS):
-                state.pop(self.TRUST_MONITOR_NEXT_PAGE_PARAMS)
-            if auth_logs_next_page_params:
-                state[self.AUTH_LOGS_NEXT_PAGE_PARAMS] = auth_logs_next_page_params
-                has_more_pages = True
-            elif state.get(self.AUTH_LOGS_NEXT_PAGE_PARAMS):
-                state.pop(self.AUTH_LOGS_NEXT_PAGE_PARAMS)
-            if admin_logs_next_page_params:
-                state[self.ADMIN_LOGS_NEXT_PAGE_PARAMS] = admin_logs_next_page_params
-                has_more_pages = True
-            elif state.get(self.ADMIN_LOGS_NEXT_PAGE_PARAMS):
-                state.pop(self.ADMIN_LOGS_NEXT_PAGE_PARAMS)
-
-            return new_logs, state, has_more_pages
-        except ApiException as error:
-            state[self.STATUS_CODE] = error.status_code
+                return new_logs, state, has_more_pages, 200, None
+            except ApiException as error:
+                state[self.PREVIOUS_TRUST_MONITOR_EVENT_HASHES] = []
+                state[self.PREVIOUS_ADMIN_LOG_HASHES] = []
+                state[self.PREVIOUS_AUTH_LOG_HASHES] = []
+                return [], state, False, error.status_code, error
+        except Exception as error:
             state[self.PREVIOUS_TRUST_MONITOR_EVENT_HASHES] = []
             state[self.PREVIOUS_ADMIN_LOG_HASHES] = []
             state[self.PREVIOUS_AUTH_LOG_HASHES] = []
-            return [], state, False
+            return [], state, has_more_pages, 500, PluginException(preset=PluginException.Preset.UNKNOWN, data=error)
 
     @staticmethod
     def get_current_time():
