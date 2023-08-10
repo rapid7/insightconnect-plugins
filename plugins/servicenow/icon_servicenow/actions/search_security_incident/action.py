98a1e7d15ddaf09354f6d4b0e97c4ca218bb875a
Dympna Laverty <118898375+dlaverty-r7@users.noreply.github.com>
2023-08-10 09:15:29 +0100

    [MC-1502] ServiceNow plugin new actions (#1909) (#1919)
    * [MC-1502] ServiceNow plugin new actions
    
    * [MC-1502] Fix prospector
    
    * [MC-1502] Add connection version in plugin spec
    
    Co-authored-by: dsliwinski-r7 <73613193+dsliwinski-r7@users.noreply.github.com>

---
+++ plugins/servicenow/icon_servicenow/actions/search_security_incident/action.py
@@ -0,0 +1,44 @@
+import insightconnect_plugin_runtime
+from .schema import SearchSecurityIncidentInput, SearchSecurityIncidentOutput, Input, Output, Component
+
+# Custom imports below
+from icon_servicenow.util.security_incident_helper import convert_security_incident_fields
+from insightconnect_plugin_runtime.helper import return_non_empty
+
+
+class SearchSecurityIncident(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="search_security_incident",
+            description=Component.DESCRIPTION,
+            input=SearchSecurityIncidentInput(),
+            output=SearchSecurityIncidentOutput(),
+        )
+
+    def run(self, params={}):
+        search_params = {
+            "sysparm_query": params.get(Input.QUERY),
+            "sysparm_limit": params.get(Input.LIMIT)
+            if params.get(Input.LIMIT) and params.get(Input.LIMIT) > 0
+            else None,
+            "sysparm_offset": params.get(Input.OFFSET)
+            if params.get(Input.OFFSET) and params.get(Input.OFFSET) >= 0
+            else None,
+            "sysparm_fields": params.get(Input.FIELDS),
+        }
+
+        response = (
+            self.connection.request.make_request(
+                endpoint=self.connection.security_incident_url,
+                method="GET",
+                params=return_non_empty(search_params),
+            )
+            .get("resource", {})
+            .get("result", [])
+        )
+
+        return {
+            Output.SECURITY_INCIDENTS: [
+                convert_security_incident_fields(return_non_empty(security_incident)) for security_incident in response
+            ]
+        }
---
+++ plugins/servicenow/unit_test/test_delete_security_incident.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from unittest.mock import patch
+
+from icon_servicenow.actions.delete_security_incident import DeleteSecurityIncident
+from icon_servicenow.actions.delete_security_incident.schema import Input
+
+from util import Util
+from insightconnect_plugin_runtime.exceptions import PluginException
+from parameterized import parameterized
+
+
+@patch("requests.sessions.Session.delete", side_effect=Util.mocked_requests)
+class TestDeleteSecurityIncident(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(DeleteSecurityIncident())
+
+    @parameterized.expand(
+        [
+            [
+                "success",
+                Util.read_file_to_dict("inputs/delete_security_incident_success.json.inp"),
+                Util.read_file_to_dict("expected/delete_security_incident_success.json.exp"),
+            ],
+        ]
+    )
+    def test_delete_security_incident(self, mock_request, test_name, input_params, expected):
+        actual = self.action.run(input_params)
+        self.assertDictEqual(actual, expected)
+
+    @parameterized.expand(
+        [
+            [
+                "invalid_sys_id",
+                Util.read_file_to_dict("inputs/delete_security_incident_invalid_id.json.inp"),
+                "Error in API request to ServiceNow. ",
+                """Status code: 404, Error: {'error': {'message': 'No Record found', 'detail': "Record doesn't exist or ACL restricts the record retrieval"}, 'status': 'failure'}""",
+            ],
+        ]
+    )
+    def test_delete_security_incident_raise_exception(self, mock_request, test_name, input_params, cause, assistance):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(input_params)
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
