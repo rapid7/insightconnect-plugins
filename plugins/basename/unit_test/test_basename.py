3a5eeb91cf49c18d17c62fdb8f18bdb88f8d5802
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2023-08-15 11:49:57 +0100

    [PLGN-53] Basename Update SDK (#1926)
    * Update SDK V 5
    
    * Add unit test
    
    * Fix indentation
    
    * Fix plugin spec formatting | refresh
    
    * Fix plugin spec formatting | refresh
    
    * refresh

---
+++ plugins/basename/unit_test/test_basename.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from komand_basename.actions.basename import Basename
+from komand_basename.actions.basename.schema import Input
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+class TestBasename(TestCase):
+    def test_basename(self):
+        action = Basename()
+        expected = {"basename": "text.txt"}
+        result = action.run({Input.PATH: "example/text.txt"})
+        self.assertEqual(result, expected)
+
+    def test_basename_failure(self):
+        action = Basename()
+        cause = "Unable to find basename."
+        assistance = "Not able to retrieve basename of example/."
+        with self.assertRaises(PluginException) as error:
+            action.run({Input.PATH: "example/"})
+        self.assertEqual(cause, error.exception.cause)
+        self.assertEqual(assistance, error.exception.assistance)
