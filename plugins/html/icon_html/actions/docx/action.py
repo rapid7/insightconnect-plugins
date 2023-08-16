6a490b2733eeccc59fa4577ec4b6ae6900947cc5
abmccann-r7 <Abbie_McCann@rapid7.com>
2023-08-16 12:29:25 +0100

    Changed exception handling

--- plugins/html/icon_html/actions/docx/action.py
+++ plugins/html/icon_html/actions/docx/action.py
@@ -4,7 +4,7 @@ import pypandoc
 import re
 
 from insightconnect_plugin_runtime.exceptions import PluginException
-from .schema import DocxInput, DocxOutput
+from .schema import DocxInput, DocxOutput, Output, Input
 
 
 class Docx(insightconnect_plugin_runtime.Action):
@@ -16,16 +16,19 @@ class Docx(insightconnect_plugin_runtime.Action):
     def run(self, params={}):
         temp_file = "temp_html_2_docx.docx"
         tag_parser = "(?i)<\/?\w+((\s+\w+(\s*=\s*(?:\".*?\"|'.*?'|[^'\">\s]+))?)+\s*|\s*)\/?>"  # noqa: W605
-        tags = re.findall(tag_parser, params.get("doc"))
-        try:
-            if not len(tags):
-                raise PluginException(cause="Run: Invalid input.", assistance="Input must be of type HTML.")
-            pypandoc.convert(params.get("doc"), "docx", outputfile=temp_file, format="html")
-            with open(temp_file, "rb") as output:
-                # Reading the output and sending it in base64
-                return {"docx": base64.b64encode(output.read()).decode("utf-8")}
-        except Exception:
-            return {"error": "Error occurred please try again"}
+        tags = re.findall(tag_parser, params.get(Input.DOC))
 
-    def test(self):
-        return {"test": "test Success"}
+        try:
+            if not tags:
+                raise Exception
+        except Exception:
+            raise PluginException(cause="Run: Invalid input.", assistance="Input must be of type HTML.")
+
+        try:
+            pypandoc.convert(params.get(Input.DOC), Output.DOCX, outputfile=temp_file, format="html")
+        except RuntimeError as error:
+            raise PluginException(cause="Pypandoc Runtime Error: Invalid input format",
+                                  assistance="Check stack trace log", data=error)
+        with open(temp_file, "rb") as output:
+            # Reading the output and sending it in base64
+            return {Output.DOCX: base64.b64encode(output.read()).decode("utf-8")}
