d7ff9e09562a127b3ae160e84ca318759427ba0e
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-08-21 16:00:56 +0200

    ExtractIt - 339 - Fixed issues related to PDF, ODP, ODT, ODF files extractions (#1942)

--- plugins/extractit/unit_test/test_filepath_extractor.py
+++ plugins/extractit/unit_test/test_filepath_extractor.py
@@ -1,16 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from typing import Any, Dict, List
 from unittest import TestCase
+
 from icon_extractit.actions.filepath_extractor import FilepathExtractor
-from icon_extractit.actions.filepath_extractor.schema import Input, Output
-from unit_test.util import Util
+from icon_extractit.actions.filepath_extractor.schema import FilepathExtractorOutput, Input, Output
+from jsonschema import validate
 from parameterized import parameterized
+
+from util import Util
 
 parameters = Util.load_parameters("filepath_extractor").get("parameters")
 
 
 class TestFilepathExtractor(TestCase):
+    def setUp(self) -> None:
+        self.action = FilepathExtractor()
+
     @parameterized.expand(parameters)
-    def test_extract_filepath(self, name, string, file, expected):
-        action = FilepathExtractor()
-        actual = action.run({Input.STR: string, Input.FILE: file})
+    def test_extract_filepath(self, name: str, string: str, file: Dict[str, Any], expected: List[str]) -> None:
+        actual = self.action.run({Input.STR: string, Input.FILE: file})
         expected = {Output.FILEPATHS: expected}
+        validate(actual, FilepathExtractorOutput.schema)
         self.assertEqual(actual, expected)
--- plugins/extractit/unit_test/test_sha1_extractor.py
+++ plugins/extractit/unit_test/test_sha1_extractor.py
@@ -1,16 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from typing import Any, Dict, List
 from unittest import TestCase
+
 from icon_extractit.actions.sha1_extractor import Sha1Extractor
-from icon_extractit.actions.sha1_extractor.schema import Input, Output
-from unit_test.util import Util
+from icon_extractit.actions.sha1_extractor.schema import Input, Output, Sha1ExtractorOutput
+from jsonschema import validate
 from parameterized import parameterized
+
+from util import Util
 
 parameters = Util.load_parameters("sha1_extractor").get("parameters")
 
 
 class TestSha1Extractor(TestCase):
+    def setUp(self) -> None:
+        self.action = Sha1Extractor()
+
     @parameterized.expand(parameters)
-    def test_extract_sha1(self, name, string, file, expected):
-        action = Sha1Extractor()
-        actual = action.run({Input.STR: string, Input.FILE: file})
+    def test_extract_sha1(self, name: str, string: str, file: Dict[str, Any], expected: List[str]) -> None:
+        actual = self.action.run({Input.STR: string, Input.FILE: file})
         expected = {Output.SHA1: expected}
+        validate(actual, Sha1ExtractorOutput.schema)
         self.assertEqual(actual, expected)
