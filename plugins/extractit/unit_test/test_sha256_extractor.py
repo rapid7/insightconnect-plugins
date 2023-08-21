757ab537ed97ee1fe25cb85462cc311272fc211e
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-08-21 16:00:56 +0200

    ExtractIt - 339 - Fixed issues related to PDF, ODP, ODT, ODF files extractions (#1942)

--- plugins/extractit/unit_test/test_sha256_extractor.py
+++ plugins/extractit/unit_test/test_sha256_extractor.py
@@ -1,16 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from typing import Any, Dict, List
 from unittest import TestCase
+
 from icon_extractit.actions.sha256_extractor import Sha256Extractor
-from icon_extractit.actions.sha256_extractor.schema import Input, Output
-from unit_test.util import Util
+from icon_extractit.actions.sha256_extractor.schema import Input, Output, Sha256ExtractorOutput
+from jsonschema import validate
 from parameterized import parameterized
+
+from util import Util
 
 parameters = Util.load_parameters("sha256_extractor").get("parameters")
 
 
 class TestSha256Extractor(TestCase):
+    def setUp(self) -> None:
+        self.action = Sha256Extractor()
+
     @parameterized.expand(parameters)
-    def test_extract_sha256(self, name, string, file, expected):
-        action = Sha256Extractor()
-        actual = action.run({Input.STR: string, Input.FILE: file})
+    def test_extract_sha256(self, name: str, string: str, file: Dict[str, Any], expected: List[str]) -> None:
+        actual = self.action.run({Input.STR: string, Input.FILE: file})
         expected = {Output.SHA256: expected}
+        validate(actual, Sha256ExtractorOutput.schema)
         self.assertEqual(actual, expected)
