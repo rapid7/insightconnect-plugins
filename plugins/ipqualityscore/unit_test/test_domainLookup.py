777afa8f82cc073fc259c79e533e95dc3fcb55c5
joneill-r7 <joneill@rapid7.com>
2023-08-22 15:10:01 +0100

    PLGN-347: run black on unit tests.

--- plugins/ipqualityscore/unit_test/test_domainLookup.py
+++ plugins/ipqualityscore/unit_test/test_domainLookup.py
@@ -1,6 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.abspath('../'))
+
+sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
 from icon_ipqualityscore.connection.connection import Connection
