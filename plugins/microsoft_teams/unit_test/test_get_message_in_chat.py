11459f1bcec5598a2e989d894dac84093a90989b
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-08-23 14:57:46 +0200

    plgn-286 microsoft teams update sdk, add new actions (#1949)

---
+++ plugins/microsoft_teams/unit_test/test_get_message_in_chat.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
+from unittest.mock import Mock
+
+from icon_microsoft_teams.actions.get_message_in_chat import GetMessageInChat
+from icon_microsoft_teams.actions.get_message_in_chat.schema import Input
+from util import Util
+from icon_microsoft_teams.util.komand_clean_with_nulls import remove_null_and_clean
+
+
+class TestGetMessageInChat(TestCase):
+    def setUp(self) -> None:
+        self.action = Util.default_connector(GetMessageInChat())
+        self.payload = {
+            Input.USERNAME: "user@example.com",
+            Input.CHAT_ID: "11:examplechat.name",
+            Input.MESSAGE_ID: "1234567890",
+        }
+
+    @mock.patch("requests.get", side_effect=Util.mocked_requests)
+    def test_get_message_in_chat(self, mock: Mock):
+        response = self.action.run(self.payload)
+
+        expected_response = remove_null_and_clean(Util.load_data("get_message_in_chat"))
+        self.assertEqual(response["message"], expected_response)
