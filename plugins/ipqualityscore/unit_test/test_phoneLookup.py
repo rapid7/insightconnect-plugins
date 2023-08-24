7aff68f85cc1c14eecd973f135e97829630b00c3
Johnny O'Neill <139136675+joneill-r7@users.noreply.github.com>
2023-08-24 09:46:56 +0100

    PLGN-347: Validator fixes for vendor plugin submission (#1950)
    * PLGN-347: icon-plugin refresh - fix validation issues.
    
    * PLGN-347: fix more validators, checksum and json.
    
    * PLGN-347: run black on unit tests.
    
    * PLGN-347: further changes required for extension library.
    
    * PLGN-347: PR comment re-run refresh with latest tooling to move imports etc

---
+++ plugins/ipqualityscore/unit_test/test_phoneLookup.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from icon_ipqualityscore.connection.connection import Connection
+from icon_ipqualityscore.actions.phoneLookup import PhoneLookup
+import json
+import logging
+
+
+class TestPhonelookup(TestCase):
+    def test_phoneLookup(self):
+        """
+        DO NOT USE PRODUCTION/SENSITIVE DATA FOR UNIT TESTS
+
+        TODO: Implement test cases here
+        """
+
+        self.fail("Unimplemented Test Case")
