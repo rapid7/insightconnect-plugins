bd88d5ab74240db8d97a791e2a31dae723561ae2
Peter Kuik <peter_kuik@trendmicro.com>
2023-08-29 16:11:22 +0200

    avoid None values

--- plugins/trendmicro_visionone/icon_trendmicro_visionone/actions/get_task_result/action.py
+++ plugins/trendmicro_visionone/icon_trendmicro_visionone/actions/get_task_result/action.py
@@ -68,37 +68,36 @@ class GetTaskResult(insightconnect_plugin_runtime.Action):
                 assistance="Please check the task ID and try again.",
                 data=response,
             )
+        # Avoid None value
+        response_dict = response.response.dict()
+        for key, value in response_dict.items():
+            if value is None:
+                response_dict[key] = "None"
         # Return results
         self.logger.info("Returning Results...")
         return {
-            Output.ACTION: response.response.dict().get("action", ""),
-            Output.ACCOUNT: response.response.dict().get("account", ""),
-            Output.CREATED_DATE_TIME: response.response.dict().get(
-                "created_date_time", ""
-            ),
-            Output.AGENT_GUID: response.response.dict().get("agent_guid", ""),
-            Output.ID: response.response.dict().get("id", ""),
-            Output.LAST_ACTION_DATE_TIME: response.response.dict().get(
+            Output.ACTION: response_dict.get("action", ""),
+            Output.ACCOUNT: response_dict.get("account", ""),
+            Output.CREATED_DATE_TIME: response_dict.get("created_date_time", ""),
+            Output.AGENT_GUID: response_dict.get("agent_guid", ""),
+            Output.ID: response_dict.get("id", ""),
+            Output.LAST_ACTION_DATE_TIME: response_dict.get(
                 "last_action_date_time", ""
             ),
-            Output.RESOURCE_LOCATION: response.response.dict().get(
-                "resource_location", ""
-            ),
-            Output.STATUS: response.response.dict().get("status", ""),
-            Output.DESCRIPTION: response.response.dict().get("description", ""),
-            Output.ENDPOINT_NAME: response.response.dict().get("endpoint_name", ""),
-            Output.EXPIRED_DATE_TIME: response.response.dict().get(
-                "expired_date_time", ""
-            ),
-            Output.FILE_PATH: response.response.dict().get("file_path", ""),
-            Output.FILE_SHA1: response.response.dict().get("file_sha1", ""),
-            Output.FILE_SHA256: response.response.dict().get("file_sha256", ""),
-            Output.FILE_SIZE: response.response.dict().get("file_size", 0),
-            Output.FILENAME: response.response.dict().get("filename", ""),
-            Output.IMAGE_PATH: response.response.dict().get("image_path", ""),
-            Output.PASSWORD: response.response.dict().get("password", ""),
-            Output.PID: response.response.dict().get("pid", 0),
-            Output.SANDBOX_TASK_ID: response.response.dict().get("sandbox_task_id", ""),
-            Output.TASKS: response.response.dict().get("tasks", []),
-            Output.URL: response.response.dict().get("url", ""),
+            Output.RESOURCE_LOCATION: response_dict.get("resource_location", ""),
+            Output.STATUS: response_dict.get("status", ""),
+            Output.DESCRIPTION: response_dict.get("description", ""),
+            Output.ENDPOINT_NAME: response_dict.get("endpoint_name", ""),
+            Output.EXPIRED_DATE_TIME: response_dict.get("expired_date_time", ""),
+            Output.FILE_PATH: response_dict.get("file_path", ""),
+            Output.FILE_SHA1: response_dict.get("file_sha1", ""),
+            Output.FILE_SHA256: response_dict.get("file_sha256", ""),
+            Output.FILE_SIZE: response_dict.get("file_size", 0),
+            Output.FILENAME: response_dict.get("filename", ""),
+            Output.IMAGE_PATH: response_dict.get("image_path", ""),
+            Output.PASSWORD: response_dict.get("password", ""),
+            Output.PID: response_dict.get("pid", 0),
+            Output.SANDBOX_TASK_ID: response_dict.get("sandbox_task_id", ""),
+            Output.TASKS: response_dict.get("tasks", []),
+            Output.URL: response_dict.get("url", ""),
         }
