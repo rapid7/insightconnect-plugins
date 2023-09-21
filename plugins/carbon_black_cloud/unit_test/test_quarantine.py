e4c99a983acc045377784aaee38382deeceb9a95
igorski-r7 <igor_gorski@rapid7.com>
2023-09-21 16:45:44 +0200

    Carbon Black Cloud - 386 - Updated the SDK version | Cloud enabled

--- plugins/carbon_black_cloud/unit_test/test_quarantine.py
+++ plugins/carbon_black_cloud/unit_test/test_quarantine.py
@@ -1,40 +1,60 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
-from icon_carbon_black_cloud.connection.connection import Connection
+from unittest.mock import MagicMock, patch
+
 from icon_carbon_black_cloud.actions.quarantine import Quarantine
-import json
-import logging
+from icon_carbon_black_cloud.actions.quarantine.schema import Input, Output, QuarantineOutput
+from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
+from parameterized import parameterized
+
+from util import (
+    Util,
+    mock_request_200,
+    mock_request_400,
+    mock_request_401,
+    mock_request_403,
+    mock_request_404,
+    mock_request_409,
+    mock_request_503,
+    mocked_request,
+)
+
+STUB_AGENT_ID = "192.168.0.1"
 
 
 class TestQuarantine(TestCase):
-    def test_integration_quarantine(self):
-        log = logging.getLogger("Test")
-        test_conn = Connection()
-        test_action = Quarantine()
+    def setUp(self) -> None:
+        self.action = Util.default_connector(Quarantine())
+        self.payload = {Input.AGENT: STUB_AGENT_ID, Input.WHITELIST: [], Input.QUARANTINE_STATE: False}
 
-        test_conn.logger = log
-        test_action.logger = log
+    @patch("requests.post", side_effect=mock_request_200)
+    def test_quarantine(self, mocked_post: MagicMock) -> None:
+        response = self.action.run(self.payload)
+        expected = {Output.QUARANTINED: False}
+        validate(response, QuarantineOutput.schema)
+        self.assertEqual(response, expected)
+        mocked_post.assert_called()
 
-        try:
-            with open("../tests/quarantine.json") as file:
-                test_json = json.loads(file.read()).get("body")
-                connection_params = test_json.get("connection")
-                action_params = test_json.get("input")
-        except Exception as e:
-            message = """
-            Could not find or read sample tests from /tests directory
-            
-            An exception here likely means you didn't fill out your samples correctly in the /tests directory 
-            Please use 'icon-plugin generate samples', and fill out the resulting test files in the /tests directory
-            """
-            self.fail(message)
-
-        test_conn.connect(connection_params)
-        test_action.connection = test_conn
-        results = test_action.run(action_params)
-
-        self.assertIn("quarantined", results.keys())
+    @parameterized.expand(
+        [
+            (mock_request_400, "400 Bad Request"),
+            (mock_request_401, "Authentication Error"),
+            (mock_request_403, "The specified object cannot be accessed or changed."),
+            (mock_request_404, "The object referenced in the request cannot be found."),
+            (mock_request_409, "Either the name you chose already exists, or there is an unacceptable character used."),
+            (mock_request_503, PluginException.causes[PluginException.Preset.UNKNOWN]),
+        ],
+    )
+    def test_quarantine_exception(self, mock_request: MagicMock, exception: str) -> None:
+        mocked_request(mock_request)
+        with self.assertRaises(PluginException) as context:
+            self.action.run(self.payload)
+        self.assertEqual(
+            context.exception.cause,
+            exception,
+        )
