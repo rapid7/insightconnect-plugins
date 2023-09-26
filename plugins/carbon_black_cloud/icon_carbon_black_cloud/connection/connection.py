30eaf12deaee683b0c187845722c902f27d9d8b0
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-09-26 11:43:58 +0200

    Carbon Black Cloud - 386 - Updated the SDK version | Cloud enabled (#1988)

--- plugins/carbon_black_cloud/icon_carbon_black_cloud/connection/connection.py
+++ plugins/carbon_black_cloud/icon_carbon_black_cloud/connection/connection.py
@@ -5,8 +5,8 @@ from .schema import ConnectionSchema, Input
 import requests
 from insightconnect_plugin_runtime.exceptions import PluginException, ConnectionTestException
 import time
-import icon_carbon_black_cloud.util.agent_typer as agent_typer
-import urllib.parse
+from icon_carbon_black_cloud.util import agent_typer
+from icon_carbon_black_cloud.util.constants import DEFAULT_TIMEOUT
 
 
 class Connection(insightconnect_plugin_runtime.Connection):
@@ -14,15 +14,10 @@ class Connection(insightconnect_plugin_runtime.Connection):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
     def connect(self, params):
-        self.api_id = params.get(Input.API_ID)
-        self.api_secret = params.get(Input.API_SECRET_KEY).get("secretKey")
-
-        base_url = params.get(Input.URL)
-        self.base_url = urllib.parse.urlparse(base_url).hostname
-        self.base_url = f"https://{self.base_url}"
-
-        self.org_key = params.get(Input.ORG_KEY)
-
+        self.api_id = params.get(Input.API_ID, "")
+        self.api_secret = params.get(Input.API_SECRET_KEY, {}).get("secretKey", "")
+        self.org_key = params.get(Input.ORG_KEY, "")
+        self.base_url = f"https://{params.get(Input.URL, '')}"
         self.x_auth_token = f"{self.api_secret}/{self.api_id}"
         self.headers = {"X-Auth-Token": self.x_auth_token}
 
@@ -38,28 +33,32 @@ class Connection(insightconnect_plugin_runtime.Connection):
 
         device = None
         if agent_type == agent_typer.DEVICE_ID:
-            device = next((x for x in results if str(x.get("id", "")) == agent), None)
+            device = next((element for element in results if str(element.get("id", "")) == agent), None)
         if agent_type == agent_typer.IP_ADDRESS:
             device = next(
-                (x for x in results if x.get("last_internal_ip_address") == agent or x.get("last_external_ip_address")),
+                (
+                    element
+                    for element in results
+                    if element.get("last_internal_ip_address") == agent or element.get("last_external_ip_address")
+                ),
                 None,
             )
         if agent_type == agent_typer.HOSTNAME:
-            device = next((x for x in results if x.get("name", "") == agent), None)
+            device = next((element for element in results if element.get("name", "") == agent), None)
         if agent_typer == agent_typer.MAC_ADDRESS:
-            device = next((x for x in results if x.get("mac_address", "") == agent), None)
+            device = next((element for element in results if element.get("mac_address", "") == agent), None)
 
         if not device:
             self.logger.error(f"Could not find any device that matched {agent}")
 
         return device
 
-    def post_to_api(self, url, payload, retry=True):
-        result = requests.post(url, headers=self.headers, json=payload)
+    def post_to_api(self, url, payload, retry=True):  # noqa: MC0001
+        result = requests.post(url, headers=self.headers, json=payload, timeout=DEFAULT_TIMEOUT)
 
         try:
             result.raise_for_status()
-        except Exception as e:
+        except Exception as error:
             if result.status_code == 400:
                 raise PluginException(
                     cause="400 Bad Request",
@@ -96,9 +95,9 @@ class Connection(insightconnect_plugin_runtime.Connection):
                     return self.post_to_api(url, payload, False)
 
                 self.logger.error("Retry on 503 failed.")
-                self.logger.error(str(e))
+                self.logger.error(str(error))
                 self.logger.error(result.text)
-                raise PluginException(PluginException.Preset.UNKNOWN)
+                raise PluginException(preset=PluginException.Preset.UNKNOWN)
 
         if result.status_code != 204:
             return result.json()
@@ -110,14 +109,13 @@ class Connection(insightconnect_plugin_runtime.Connection):
         endpoint = self.base_url + device_endpoint
 
         self.logger.info(endpoint)
-        result = requests.get(endpoint, headers=self.headers)
+        result = requests.get(endpoint, headers=self.headers, timeout=DEFAULT_TIMEOUT)
         try:
             result.raise_for_status()
-        except Exception as e:
+        except Exception as error:
             raise ConnectionTestException(
                 cause="Connection test to Carbon Black Cloud failed.\n",
                 assistance=f"{result.text}\n",
-                data=str(e),
+                data=str(error),
             )
-
         return {"success": True}
