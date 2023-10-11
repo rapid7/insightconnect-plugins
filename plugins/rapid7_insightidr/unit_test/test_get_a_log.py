804add5d2b03c4810032038562296fc4286f1493
conor-mcnally <conor_mcnally@rapid7.com>
2023-10-11 11:19:40 +0100

    Black and some helpmd fixes

--- plugins/rapid7_insightidr/unit_test/test_get_a_log.py
+++ plugins/rapid7_insightidr/unit_test/test_get_a_log.py
@@ -1,6 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.abspath('../'))
+
+sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
 from komand_rapid7_insightidr.connection.connection import Connection
