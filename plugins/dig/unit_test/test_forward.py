69ea39b32d7cb5b00c464123195ef257c044202b
abmccann-r7 <Abbie_McCann@rapid7.com>
2023-10-26 08:53:49 +0100

    unit test

--- plugins/dig/unit_test/test_forward.py
+++ plugins/dig/unit_test/test_forward.py
@@ -2,34 +2,37 @@ import sys
 import os
 sys.path.append(os.path.abspath('../'))
 
-from unittest import TestCase
 from komand_dig.connection.connection import Connection
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+from util import Util
 from komand_dig.actions.forward import Forward
-import json
-import logging
 
 
+@patch("insightconnect_plugin_runtime.helper.exec_command", side_effect=Util.mock_dig)
 class TestForward(TestCase):
-    def test_forward(self):
-        params = {
-            "domain": "rapid7.com",
-            "resolver": "8.8.8.8",
-            "query": "MX"
-        }
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(Forward())
 
-        test_action = Forward()
-        result = test_action.run(params)
+    @parameterized.expand(
+        [
+            [
+                "forward",
+                Util.read_file_to_dict("inputs/forward.json.inp"),
+                Util.read_file_to_dict("expected/forward.json.exp")
 
-        self.assertEqual(result, {})
+            ]
+        ]
+    )
+    def test_forward(self, _mock_request: MagicMock, _test_name: str, input_params: dict, expected: dict):
 
-    def test_error(self):
-        params = {
-            "domain": "..com",
-            "resolver": "8.8.8",
-            "query": "p"
-        }
+        actual = self.action.run(input_params)
+        self.assertEqual(actual, expected)
 
-        test_action = Forward()
-        result = test_action.run(params)
-
-        self.assertEqual(result, {})
+    # def test_error(self, _mock_request: MagicMock, _test_name: str, input_params: dict, cause: str, assistance: str):
+    #
+    #     actual = self.action.run(input_params)
+    #     self.assertEqual(error.exception.cause, cause)
+    #     self.assertEqual(error.exception.assistance, assistance)
