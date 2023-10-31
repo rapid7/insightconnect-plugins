574ea3f7675acf714adb4696c8e0896ccac3a17f
rbowden-r7 <144030336+rbowden-r7@users.noreply.github.com>
2023-10-31 10:50:51 +0000

    [PLGN-362]- Pager Duty - Refactor to use requests rather than pypd (#2033)
    * PLGN-362-Updating pager duty to use http rather than pypd
    
    * PLGN-362-Reducing complexity of get_on_calls method and send_request method
    
    * PLGN-362-Adding parameterized to requirements.txt to allow unit tests to now run on git
    
    * PLGN-362-Running black formatter on get_on_call and util/api
    
    * PLGN-362-Updating to use correct input and output objects, also removing redunant checks for required fields
    
    * PLGN-362-Updating to use newest version of error messages from insightconnect_plugin_runtime.exceptions
    
    * PLGN-362-Removing redunant required check
    
    * PLGN-362-Running black formatter on get_on_call and util/api
    
    * PLGN-362-Adding docstrings, using the input object in the connection class
    
    * PLGN-362-Running black formatter on get_on_call and util/api
    
    * PLGN-362-Updating docstrings to use sphinx format
    
    * PLGN-362-Updating examples to user real fake data
    
    * PLGN-362-Updating black format
    
    * PLGN-362-Making sure return for delete is of type string
    
    * PLGN-362-Making sure return for delete is of type string
    
    * PLGN-362-Fixing typos / tidying up code
    
    * PLGN-362-Adding back in get user by email action + tests
    
    * PLGN-362-using black format
    
    * PLGN-362-adding back in  prefix to eah actions description

--- plugins/pagerduty/komand_pagerduty/actions/get_on_call/action.py
+++ plugins/pagerduty/komand_pagerduty/actions/get_on_call/action.py
@@ -3,7 +3,7 @@ from .schema import GetOnCallInput, GetOnCallOutput, Input, Output, Component
 
 # Custom imports below
 from insightconnect_plugin_runtime.exceptions import PluginException
-import asyncio
+from komand_pagerduty.util.util import normalize_user
 
 
 class GetOnCall(insightconnect_plugin_runtime.Action):
@@ -16,14 +16,9 @@ class GetOnCall(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        schedule_id = params.get(Input.SCHEDULE_ID, None)
-        user_ids = []
-        for oncall_object in self.connection.api.get_on_calls(schedule_id).get("oncalls", []):
-            try:
-                user_ids.append(oncall_object["user"]["id"])
-            except KeyError as e:
-                self.logger.warning(f"User ID not available: {str(e)}")
-                continue
+        schedule_id = params.get(Input.SCHEDULE_ID)
+
+        user_ids = self.get_user_ids(schedule_id=schedule_id)
 
         if not user_ids and schedule_id:
             self.logger.warning(
@@ -31,19 +26,35 @@ class GetOnCall(insightconnect_plugin_runtime.Action):
                 "Please make sure that the schedule used is correct."
             )
 
-        users = asyncio.run(self.async_get_users(user_ids))
-        return {Output.USERS: insightconnect_plugin_runtime.helper.clean(users)}
+        list_of_users = self.get_list_of_users_from_ids(user_ids=user_ids)
 
-    async def async_get_users(self, user_ids: [str]) -> list:
-        connection = self.connection.async_connection
-        async with connection.get_async_session() as async_session:
-            tasks: [asyncio.Future] = []
-            for user_id in user_ids:
-                url = f"https://api.pagerduty.com/users/{user_id}"
-                tasks.append(
-                    asyncio.ensure_future(connection.async_request(session=async_session, url=url, method="get"))
-                )
-            user_objects = await asyncio.gather(*tasks)
-            users = [u.get("user") for u in user_objects]
+        return {Output.USERS: list_of_users}
 
-            return users
+    def get_user_ids(self, schedule_id: str):
+        user_ids = []
+        for oncall_object in self.connection.api.get_on_calls(schedule_id).get("schedule", {}).get("users", []):
+            try:
+                if oncall_object.get("deleted_at", None):
+                    self.logger.info(oncall_object)
+                    self.logger.warning(
+                        f"The following user {oncall_object.get('id')} is part of the schedule but has been deleted"
+                    )
+                else:
+                    user_ids.append(oncall_object["id"])
+            except KeyError as error:
+                self.logger.warning(f"User ID not available: {str(error)}")
+                continue
+        return user_ids
+
+    def get_list_of_users_from_ids(self, user_ids: list):
+        list_of_users = []
+        for user_id in user_ids:
+            try:
+                user_info = self.connection.api.get_user_by_id(user_id).get("user", {})
+            except Exception:
+                self.logger.warning(f"No information was found for the user - {user_id}")
+                continue
+
+            if user_info:
+                list_of_users.append(normalize_user(user_info))
+        return list_of_users
