0d4d6288c77a6c39a6c6376d2af1d2844e47b5c5
rbowden-r7 <144030336+rbowden-r7@users.noreply.github.com>
2023-10-31 10:50:51 +0000

    [PLGN-362]- Pager Duty - Refactor to use requests rather than pypd (#2033)
    * PLGN-362-Updating pager duty to use http rather than pypd
    
    * PLGN-362-Reducing complexity of get_on_calls method and send_request method
    
    * PLGN-362-Adding parameterized to requirements.txt to allow unit tests to now run on git
    
    * PLGN-362-Running black formatter on get_on_call and util/api
    
    * PLGN-362-Updating to use correct input and output objects, also removing redunant checks for required fields
    
    * PLGN-362-Updating to use newest version of error messages from insightconnect_plugin_runtime.exceptions
    
    * PLGN-362-Removing redunant required check
    
    * PLGN-362-Running black formatter on get_on_call and util/api
    
    * PLGN-362-Adding docstrings, using the input object in the connection class
    
    * PLGN-362-Running black formatter on get_on_call and util/api
    
    * PLGN-362-Updating docstrings to use sphinx format
    
    * PLGN-362-Updating examples to user real fake data
    
    * PLGN-362-Updating black format
    
    * PLGN-362-Making sure return for delete is of type string
    
    * PLGN-362-Making sure return for delete is of type string
    
    * PLGN-362-Fixing typos / tidying up code
    
    * PLGN-362-Adding back in get user by email action + tests
    
    * PLGN-362-using black format
    
    * PLGN-362-adding back in  prefix to eah actions description

--- plugins/pagerduty/komand_pagerduty/actions/get_user_by_email/action.py
+++ plugins/pagerduty/komand_pagerduty/actions/get_user_by_email/action.py
@@ -1,35 +1,28 @@
 import insightconnect_plugin_runtime
-from .schema import GetUserByEmailInput, GetUserByEmailOutput
+from insightconnect_plugin_runtime.exceptions import PluginException
+from .schema import GetUserByEmailInput, GetUserByEmailOutput, Input, Output, Component
 
 # Custom imports below
-import pypd
-from komand_pagerduty.util.util import empty_user, normalize_user
+from komand_pagerduty.util.util import normalize_user
 
 
 class GetUserByEmail(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="get_user_by_email",
-            description="Get a User By Email",
+            description=Component.DESCRIPTION,
             input=GetUserByEmailInput(),
             output=GetUserByEmailOutput(),
         )
 
     def run(self, params={}):
-        """Get a user by email"""
+        user_email = params.get(Input.USER_EMAIL)
 
-        if not params["email"]:
-            raise Exception("Email required")
+        response = self.connection.api.get_user_by_email(user_email=user_email)
 
-        user = pypd.User.find_one(email=params["email"])
-        if user:
-            user = normalize_user(user.json)
-        else:
-            user = None
+        for user in response.get("users", []):
+            if user.get("email", "") == user_email:
+                normalized_user = normalize_user(user)
+                return {Output.USER: normalized_user}
 
-        self.logger.debug("Returned: %s", user)
-        return {"found": bool(user), "user": user or empty_user}
-
-    def test(self):
-        """Test action"""
-        return {"found": False, "user": empty_user}
+        raise PluginException(cause=f"No user found for email {user_email}")
