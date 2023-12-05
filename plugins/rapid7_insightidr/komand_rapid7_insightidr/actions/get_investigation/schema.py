f315c087684432a1f2b1941270cac493d2727ed3
llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
2023-12-05 13:43:42 +0100

    plgn-618 insight idr update schema (#2164)

--- plugins/rapid7_insightidr/komand_rapid7_insightidr/actions/get_investigation/schema.py
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/actions/get_investigation/schema.py
@@ -56,24 +56,6 @@ class GetInvestigationOutput(insightconnect_plugin_runtime.Output):
     "investigation"
   ],
   "definitions": {
-    "assignee": {
-      "type": "object",
-      "title": "assignee",
-      "properties": {
-        "email": {
-          "type": "string",
-          "title": "Email",
-          "description": "The email of the assigned user",
-          "order": 1
-        },
-        "name": {
-          "type": "string",
-          "title": "Name",
-          "description": "The name of the assigned user",
-          "order": 2
-        }
-      }
-    },
     "investigation": {
       "type": "object",
       "title": "investigation",
@@ -150,36 +132,23 @@ class GetInvestigationOutput(insightconnect_plugin_runtime.Output):
           "description": "Investigation title",
           "order": 12
         }
-      },
-      "required": [
-        "created_time",
-        "disposition",
-        "last_accessed",
-        "organization_id",
-        "priority",
-        "rrn",
-        "source",
-        "status",
-        "title"
-      ],
-      "definitions": {
-        "assignee": {
-          "type": "object",
-          "title": "assignee",
-          "properties": {
-            "email": {
-              "type": "string",
-              "title": "Email",
-              "description": "The email of the assigned user",
-              "order": 1
-            },
-            "name": {
-              "type": "string",
-              "title": "Name",
-              "description": "The name of the assigned user",
-              "order": 2
-            }
-          }
+      }
+    },
+    "assignee": {
+      "type": "object",
+      "title": "assignee",
+      "properties": {
+        "email": {
+          "type": "string",
+          "title": "Email",
+          "description": "The email of the assigned user",
+          "order": 1
+        },
+        "name": {
+          "type": "string",
+          "title": "Name",
+          "description": "The name of the assigned user",
+          "order": 2
         }
       }
     }
