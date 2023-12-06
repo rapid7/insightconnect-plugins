f2ad5458f7b9d4ad7b20a2a3e516131f7a75f1d1
conor-mcnally <conor_mcnally@rapid7.com>
2023-12-06 16:00:28 +0000

    Update script

--- tools/update-tools.sh
+++ tools/update-tools.sh
@@ -12,31 +12,25 @@ platform=$(uname)
 
 if [[ "${platform}" == "Darwin" ]]; then
 	echo "[*] Executing update/installation for MacOS!"
-	echo "[*] Installing/updating insight-plugin via PyPi..."
-	pip install insight-plugin > /dev/null; true
-
 	echo "[*] Installing/updating jq via homebrew"
 	brew reinstall jq > /dev/null; true
 
 elif [[ "${platform}" == "Linux" ]] && [[ -f /etc/debian_version ]]; then
 	echo "[*] Executing update/installation for Debian Linux!"
-	curl -s https://packagecloud.io/install/repositories/rapid7/insightconnect_plugin_tooling/script.deb.sh | sudo bash
-	sudo apt-get update
-	sudo apt-get -qq install -y icon-plugin
-
 	echo "[*] Installing/updating jq..."
 	sudo apt-get -qq install -y jq
 
 elif [[ "${platform}" == "Linux" ]] && [[ -f /etc/redhat-release ]]; then
 	echo "[*] Executing update/installation for Red Hat Linux!"
-	curl -s https://packagecloud.io/install/repositories/rapid7/insightconnect_plugin_tooling/script.rpm.sh | sudo bash
-	sudo yum install -q -y icon-plugin
-
 	echo "[*] Installing/updating jq..."
 	sudo yum install -q -y jq
+
 else
-	echo "[!] Unsupported OS found! Unable to install icon-plugin and jq!"
+	echo "[!] Unsupported OS found! Unable to install insight-plugin and jq!"
 fi
+
+echo "[*] Installing/updating insight-plugin via PyPi..."
+sudo -H python3 -m pip install --user --upgrade insight-plugin > /dev/null; true
 
 echo "[*] Installing InsightConnect validator tooling..."
 sudo -H python3 -m pip install --user --upgrade insightconnect-integrations-validators > /dev/null; true
