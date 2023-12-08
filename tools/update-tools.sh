c11d88c8f843b97471762111cf2bab67011ead1f
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2023-12-08 14:38:59 +0000

    [SOAR-14960] Update Install Script (#1924) (#2169)
    * Initial commit
    
    * Added comments on next steps
    
    * Update script
    
    * Move pip install and remove comments
    
    * update loggers

--- tools/update-tools.sh
+++ tools/update-tools.sh
@@ -4,32 +4,25 @@ platform=$(uname)
 
 if [[ "${platform}" == "Darwin" ]]; then
 	echo "[*] Executing update/installation for MacOS!"
-	echo "[*] Installing/updating icon-plugin via homebrew..."
-	brew tap rapid7/icon-plugin-homebrew https://github.com/rapid7/icon-plugin-homebrew > /dev/null
-	brew reinstall icon-plugin > /dev/null; true
-
 	echo "[*] Installing/updating jq via homebrew"
 	brew reinstall jq > /dev/null; true
 
 elif [[ "${platform}" == "Linux" ]] && [[ -f /etc/debian_version ]]; then
 	echo "[*] Executing update/installation for Debian Linux!"
-	curl -s https://packagecloud.io/install/repositories/rapid7/insightconnect_plugin_tooling/script.deb.sh | sudo bash
-	sudo apt-get update
-	sudo apt-get -qq install -y icon-plugin
-
 	echo "[*] Installing/updating jq..."
 	sudo apt-get -qq install -y jq
 
 elif [[ "${platform}" == "Linux" ]] && [[ -f /etc/redhat-release ]]; then
 	echo "[*] Executing update/installation for Red Hat Linux!"
-	curl -s https://packagecloud.io/install/repositories/rapid7/insightconnect_plugin_tooling/script.rpm.sh | sudo bash
-	sudo yum install -q -y icon-plugin
-
 	echo "[*] Installing/updating jq..."
 	sudo yum install -q -y jq
+
 else
-	echo "[!] Unsupported OS found! Unable to install icon-plugin and jq!"
+	echo "[!] Unsupported OS found! Unable to install jq!"
 fi
+
+echo "[*] Installing/updating insight-plugin..."
+sudo -H python3 -m pip install --user --upgrade insight-plugin > /dev/null; true
 
 echo "[*] Installing InsightConnect validator tooling..."
 sudo -H python3 -m pip install --user --upgrade insightconnect-integrations-validators > /dev/null; true
