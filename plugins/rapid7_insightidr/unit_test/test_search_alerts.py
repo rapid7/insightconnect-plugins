3913a243a79aaa46a2da7c2213aed3e02633ec91
rbowden-r7 <144030336+rbowden-r7@users.noreply.github.com>
2023-12-12 11:40:51 +0000

    [PLGN-408] Insight IDR - Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert (#2175)
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Re-adding back in size and index to action, dropping version to make a multiple plugin release, adding in type hints
    
    * PLGN-408-Running black format
    
    * PLGN-408-Updating data to be of type object
    
    * PLGN-408-Updating to use f string
    
    * PLGN-408-Removing debug print
    
    * PLGN-408-Updating error var name

---
+++ plugins/rapid7_insightidr/unit_test/test_search_alerts.py
@@ -0,0 +1,68 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_rapid7_insightidr.actions.search_alerts import SearchAlerts
+from komand_rapid7_insightidr.actions.search_alerts.schema import (
+    Input,
+    SearchAlertsInput,
+    SearchAlertsOutput,
+)
+from util import Util
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
+
+
+@patch("requests.Session.request", side_effect=Util.mocked_requests)
+class TestSearchAlerts(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(SearchAlerts())
+
+    @parameterized.expand(Util.load_parameters("search_alerts").get("parameters"))
+    def test_search_alerts(
+        self,
+        mock_request: MagicMock,
+        start_time: str,
+        end_time: str,
+        leql: str,
+        terms: list,
+        sorts: list,
+        field_ids: list,
+        aggregates: list,
+        size: int,
+        index: int,
+        rrns_only: bool,
+        expected: dict,
+    ) -> None:
+        test_input = {
+            Input.START_TIME: start_time,
+            Input.END_TIME: end_time,
+            Input.LEQL: leql,
+            Input.TERMS: terms,
+            Input.SORTS: sorts,
+            Input.FIELD_IDS: field_ids,
+            Input.AGGREGATES: aggregates,
+            Input.SIZE: size,
+            Input.INDEX: index,
+            Input.RRNS_ONLY: rrns_only,
+        }
+        validate(test_input, SearchAlertsInput.schema)
+        actual = self.action.run(test_input)
+        self.assertEqual(actual, expected)
+        validate(actual, SearchAlertsInput.schema)
+
+    @parameterized.expand(Util.load_parameters("search_alerts_bad_times").get("parameters"))
+    def test_search_alerts_bad_times(
+        self, mock_request: MagicMock, start_time: str, end_time: str, cause: str, assistance: str
+    ) -> None:
+        test_input = {Input.START_TIME: start_time, Input.END_TIME: end_time}
+        validate(test_input, SearchAlertsInput.schema)
+        with self.assertRaises(PluginException) as error:
+            self.action.run(test_input)
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
