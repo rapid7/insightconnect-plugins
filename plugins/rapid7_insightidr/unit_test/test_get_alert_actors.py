c9f10d4ce5c89230a2c96f8c74844a38f50c7279
rbowden-r7 <144030336+rbowden-r7@users.noreply.github.com>
2023-12-12 11:40:51 +0000

    [PLGN-408] Insight IDR - Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert (#2175)
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Re-adding back in size and index to action, dropping version to make a multiple plugin release, adding in type hints
    
    * PLGN-408-Running black format
    
    * PLGN-408-Updating data to be of type object
    
    * PLGN-408-Updating to use f string
    
    * PLGN-408-Removing debug print
    
    * PLGN-408-Updating error var name

---
+++ plugins/rapid7_insightidr/unit_test/test_get_alert_actors.py
@@ -0,0 +1,51 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_rapid7_insightidr.actions.get_alert_actors import GetAlertActors
+from komand_rapid7_insightidr.actions.get_alert_actors.schema import (
+    Input,
+    GetAlertActorsInput,
+    GetAlertActorsOutput,
+)
+from util import Util
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
+
+
+@patch("requests.Session.request", side_effect=Util.mocked_requests)
+class TestGetAlertActors(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(GetAlertActors())
+
+    @parameterized.expand(Util.load_parameters("get_alert_actors_minimum").get("parameters"))
+    def test_get_alert_actors_minimum(self, mock_request: MagicMock, alert_rrn: str, expected: dict) -> None:
+        test_input = {Input.ALERT_RRN: alert_rrn}
+        validate(test_input, GetAlertActorsInput.schema)
+        actual = self.action.run(test_input)
+        self.assertEqual(actual, expected)
+        validate(actual, GetAlertActorsOutput.schema)
+
+    @parameterized.expand(Util.load_parameters("get_alert_actors").get("parameters"))
+    def test_get_alert_actors(
+        self, mock_request: MagicMock, alert_rrn: str, size: int, index: int, expected: dict
+    ) -> None:
+        test_input = {Input.ALERT_RRN: alert_rrn, Input.SIZE: size, Input.INDEX: index}
+        validate(test_input, GetAlertActorsInput.schema)
+        actual = self.action.run(test_input)
+        self.assertEqual(actual, expected)
+        validate(actual, GetAlertActorsOutput.schema)
+
+    @parameterized.expand(Util.load_parameters("get_alert_actors_not_found").get("parameters"))
+    def test_get_alert_actors_bad(self, mock_request: MagicMock, alert_rrn: str, cause: str, assistance: str) -> None:
+        test_input = {Input.ALERT_RRN: alert_rrn}
+        validate(test_input, GetAlertActorsInput.schema)
+        with self.assertRaises(PluginException) as error:
+            self.action.run(test_input)
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
