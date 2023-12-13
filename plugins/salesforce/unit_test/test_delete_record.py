7a515910070d8de1fb2e18b72e5e3ec9b03ec0fd
Johnny O'Neill <139136675+joneill-r7@users.noreply.github.com>
2023-12-13 13:47:22 +0000

    Salesforce - Task Monitor Users: Improved logging (#2170) (#2174)
    Co-authored-by: igorski-r7 <99184344+igorski-r7@users.noreply.github.com>

--- plugins/salesforce/unit_test/test_delete_record.py
+++ plugins/salesforce/unit_test/test_delete_record.py
@@ -1,15 +1,20 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.path.abspath("../"))
 
+from typing import Any, Dict
 from unittest import TestCase
-from komand_salesforce.actions.delete_record.action import DeleteRecord
-from unittest.mock import patch
-from parameterized import parameterized
-from komand_salesforce.util.exceptions import ApiException
-from util import Util
+from unittest.mock import MagicMock, patch
+
 from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
+from komand_salesforce.actions.delete_record.action import DeleteRecord
+from komand_salesforce.actions.delete_record.schema import DeleteRecordOutput
+from komand_salesforce.util.exceptions import ApiException
+from parameterized import parameterized
+
+from util import Util
 
 
 @patch("requests.request", side_effect=Util.mock_request)
@@ -27,8 +32,11 @@ class TestDeleteRecord(TestCase):
             ],
         ]
     )
-    def test_delete_record(self, mock_request, test_name, input_params, expected):
+    def test_delete_record(
+        self, mock_request: MagicMock, test_name: str, input_params: Dict[str, Any], expected: str
+    ) -> None:
         actual = self.action.run(input_params)
+        validate(actual, DeleteRecordOutput.schema)
         self.assertEqual(actual, expected)
 
     @parameterized.expand(
@@ -47,7 +55,9 @@ class TestDeleteRecord(TestCase):
             ],
         ]
     )
-    def test_delete_record_raise_api_exception(self, mock_request, test_name, input_params, cause, assistance):
+    def test_delete_record_raise_api_exception(
+        self, mock_request: MagicMock, test_name: str, input_params: Dict[str, Any], cause: str, assistance: str
+    ) -> None:
         with self.assertRaises(ApiException) as error:
             self.action.run(input_params)
         self.assertEqual(error.exception.cause, cause)
