1ed1ebb5d71ae5a825ba7daf11bc5754b97261d2
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2023-12-13 13:32:39 +0000

    [PLGN-355] Confluence - Update auth and API Library (#2177)
    * Intiial Commit
    
    * Add new actions
    
    * Add unit tests for get actions
    
    * Requires refresh
    
    * Refreshed
    
    * Refreshed
    
    * Commit tests
    
    * Commit tests
    
    * Commit actions
    
    * black format
    
    * Run black | Refresh
    
    * Fix static code analysis | add requirements
    
    * Remove unused param
    
    * Add connection creds
    
    * Fix test connections
    
    * Fix import
    
    * Fix import
    
    * [PLGN-355]- Fixing lint errors
    
    * Update Dockerfile
    
    * Update action.py
    
    * plgn-618 insight idr update schema (#2164)
    
    * [PLGN-408] Insight IDR - Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert (#2175)
    
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Adding new actions for Get Alert Information, Search Alerts, Retrieve Evidence for a Single Alert and Retrieve Actors for a Single Alert
    
    * PLGN-408-Re-adding back in size and index to action, dropping version to make a multiple plugin release, adding in type hints
    
    * PLGN-408-Running black format
    
    * PLGN-408-Updating data to be of type object
    
    * PLGN-408-Updating to use f string
    
    * PLGN-408-Removing debug print
    
    * PLGN-408-Updating error var name
    
    * Run black | Refresh
    
    * Black format
    
    * Suggestions | Refactors
    
    * Suggestions | Refactors
    
    * Remove vendor directory
    
    * Type hints
    
    * Print page id
    
    * Print page id
    
    ---------
    
    Co-authored-by: Jenna Richardson <jenna_richardson@rapid7.com>
    Co-authored-by: llaszuk-r7 <99184394+llaszuk-r7@users.noreply.github.com>
    Co-authored-by: rbowden-r7 <144030336+rbowden-r7@users.noreply.github.com>

--- plugins/confluence/komand_confluence/connection/connection.py
+++ plugins/confluence/komand_confluence/connection/connection.py
@@ -1,11 +1,11 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import ConnectionSchema
 
 # Custom imports below
-from confluence import Confluence
+from ..util.api import ConfluenceAPI
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
@@ -14,8 +14,14 @@ class Connection(komand.Connection):
         Connect to Confluence
         """
         self.logger.info("Connecting to Confluence: %s", params.get("url"))
-        self.client = Confluence(
+        self.client = ConfluenceAPI(
             url=params.get("url"),
-            username=params.get("credentials").get("username"),
-            password=params.get("credentials").get("password"),
+            username=params.get("username"),
+            api_token=params.get("api_token").get("secretKey"),
+            cloud=params.get("cloud"),
         )
+        self.client.login()
+
+    def test(self):
+        self.client.health_check()
+        return {"success": True}
---
+++ plugins/confluence/unit_test/test_store_page_content.py
@@ -0,0 +1,85 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
+from unittest.mock import Mock
+from parameterized import parameterized
+from unittest import TestCase
+from util import Util
+from insightconnect_plugin_runtime.exceptions import PluginException
+from komand_confluence.actions.store_page_content import StorePageContent
+from komand_confluence.actions.store_page_content.schema import (
+    StorePageContentOutput,
+    StorePageContentInput,
+    Input,
+)
+from jsonschema import validate
+
+
+class TestStorePageContent(TestCase):
+    def setUp(self) -> None:
+        self.action = Util.default_connector(StorePageContent())
+
+    @parameterized.expand(
+        [
+            ("Test Page", "Test Space", "<p>Test Content</p>", "store_page_content"),
+            (
+                "Test Page New",
+                "Test Space",
+                "<p>Test Content</p>",
+                "store_page_content_new",
+            ),
+        ]
+    )
+    @mock.patch("atlassian.Confluence.update_page", side_effect=Util.mocked_requests)
+    @mock.patch("atlassian.Confluence.create_page", side_effect=Util.mocked_requests)
+    @mock.patch("atlassian.Confluence.page_exists", side_effect=Util.mocked_requests)
+    @mock.patch("atlassian.Confluence.get_page_id", side_effect=Util.mocked_requests)
+    def test_get_page_content(
+        self,
+        page: str,
+        space: str,
+        content: str,
+        expected_filename: str,
+        mock_update: Mock,
+        mock_create: Mock,
+        mock_exists: Mock,
+        mock_get_id: Mock,
+    ) -> None:
+        test_input = {Input.PAGE: page, Input.SPACE: space, Input.CONTENT: content}
+        validate(test_input, StorePageContentInput.schema)
+        response = self.action.run(test_input)
+        expected = Util.load_data(expected_filename, "expected")
+        self.assertEqual(expected, response)
+        validate(response, StorePageContentOutput.schema)
+
+    @parameterized.expand(
+        [
+            (
+                "Test Page",
+                "Test Space",
+                "<p>Test Content Failure</p>",
+            )
+        ]
+    )
+    @mock.patch("atlassian.Confluence.update_page", side_effect=Util.mocked_requests)
+    @mock.patch("atlassian.Confluence.create_page", side_effect=Util.mocked_requests)
+    @mock.patch("atlassian.Confluence.page_exists", side_effect=Util.mocked_requests)
+    @mock.patch("atlassian.Confluence.get_page_id", side_effect=Util.mocked_requests)
+    def test_get_page_content_failure(
+        self,
+        page: str,
+        space: str,
+        content: str,
+        mock_update: Mock,
+        mock_create: Mock,
+        mock_exists: Mock,
+        mock_get_id: Mock,
+    ) -> None:
+        cause = "Something unexpected occurred. See error log for details."
+        assistance = "Please check your input and connection details."
+        with self.assertRaises(PluginException) as error:
+            self.action.run({Input.PAGE: page, Input.SPACE: space, Input.CONTENT: content})
+        self.assertEqual(cause, error.exception.cause)
