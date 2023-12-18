dc06708132f3eac0c45fee3b158e27f71749f490
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-12-18 12:46:45 +0100

    Recorded Future - 366, 352 - Fixed schemas | Updated the SDK | Refreshed with latest plugin tooling (#2191)

---
+++ plugins/recorded_future/komand_recorded_future/actions/list_ip_addresses_risk_rules/action.py
@@ -0,0 +1,27 @@
+import insightconnect_plugin_runtime
+from .schema import ListIpAddressesRiskRulesInput, ListIpAddressesRiskRulesOutput, Output, Component
+
+# Custom imports below
+from insightconnect_plugin_runtime.exceptions import PluginException
+from komand_recorded_future.util.api import Endpoint
+
+
+class ListIpAddressesRiskRules(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="list_IP_addresses_risk_rules",
+            description=Component.DESCRIPTION,
+            input=ListIpAddressesRiskRulesInput(),
+            output=ListIpAddressesRiskRulesOutput(),
+        )
+
+    def run(self, params={}):
+        # pylint: disable=unused-argument
+        try:
+            return {
+                Output.RISK_RULES: self.connection.client.make_request(Endpoint.list_ip_risk_rules())
+                .get("data", {})
+                .get("results")
+            }
+        except AttributeError as error:
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=error)
