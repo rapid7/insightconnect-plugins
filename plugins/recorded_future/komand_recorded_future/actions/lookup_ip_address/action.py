7c61a1c877c6dabd7fc7a6bca10a523faeab490c
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2023-12-18 12:46:45 +0100

    Recorded Future - 366, 352 - Fixed schemas | Updated the SDK | Refreshed with latest plugin tooling (#2191)

---
+++ plugins/recorded_future/komand_recorded_future/actions/lookup_ip_address/action.py
@@ -0,0 +1,43 @@
+import insightconnect_plugin_runtime
+from .schema import LookupIpAddressInput, LookupIpAddressOutput, Input, Output, Component
+
+# Custom imports below
+from insightconnect_plugin_runtime.exceptions import PluginException
+from komand_recorded_future.util.util import AvailableInputs
+from komand_recorded_future.util.api import Endpoint
+
+
+class LookupIpAddress(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="lookup_IP_address",
+            description=Component.DESCRIPTION,
+            input=LookupIpAddressInput(),
+            output=LookupIpAddressOutput(),
+        )
+
+    def run(self, params={}):
+        comment = params.get(Input.COMMENT)
+        if not comment:
+            comment = None
+
+        try:
+            return {
+                Output.RESULT_FOUND: True,
+                Output.DATA: insightconnect_plugin_runtime.helper.clean(
+                    self.connection.client.make_request(
+                        Endpoint.lookup_ip_address(params.get(Input.IP_ADDRESS)),
+                        {"fields": AvailableInputs.IpFields, "comment": comment},
+                    ).get("data")
+                ),
+            }
+        except AttributeError as error:
+            raise PluginException(
+                cause="Recorded Future returned unexpected response.",
+                assistance="Please check that the provided inputs are correct and try again.",
+                data=error,
+            )
+        except PluginException as error:
+            if "No results found." in error.cause:
+                return {Output.RESULT_FOUND: False, Output.DATA: {}}
+            raise error
