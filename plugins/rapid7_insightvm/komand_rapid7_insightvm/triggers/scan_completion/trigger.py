94afecb3c9087984cd8c100dfe7eb99d5f6ec6b7
conor-mcnally <conor_mcnally@rapid7.com>
2023-12-21 14:38:51 +0000

    Fix scan id input again

--- plugins/rapid7_insightvm/komand_rapid7_insightvm/triggers/scan_completion/trigger.py
+++ plugins/rapid7_insightvm/komand_rapid7_insightvm/triggers/scan_completion/trigger.py
@@ -152,7 +152,7 @@ class ScanQueries:
             f"INNER JOIN dim_solution AS ds ON (dv.nexpose_id = ds.nexpose_id) "
             f"INNER JOIN matching_asset_group_ids AS magi ON (fasvi.asset_id = magi.asset_id) "
             f"LEFT JOIN dim_vulnerability_reference AS dvr ON (fasvi.vulnerability_id = dvr.vulnerability_id) "
-            f"WHERE fasvi.scan_id = 11630 "
+            f"WHERE fasvi.scan_id = {scan_id} "
             f"GROUP BY fasvi.scan_id, fasvi.asset_id, fasvi.vulnerability_id, magi.asset_group_ids, dv.nexpose_id, dv.cvss_v3_score, dvc.category_name, ds.solution_id, ds.summary, dvr.source, dv.severity "
         )
 
