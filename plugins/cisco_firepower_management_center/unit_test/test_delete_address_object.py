f3207693e4cc8787324af58e2aa7bb9d5a44ab2a
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-01-05 12:20:03 +0100

    Cisco Firepower Management Center - 608 - Check if Address in Group: Extended search for manually added literals | Added new output field literal_objects (#2197)

--- plugins/cisco_firepower_management_center/unit_test/test_delete_address_object.py
+++ plugins/cisco_firepower_management_center/unit_test/test_delete_address_object.py
@@ -1,28 +1,38 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.path.abspath("../"))
 
+from typing import Any, Dict
 from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
 from icon_cisco_firepower_management_center.actions.delete_address_object import DeleteAddressObject
 from icon_cisco_firepower_management_center.actions.delete_address_object.schema import Input
-from unit_test.util import Util
-from unittest.mock import patch
+from jsonschema import validate
 from parameterized import parameterized
 
-sys.path.append(os.path.abspath("../"))
+from util import Util
 
 
 @patch("requests.post", side_effect=Util.mocked_requests)
 @patch("requests.request", side_effect=Util.mocked_requests)
-@patch("ssl.SSLSocket.write", side_effect=Util.mock_write)
-@patch("ssl.SSLSocket.connect", side_effect=Util.mock_connect)
-@patch("ssl.SSLSocket.recv", side_effect=Util.mock_recv)
+@patch("ssl.SSLSocket._create", side_effect=Util.MockSSLSocket)
 class TestDeleteAddressObject(TestCase):
     @parameterized.expand(Util.load_parameters("delete_address_object").get("parameters"))
     def test_delete_address_object(
-        self, mock_post, mock_request, mock_write, mock_connect, mock_recv, name, address_object, expected
-    ):
+        self,
+        mock_post: MagicMock,
+        mock_request: MagicMock,
+        mock_create: MagicMock,
+        name: str,
+        address_object: str,
+        expected: Dict[str, Any],
+    ) -> None:
         action = Util.default_connector(DeleteAddressObject())
         actual = action.run({Input.ADDRESS_OBJECT: address_object})
+        validate(actual, action.output.schema)
         self.assertEqual(actual, expected)
+        mock_post.assert_called()
+        mock_request.assert_called()
+        mock_create.assert_called()
