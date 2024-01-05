75d786149ebeb78d3145b91ab3a95bc1e3c55e11
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-01-05 12:20:03 +0100

    Cisco Firepower Management Center - 608 - Check if Address in Group: Extended search for manually added literals | Added new output field literal_objects (#2197)

--- plugins/cisco_firepower_management_center/unit_test/test_create_address_object.py
+++ plugins/cisco_firepower_management_center/unit_test/test_create_address_object.py
@@ -1,38 +1,36 @@
-import sys
 import os
-
-from unittest import TestCase
-from icon_cisco_firepower_management_center.actions.create_address_object import CreateAddressObject
-from icon_cisco_firepower_management_center.actions.create_address_object.schema import Input
-from unit_test.util import Util
-from unittest.mock import patch
-from parameterized import parameterized
-from insightconnect_plugin_runtime.exceptions import PluginException
+import sys
 
 sys.path.append(os.path.abspath("../"))
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from icon_cisco_firepower_management_center.actions.create_address_object import CreateAddressObject
+from icon_cisco_firepower_management_center.actions.create_address_object.schema import Input
+from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
+from parameterized import parameterized
+
+from util import Util
 
 
 @patch("requests.post", side_effect=Util.mocked_requests)
 @patch("requests.request", side_effect=Util.mocked_requests)
-@patch("ssl.SSLSocket.write", side_effect=Util.mock_write)
-@patch("ssl.SSLSocket.connect", side_effect=Util.mock_connect)
-@patch("ssl.SSLSocket.recv", side_effect=Util.mock_recv)
+@patch("ssl.SSLSocket._create", side_effect=Util.MockSSLSocket)
 class TestCreateAddressObject(TestCase):
     @parameterized.expand(Util.load_parameters("create_address_object").get("parameters"))
     def test_create_address_object(
         self,
-        mock_post,
-        mock_request,
-        mock_write,
-        mock_connect,
-        mock_recv,
-        name,
-        address_name,
-        address,
-        skip_private,
-        whitelist,
-        expected,
-    ):
+        mock_post: MagicMock,
+        mock_request: MagicMock,
+        mock_create: MagicMock,
+        name: str,
+        address_name: str,
+        address: str,
+        skip_private: bool,
+        whitelist: str,
+        expected: str,
+    ) -> None:
         action = Util.default_connector(CreateAddressObject())
         actual = action.run(
             {
@@ -42,23 +40,25 @@ class TestCreateAddressObject(TestCase):
                 Input.WHITELIST: whitelist,
             }
         )
+        validate(actual, action.output.schema)
         self.assertEqual(actual, expected)
+        mock_post.assert_called()
+        mock_request.assert_called()
+        mock_create.assert_called()
 
     @parameterized.expand(Util.load_parameters("create_address_object_bad").get("parameters"))
     def test_create_address_object_bad(
         self,
-        mock_post,
-        mock_request,
-        mock_write,
-        mock_connect,
-        mock_recv,
-        name,
-        address_name,
-        address,
-        skip_private,
-        whitelist,
-        cause,
-        assistance,
+        mock_post: MagicMock,
+        mock_request: MagicMock,
+        mock_create: MagicMock,
+        name: str,
+        address_name: str,
+        address: str,
+        skip_private: bool,
+        whitelist: str,
+        cause: str,
+        assistance: str,
     ):
         action = Util.default_connector(CreateAddressObject())
         with self.assertRaises(PluginException) as error:
@@ -72,3 +72,6 @@ class TestCreateAddressObject(TestCase):
             )
         self.assertEqual(error.exception.cause, cause)
         self.assertEqual(error.exception.assistance, assistance)
+        mock_post.assert_called()
+        mock_request.assert_called()
+        mock_create.assert_called()
