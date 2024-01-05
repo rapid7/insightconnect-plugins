b59b8d71d7d93380787aa7f7f8b0df16020bcf3e
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-01-05 12:20:03 +0100

    Cisco Firepower Management Center - 608 - Check if Address in Group: Extended search for manually added literals | Added new output field literal_objects (#2197)

--- plugins/cisco_firepower_management_center/unit_test/test_add_scan_result.py
+++ plugins/cisco_firepower_management_center/unit_test/test_add_scan_result.py
@@ -1,24 +1,28 @@
-import sys
 import os
-
-from unittest import TestCase
-from icon_cisco_firepower_management_center.actions.add_scan_result import AddScanResult
-from icon_cisco_firepower_management_center.actions.add_scan_result.schema import Input
-from unit_test.util import Util
-from unittest.mock import patch
-from insightconnect_plugin_runtime.exceptions import PluginException
+import sys
 
 sys.path.append(os.path.abspath("../"))
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from icon_cisco_firepower_management_center.actions.add_scan_result import AddScanResult
+from icon_cisco_firepower_management_center.actions.add_scan_result.schema import Input
+from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
+
+from util import Util
 
 
-@patch("ssl.SSLSocket.connect", side_effect=Util.mock_connect)
-@patch("ssl.SSLSocket.write", side_effect=Util.mock_write)
-@patch("ssl.SSLSocket.send", side_effect=Util.mock_send)
-@patch("ssl.SSLSocket.recv", side_effect=Util.mock_recv)
 @patch("requests.post", side_effect=Util.mocked_requests)
 @patch("requests.request", side_effect=Util.mocked_requests)
+@patch("ssl.SSLSocket._create", side_effect=Util.MockSSLSocket)
 class TestAddScanResult(TestCase):
-    def test_add_scan_result(self, mock_connect, mock_write, mock_send, mock_recv, mock_post, mock_request):
+    def test_add_scan_result(
+        self,
+        mock_post: MagicMock,
+        mock_request: MagicMock,
+        mock_create: MagicMock,
+    ) -> None:
         action = Util.default_connector(AddScanResult())
         actual = action.run(
             {
@@ -40,9 +44,18 @@ class TestAddScanResult(TestCase):
             }
         )
         expected = {"errors": 0, "commands_processed": 4}
+        validate(actual, action.output.schema)
         self.assertEqual(actual, expected)
+        mock_post.assert_called()
+        mock_request.assert_called()
+        mock_create.assert_called()
 
-    def test_add_scan_result_bad(self, mock_connect, mock_write, mock_send, mock_recv, mock_post, mock_request):
+    def test_add_scan_result_bad(
+        self,
+        mock_post: MagicMock,
+        mock_request: MagicMock,
+        mock_create: MagicMock,
+    ) -> None:
         action = Util.default_connector(AddScanResult())
         with self.assertRaises(PluginException) as error:
             action.run(
@@ -66,3 +79,6 @@ class TestAddScanResult(TestCase):
             )
         self.assertEqual(error.exception.cause, "The provided IP address 999.999.999.999 is invalid.")
         self.assertEqual(error.exception.assistance, "Please provide a valid IP address for the host and try again.")
+        mock_post.assert_called()
+        mock_request.assert_called()
+        mock_create.assert_called()
--- plugins/cisco_firepower_management_center/unit_test/test_bulk_add_scan_result.py
+++ plugins/cisco_firepower_management_center/unit_test/test_bulk_add_scan_result.py
@@ -1,24 +1,28 @@
-import sys
 import os
-
-from unittest import TestCase
-from icon_cisco_firepower_management_center.actions.bulk_add_scan_result import BulkAddScanResult
-from icon_cisco_firepower_management_center.actions.bulk_add_scan_result.schema import Input
-from unit_test.util import Util
-from unittest.mock import patch
-from insightconnect_plugin_runtime.exceptions import PluginException
+import sys
 
 sys.path.append(os.path.abspath("../"))
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from icon_cisco_firepower_management_center.actions.bulk_add_scan_result import BulkAddScanResult
+from icon_cisco_firepower_management_center.actions.bulk_add_scan_result.schema import Input
+from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
+
+from util import Util
 
 
-@patch("ssl.SSLSocket.connect", side_effect=Util.mock_connect)
-@patch("ssl.SSLSocket.write", side_effect=Util.mock_write)
-@patch("ssl.SSLSocket.send", side_effect=Util.mock_send)
-@patch("ssl.SSLSocket.recv", side_effect=Util.mock_recv)
 @patch("requests.post", side_effect=Util.mocked_requests)
 @patch("requests.request", side_effect=Util.mocked_requests)
+@patch("ssl.SSLSocket._create", side_effect=Util.MockSSLSocket)
 class TestBulkAddScanResult(TestCase):
-    def test_bulk_add_scan_result(self, mock_connect, mock_write, mock_send, mock_recv, mock_post, mock_request):
+    def test_bulk_add_scan_result(
+        self,
+        mock_post: MagicMock,
+        mock_request: MagicMock,
+        mock_create: MagicMock,
+    ) -> None:
         action = Util.default_connector(BulkAddScanResult())
         actual = action.run(
             {
@@ -42,9 +46,18 @@ class TestBulkAddScanResult(TestCase):
             }
         )
         expected = {"errors": 0, "commands_processed": 4}
+        validate(actual, action.output.schema)
         self.assertEqual(actual, expected)
+        mock_post.assert_called()
+        mock_request.assert_called()
+        mock_create.assert_called()
 
-    def test_bulk_add_scan_result_bad(self, mock_connect, mock_write, mock_send, mock_recv, mock_post, mock_request):
+    def test_bulk_add_scan_result_bad(
+        self,
+        mock_post: MagicMock,
+        mock_request: MagicMock,
+        mock_create: MagicMock,
+    ) -> None:
         action = Util.default_connector(BulkAddScanResult())
         with self.assertRaises(PluginException) as error:
             action.run(
@@ -70,3 +83,6 @@ class TestBulkAddScanResult(TestCase):
             )
         self.assertEqual(error.exception.cause, "The provided IP address 999.999.999.999 is invalid.")
         self.assertEqual(error.exception.assistance, "Please provide a valid IP address for the host and try again.")
+        mock_post.assert_called()
+        mock_request.assert_called()
+        mock_create.assert_called()
