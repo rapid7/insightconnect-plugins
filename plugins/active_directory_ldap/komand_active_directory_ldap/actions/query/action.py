da993204e704995cc517f133e18d82f9f750f4fe
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-01-05 12:24:40 +0100

    Active Directory LDAP - 629 - Fix problem where some ASCII characters were not escaped properly (#2201)

--- plugins/active_directory_ldap/komand_active_directory_ldap/actions/query/action.py
+++ plugins/active_directory_ldap/komand_active_directory_ldap/actions/query/action.py
@@ -2,6 +2,7 @@ import insightconnect_plugin_runtime
 import ldap3
 
 # Custom imports below
+from insightconnect_plugin_runtime.exceptions import PluginException
 from komand_active_directory_ldap.util.utils import ADUtils
 from .schema import QueryInput, QueryOutput, Input, Output
 
@@ -13,22 +14,27 @@ class Query(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        formatter = ADUtils()
-        query = params.get(Input.SEARCH_FILTER)
-
-        query = query.replace("\\>=", ">=")
-        query = query.replace("\\<=", "<=")
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
+        query = params.get(Input.SEARCH_FILTER, "").replace("\\>=", ">=").replace("\\<=", "<=")
+        search_base = params.get(Input.SEARCH_BASE)
+        attributes = params.get(Input.ATTRIBUTES, [])
+        # END INPUT BINDING - DO NOT REMOVE
 
         # replace ( and ) when they are part of a name rather than a search parameter
-        escaped_query = formatter.escape_brackets_for_query(query)
-        self.logger.info("Escaped query: %s", escaped_query)
+        escaped_query = ADUtils.escape_brackets_for_query(query)
+        self.logger.info(f"Escaped query: {escaped_query}")
 
-        attributes = params.get(Input.ATTRIBUTES, [])
         if not attributes:
             attributes = [ldap3.ALL_ATTRIBUTES, ldap3.ALL_OPERATIONAL_ATTRIBUTES]
 
-        entries = self.connection.client.query(params.get(Input.SEARCH_BASE), escaped_query, attributes)
+        try:
+            entries = self.connection.client.query(search_base, escaped_query, attributes)
+        except PluginException:
+            self.logger.info("Escaping non-ascii characters...")
+            entries = self.connection.client.query(
+                search_base, ADUtils.escape_non_ascii_characters(escaped_query), attributes
+            )
+
         if entries:
             return {Output.RESULTS: entries, Output.COUNT: len(entries)}
-        else:
-            return {Output.RESULTS: [], Output.COUNT: 0}
+        return {Output.RESULTS: [], Output.COUNT: 0}
--- plugins/active_directory_ldap/komand_active_directory_ldap/actions/query_group_membership/action.py
+++ plugins/active_directory_ldap/komand_active_directory_ldap/actions/query_group_membership/action.py
@@ -1,6 +1,7 @@
 import insightconnect_plugin_runtime
 
 # Custom imports below
+from insightconnect_plugin_runtime.exceptions import PluginException
 from .schema import QueryGroupMembershipInput, QueryGroupMembershipOutput, Input, Output, Component
 
 
@@ -14,10 +15,12 @@ class QueryGroupMembership(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
         base = params.get(Input.SEARCH_BASE)
+        group_name = params.get(Input.GROUP_NAME)
         include_groups = params.get(Input.INCLUDE_GROUPS)
         expand_nested_groups = params.get(Input.EXPAND_NESTED_GROUPS)
-        entries = self.connection.client.query_group_membership(
-            base, params.get(Input.GROUP_NAME), include_groups, expand_nested_groups
-        )
+        # END INPUT BINDING - DO NOT REMOVE
+
+        entries = self.connection.client.query_group_membership(base, group_name, include_groups, expand_nested_groups)
         return {Output.RESULTS: entries, Output.COUNT: len(entries)}
