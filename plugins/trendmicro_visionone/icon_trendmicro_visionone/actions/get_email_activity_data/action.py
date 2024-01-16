9f0a0eab25fcec4be55ba198f5b3298351b9ab21
Peter Kuik <peter_kuik@trendmicro.com>
2024-01-16 14:30:58 +0100

    after make regen

--- plugins/trendmicro_visionone/icon_trendmicro_visionone/actions/get_email_activity_data/action.py
+++ plugins/trendmicro_visionone/icon_trendmicro_visionone/actions/get_email_activity_data/action.py
@@ -1,60 +1,17 @@
 import insightconnect_plugin_runtime
-from .schema import (
-    GetEmailActivityDataInput,
-    GetEmailActivityDataOutput,
-    Input,
-    Output,
-    Component,
-)
-from insightconnect_plugin_runtime.exceptions import PluginException
-
+from .schema import GetEmailActivityDataInput, GetEmailActivityDataOutput, Input, Output, Component
 # Custom imports below
-import pytmv1
-import json
 
 
 class GetEmailActivityData(insightconnect_plugin_runtime.Action):
+
     def __init__(self):
         super(self.__class__, self).__init__(
-            name="get_email_activity_data",
-            description=Component.DESCRIPTION,
-            input=GetEmailActivityDataInput(),
-            output=GetEmailActivityDataOutput(),
-        )
+                name='get_email_activity_data',
+                description=Component.DESCRIPTION,
+                input=GetEmailActivityDataInput(),
+                output=GetEmailActivityDataOutput())
 
     def run(self, params={}):
-        # Get Connection Client
-        client = self.connection.client
-        # Get Action Parameters
-        top = params.get(Input.TOP)
-        start_date_time = params.get(Input.START_DATE_TIME)
-        end_date_time = params.get(Input.END_DATE_TIME)
-        select = params.get(Input.SELECT)
-        query_op = params.get(Input.QUERY_OP)
-        fields = params.get(Input.FIELDS)
-        # Choose enum
-        if "or" in query_op:
-            query_op = pytmv1.QueryOp.OR
-        elif "and" in query_op:
-            query_op = pytmv1.QueryOp.AND
-        new_email_activity_data = []
-        # Make Action API Call
-        self.logger.info("Making API Call...")
-        response = client.email.consume_activity(
-            lambda email_activity_data: new_email_activity_data.append(email_activity_data.model_dump()),
-            start_time=start_date_time,
-            end_time=end_date_time,
-            select=select,
-            top=top,
-            op=pytmv1.QueryOp(query_op),
-            **fields,
-        )
-        if "error" in response.result_code.lower():
-            raise PluginException(
-                cause="An error occurred while getting email activity data.",
-                assistance="Please check your inputs and try again.",
-                data=response.error,
-            )
-        # Return results
-        self.logger.info("Returning Results...")
-        return {Output.EMAIL_ACTIVITY_DATA_RESP: new_email_activity_data}
+        # TODO: Implement run function
+        return {}
--- plugins/trendmicro_visionone/icon_trendmicro_visionone/actions/get_endpoint_activity_data_count/action.py
+++ plugins/trendmicro_visionone/icon_trendmicro_visionone/actions/get_endpoint_activity_data_count/action.py
@@ -1,58 +1,17 @@
 import insightconnect_plugin_runtime
-from .schema import (
-    GetEndpointActivityDataCountInput,
-    GetEndpointActivityDataCountOutput,
-    Input,
-    Output,
-    Component,
-)
-from insightconnect_plugin_runtime.exceptions import PluginException
-
+from .schema import GetEndpointActivityDataCountInput, GetEndpointActivityDataCountOutput, Input, Output, Component
 # Custom imports below
-import pytmv1
-import json
 
 
 class GetEndpointActivityDataCount(insightconnect_plugin_runtime.Action):
+
     def __init__(self):
         super(self.__class__, self).__init__(
-            name="get_endpoint_activity_data_count",
-            description=Component.DESCRIPTION,
-            input=GetEndpointActivityDataCountInput(),
-            output=GetEndpointActivityDataCountOutput(),
-        )
+                name='get_endpoint_activity_data_count',
+                description=Component.DESCRIPTION,
+                input=GetEndpointActivityDataCountInput(),
+                output=GetEndpointActivityDataCountOutput())
 
     def run(self, params={}):
-        # Get Connection Client
-        client = self.connection.client
-        # Get Action Parameters
-        top = params.get(Input.TOP)
-        start_date_time = params.get(Input.START_DATE_TIME)
-        end_date_time = params.get(Input.END_DATE_TIME)
-        select = params.get(Input.SELECT)
-        query_op = params.get(Input.QUERY_OP)
-        fields = params.get(Input.FIELDS)
-        # Choose enum
-        if "or" in query_op:
-            query_op = pytmv1.QueryOp.OR
-        elif "and" in query_op:
-            query_op = pytmv1.QueryOp.AND
-        # Make Action API Call
-        self.logger.info("Making API Call...")
-        response = client.endpoint.get_activity_count(
-            start_time=start_date_time,
-            end_time=end_date_time,
-            select=select,
-            top=top,
-            op=pytmv1.QueryOp(query_op),
-            **fields,
-        )
-        if "error" in response.result_code.lower():
-            raise PluginException(
-                cause="An error occurred while getting endpoint activity data count.",
-                assistance="Please check your inputs and try again.",
-                data=response.error,
-            )
-        # Return results
-        self.logger.info("Returning Results...")
-        return {Output.TOTAL_COUNT: response.response.total_count}
+        # TODO: Implement run function
+        return {}
