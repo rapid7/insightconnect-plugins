6f6109f136095d16d7f4fcead0661caabb7f2473
Peter Kuik <peter_kuik@trendmicro.com>
2024-01-19 15:23:15 +0100

    updated help.md

--- plugins/trendmicro_visionone/setup.py
+++ plugins/trendmicro_visionone/setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup, find_packages
 
 
-setup(name="trendmicro_visionone-trendmicro-plugin",
-      version="3.0.0",
-      description="Trend Micro Vision One is an enhanced threat defense platform, surpassing standard XDR solutions. It offers comprehensive detection and response across various security layers and automates data correlation for rapid response, effectively preventing most attacks",
-      author="trendmicro",
-      author_email="",
-      url="",
-      packages=find_packages(),
-      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/icon_trendmicro_visionone']
-      )
+setup(
+    name="trendmicro_visionone-trendmicro-plugin",
+    version="2.1.0",
+    description="Trend Micro Vision One is an enhanced threat defense platform, surpassing standard XDR solutions. It offers comprehensive detection and response across various security layers and automates data correlation for rapid response, effectively preventing most attacks",
+    author="trendmicro",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["insightconnect-plugin-runtime"],  # Add third-party dependencies to requirements.txt, not here!
+    scripts=["bin/icon_trendmicro_visionone"],
+)
