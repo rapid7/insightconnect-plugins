ca42abdf18a8a79ebf3531d6e29b4a83df08fdca
conor-mcnally <conor_mcnally@rapid7.com>
2024-01-19 14:29:12 +0000

    .

--- plugins/domaintools_phisheye/unit_test/mock.py
+++ plugins/domaintools_phisheye/unit_test/mock.py
@@ -12,21 +12,6 @@ from insightconnect_plugin_runtime.action import Action
 STUB_CONNECTION = {Input.API_KEY: "11111-aaaaa-aaa11-111aa-aaa11", Input.USERNAME: "user1"}
 
 
-class MockResponse:
-    def __init__(self, filename: str, status_code: int) -> None:
-        self.filename = filename
-        self.status_code = status_code
-        self.text = json.dumps(self.json())
-        self.request = MagicMock()
-        self.headers = MagicMock()
-
-    def json(self):
-        with open(
-            os.path.join(os.path.dirname(os.path.realpath(__file__)), f"responses/{self.filename}.json.resp")
-        ) as file:
-            return json.load(file)
-
-
 class Util:
     @staticmethod
     def default_connector(action: Action) -> Action:
@@ -36,3 +21,27 @@ class Util:
         action.connection = default_connection
         action.logger = logging.getLogger("action logger")
         return action
+
+    @staticmethod
+    def read_file_to_string(filename: str) -> str:
+        with open(
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), filename), "r", encoding="utf-8"
+        ) as file_reader:
+            return file_reader.read()
+
+    @staticmethod
+    def read_file_to_dict(filename: str) -> dict:
+        return json.loads(Util.read_file_to_string(filename))
+
+
+class MockResponse:
+    def __init__(self, status_code: int, json_object: str = ""):
+        self.status_code = status_code
+        self.text = json.dumps(json_object)
+        self.content = json.dumps(json_object)
+        self.json_object = json_object
+
+    def json(self):
+        return self.json_object
+
+
--- plugins/domaintools_phisheye/unit_test/test_domain_list.py
+++ plugins/domaintools_phisheye/unit_test/test_domain_list.py
@@ -4,32 +4,45 @@ import os
 sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
-from icon_domaintools_phisheye.connection.connection import Connection
 from icon_domaintools_phisheye.actions.domain_list import DomainList
-import json
+from icon_domaintools_phisheye.actions.domain_list.schema import Input, DomainListInput, DomainListOutput
 from unit_test.mock import Util
+from parameterized import parameterized
+from jsonschema import validate
 
 
 class TestDomainList(TestCase):
     def setUp(self) -> None:
         self.action = Util.default_connector(DomainList())
 
-    def test_domain_list(self):
-        response = self.action.run()
-        expected_response = {
-            "date": "2016-11-01",
-            "domains": [
+        self.params_only_query = {Input.QUERY: "example"}
+
+    @parameterized.expand(
+        [
+            [
+                "valid_query_with_days_back",
+                {Input.QUERY: "example", Input.DAYS_BACK: 1},
                 {
-                    "Created Date": {},
-                    "Domain": "appeltypoexample.com",
-                    "IP Addresses": [{"Country Code": {}, "IPv4": {}}],
-                    "Name Servers": "ns57.domaincontrol.com",
-                    "Registrant Email": {},
-                    "Registrar Name": {},
-                    "Risk Score": 24,
-                    "TLD": {},
+                    "date": "2016-11-01",
+                    "domains": [
+                        {
+                            "Created Date": {},
+                            "Domain": "appeltypoexample.com",
+                            "IP Addresses": [{"Country Code": {}, "IPv4": {}}],
+                            "Name Servers": "ns57.domaincontrol.com",
+                            "Registrant Email": {},
+                            "Registrar Name": {},
+                            "Risk Score": 24,
+                            "TLD": {},
+                        }
+                    ],
+                    "term": "apple",
                 }
-            ],
-            "term": "apple",
-        }
-        self.assertEqual(response, expected_response)
+            ]
+        ]
+    )
+    def test_domain_list(self, _test_name: str, input_params: dict, expected: dict):
+        validate(input_params, DomainListInput.schema)
+        response = self.action.run(input_params)
+        self.assertEqual(response, expected)
+        validate(response, DomainListOutput)
