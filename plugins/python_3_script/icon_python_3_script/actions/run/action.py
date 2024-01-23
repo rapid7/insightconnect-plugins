a26ebf0a4760ccbac57c3b44e383d2fc2d8ef59e
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-01-23 16:17:31 +0100

    Python 3 Script - 648 - Updated the SDK to latest version | Fix issue where input argument was too long (#2261)

--- plugins/python_3_script/icon_python_3_script/actions/run/action.py
+++ plugins/python_3_script/icon_python_3_script/actions/run/action.py
@@ -1,5 +1,6 @@
 import subprocess  # nosec B404
 import sys
+from pathlib import Path
 from typing import Any, Dict, Union
 from uuid import uuid4
 
@@ -7,7 +8,12 @@ import insightconnect_plugin_runtime
 from insightconnect_plugin_runtime.exceptions import PluginException
 from insightconnect_plugin_runtime.helper import clean
 
-from icon_python_3_script.util.constants import DEFAULT_ENCODING, DEFAULT_PROCESS_TIMEOUT, INDENTATION_CHARACTER
+from icon_python_3_script.util.constants import (
+    DEFAULT_ENCODING,
+    DEFAULT_PROCESS_TIMEOUT,
+    INDENTATION_CHARACTER,
+    RUN_FUNCTION_TEMPLATE,
+)
 from icon_python_3_script.util.util import extract_output_from_stdout
 
 from .schema import Component, Input, RunInput, RunOutput
@@ -22,16 +28,16 @@ class Run(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
         function_ = params.get(Input.FUNCTION, "")
-        function_ = self._add_credentials_to_function(
-            function_, self.connection.script_credentials, self._check_indentation_character(function_)
-        )
+        input_parameters = params.get(Input.INPUT, {})
+        # END INPUT BINDING - DO NOT REMOVE
 
         self.logger.info(f"Input: (below)\n\n{params.get(Input.INPUT)}\n")
         self.logger.info(f"Function: (below)\n\n{params.get(Input.FUNCTION)}\n")
 
         try:
-            output = self._execute_function_as_process(function_, params.get(Input.INPUT, {}))
+            output = self._execute_function_as_process(function_, input_parameters, self.connection.script_credentials)
         except Exception as error:
             raise PluginException(cause="Could not run supplied script", data=error) from None
         try:
@@ -63,8 +69,9 @@ class Run(insightconnect_plugin_runtime.Action):
         function_name = function_.split(" ")[1].split("(")[0]
         return locals()[function_name](params.get(Input.INPUT))
 
-    @staticmethod
-    def _execute_function_as_process(function_: str, parameters: Dict[str, Any]) -> Union[Dict[str, Any], None]:
+    def _execute_function_as_process(
+        self, function_: str, parameters: Dict[str, Any], credentials: Dict[str, Any]
+    ) -> Union[Dict[str, Any], None]:
         """
         Execute a function as a separate process and return its data.
 
@@ -74,28 +81,68 @@ class Run(insightconnect_plugin_runtime.Action):
         :param parameters: The input parameters to pass to the function.
         :type: Dict[str, Any]
 
+        :param credentials: The credentials to pass to the function.
+        :type: Dict[str, Any]
+
         :return: The result of the function execution.
         :rtype: Union[Dict[str, Any], None]
         """
 
-        execution_id = f"Python3Script-ActionRun-{uuid4()}:"
-        function_name = function_.split(" ")[1].split("(")[0]
+        execution_id = f"Python3Script-ActionRun-{uuid4()}"
+        execution_file = self.create_execution_file(execution_id, function_, parameters)
+        execution_arguments = ["python", execution_file.name]
+
+        if credentials:
+            for key, value in credentials.items():
+                if value:
+                    execution_arguments.append(f"--{key}={value}")
+
         try:
             output = subprocess.check_output(  # nosec B603, B607
-                [
-                    "python",
-                    "-c",
-                    f'import sys\n\nsys.path.append("/var/cache/python_dependencies/lib/python3.9/site-packages")\n\n{function_}\nsys.stdout.write("{execution_id}" + str({function_name}({parameters})))',
-                ],
+                execution_arguments,
                 shell=False,
                 stderr=subprocess.PIPE,
                 timeout=DEFAULT_PROCESS_TIMEOUT,
             )
             return extract_output_from_stdout(output.decode(DEFAULT_ENCODING), execution_id)
         except subprocess.CalledProcessError as error:
-            raise PluginException(error.stderr.decode(DEFAULT_ENCODING)) from None
+            raise PluginException(error.stderr.decode(DEFAULT_ENCODING).replace(execution_id, "")) from None
         except subprocess.TimeoutExpired:
             raise PluginException(f"Function got timed out after {DEFAULT_PROCESS_TIMEOUT} seconds.") from None
+        finally:
+            if execution_file.is_file():
+                execution_file.unlink()
+
+    @staticmethod
+    def create_execution_file(execution_id: str, function_: str, parameters: Dict[str, Any]) -> Path:
+        """
+        Create an execution file with the given execution ID, function name, and parameters.
+
+        :param execution_id: A unique identifier for the execution.
+        :type: str
+
+        :param function_: The body of the function to be executed.
+        :type: str
+
+        :param parameters: A dictionary of parameters to be passed to the function.
+        :type: Dict[str, Any]
+
+        :return: The path to the created execution file.
+        :rtype: Path
+        """
+
+        filename = f"{execution_id}.py"
+        function_name = function_.split(" ")[1].split("(")[0]
+        with open(filename, "w", encoding=DEFAULT_ENCODING) as file_:
+            file_.write(
+                RUN_FUNCTION_TEMPLATE.format(
+                    execution_id=execution_id,
+                    function_=function_,
+                    function_name=function_name,
+                    parameters=parameters,
+                )
+            )
+        return Path(filename)
 
     @staticmethod
     def _add_credentials_to_function(
