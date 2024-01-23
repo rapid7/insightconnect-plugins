a7a94a144b6d91db2ebfc2e6ce517142eeef6a63
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-01-23 16:17:31 +0100

    Python 3 Script - 648 - Updated the SDK to latest version | Fix issue where input argument was too long (#2261)

--- plugins/python_3_script/icon_python_3_script/util/constants.py
+++ plugins/python_3_script/icon_python_3_script/util/constants.py
@@ -1,3 +1,22 @@
 INDENTATION_CHARACTER = " " * 4
 DEFAULT_ENCODING = "utf-8"
 DEFAULT_PROCESS_TIMEOUT = 5 * 60
+
+RUN_FUNCTION_TEMPLATE = """import sys
+from argparse import ArgumentParser
+
+sys.path.append("/var/cache/python_dependencies/lib/python3.9/site-packages")
+
+parser = ArgumentParser()
+parser.add_argument("--username")
+parser.add_argument("--password")
+parser.add_argument("--secret_key")
+
+arguments = parser.parse_args()
+username = arguments.username
+password = arguments.password
+secret_key = arguments.secret_key
+
+{function_}
+sys.stdout.write("{execution_id}" + str({function_name}({parameters})))
+"""
