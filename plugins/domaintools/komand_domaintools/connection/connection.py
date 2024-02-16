3ad6904b73f145d847aa967783ea7cb2a796d67e
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2024-02-16 08:44:47 +0000

    Add connection test

--- plugins/domaintools/komand_domaintools/connection/connection.py
+++ plugins/domaintools/komand_domaintools/connection/connection.py
@@ -1,5 +1,5 @@
 import insightconnect_plugin_runtime
-from insightconnect_plugin_runtime.exceptions import ConnectionTestException
+from insightconnect_plugin_runtime.exceptions import ConnectionTestException, PluginException
 from .schema import ConnectionSchema, Input
 
 # Custom imports below
@@ -24,9 +24,25 @@ class Connection(insightconnect_plugin_runtime.Connection):
             response.data()
         except NotAuthorizedException as exception:
             self.logger.error(f"DomainTools: Connect: error {exception}")
-            raise ConnectionTestException("DomainTools: Connect: Authorization failed. Please try again")
+            raise PluginException(
+                cause="DomainTools: Connect: Authorization failed. Please try again",
+                assistance="Please review your connection details",
+                data=response,
+            )
         except Exception as exception:
             self.logger.error(f"DomainTools: Connect: error {exception}")
-            raise ConnectionTestException(f"DomainTools: Connect: Failed to connect to server {exception}")
-
+            raise PluginException(cause=f"DomainTools: Connect: Failed to connect to server {exception}", data=response)
         self.api = api
+
+    def test(self):
+        try:
+            response = self.api.account_information()
+            response.data()
+        except Exception as exception:
+            self.logger.error(f"DomainTools: Connect: error {exception}")
+            raise ConnectionTestException(
+                cause="DomainTools: Connect: Authorization failed. Please try again",
+                assistance="Please review your connection details",
+                data=exception,
+            )
+        return {"success": True}
