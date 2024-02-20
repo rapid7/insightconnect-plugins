eb0ed0fe6ad737c1ad29cd53494a905f2e13ad67
Robert <144030336+rbowden-r7@users.noreply.github.com>
2024-02-20 17:10:41 +0000

    [PLGN-734] - Zoom -Adding better handling to  if the users account does not have all of the required permsions (#2322)
    * PLGN-734-Adding better handling to  if the users account does not have all of the required permsions
    
    * PLGN-734-Adding better handling to  if the users account does not have all of the required permissions

--- plugins/zoom/unit_test/test_delete_user.py
+++ plugins/zoom/unit_test/test_delete_user.py
@@ -9,8 +9,9 @@ from unittest import TestCase, mock
 from unittest.mock import MagicMock
 
 from icon_zoom.actions.delete_user import DeleteUser
-from icon_zoom.actions.delete_user.schema import Input
+from icon_zoom.actions.delete_user.schema import Input, DeleteUserInput, DeleteUserOutput
 from insightconnect_plugin_runtime.exceptions import PluginException
+from jsonschema import validate
 
 from mock import Util, mock_request_204, mock_request_400, mock_request_404, mocked_request
 
@@ -18,9 +19,9 @@ STUB_DELETE_USER_QUERY_PARAMS = {
     Input.ID: "12345",
     Input.ACTION: "delete",
     Input.TRANSFER_EMAIL: "user@example.com",
-    Input.TRANSFER_WEBINARS: "",
-    Input.TRANSFER_RECORDINGS: "",
-    Input.TRANSFER_MEETINGS: "",
+    Input.TRANSFER_WEBINARS: False,
+    Input.TRANSFER_RECORDINGS: False,
+    Input.TRANSFER_MEETINGS: False,
 }
 
 
@@ -33,10 +34,11 @@ class TestDeleteUser(TestCase):
     @mock.patch("requests.request", side_effect=mock_request_204)
     def delete_user_success(self, mock_delete: MagicMock) -> None:
         # mocked_request(mock_request_204)
+        validate(self.params, DeleteUserInput.schema)
         response = self.action.run(self.params)
         expected_response = None
-
         self.assertEqual(response, expected_response)
+        validate(response, DeleteUserOutput.schema)
 
     @parameterized.expand(
         [
@@ -46,8 +48,8 @@ class TestDeleteUser(TestCase):
     )
     @mock.patch("icon_zoom.util.api.ZoomAPI._refresh_oauth_token", return_value=None)
     def test_not_ok(self, mock_request: MagicMock, exception: str, mock_refresh: MagicMock) -> None:
+        validate(self.params, DeleteUserInput.schema)
         mocked_request(mock_request)
-
         with self.assertRaises(PluginException) as context:
             self.action.run(self.params)
         self.assertEqual(context.exception.cause, exception)
