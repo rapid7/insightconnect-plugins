3e42ece8ab7044f2b675eb61fecbb37cf85efd8e
joneill-r7 <joneill@rapid7.com>
2024-02-20 11:13:00 +0000

    PLGN-700: implement new SDK for custom_configs in tasks.

--- plugins/mimecast/komand_mimecast/tasks/monitor_siem_logs/task.py
+++ plugins/mimecast/komand_mimecast/tasks/monitor_siem_logs/task.py
@@ -29,10 +29,13 @@ class MonitorSiemLogs(insightconnect_plugin_runtime.Task):
             state=MonitorSiemLogsState(),
         )
 
-    def run(self, params={}, state={}) -> (List[Dict], Dict):  # pylint: disable=unused-argument # noqa: MC0001
+    def run(  # pylint: disable=unused-argument # noqa: MC0001
+        self, params={}, state={}, custom_config={}
+    ) -> (List[Dict], Dict, Dict):
         try:
             has_more_pages = False
             header_next_token = state.get(self.NEXT_TOKEN, "")
+            filter_time = self._get_filter_time(custom_config)
 
             if not header_next_token:
                 self.logger.info("First run...")
@@ -52,7 +55,7 @@ class MonitorSiemLogs(insightconnect_plugin_runtime.Task):
                     )
                     return [], state, has_more_pages, error.status_code, error
                 header_next_token = headers.get(self.HEADER_NEXT_TOKEN)
-                output = self._filter_and_sort_recent_events(output)
+                output = self._filter_and_sort_recent_events(output, filter_time)
                 if output:
                     break
 
@@ -71,31 +74,52 @@ class MonitorSiemLogs(insightconnect_plugin_runtime.Task):
                 exp = PluginException(preset=PluginException.Preset.UNKNOWN, data=gen_error)
             return [], state, has_more_pages, 500, exp
 
-    def _filter_and_sort_recent_events(self, task_output: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    def _filter_and_sort_recent_events(self, task_output: List[Dict[str, Any]], cut_off: int) -> List[Dict[str, Any]]:
         """
         Filters and sorts a list of events to retrieve only the recent events.
 
         :param task_output: A list of dictionaries representing the task output to be filtered and sorted.
         :type: List[Dict[str, Any]]
 
+        :param cut_off: integer specifying how far back in time we filter events from
+        :type: integer
+
         :return: A new list of dictionaries representing the filtered and sorted recent events.
         :rtype: List[Dict[str, Any]]
         """
 
         self.logger.info(f"Number of raw logs returned from Mimecast: {len(task_output)}")
+        filter_time = get_time_hours_ago(hours_ago=cut_off)
         task_output = sorted(
             map(
                 lambda event: event.get_dict(),
                 filter(
-                    lambda event: event.compare_datetime(get_time_hours_ago(hours_ago=CUTOFF)),
+                    lambda event: event.compare_datetime(filter_time),
                     [EventLogs(data=event) for event in task_output],
                 ),
             ),
             key=itemgetter(EventLogs.FILTER_DATETIME),
         )
-
+        latest_time = filter_time
         for event in task_output:
+            event_date_time = event.get(EventLogs.FILTER_DATETIME)
+            latest_time = event_date_time if event_date_time > latest_time else latest_time
             event.pop(EventLogs.FILTER_DATETIME, None)
         self.logger.info(f"Number of returned logs after filtering performed: {len(task_output)}")
-
+        if task_output:
+            self.logger.info(f"Latest event time returned from Mimecast logs: {latest_time}")
         return task_output
+
+    def _get_filter_time(self, custom_config: Dict[str, int]) -> int:
+        """
+        We want to apply the custom_config params if provided to the task. The logic is that if
+        a lookback value exists, this should take precedence to allow a larger filter time and only
+        once this no longer exists do we want to use the default value.
+        :param custom_config: dictionary passed containing `default` or `lookback` values
+        :return: filter time to be applied in `_filter_and_sort_recent_events`
+        """
+
+        default, lookback = custom_config.get("default", CUTOFF), custom_config.get("lookback")
+        filter_value = lookback if lookback else default
+        self.logger.info(f"Task execution will be applying filter period of {filter_value} hours...")
+        return int(filter_value)
