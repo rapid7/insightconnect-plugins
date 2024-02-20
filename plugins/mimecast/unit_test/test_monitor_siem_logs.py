a3e59ea5c72636b95c747697757f13a26ea8997a
Robert <144030336+rbowden-r7@users.noreply.github.com>
2024-02-20 11:11:46 +0000

    [PLGN-700] - Mimecast - Adding in file sanitation from zip files (#2290)
    * PLGN-700-Adding in file sanitation from zip files
    
    * PLGN-700-Adding in file sanitation from zip files
    
    * PLGN-700-Adding in explanation for why file_names are being are being parsed by secure_filename

--- plugins/mimecast/unit_test/test_monitor_siem_logs.py
+++ plugins/mimecast/unit_test/test_monitor_siem_logs.py
@@ -50,7 +50,6 @@ class TestMonitorSiemLogs(TestCase):
         self.assertEqual(new_state, state_params)  # we shouldn't change the state if we encounter an error
         self.assertEqual(type(error), ApiClientException)
 
-    @skip("Have pulled the sanitise method as possible negative seek cause - TODO in 5.3.6")
     @patch("logging.Logger.error")
     def test_monitor_siem_logs_stops_path_traversal(self, mock_logger, _mock_data):
         test_state = {"next_token": "path_traversal"}  # this forces our mock util to append `../` into filenames
@@ -60,7 +59,9 @@ class TestMonitorSiemLogs(TestCase):
         self.assertEqual(response, [])  # no logs will be parsed as we raise error after catching BadZipFile
         self.assertEqual(new_state, test_state)  # we shouldn't change the state if we encounter an error
         mock_logger.assert_called()
-        self.assertIn("Hit BadZipFile, returning []. Error", mock_logger.call_args[0][0])
+        self.assertIn(
+            "There is no item named 'filename-1-from-mimecast.json' in the archive", mock_logger.call_args[0][0]
+        )
 
     @patch("logging.Logger.error")
     def test_monitor_siem_logs_raises_json_error(self, mock_logger, _mock_data):
