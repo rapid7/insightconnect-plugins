4106bc7e9164d978efd61d82b50f7c0bb819f1b4
joneill-r7 <joneill@rapid7.com>
2024-02-21 11:09:04 +0000

    PLGN-727: implement SDK 5.4 for custom_config task parameter.

--- plugins/proofpoint_tap/unit_test/test_monitor_events.py
+++ plugins/proofpoint_tap/unit_test/test_monitor_events.py
@@ -7,11 +7,13 @@ from komand_proofpoint_tap.tasks import MonitorEvents
 from test_util import Util
 from unittest import TestCase
 from parameterized import parameterized
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+from json import loads
 
 sys.path.append(os.path.abspath("../"))
 
 ENV_VALUE = '{"year": 2024, "month": 1, "day": 27, "hour": 0, "minute": 0, "second": 0}'
+ENV_VALUE_2 = '{"year": 2024, "month": 2, "day": 20, "hour": 12, "minute": 0, "second": 0}'
 TEST_PAGE_SIZE = 2
 
 
@@ -66,7 +68,9 @@ class TestMonitorEvents(TestCase):
         ]
     )
     def test_monitor_events(self, _mock_request, _mock_get_time, _test_name, current_state, expected):
-        actual, actual_state, has_more_pages, status_code, error = self.action.run(state=current_state)
+        actual, actual_state, has_more_pages, status_code, error = self.action.run(
+            state=current_state, custom_config={}
+        )
         self.assertEqual(actual, expected.get("events"))
         self.assertEqual(actual_state, expected.get("state"))
 
@@ -74,10 +78,10 @@ class TestMonitorEvents(TestCase):
     @patch("logging.Logger.info")
     def test_monitor_events_with_env_variable_empty_state(self, mock_logger, _mock_request, _mock_time):
         # When we inject the env variable into the pod we should change the time we retrieve events from.
-        response, state, has_more_pages, status_code, _error = self.action.run(state={})
+        response, state, has_more_pages, status_code, _error = self.action.run(state={}, custom_config={})
 
         # we should have logged using env var
-        self.assertIn("Using env var value", mock_logger.call_args_list[1][0][0])
+        self.assertIn("Using custom value of", mock_logger.call_args_list[2][0][0])
 
         # state last time should be injected env var + 1 hour
         self.assertEqual("2024-01-27T01:00:00+00:00", state["last_collection_date"])
@@ -90,15 +94,57 @@ class TestMonitorEvents(TestCase):
         self.assertEqual(1, state["next_page_index"])
 
     @patch("komand_proofpoint_tap.tasks.monitor_events.task.SPECIFIC_DATE", new=ENV_VALUE)
-    @patch("logging.Logger.info")
-    def test_monitor_events_with_env_variable_existing_state(self, mock_logger, _mock_request, _mock_time):
+    def test_monitor_events_with_env_variable_existing_state(self, _mock_request, _mock_time):
         # Although we inject the env var because we have a state saved we can ignore and continue our usual logic
         # use this date to avoid cut off logic from mocked current time.
         existing_state = {"last_collection_date": "2023-04-04T01:00:00+00:00"}
-        response, state, has_more_pages, status_code, _error = self.action.run(state=existing_state)
+        response, state, has_more_pages, status_code, _error = self.action.run(state=existing_state, custom_config={})
 
         # state last time should be last_collected_date + 1 hour
         self.assertEqual("2023-04-04T02:00:00+00:00", state["last_collection_date"])
 
         # Split the logs being returned by the SPLIT_SIZE variable
         self.assertEqual(TEST_PAGE_SIZE, len(response))
+
+    @patch("komand_proofpoint_tap.tasks.monitor_events.task.SPECIFIC_DATE", new=ENV_VALUE_2)
+    @patch("logging.Logger.info")
+    def test_monitor_events_uses_custom_config_data(self, mock_logger, _mock_request, _mock_time):
+        # Even though we have an environment variable specified we use the custom_config passed from CPS
+        full_lookback_value = loads(ENV_VALUE)
+        cps_config = {"lookback": full_lookback_value}  # pass this as a dict as our API will supply as it like this
+        response, state, has_more_pages, status_code, _error = self.action.run({}, {}, cps_config)
+
+        # In this example we have an env var specified but this is ignored to use the value in the custom_config
+        self.assertIn(f"Using custom value of {full_lookback_value}", mock_logger.call_args_list[2][0][0])
+
+        # state last time should be custom config value + 1 hour
+        self.assertEqual("2024-01-27T01:00:00+00:00", state["last_collection_date"])
+        self.assertEqual(1, state["next_page_index"])
+
+        # second time we call we will then pass no lookback from SDK level and now use the cutoff date value
+        with patch("komand_proofpoint_tap.tasks.monitor_events.task.SPECIFIC_DATE", new=""):  # clear env var
+            cps_config = {"cutoff": {"date": full_lookback_value}}
+            _resp, state_2, _more_pages, _status_code, _error = self.action.run({}, state.copy(), cps_config)
+
+            # Due to pagination the last collection date will not change in our state
+            self.assertEqual(state["last_collection_date"], state_2["last_collection_date"])
+            self.assertEqual(2, state_2["next_page_index"])  # however we should be on the second page
+
+            # Third time we call - we increase page size to return more_pages=False from the task
+            with patch("komand_proofpoint_tap.tasks.monitor_events.task.MonitorEvents.SPLIT_SIZE", new=40000):
+                _resp, state_3, more_pages, _status_code, _error = self.action.run({}, state_2.copy(), cps_config)
+                self.assertEqual(state_2["last_collection_date"], state_3["last_collection_date"])
+                self.assertEqual(False, more_pages)  # finished pagination now due to split size including all logs
+
+            # Fourth time task is called - custom_config is 'normal' and we do normal time calculations / cut off.
+            new_now = datetime.strptime("2024-03-25T08:00:00", "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc)
+            with patch(
+                "komand_proofpoint_tap.tasks.monitor_events.task.MonitorEvents.get_current_time", return_value=new_now
+            ):
+                mock_logger.reset_mock()
+                config = {"cutoff": {"hours": 24}}  # config reverted to normal behaviour
+                _resp, _state_4, _more_pages, _status_code, _error = self.action.run({}, state_3.copy(), config)
+                # get lookback time as task.py does with using now - (lookback + 1 minute latency delay)
+                lookback = new_now - timedelta(hours=config["cutoff"]["hours"], minutes=1)
+                lookback_msg = f"Last collection date reset to max lookback allowed: {lookback.isoformat()}"
+                self.assertEquals(lookback_msg, mock_logger.call_args_list[2][0][0])
