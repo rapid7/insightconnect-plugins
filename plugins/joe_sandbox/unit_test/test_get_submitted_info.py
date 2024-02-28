d1861c356d2217755a6173c0c8d76fce7a201af4
conor-mcnally <conor_mcnally@rapid7.com>
2024-02-28 12:19:19 +0000

    New action

---
+++ plugins/joe_sandbox/unit_test/test_get_submitted_info.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../'))
+
+from unittest import TestCase
+from icon_joe_sandbox.connection.connection import Connection
+from icon_joe_sandbox.actions.get_submitted_info import GetSubmittedInfo
+import json
+import logging
+
+
+class TestGetSubmittedInfo(TestCase):
+    def test_get_submitted_info(self):
+        """
+        DO NOT USE PRODUCTION/SENSITIVE DATA FOR UNIT TESTS
+
+        TODO: Implement test cases here
+        """
+
+        self.fail("Unimplemented Test Case")
