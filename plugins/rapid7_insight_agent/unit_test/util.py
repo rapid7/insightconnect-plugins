4325821b0f330e57d5b6b1a7a643f89cfcbe97f6
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-03-05 11:28:11 +0100

    Insight Agent - 740 - Updated SDK to the latest version | New action added Get All Agents by IP Address (#2342)

--- plugins/rapid7_insight_agent/unit_test/util.py
+++ plugins/rapid7_insight_agent/unit_test/util.py
@@ -30,45 +30,59 @@ class Util:
     @staticmethod
     def mocked_request(*args, **kwargs):
         class MockResponse:
-            def __init__(self, filename: str = None):
+            HTTP_EXCEPTION = "ExampleHttpException"
+
+            def __init__(self, filename: str = None, text: str = ""):
                 self.filename = filename
+                self.text = text
 
             def json(self):
                 return Util.load_json(f"responses/{self.filename}")
 
             def raise_for_status(self):
-                pass
+                if self.HTTP_EXCEPTION in self.text:
+                    raise Exception(self.HTTP_EXCEPTION)
 
-        if kwargs.get("json").get("query") == "{ organizations(first: 1) { edges { node { id name } } } }":
+        if kwargs.get("json", {}).get("query") == "{ organizations(first: 1) { edges { node { id name } } } }":
             return MockResponse("org_key.json.resp")
-        elif kwargs.get("json").get("variables").get("agentID") == "goodID":
+        elif (
+            kwargs.get("json", {}).get("query")
+            == "query( $orgId:String! ) { organization(id: $orgId) { assets( first: 10000 ) { pageInfo { hasNextPage endCursor } edges { node { id platform host { vendor version description hostNames { name } primaryAddress { ip mac } uniqueIdentity { source id } attributes { key value } } publicIpAddress agent { agentSemanticVersion agentStatus quarantineState { currentState } } } } } } }"
+        ):
+            return MockResponse("get_all_agents_by_ip.resp")
+        elif kwargs.get("json", {}).get("variables", {}).get("agentID") == "goodID":
             return MockResponse("check_agent_status.resp")
-        elif kwargs.get("json").get("variables").get("agentID") == "badID":
+        elif kwargs.get("json", {}).get("variables", {}).get("agentID") == "badID":
             return MockResponse("check_agent_status_bad.resp")
-        elif kwargs.get("json").get("variables").get("agentID") == "goodIDQuarantine":
+        elif kwargs.get("json", {}).get("variables", {}).get("agentID", {}) == "goodIDQuarantine":
             return MockResponse("quarantine.resp")
         elif (
-            kwargs.get("json").get("variables").get("agentID") == "a1cfb273EQWE12312EDSAXZc8e7d46a9e2a0e2dae01a0ce"
+            kwargs.get("json", {}).get("variables", {}).get("agentID")
+            == "a1cfb273EQWE12312EDSAXZc8e7d46a9e2a0e2dae01a0ce"
             and kwargs.get("json").get("query")
             == "query( $orgID: String! $agentID: String! ) { assets( orgId: $orgID ids: [$agentID] ){ agent { id quarantineState{ currentState } agentStatus } } }"
         ):
             return MockResponse("unquarantine_check.resp")
         elif (
-            kwargs.get("json").get("variables").get("agentID") == "a1cfb273EQWE12312EDSAXZc8e7d46a9e2a0e2dae01a0ce"
-            and kwargs.get("json").get("query")
+            kwargs.get("json", {}).get("variables", {}).get("agentID")
+            == "a1cfb273EQWE12312EDSAXZc8e7d46a9e2a0e2dae01a0ce"
+            and kwargs.get("json", {}).get("query")
             == "mutation( $orgID:String! $agentID:String!) { unquarantineAssets( orgId:$orgID assetIds: [$agentID] ) { results { assetId failed } } }"
         ):
             return MockResponse("unquarantine.resp")
-        elif kwargs.get("json").get("variables").get("agentID") == "a1cfb273EQWE12312EDSAXZc8e7d46a9e2a0e2dae01a0ce":
+        elif (
+            kwargs.get("json", {}).get("variables", {}).get("agentID")
+            == "a1cfb273EQWE12312EDSAXZc8e7d46a9e2a0e2dae01a0ce"
+        ):
             return MockResponse("quarantine.resp")
-        elif kwargs.get("json").get("variables").get("agentID") == "badIDQuarantine":
+        elif kwargs.get("json", {}).get("variables", {}).get("agentID") == "badIDQuarantine":
             return MockResponse("quarantine_bad.resp")
-        elif kwargs.get("json").get("variables").get("agentID") == "agent_id":
+        elif kwargs.get("json", {}).get("variables", {}).get("agentID") == "agent_id":
             return MockResponse("quarantine_multiple.resp")
-        elif kwargs.get("json").get("variables").get("agentID") == "agent_id_bad":
+        elif kwargs.get("json", {}).get("variables", {}).get("agentID") == "agent_id_bad":
             return MockResponse("quarantine_multiple_failure.resp")
-        elif kwargs.get("json").get("variables").get("orgId") == "9de5069c5afe602b2ea0a04b66beb2c0":
+        elif kwargs.get("json", {}).get("variables", {}).get("orgId") == "9de5069c5afe602b2ea0a04b66beb2c0":
             return MockResponse("get_agent_details.resp")
-        elif kwargs.get("json").get("variables").get("orgId") == "9de5069c5afe602b2ea0a04b66beb2c0":
+        elif kwargs.get("json", {}).get("variables", {}).get("orgId") == "9de5069c5afe602b2ea0a04b66beb2c0":
             return MockResponse()
-        return "Not implemented"
+        raise Exception("Not implemented")
