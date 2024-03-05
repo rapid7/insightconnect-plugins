fc8d87984872e8cf039c3b98d6ed900ed40dbb43
conor-mcnally <conor_mcnally@rapid7.com>
2024-03-05 13:52:19 +0000

    Black and sca

--- plugins/joe_sandbox/icon_joe_sandbox/actions/list_keyboard_layouts/action.py
+++ plugins/joe_sandbox/icon_joe_sandbox/actions/list_keyboard_layouts/action.py
@@ -1,18 +1,19 @@
 import insightconnect_plugin_runtime
 from .schema import ListKeyboardLayoutsInput, ListKeyboardLayoutsOutput, Input, Output, Component
+
 # Custom imports below
 
 
 class ListKeyboardLayouts(insightconnect_plugin_runtime.Action):
-
     def __init__(self):
         super(self.__class__, self).__init__(
-                name="list_keyboard_layouts",
-                description=Component.DESCRIPTION,
-                input=ListKeyboardLayoutsInput(),
-                output=ListKeyboardLayoutsOutput())
+            name="list_keyboard_layouts",
+            description=Component.DESCRIPTION,
+            input=ListKeyboardLayoutsInput(),
+            output=ListKeyboardLayoutsOutput(),
+        )
 
-    def run(self, params={}):
+    def run(self):
         self.logger.info("Running server_lia_countries")
         locales = self.connection.api.server_languages_and_locales()
         return {Output.KEYBOARD_LAYOUTS: locales}
