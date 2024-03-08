1969a31cb5f3633a7181a160fcdf96ef7f76f0d7
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-03-08 10:56:50 +0100

    Palo Alto Mine Meld - 288 - Action: Update External Dynamic List - Added new fields to the input: Direction, Share Level, and Comment (#2390)

--- plugins/palo_alto_mine_meld/icon_palo_alto_mine_meld/util/api.py
+++ plugins/palo_alto_mine_meld/icon_palo_alto_mine_meld/util/api.py
@@ -1,30 +1,42 @@
+import json
+from logging import Logger
+from typing import Any, Dict, List
+
 import requests
 from insightconnect_plugin_runtime.exceptions import PluginException
-import json
 
 
 class PaloAltoMineMeldAPI:
-    def __init__(self, url: str, username: str, password: str, ssl_verify: bool, logger: object):
+    def __init__(self, url: str, username: str, password: str, ssl_verify: bool, logger: Logger) -> None:
         self.url = url
         self.username = username
         self.password = password
         self.ssl_verify = ssl_verify
         self.logger = logger
 
-    def update_external_dynamic_list(self, list_name, updated_indicators_list):
+    def update_external_dynamic_list(
+        self, list_name: str, updated_indicators_list: List[Dict[str, Any]]
+    ) -> Dict[str, Any]:
         return self._call_api(
             "PUT",
             f"{self.url}/config/data/{list_name}_indicators",
             json_data=updated_indicators_list,
         )
 
-    def get_indicators(self, list_name):
+    def get_indicators(self, list_name: str) -> Dict[str, Any]:
         return self._call_api("GET", f"{self.url}/config/data/{list_name}_indicators")
 
-    def health_check(self):
+    def health_check(self) -> Dict[str, Any]:
         return self._call_api("GET", f"{self.url}/config/full", full_response=True).status_code == 200
 
-    def _call_api(self, method, url, params=None, json_data=None, full_response: bool = False):
+    def _call_api(  # noqa: MC0001
+        self,
+        method: str,
+        url: str,
+        params: Dict[str, Any] = None,
+        json_data: Dict[str, Any] = None,
+        full_response: bool = False,
+    ) -> Dict[str, Any]:
         response = {"text": ""}
         try:
             response = requests.request(
@@ -36,12 +48,7 @@ class PaloAltoMineMeldAPI:
                 verify=self.ssl_verify,
             )
             if response.status_code == 400:
-                raise PluginException(
-                    cause="Bad request.",
-                    assistance="Verify your inputs are correct and try again. If the issue persists, please contact "
-                    "support.",
-                    data=response.text,
-                )
+                raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.text)
             if response.status_code == 401:
                 raise PluginException(preset=PluginException.Preset.USERNAME_PASSWORD)
             if response.status_code == 403:
@@ -56,13 +63,12 @@ class PaloAltoMineMeldAPI:
             if 200 <= response.status_code < 300:
                 if full_response:
                     return response
-
                 return response.json()
 
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
-        except json.decoder.JSONDecodeError as e:
-            self.logger.info(f"Invalid json: {e}")
+        except json.decoder.JSONDecodeError as error:
+            self.logger.info(f"Invalid json: {error}")
             raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=response.text)
-        except requests.exceptions.HTTPError as e:
-            self.logger.info(f"Call to Palo Alto MineMeld failed: {e}")
+        except requests.exceptions.HTTPError as error:
+            self.logger.info(f"Call to Palo Alto MineMeld failed: {error}")
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
