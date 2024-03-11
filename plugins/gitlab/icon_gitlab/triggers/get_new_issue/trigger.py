5d7618337f480cc4b90465ff642e019d1821a735
conor-mcnally <conor_mcnally@rapid7.com>
2024-03-11 15:30:40 +0000

    Trigger run method complexity reduced

--- plugins/gitlab/icon_gitlab/triggers/get_new_issue/trigger.py
+++ plugins/gitlab/icon_gitlab/triggers/get_new_issue/trigger.py
@@ -15,24 +15,13 @@ class GetNewIssue(insightconnect_plugin_runtime.Trigger):
     def run(self, params={}):
         new_issues = []
         seen = []
-        issue_params = []
-
-        # Required: False
-        if params.get(Input.LABELS):
-            issue_params.append(("labels", params.get(Input.LABELS)))
-        if params.get(Input.MILESTONE):
-            issue_params.append(("milestone", params.get(Input.MILESTONE)))
-        if params.get(Input.STATE):
-            issue_params.append(("state", params.get(Input.STATE).lower()))
-        if params.get(Input.SEARCH):
-            issue_params.append(("search", params.get(Input.SEARCH)))
-        if params.get(Input.IIDS):
-            issue_params.append(("iid", params.get(Input.IIDS)))
+        issue_params = self.get_issue_params(params=params)
 
         while True:
             self.logger.info("Searching for new issues.. ")
 
             issues = self.connection.client.get_issues(issue_params=issue_params)
+
             for issue in issues:
                 issue = Util.clean_json(issue)
                 if Util.is_issue_new(issue.get("updated_at", "")):
@@ -48,3 +37,27 @@ class GetNewIssue(insightconnect_plugin_runtime.Trigger):
                     continue
 
             time.sleep(params.get(Input.INTERVAL, 5))
+
+    @staticmethod
+    def get_issue_params(params: dict):
+        """
+        Helper method to retrieve inputs and reduce 'run' method complexity
+
+        :param params:
+        :return:
+        """
+        issue_params = []
+
+        # Required: False
+        if params.get(Input.LABELS):
+            issue_params.append(("labels", params.get(Input.LABELS)))
+        if params.get(Input.MILESTONE):
+            issue_params.append(("milestone", params.get(Input.MILESTONE)))
+        if params.get(Input.STATE):
+            issue_params.append(("state", params.get(Input.STATE).lower()))
+        if params.get(Input.SEARCH):
+            issue_params.append(("search", params.get(Input.SEARCH)))
+        if params.get(Input.IIDS):
+            issue_params.append(("iid", params.get(Input.IIDS)))
+
+        return issue_params
