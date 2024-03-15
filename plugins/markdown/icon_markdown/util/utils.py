3fd05be5d1a0ade3bf5cc4407f06eced468dde7e
conor-mcnally <conor_mcnally@rapid7.com>
2024-03-15 16:02:24 +0000

    Update comment on util

--- plugins/markdown/icon_markdown/util/utils.py
+++ plugins/markdown/icon_markdown/util/utils.py
@@ -31,6 +31,10 @@ def convert(content, from_format, to_format, use_file=False):
         filename = make_file(to_format)
     else:
         filename = None
+    # The extra args will cause unit tests to fail on macos but work on ubuntu-bullseye (jenkins).
+    # ubuntu-bullseye = --atx-headers
+    # macos = --markdown-headings=atx
+    # (This is due to apt not being able to provide the latest version of pandoc)
     output = pypandoc.convert_text(
         content, to_format, format=from_format, outputfile=filename, extra_args=["--atx-headers"]
     )
