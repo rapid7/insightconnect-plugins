603910db614bed214a3fed312b0a0eaf7a400c0a
igorski-r7 <igor_gorski@rapid7.com>
2024-03-28 12:15:42 +0100

    Python 3 Script - 799 - Updated the SDK to the latest version | Updated Python version to 3.9.19 | Fixed issue with invalid unicode character

--- plugins/python_3_script/icon_python_3_script/util/constants.py
+++ plugins/python_3_script/icon_python_3_script/util/constants.py
@@ -3,6 +3,7 @@ DEFAULT_ENCODING = "utf-8"
 DEFAULT_PROCESS_TIMEOUT = 5 * 60
 
 RUN_FUNCTION_TEMPLATE = """import sys
+import json
 from argparse import ArgumentParser
 
 sys.path.append("/var/cache/python_dependencies/lib/python3.9/site-packages")
@@ -18,5 +19,5 @@ password = arguments.password
 secret_key = arguments.secret_key
 
 {function_}
-sys.stdout.write("{execution_id}" + str({function_name}({parameters})))
+sys.stdout.write("{execution_id}" + json.dumps({function_name}({parameters})))
 """
