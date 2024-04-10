9db546c911cd316eabc8457bdfd408464ea5ea67
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-04-10 09:41:01 +0200

    Insight Agent - 817 - Get All Agents by IP Address: Fixed issue where action failed when agent did not have a primary address, and extended output to include agent location details | Get Agent Details: Extended output to include agent's public IP address and location (#2456)

--- plugins/rapid7_insight_agent/unit_test/test_quarantine.py
+++ plugins/rapid7_insight_agent/unit_test/test_quarantine.py
@@ -1,13 +1,16 @@
 import os
 import sys
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from parameterized import parameterized
 
 sys.path.append(os.path.abspath("../"))
+from typing import Any, Dict
+
 from icon_rapid7_insight_agent.actions.quarantine.action import Quarantine
 from icon_rapid7_insight_agent.actions.quarantine.schema import Input
+from jsonschema import validate
 
 from util import Util
 
@@ -15,9 +18,18 @@ from util import Util
 @patch("requests.sessions.Session.post", side_effect=Util.mocked_request)
 class TestQuarantine(TestCase):
     @parameterized.expand(Util.load_json("parameters/quarantine.json.resp").get("parameters"))
-    def test_add_address_to_group(self, mock_post, name, agent_id, interval, quarantine_state, expected):
+    def test_add_address_to_group(
+        self,
+        mock_post: MagicMock,
+        name: str,
+        agent_id: str,
+        interval: int,
+        quarantine_state: str,
+        expected: Dict[str, Any],
+    ) -> None:
         action = Util.default_connector(Quarantine())
         actual = action.run(
             {Input.AGENT_ID: agent_id, Input.INTERVAL: interval, Input.QUARANTINE_STATE: quarantine_state}
         )
+        validate(actual, action.output.schema)
         self.assertEqual(actual, expected)
