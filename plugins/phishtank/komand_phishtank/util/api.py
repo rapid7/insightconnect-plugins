b5523e2bcc241bf7d24a70d51eb123b7d5d9fd7b
Abbie <135721892+abmccann-r7@users.noreply.github.com>
2024-04-15 15:00:35 +0100

    PLGN-761: Phishtank 2.0.0 Release (#2473)
    * [PLGN-761] Phishtank 2.0.0 - Snyk Remediation  (#2411)
    
    * initial push for snyk vuln fixes and updating plugin
    
    * fixing some checks and plugin.spec.yaml
    
    * prospector fix and ran black
    
    * fixing checks
    
    * temp changes and rebasing
    
    * unit tests added
    
    * changes to connection and unit test util
    
    * fixing checks
    
    * more fixes to checks and unit tests
    
    * specify sdk version
    
    * added connection param,fixed unit tests and error handling
    
    * comment fixes
    
    * validator checks and refresh
    
    * json fixes and output field fixes
    
    * fixing unit test import
    
    * comment fixes and additions to action file
    
    * removing unused imports
    
    * more comment fixes
    
    * sdk upgrade
    
    * removing a repeating link in helpmd
    
    * additional fix ups
    
    * fixing checks
    
    * quick variable rename
    
    * error handling for error caught while testing (#2471)

---
+++ plugins/phishtank/komand_phishtank/util/api.py
@@ -0,0 +1,55 @@
+import uuid
+
+from insightconnect_plugin_runtime.exceptions import PluginException
+import requests
+from requests import Response
+from urllib.parse import quote
+from .constants import TIMEOUT, URL
+
+from typing import Dict, Any
+
+
+class API:
+    def __init__(self, credentials: str, username: str) -> None:
+        self.credentials = credentials
+        self.username = username
+
+    def check(self, url: str) -> Dict[str, Any]:
+        if not self.username:
+            self.username = f"rapid7-plugin-{uuid.uuid4()}"
+
+        response = requests.post(
+            URL,
+            data={"format": "json", "url": quote(url), "app_key": self.credentials},
+            headers={"User-Agent": f"phishtank/{self.username}"},
+            timeout=TIMEOUT,
+        )
+        try:
+            self.response_handler(response)
+            if "errortext" in response.text:
+                raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.text)
+            return response.json()
+        except requests.exceptions.JSONDecodeError:
+            raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=response)
+
+    def response_handler(self, response: Response) -> None:
+        """
+        Handles response codes, returning appropriate PluginException Preset
+        """
+        if 200 <= response.status_code < 300:
+            return
+        if response.status_code == 400:
+            raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.text)
+        if response.status_code == 401:
+            raise PluginException(preset=PluginException.Preset.UNAUTHORIZED, data=response.text)
+        if response.status_code == 403:
+            raise PluginException(preset=PluginException.Preset.API_KEY, data=response.text)
+        if response.status_code == 404:
+            raise PluginException(preset=PluginException.Preset.NOT_FOUND, data=response.text)
+        if response.status_code == 409:
+            raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=response.text)
+        if response.status_code == 429:
+            raise PluginException(preset=PluginException.Preset.RATE_LIMIT, data=response.text)
+        if response.status_code >= 500:
+            raise PluginException(preset=PluginException.Preset.SERVER_ERROR, data=response.text)
+        raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
