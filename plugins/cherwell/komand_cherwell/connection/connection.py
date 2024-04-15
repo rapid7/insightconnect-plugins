c022821217a46cc4546e3628887eca1472255c83
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2024-04-15 14:34:37 +0100

    [PLGN-216] Cherwell (#2418)
    * Update SDK version | Update outputs to unrequired | Add unit test skeleton | add parameterized to requirements
    
    * Run black
    
    * Change variable access in util set_field_values
    
    * Set unit test values
    
    * Fix file retrieval address
    
    * Fix secret key in tests
    
    * Fix secret key in tests
    
    * Fix secret key in tests
    
    * Fix unit tests
    
    * Remove print
    
    * Fix import
    
    * Refactor
    
    * Fix unit test
    
    * Update with logging and SSL Verify
    
    * Extra logging
    
    * Update connection
    
    * Refresh | Refactor connection | add unit tets
    
    * Remove print
    
    * Update scheme handling in URL connection input
    
    * Update sdk version

--- plugins/cherwell/komand_cherwell/connection/connection.py
+++ plugins/cherwell/komand_cherwell/connection/connection.py
@@ -1,35 +1,43 @@
-import komand
-from .schema import ConnectionSchema
+import insightconnect_plugin_runtime
+from .schema import ConnectionSchema, Input
+from insightconnect_plugin_runtime.exceptions import ConnectionTestException, PluginException
 
 # Custom imports below
 from komand_cherwell.util.api import Cherwell
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
-        self._base_url = None
         self.api = None
 
     def connect(self, params={}):
 
-        base_uri = params["url"]
+        base_uri = params.get(Input.URL)
         username, password = (
-            params["username_and_password"]["username"],
-            params["username_and_password"]["password"],
+            params.get(Input.USERNAME_AND_PASSWORD, {}).get("username"),
+            params.get(Input.USERNAME_AND_PASSWORD, {}).get("password"),
         )
-        client_id = params["client_id"]["secretKey"]
-        authentication_mode = params["authentication_mode"]
+        client_id = params.get(Input.CLIENT_ID, {}).get("secretKey")
+        authentication_mode = params.get(Input.AUTHENTICATION_MODE)
+        ssl_verify = params.get(Input.SSL_VERIFY)
 
         # Form the base URL for the Cherwell server
-        scheme = "https://" if params["ssl_verify"] else "http://"
-        self._base_url = scheme + base_uri
+        if not (base_uri.startswith("http://") or base_uri.startswith("https://")):
+            self.logger.info("No scheme found, defaulting to use HTTPS.")
+            base_uri = f"https://{base_uri}"
 
-        self.api = Cherwell(self._base_url, self.logger, username, password, client_id, authentication_mode)
+        if base_uri.startswith("http://"):
+            self.logger.info(
+                "Using HTTP may result in server-side errors originating from mishandled parameters. If "
+                "you encounter such errors, it is recommended to use HTTPS."
+            )
+        self.api = Cherwell(base_uri, self.logger, username, password, client_id, authentication_mode, ssl_verify)
 
     def test(self):
         try:
-            _ = self.api.get_serviceinfo()
-        except Exception as e:
-            self.logger.error(f"An error occurred while testing Cherwell credentials: {e}")
-            raise
+            self.api.get_serviceinfo()
+            return {"success": True}
+        except PluginException as exception:
+            self.logger.error(f"An error occurred while testing Cherwell credentials: {exception}")
+            raise ConnectionTestException()
