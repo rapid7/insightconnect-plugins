e39c6253c7131caad44ef1ad570317dd193da214
Abbie <135721892+abmccann-r7@users.noreply.github.com>
2024-04-16 11:49:38 +0100

    PLGN-761: Fixing phishtank connection test (#2476)
    * fixing connection test
    
    * reformatting connection test

--- plugins/phishtank/setup.py
+++ plugins/phishtank/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="phishtank-rapid7-plugin",
-      version="2.0.0",
+      version="2.0.1",
       description="Phishtank is a community-driven anti-phishing site where users submit suspected phishes and other users 'vote' if it is a phish or not. This plugin utilizes the Phishtank API to look up URLs in the PhishTank database",
       author="rapid7",
       author_email="",
