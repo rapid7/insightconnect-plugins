f3830fddc0c49d2aa4307c2411d2fd4627d3fc13
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2024-04-16 14:17:09 +0100

    [PLGN-216] Cherwell (#2418) (#2474)
    * Update SDK version | Update outputs to unrequired | Add unit test skeleton | add parameterized to requirements
    
    * Run black
    
    * Change variable access in util set_field_values
    
    * Set unit test values
    
    * Fix file retrieval address
    
    * Fix secret key in tests
    
    * Fix secret key in tests
    
    * Fix secret key in tests
    
    * Fix unit tests
    
    * Remove print
    
    * Fix import
    
    * Refactor
    
    * Fix unit test
    
    * Update with logging and SSL Verify
    
    * Extra logging
    
    * Update connection
    
    * Refresh | Refactor connection | add unit tets
    
    * Remove print
    
    * Update scheme handling in URL connection input
    
    * Update sdk version

--- plugins/cherwell/komand_cherwell/util/api.py
+++ plugins/cherwell/komand_cherwell/util/api.py
@@ -1,20 +1,24 @@
 import json
-from requests import Session, Request, request
+
+import requests.exceptions
+from requests import Session, Request, request, Response
 from json import JSONDecodeError
 from urllib.parse import urlencode
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 
 class Cherwell:
 
     TOKEN = ""  # noqa: B105
 
-    def __init__(self, base_url, logger, username, password, client_id, authentication_mode):
+    def __init__(self, base_url, logger, username, password, client_id, authentication_mode, ssl_verify):
         self._base_url = base_url
         self.logger = logger
         self.username = username
         self.password = password
         self.client_id = client_id
         self.authentication_mode = authentication_mode
+        self.ssl_verify = ssl_verify
         self.session = Session()
 
     def _call_api(
@@ -30,7 +34,15 @@ class Cherwell:
         url = self._base_url + endpoint
 
         if "Authorization" not in self.session.headers:
-            Cherwell.TOKEN = self._token(self.client_id, self.username, self.password, self.authentication_mode)
+            self.logger.info("Authorization not in session headers, generating token...")
+            self.logger.info(f"Using authentication mode {self.authentication_mode}")
+            Cherwell.TOKEN = self._token(
+                self.client_id,
+                self.username,
+                self.password,
+                self.authentication_mode,
+                self.ssl_verify,
+            )
             self.session.headers.update(
                 {
                     "Authorization": f"Bearer {Cherwell.TOKEN}",
@@ -52,49 +64,42 @@ class Cherwell:
         try:
             # Prep request
             req = req.prepare()
-            resp = self.session.send(req)
-            # Check for custom errors
-            if custom_error and resp.status_code not in range(200, 299):
-                raise Exception(
-                    f"An error was received when running {action_name}."
-                    f"Request status code of {resp.status_code} was returned."
-                    f"{custom_error.get(resp.status_code, custom_error.get(000))}"
-                )
-            elif resp.status_code == 405:
-                raise Exception(
-                    f"An error was received when running {action_name}."
-                    f"Request status code of {resp.status_code} was returned."
-                    "Please make sure connections have been configured correctly"
-                )
-            elif resp.status_code != 200:
-                raise Exception(
-                    f"An error was received when running {action_name}."
-                    f" Request status code of {resp.status_code} was returned."
-                    " Please make sure connections have been configured correctly "
-                    f"as well as the correct input for the action. Response was: {resp.text}"
-                )
-
-        except Exception as e:
-            self.logger.error(f"An error had occurred : {e}" "If the issue persists please contact support")
-            raise
-
-        try:
+            resp = self.session.send(req, verify=self.ssl_verify)
+            self.response_handler(resp, action_name, custom_error)
             results = resp.json()
             return results
+        except requests.exceptions.HTTPError as exception:
+            self.logger.error(f"An error has occurred : {exception}" "If the issue persists please contact support")
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=exception)
         except JSONDecodeError:
-            raise Exception(
-                f"Error: Received an unexpected response from {action_name}"
-                f"(non-JSON or no response was received). Response was: {resp.text}"
-            )
+            raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=resp.text)
 
-    def _token(self, client_id: str, username: str, password: str, authentication_mode: str, debug=False) -> str:
+    def response_handler(self, resp: Response, action_name: str = "", custom_error=None) -> None:
+        if resp.status_code in range(200, 299):
+            return
+        cause_info = f"An error was received when running {action_name}."
+        assistance_info = f"Request status code of {resp.status_code} was returned."
+        data = None
+        if resp.status_code == 405:
+            assistance_info += " Please make sure connections have been configured correctly."
+        elif custom_error and resp.status_code not in range(200, 299):
+            data = f"{custom_error.get(resp.status_code, custom_error.get(000))}"
+        else:
+            assistance_info += (
+                " Please make sure connections have been configured correctly as well as the correct "
+                "input for the action."
+            )
+            data = f"Response was: {resp.text}"
+        raise PluginException(cause=cause_info, assistance=assistance_info, data=data)
+
+    def _token(self, client_id: str, username: str, password: str, authentication_mode: str, verify: bool) -> str:
         """
         Issues an authorization request to the Cherwell server asking for an access token.
         :param client_id: Client ID
         :param username: Cherwell account username
         :param password: Cherwell account password
         :param authentication_mode: Authentication mode used by the Cherwell server
-        :param debug: If true, log the response data
+        :param verify: SSL Verification bool
         :return: Access token as a string
         """
         url = self._base_url + "/CherwellAPI/token"
@@ -114,15 +119,15 @@ class Cherwell:
             "Content-Type": "application/x-www-form-urlencoded",
             "Accept": "application/json",
         }
-
-        response = request("POST", url=url, data=query_params, headers=headers, params=querystring)
+        response = request("POST", url=url, data=query_params, headers=headers, params=querystring, verify=verify)
 
         # Check status code - this is a great early indicator of success/failure. If non-2xx, bail early.
         if response.status_code not in range(200, 299):
-            raise Exception(
-                "Error: Received HTTP %d status code from Cherwell. Please verify your Cherwell server "
-                "status and try again. If the issue persists please contact support. "
-                "Get Access Token - Server response was: %s" % (response.status_code, response.text)
+            raise PluginException(
+                cause=f"Error: Received HTTP {response.status_code} status code from Cherwell."
+                "Please verify your Cherwell server status and try again.",
+                assistance="If the issue persists please contact support.",
+                data=response.text,
             )
 
         # Let's see if we actually have a JSON response from the server. It looks bad if we dump a JSONDecodeError
@@ -130,22 +135,17 @@ class Cherwell:
         try:
             response_data = response.json()
         except JSONDecodeError:
-            raise Exception(
-                "Error: Received an unexpected response from Cherwell during authentication "
-                "(non-JSON or no response was received). Response was: %s" % response.text
-            )
-
-        if debug:
-            self.logger.debug(f"Auth Request Response: {response_data}")
+            raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=response.text)
 
         # Verify the access token is present in the response. If not, there is something wrong.
         if "access_token" in response_data:
-            return response_data["access_token"]
+            return response_data.get("access_token")
         else:
-            raise Exception(
-                "Error: Authentication access token was not present in the authentication response from "
-                "the Cherwell server. Please verify the status of your Cherwell server and try again. "
-                "If the issue persists please contact support."
+            raise PluginException(
+                cause="Error: Authentication access token was not present in the authentication response from "
+                "the Cherwell server.",
+                assistance="Please verify the status of your Cherwell server and try again. "
+                "If the issue persists please contact support.",
             )
 
     def get_businessobjectsummary(self, busOb):
---
+++ plugins/cherwell/unit_test/test_lookup_incident.py
@@ -0,0 +1,88 @@
+import sys
+import os
+
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from komand_cherwell.actions.lookup_incident import LookupIncident
+from komand_cherwell.actions.lookup_incident.schema import LookupIncidentOutput
+from util import Util
+from unittest.mock import patch
+from parameterized import parameterized
+from jsonschema import validate
+
+
+@patch("requests.Session.send", side_effect=Util.mock_request)
+class TestLookupIncident(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connector(LookupIncident())
+
+    @parameterized.expand(
+        [
+            [
+                "Success",
+                Util.read_file_to_dict("inputs/lookup_incident_success.json.inp"),
+                Util.read_file_to_dict("expected/lookup_incident_success.json.exp"),
+            ],
+        ]
+    )
+    def test_lookup_incident(self, mock_request, test_name, input, expected):
+        actual = self.action.run(input)
+        self.assertEqual(expected, actual)
+        validate(actual, LookupIncidentOutput.schema)
+
+    @parameterized.expand(
+        [
+            [
+                "400",
+                Util.read_file_to_dict("inputs/lookup_incident_400.json.inp"),
+                "An error was received when running Lookup Incident.",
+                "Request status code of 400 was returned. Please make sure connections have been configured correctly as well as the correct input for the action.",
+                "Response was: {}",
+            ],
+            [
+                "401",
+                Util.read_file_to_dict("inputs/lookup_incident_401.json.inp"),
+                "An error was received when running Lookup Incident.",
+                "Request status code of 401 was returned. Please make sure connections have been configured correctly as well as the correct input for the action.",
+                "Response was: {}",
+            ],
+            [
+                "403",
+                Util.read_file_to_dict("inputs/lookup_incident_403.json.inp"),
+                "An error was received when running Lookup Incident.",
+                "Request status code of 403 was returned. Please make sure connections have been configured correctly as well as the correct input for the action.",
+                "Response was: {}",
+            ],
+            [
+                "404",
+                Util.read_file_to_dict("inputs/lookup_incident_404.json.inp"),
+                "An error was received when running Lookup Incident.",
+                "Request status code of 404 was returned. Please make sure connections have been configured correctly as well as the correct input for the action.",
+                "Response was: {}",
+            ],
+            [
+                "409",
+                Util.read_file_to_dict("inputs/lookup_incident_409.json.inp"),
+                "An error was received when running Lookup Incident.",
+                "Request status code of 409 was returned. Please make sure connections have been configured correctly as well as the correct input for the action.",
+                "Response was: {}",
+            ],
+            [
+                "500",
+                Util.read_file_to_dict("inputs/lookup_incident_500.json.inp"),
+                "An error was received when running Lookup Incident.",
+                "Request status code of 500 was returned. Please make sure connections have been configured correctly as well as the correct input for the action.",
+                "Response was: {}",
+            ],
+        ]
+    )
+    def test_lookup_incident_errors(self, mock_request, test_name, input_params, cause, assistance, data):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(input_params)
+        self.assertEqual(cause, error.exception.cause)
+        self.assertEqual(assistance, error.exception.assistance)
+        self.assertEqual(data, error.exception.data)
