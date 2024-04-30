316bc187c981563991a0d69e0ea2b1d43ec2e86e
igorski-r7 <igor_gorski@rapid7.com>
2024-04-30 18:09:54 +0200

    SonicWall - 378 - Add Address Object to Group: Fixed issue with payload syntax

--- plugins/sonicwall/icon_sonicwall/actions/add_address_object_to_group/action.py
+++ plugins/sonicwall/icon_sonicwall/actions/add_address_object_to_group/action.py
@@ -28,26 +28,20 @@ class AddAddressObjectToGroup(insightconnect_plugin_runtime.Action):
 
         object_type = self.connection.sonicwall_api.get_object_type(object_name)
         group_type = self.connection.sonicwall_api.get_group_type(group_name)
-        object_action = None
 
         try:
-            object_action = self.connection.sonicwall_api.add_address_object_to_group(
-                group_type,
-                group_name,
-                {
-                    "address_group": {
-                        group_type: {
-                            "name": group_name,
-                            "address_object": {object_type: [{"name": object_name}]},
-                        }
-                    }
-                },
-            )
+            return {
+                Output.OBJECT_ACTION: self.connection.sonicwall_api.add_address_object_to_group(
+                    group_type,
+                    group_name,
+                    object_type,
+                    object_name,
+                )
+            }
         except PluginException as error:
             if "E_NO_MATCH" in error.data:
                 raise PluginException(
                     cause="The address object or address group does not exist in SonicWall.",
                     assistance="Please enter valid names and try again.",
                 )
-
-        return {Output.OBJECT_ACTION: object_action}
+            raise
