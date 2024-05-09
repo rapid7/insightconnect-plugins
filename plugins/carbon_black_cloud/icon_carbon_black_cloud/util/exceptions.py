833d06a8d291005c674e51dbcc2790577af3451d
Johnny O'Neill <139136675+joneill-r7@users.noreply.github.com>
2024-05-09 16:21:35 +0100

    SOAR-16699: raise CB status code to the user and handle 404 scenarios. (#2515) (#2517)

--- plugins/carbon_black_cloud/icon_carbon_black_cloud/util/exceptions.py
+++ plugins/carbon_black_cloud/icon_carbon_black_cloud/util/exceptions.py
@@ -3,3 +3,9 @@ from insightconnect_plugin_runtime.exceptions import PluginException
 
 class RateLimitException(PluginException):
     """Raise when we hit get rate limited so the task can delay running again."""
+
+
+class HTTPErrorException(PluginException):
+    def __init__(self, cause=None, assistance=None, data=None, preset=None, status_code=None):
+        super().__init__(cause, assistance, data, preset)
+        self.status_code = status_code
