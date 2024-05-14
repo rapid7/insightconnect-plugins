33bea4ae5802fd11e43acbee6c30d6888601732d
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2024-05-14 14:56:29 +0100

    [PLGN-366] recorded future (#2512)
    * Update parsing of XML to dict
    
    * Update error handling Memory Error
    
    * Remove unused imports
    
    * Refresh
    
    * Add unit tests | Update handling of GZIP
    
    * fix help.md issues
    
    * Linter
    
    * Help sync
    
    * Revert to previous actions
    
    * Update unit test imports
    
    * Update version
    
    * Update connection-version
    
    * Update to stream and write to file for risk_list
    
    * Update unit tests | Run black
    
    * Update domain risk list
    
    * Update magic_mock status code
    
    * Update unit tests / help.md troubleshooting / docstring
    
    * Remove import

--- plugins/recorded_future/unit_test/test_search_ip_addresses.py
+++ plugins/recorded_future/unit_test/test_search_ip_addresses.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.abspath("../"))
 from typing import Any, Dict
 
 from jsonschema import validate
-from komand_recorded_future.actions.search_ip_addresses import SearchIpAddresses
+from komand_recorded_future.actions.search_IP_addresses import SearchIPAddresses
 from komand_recorded_future.connection.schema import Input
 from parameterized import parameterized
 
@@ -21,7 +21,7 @@ from util import Util
 class TestSearchIpAddresses(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.action = Util.default_connector(SearchIpAddresses())
+        cls.action = Util.default_connector(SearchIPAddresses())
 
     @parameterized.expand(
         [
@@ -69,7 +69,7 @@ class TestSearchIpAddresses(TestCase):
     def test_search_ip_addresses_raise_exception(
         self, mock_request: MagicMock, token: str, input_parameters: Dict[str, Any], cause: str, assistance: str
     ) -> None:
-        action = Util.default_connector(SearchIpAddresses(), {Input.API_KEY: {"secretKey": token}})
+        action = Util.default_connector(SearchIPAddresses(), {Input.API_KEY: {"secretKey": token}})
         with self.assertRaises(PluginException) as error:
             action.run(input_parameters)
         self.assertEqual(error.exception.cause, cause)
