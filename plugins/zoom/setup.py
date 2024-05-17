bc4edb182ba772f26eb8b3b78af8e3dea39987ac
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2024-05-17 17:12:27 +0100

    [SOAR-16986] Zoom Update task to handle pagination error (#2538)
    * Update task to handle pagination error
    
    * Update lint and static code anlysis
    
    * Update version | Update has_more_pages to False for no pagination token returned, and pagination error.
    
    * Update timeout
    
    * Update has_next_pages
    
    * Remove print
    
    * Undo has_next_page update | Undo set last_event_timestamp logic for exiting pagintation

--- plugins/zoom/setup.py
+++ plugins/zoom/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="zoom-rapid7-plugin",
-      version="4.1.9",
+      version="4.1.10",
       description="[Zoom](https://zoom.us) is a cloud platform for video and audio conferencing, chat, and webinars. The Zoom plugin allows you to add and remove users as part of of workflow, while also providing the ability to trigger workflows on new user sign-in and sign-out activity events. This plugin uses the [Zoom API](https://marketplace.zoom.us/docs/api-reference/introduction) and requires a Pro, Business, or Enterprise plan",
       author="rapid7",
       author_email="",
