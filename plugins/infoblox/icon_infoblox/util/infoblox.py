03ce229e564da499668bd55d8cd325c2f8d6a364
conor-mcnally <conor_mcnally@rapid7.com>
2024-05-20 14:19:06 +0100

    Validator fixed and most prospector fixed

--- plugins/infoblox/icon_infoblox/util/infoblox.py
+++ plugins/infoblox/icon_infoblox/util/infoblox.py
@@ -26,38 +26,38 @@ class InfobloxConnection:
         self.logger.info("Validate: Successfully connected to Infoblox instance")
 
     def add_host(self, host):
-        self.logger.info("AddHost: Creating new host: {}".format(str(host)))
+        self.logger.info(f"AddHost: Creating new host: {str(host)}")
         return self._call_api("POST", "record:host", json=host)
 
     def get_host(self, ref):
         ref_url = self._format_url_with_ref("record:host", ref)
-        self.logger.info("GetHost: Getting host: {}".format(ref_url))
+        self.logger.info(f"GetHost: Getting host: {ref_url}")
         return self._call_api("GET", ref_url, params={"_return_fields+": "aliases,extattrs"})
 
     def modify_host(self, ref, updated_host):
         ref_url = self._format_url_with_ref("record:host", ref)
-        self.logger.info("ModifyHost: Updating host: {}".format(ref_url))
+        self.logger.info(f"ModifyHost: Updating host: {ref_url}")
         return self._call_api("PUT", ref_url, json=updated_host)
 
     def delete_host(self, ref):
         ref_url = self._format_url_with_ref("record:host", ref)
-        self.logger.info("DeleteHost: Deleting host: {}".format(ref_url))
+        self.logger.info(f"DeleteHost: Deleting host: {ref_url}")
         return self._call_api("DELETE", ref_url)
 
     def add_fixed_address(self, address):
-        self.logger.info("AddFixedAddress: Adding address: {}".format(str(address)))
+        self.logger.info(f"AddFixedAddress: Adding address: {str(address)}")
         return self._call_api("POST", "fixedaddress", json=address)
 
     def search_by_name(self, name_pattern):
-        self.logger.info("SearchByName: Searching for {}".format(name_pattern))
+        self.logger.info(f"SearchByName: Searching for {name_pattern}")
         return self._call_api("GET", "record:host", params={"name~": name_pattern})
 
     def search_by_ip(self, ip):
-        self.logger.info("SearchByIP: Searching for {}".format(ip))
+        self.logger.info(f"SearchByIP: Searching for {ip}")
         return self._call_api("GET", "ipv4address", params={"ip_address": ip, "status": "USED"})
 
     def search_by_mac(self, mac):
-        self.logger.info("SearchByMAC: Searching for {}".format(mac))
+        self.logger.info(f"SearchByMAC: Searching for {mac}")
         return self._call_api("GET", "fixedaddress", params={"mac": mac, "_return_fields+": "mac"})
 
     @staticmethod
@@ -74,8 +74,8 @@ class InfobloxConnection:
             params = {}
         params["_return_as_object"] = 1
 
-        api_url = "{}/wapi/v{}/{}".format(self.host, self.api_version, url)
-        self.logger.info("CallAPI: Calling {} (json: {}, params: {})".format(api_url, json, params))
+        api_url = f"{self.host}/wapi/v{self.api_version}/{url}"
+        self.logger.info(f"CallAPI: Calling {api_url} (json: {json}, params: {params})")
 
         try:
             response = request(
@@ -88,9 +88,7 @@ class InfobloxConnection:
             )
             response.raise_for_status()
         except Exception:
-            message = "Requests: Failed to call {} (status {}):\n{}".format(
-                api_url, response.status_code, response.json()
-            )
+            message = f"Requests: Failed to call {api_url} (status {response.status_code}):\n{response.json()}"
             self.logger.error(message)
             raise Exception(message)
 
