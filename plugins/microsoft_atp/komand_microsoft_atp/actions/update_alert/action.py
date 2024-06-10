3a8bd920a9946361d4b74fc67539684ebdfa6234
ewilson-r7 <eric_wilson@rapid7.com>
2024-06-10 12:13:14 -0500

    Update api.py with new endpoint | action.py

--- plugins/microsoft_atp/komand_microsoft_atp/actions/update_alert/action.py
+++ plugins/microsoft_atp/komand_microsoft_atp/actions/update_alert/action.py
@@ -1,23 +1,25 @@
 import insightconnect_plugin_runtime
 from .schema import UpdateAlertInput, UpdateAlertOutput, Input, Output, Component
+
 # Custom imports below
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 
 class UpdateAlert(insightconnect_plugin_runtime.Action):
-
     def __init__(self):
         super(self.__class__, self).__init__(
-                name="update_alert",
-                description=Component.DESCRIPTION,
-                input=UpdateAlertInput(),
-                output=UpdateAlertOutput())
+            name="update_alert", description=Component.DESCRIPTION, input=UpdateAlertInput(), output=UpdateAlertOutput()
+        )
 
     def run(self, params={}):
         # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
         alert_fields = params.get(Input.ALERT_FIELDS)
         alert_id = params.get(Input.ALERT_ID)
         # END INPUT BINDING - DO NOT REMOVE
+        self.logger.info("Running...")
+        self.logger.info(f"alert_id: {alert_id}")
+        self.logger.info(f"payload: {alert_fields}")
 
         return {
-            Output.ALERT: None,
+            Output.ALERT: self.connection.client.update_alert(alert_id, payload=alert_fields),
         }
