7ad600c63a4d474b84a940e328717350a9beeafe
Eric Wilson <74607561+ewilson-r7@users.noreply.github.com>
2024-06-12 08:20:20 -0500

    microsoft_atp-5.2.0-release (#2580) (#2582)
    * Defender_ATP - NOTICKET - Add New Action: Update Alert (#2580)
    
    * Update spec file to add new action
    
    * Update api.py with new endpoint | action.py
    
    * reformat api.py
    
    * Fix punctuation in spec file and refresh plugin
    
    * Fix field titles
    
    * Remove unnecessary logging
    
    * update Dockerfile to use latest base image

---
+++ plugins/microsoft_atp/komand_microsoft_atp/actions/update_alert/action.py
@@ -0,0 +1,23 @@
+import insightconnect_plugin_runtime
+from .schema import UpdateAlertInput, UpdateAlertOutput, Input, Output, Component
+
+# Custom imports below
+from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+class UpdateAlert(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="update_alert", description=Component.DESCRIPTION, input=UpdateAlertInput(), output=UpdateAlertOutput()
+        )
+
+    def run(self, params={}):
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
+        alert_fields = params.get(Input.ALERT_FIELDS)
+        alert_id = params.get(Input.ALERT_ID)
+        # END INPUT BINDING - DO NOT REMOVE
+        self.logger.info("Running...")
+
+        return {
+            Output.ALERT: self.connection.client.update_alert(alert_id, payload=alert_fields),
+        }
