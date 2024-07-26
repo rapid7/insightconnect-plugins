eed768c48882b4711a50d5e45f4c806e0a936603
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-07-26 12:23:16 +0200

    Palo Alto Cortex - Get Alerts: Fixed issue where trigger was failing due to empty or wrong typed output fields (#2535)

--- plugins/palo_alto_cortex_xdr/unit_test/test_api.py
+++ plugins/palo_alto_cortex_xdr/unit_test/test_api.py
@@ -14,10 +14,10 @@ class TestAPI(TestCase):
         api = CortexXdrAPI(1, "key", "https://fqdn", "ADVANCED", logging.getLogger())
 
         result = api._get_endpoint_type("1.2.3.4")
-        self.assertEquals(result, api.ENDPOINT_IP_TYPE)
+        self.assertEqual(result, api.ENDPOINT_IP_TYPE)
 
         result = api._get_endpoint_type("01243654cdf0444392f58ed0d251eeda")
-        self.assertEquals(result, api.ENDPOINT_ID_TYPE)
+        self.assertEqual(result, api.ENDPOINT_ID_TYPE)
 
         result = api._get_endpoint_type("something_else")
-        self.assertEquals(result, api.ENDPOINT_HOSTNAME_TYPE)
+        self.assertEqual(result, api.ENDPOINT_HOSTNAME_TYPE)
