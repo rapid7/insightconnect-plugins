7f1bac1386139fcc22d00776197d849dd1170741
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-07-26 12:23:16 +0200

    Palo Alto Cortex - Get Alerts: Fixed issue where trigger was failing due to empty or wrong typed output fields (#2535)

--- plugins/palo_alto_cortex_xdr/icon_palo_alto_cortex_xdr/triggers/get_alerts/trigger.py
+++ plugins/palo_alto_cortex_xdr/icon_palo_alto_cortex_xdr/triggers/get_alerts/trigger.py
@@ -28,7 +28,6 @@ class GetAlerts(insightconnect_plugin_runtime.Trigger):
 
         while True:
             alerts = self.connection.xdr_api.get_alerts(from_time=start_time, to_time=end_time)
-
             for alert_time in Util.send_items_to_platform_for_trigger(
                 self, alerts, Output.ALERT, last_event_processed_time_ms, alert_timestamp_field
             ):
