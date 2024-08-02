b0486c63182aae56a14d2c5153a0c922b11fe996
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-08-02 12:11:00 +0200

    Base64 - Updated SDK to the latest version (#2680)

---
+++ plugins/base64/unit_test/test_decode.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+
+from komand_base64.actions.decode import Decode
+from komand_base64.actions.decode.schema import Input, Output
+
+STUB_INPUT_PARAMS = {Input.BASE64: "UmFwaWQ3IEluc2lnaHRDb25uZWN0Cgo=", Input.ERRORS: "nothing"}
+
+
+class TestDecode(TestCase):
+    def setUp(self) -> None:
+        self.action = Decode()
+
+    def test_decode(self):
+        response = self.action.run(STUB_INPUT_PARAMS)
+        expected = "Rapid7 InsightConnect\n\n"
+        self.assertEqual(expected, response.get(Output.DATA))
