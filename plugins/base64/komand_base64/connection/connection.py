c775465ad697841aedfe79614f411b66ebc5e4b0
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-08-02 12:11:00 +0200

    Base64 - Updated SDK to the latest version (#2680)

--- plugins/base64/komand_base64/connection/connection.py
+++ plugins/base64/komand_base64/connection/connection.py
@@ -6,5 +6,5 @@ class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
-    def connect(self, params):
+    def connect(self, params={}):
         pass
