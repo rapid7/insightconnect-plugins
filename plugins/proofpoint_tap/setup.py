0c50e209d3705f4f9aa749e72c508a2d88262d69
rmurray-r7 <ryanj_murray@rapid7.com>
2024-08-15 16:15:04 +0100

    Connection Test added & SDK Bump (#2710)

--- plugins/proofpoint_tap/setup.py
+++ plugins/proofpoint_tap/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="proofpoint_tap-rapid7-plugin",
-      version="4.1.9",
+      version="4.1.10",
       description="[Proofpoint Targeted Attack Protection](https://www.proofpoint.com/us/products/ransomware-and-targeted-attack-protection) (TAP) helps you stay ahead of attackers with an innovative approach that detects, analyzes and blocks advanced threats before they reach your inbox. This plugin enables users to parse TAP alerts",
       author="rapid7",
       author_email="",
