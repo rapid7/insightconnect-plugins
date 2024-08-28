da5d8e5331361fe011c60b5a3e1e10913dc9a012
Eric Wilson <74607561+ewilson-r7@users.noreply.github.com>
2024-08-28 10:17:40 -0500

    NOTICKET-AZURE AD-New Action-List Members (#2736)
    * Initial Commit
    
    Add new action
    
    * Update Output types
    
    * Update data types with existing values
    
    * Update action to handle null values
    
    * Update script logic to utilize existing helpers/constants
    
    * Update Import functions
    
    * Fix lint issues

--- plugins/azure_ad_admin/icon_azure_ad_admin/util/constants.py
+++ plugins/azure_ad_admin/icon_azure_ad_admin/util/constants.py
@@ -5,3 +5,4 @@ class Endpoint:
     USER_ID = GRAPH_MICROSOFT_URL + "users/{}"
     DEVICE_ID = GRAPH_MICROSOFT_URL + "devices/{device_id}"
     DEVICES = GRAPH_MICROSOFT_URL + "devices"
+    MEMBERS = GRAPH_MICROSOFT_URL + "groups/{group_id}/members?$count=true"
