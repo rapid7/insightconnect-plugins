b08f2e32017d3d4256a465a7f59343d47ce49118
conor-mcnally <conor_mcnally@rapid7.com>
2024-08-28 12:04:17 +0100

    Change PluginException to APIException

--- plugins/okta/komand_okta/util/api.py
+++ plugins/okta/komand_okta/util/api.py
@@ -200,10 +200,11 @@ class OktaAPI:
 
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
         except requests.exceptions.ConnectionError as error:
-            raise PluginException(
+            raise ApiException(
                 cause="The connection has failed, perhaps due to an invalid subdomain.",
                 assistance="Please ensure the subdomain conforms to these potential formats: \n./*.okta.com ./*.oktapreview.com ./*.okta-emea.com",
                 data=error,
+                status_code=401,
             )
         except requests.exceptions.HTTPError as error:
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=error)
