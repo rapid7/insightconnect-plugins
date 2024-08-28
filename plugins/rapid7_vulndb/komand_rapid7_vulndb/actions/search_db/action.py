61d2436942201438dd04558eba7323716ca6b3db
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-08-28 09:53:37 +0200

    Rapid7 Vulndb - 16970 - Initial updates for fedramp compliance | Updated SDK to the latest version (#2734)

--- plugins/rapid7_vulndb/komand_rapid7_vulndb/actions/search_db/action.py
+++ plugins/rapid7_vulndb/komand_rapid7_vulndb/actions/search_db/action.py
@@ -3,6 +3,7 @@ from .schema import SearchDbInput, SearchDbOutput, Input, Output
 
 # Custom imports below
 from komand_rapid7_vulndb.util import extract
+from insightconnect_plugin_runtime.helper import clean
 
 
 class SearchDb(insightconnect_plugin_runtime.Action):
@@ -15,20 +16,21 @@ class SearchDb(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        # Get params
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
         search_for = params.get(Input.SEARCH)
-        db = params.get(Input.DATABASE)
-        responses = extract.Search.get_results(search_for, db)
-        results = []
+        database = params.get(Input.DATABASE)
+        # END INPUT BINDING - DO NOT REMOVE
 
+        responses = extract.Search.get_results(search_for, database)
+
+        results = []
         for response in responses:
             identifier = response.get("identifier")
             if not identifier:
                 continue
-
             dict_response = response.copy()
             dict_response["solutions"] = extract.Content.get(identifier).get("solutions")
-            results.append(dict_response)
+            results.append(clean(dict_response))
 
         return {
             Output.RESULTS_FOUND: len(results) > 0,
--- plugins/rapid7_vulndb/komand_rapid7_vulndb/connection/connection.py
+++ plugins/rapid7_vulndb/komand_rapid7_vulndb/connection/connection.py
@@ -4,15 +4,20 @@ from insightconnect_plugin_runtime.exceptions import ConnectionTestException
 
 # Custom imports below
 import requests
+from typing import Dict, Any
+from komand_rapid7_vulndb.util.extract import TIMEOUT
 
 
 class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
-    def connect(self, params):
-        result = requests.get("https://vdb-kasf1i23nr1kl2j4.rapid7.com/v1/info")
-        if result.status_code == requests.codes.ok:
+    def connect(self, params={}) -> None:
+        pass
+
+    @staticmethod
+    def test() -> Dict[str, Any]:
+        response = requests.get("https://vdb-kasf1i23nr1kl2j4.rapid7.com/v1/info", timeout=TIMEOUT)
+        if response.status_code == requests.codes.ok:
             return {"success": True}
-        else:
-            raise ConnectionTestException(preset=ConnectionTestException.Preset.SERVICE_UNAVAILABLE)
+        raise ConnectionTestException(preset=ConnectionTestException.Preset.SERVICE_UNAVAILABLE)
