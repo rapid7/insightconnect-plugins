23e0b9b1ec23a808c5a377e578450dc8baeeab03
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2024-08-29 11:56:37 +0100

    Topdesk | Create Incidents Bug fix (#2729)
    * Update spec file
    
    * Topdesk Refresh
    
    * initial commit
    
    * Added handling for no caller input | remove responded when no status
    
    * Update version in version history
    
    * Update unit test imports
    
    * Update help.md examples
    
    * Fix help md example again ..
    
    * Update create incidents unit tests
    
    * Remove duplicate logging line
    
    * New line in spec file, merge two funcs to one
    
    * key and value

--- plugins/topdesk/icon_topdesk/actions/createIncident/action.py
+++ plugins/topdesk/icon_topdesk/actions/createIncident/action.py
@@ -4,6 +4,7 @@ from .schema import CreateIncidentInput, CreateIncidentOutput, Input, Output, Co
 # Custom imports below
 from icon_topdesk.util.helpers import prepare_incident_payload
 from icon_topdesk.util.constants import INCIDENT_STATUS
+from insightconnect_plugin_runtime.exceptions import PluginException
 
 
 class CreateIncident(insightconnect_plugin_runtime.Action):
@@ -16,7 +17,17 @@ class CreateIncident(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
+        # Check if no caller or callerLookup parameters provided
+        if params.get(Input.CALLER, {}) == {} and params.get(Input.CALLERLOOKUP, "") == "":
+            raise PluginException(
+                cause="No caller or caller lookup provided",
+                assistance="One of these inputs must be supplied to create an incident.\nThe caller parameter is automatically filled in when the caller lookup parameter is provided.",
+            )
+
         parameters = prepare_incident_payload(params.copy())
-        parameters["status"] = INCIDENT_STATUS.get(params.get(Input.STATUS))
         self.logger.info(f"Creating an incident with the following parameters: \n{parameters}\n")
+
+        if parameters.get("status", ""):
+            parameters["status"] = INCIDENT_STATUS.get(params.get(Input.STATUS))
+
         return {Output.INCIDENT: self.connection.api_client.create_incident(parameters)}
