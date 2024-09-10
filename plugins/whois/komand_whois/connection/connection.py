4bc3f45dabe58ffec5d459e5ae0e5cd7fa495c9c
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-09-10 09:02:41 +0200

    WHOIS - 16964 - Initial updates for fedramp compliance | Updated SDK to the latest version (#2775)

--- plugins/whois/komand_whois/connection/connection.py
+++ plugins/whois/komand_whois/connection/connection.py
@@ -1,10 +1,14 @@
 import insightconnect_plugin_runtime
 from .schema import ConnectionSchema
+from typing import Dict, Any
 
 
 class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
-    def connect(self, params):
+    def connect(self, params={}) -> None:
         pass
+
+    def test(self) -> Dict[str, Any]:
+        return {"success": True}
