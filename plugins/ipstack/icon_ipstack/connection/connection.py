e644ca7b18a4c58e2c1d4cc9e4dec46e7a9626a2
rjmurray <ryanj_murray@rapid7.com>
2024-09-20 17:03:44 +0100

    Adding connection test, SDK bump and Plugin refresh

--- plugins/ipstack/icon_ipstack/connection/connection.py
+++ plugins/ipstack/icon_ipstack/connection/connection.py
@@ -1,4 +1,7 @@
+import json
+
 import insightconnect_plugin_runtime
+from insightconnect_plugin_runtime.exceptions import PluginException
 from .schema import ConnectionSchema, Input
 
 # Custom imports below
@@ -13,4 +16,23 @@ class Connection(insightconnect_plugin_runtime.Connection):
         self.token = params.get(Input.CRED_TOKEN).get("secretKey")
 
     def test(self):
-        return {"success": True}
+        # Sending a request using the secretKey to test if it is valid
+        url = "http://api.ipstack.com/" + "rapid7.com" + "?access_key=" + self.token + "&output=json"
+
+        invalid_status_codes = {
+            "404": "The requested resource does not exist, Error 404",
+            "101": "The access key is blank or invalid",
+            "102": "The access key was recognized but the user account is not active",
+            "104": "The maximum monthly ip lookups has been hit",
+            "106": "The supplied host address/domain is invalid"
+        }
+
+        resp = insightconnect_plugin_runtime.helper.open_url(url)
+        dic = json.loads(resp.read())
+        if "error" not in dic:
+            return {"success": True}
+        else:
+            for error, cause in invalid_status_codes.items():
+                self.logger.info(f"Error: {error} . {cause}")
+                raise PluginException(cause=cause, data=dic)
+                break
