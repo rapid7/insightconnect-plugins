b8c29593175fcf1e81cbfc660966c5f3eaa77d89
igorski-r7 <igor_gorski@rapid7.com>
2024-09-24 12:30:59 +0200

    Python 3 Script - 16967 - Updated package installation

--- plugins/python_3_script/icon_python_3_script/connection/connection.py
+++ plugins/python_3_script/icon_python_3_script/connection/connection.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from subprocess import CalledProcessError, TimeoutExpired, check_output, run  # noqa: B404
 
@@ -5,6 +6,7 @@ import insightconnect_plugin_runtime
 from insightconnect_plugin_runtime.exceptions import ConnectionTestException
 
 from .schema import ConnectionSchema, Input
+from typing import Dict, Any
 
 
 class Connection(insightconnect_plugin_runtime.Connection):
@@ -13,50 +15,47 @@ class Connection(insightconnect_plugin_runtime.Connection):
         self.dependencies, self.timeout = None, None
         self.script_credentials = None
 
-    def connect(self, params={}):
-        self.timeout = params.get(Input.TIMEOUT)
-        self.dependencies = params.get(Input.MODULES)
+    def connect(self, params={}) -> None:
+        self.timeout = params.get(Input.TIMEOUT, 60)
+        self.dependencies = params.get(Input.MODULES, [])
         self.script_credentials = {
             "username": params.get(Input.SCRIPT_USERNAME_AND_PASSWORD, {}).get("username"),
             "password": params.get(Input.SCRIPT_USERNAME_AND_PASSWORD, {}).get("password"),
             "secret_key": params.get(Input.SCRIPT_SECRET_KEY, {}).get("secretKey"),
         }
 
-    def test(self):
-        connection_test_output = {"success": True}
-
+    def test(self) -> Dict[str, Any]:
         self.logger.info("[*] Performing Python version check...")
-        check = str(check_output(["python", "--version"]), "utf-8")  # noqa: B607,B603
-        self.logger.info(check)
+        python_version = str(check_output(["python", "--version"]), "utf-8")  # noqa: B607,B603
+        self.logger.info(python_version)
 
-        if "Python 3." not in check:
+        if "Python 3." not in python_version:
             raise ConnectionTestException(cause="[-] Python 3 is not installed correctly")
 
-        if not self.dependencies:
-            return connection_test_output
-
-        self.logger.info(f"[*] Installing user-specified dependencies ({self.dependencies})...")
-        self.install_dependencies()
-        self.logger.info("[*] Dependencies installed!\n")
-        return connection_test_output
+        if self.dependencies:
+            self.logger.info(f"[*] Installing user-specified dependencies ({self.dependencies})...")
+            self.install_dependencies()
+            self.logger.info("[*] Dependencies installed!\n")
+        return {"success": True}
 
     @staticmethod
-    def _set_pythonuserbase():
+    def _set_python_userbase() -> None:
         os.environ.update({"PYTHONUSERBASE": "/var/cache/python_dependencies"})
 
-    def install_dependencies(self):
-        dependencies = " ".join(self.dependencies)
-        command = f"pip install --user {dependencies}"
-
-        self._set_pythonuserbase()
-
+    def install_dependencies(self) -> None:
+        self._set_python_userbase()
         try:
-            run(args=command.split(" "), capture_output=True, timeout=self.timeout, check=True)  # noqa: B603
+            run(  # noqa: B603
+                args=[sys.executable, "-m", "pip", "install"] + self.dependencies,
+                capture_output=True,
+                timeout=self.timeout,
+                check=True,
+            )
         except TimeoutExpired:
             raise ConnectionTestException(
                 cause="Error: Installing Python dependencies exceeded timeout", assistance="Consider increasing timeout"
             )
         except CalledProcessError as error:
             raise ConnectionTestException(
-                cause=f"Error: Non-zero exit code returned. Message: {error.output.decode('UTF8')}"
+                cause=f"Error: Non-zero exit code returned. Message: {error.output.decode('utf-8')}"
             )
