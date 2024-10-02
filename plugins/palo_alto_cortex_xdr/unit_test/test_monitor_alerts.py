6a108828740162aeb67445fca3fa24b2c3ace158
conor-mcnally <conor_mcnally@rapid7.com>
2024-10-02 11:20:59 +0100

    Refactor intial run unit test

--- plugins/palo_alto_cortex_xdr/unit_test/test_monitor_alerts.py
+++ plugins/palo_alto_cortex_xdr/unit_test/test_monitor_alerts.py
@@ -15,12 +15,20 @@ from parameterized import parameterized
 from jsonschema import validate
 from freezegun import freeze_time
 from util import Util
-from mock import mock_request_200, mocked_request
+from mock import mock_request_200
+
+STUB_STATE_EXPECTED = {
+    "last_alert_time": 1706540499609,
+    "last_alert_hash": ["a502a9c50798186882ad8dc91ac2b38eb185c404"],
+}
 
 
+@freeze_time("2024-01-29T15:01:00.000000Z")
+@patch("requests.Session.send", side_effect=mock_request_200)
 class TestMonitorAlerts(TestCase):
     @classmethod
-    def setUpClass(cls) -> None:
+    @patch("requests.Session.send", side_effect=mock_request_200)
+    def setUpClass(cls, mock_post) -> None:
         _, cls.task = Util.default_connector(
             MonitorAlerts(),
             connect_params={
@@ -31,32 +39,42 @@ class TestMonitorAlerts(TestCase):
             },
         )
 
-    @parameterized.expand(Util.load_parameters("monitor_alerts").get("parameters"))
-    @patch("requests.Session.send", side_effect=mock_request_200)
+    @parameterized.expand(
+        [
+            [
+                "initial_run_has_no_more_pages",
+                {},
+                {},
+                False,
+                Util.load_expected("monitor_alerts"),
+                STUB_STATE_EXPECTED,
+                200,
+                None,
+            ],
+        ]
+    )
     def test_monitor_alerts(
         self,
+        mock_post,
         test_name,
-        start_time,
-        input_state,
+        state,
+        custom_config,
         expected_has_more_pages,
         expected_output,
         expected_state,
         expected_status_code,
         expected_error,
-        mock_post,
     ) -> None:
 
-        with freeze_time(start_time):
-
-            mocked_request(mock_post)
-
-            actual, actual_state, has_more_pages, status_code, error = self.task.run(state=input_state)
-            self.assertEqual(actual, expected_output)
-            self.assertEqual(actual_state, expected_state)
-            self.assertEqual(has_more_pages, expected_has_more_pages)
-            self.assertEqual(status_code, expected_status_code)
-            self.assertEqual(error, expected_error)
-            validate(actual, MonitorAlertsOutput.schema)
+        output, state, has_more_pages, status_code, error = self.task.run(
+            params={}, state=state, custom_config=custom_config
+        )
+        self.assertEqual(output, expected_output)
+        self.assertEqual(state, expected_state)
+        self.assertEqual(has_more_pages, expected_has_more_pages)
+        self.assertEqual(status_code, expected_status_code)
+        self.assertEqual(error, expected_error)
+        validate(output, MonitorAlertsOutput.schema)
 
 
 # @parameterized.expand(Util.load_parameters("monitor_alerts_custom_config").get("parameters"))
@@ -85,21 +103,64 @@ class TestMonitorAlerts(TestCase):
 #         self.assertEqual(error, expected_error)
 
 # @parameterized.expand(Util.load_parameters("monitor_alerts_error").get("parameters"))
+# @patch("requests.Session.send", side_effect=mock_request_500)
+
 # def test_monitor_alerts_error(
 #     self,
-#     mock_request: Mock,
-#     mock_post: Mock,
 #     test_name,
 #     start_time,
 #     input_state,
 #     expected_has_more_pages,
+#     expected_output,
+#     expected_state,
 #     expected_status_code,
 #     expected_error,
+#     mock_post,
 # ) -> None:
 #     with freeze_time(start_time):
+
+#         mocked_request(mock_post)
+
 #         actual, actual_state, has_more_pages, status_code, error = self.task.run(state=input_state)
+#         self.assertEqual(actual, expected_output)
+#         self.assertEqual(actual_state, expected_state)
 #         self.assertEqual(has_more_pages, expected_has_more_pages)
 #         self.assertEqual(status_code, expected_status_code)
-#         self.assertEqual(error.cause, expected_error.get("cause"))
-#         self.assertEqual(error.data, expected_error.get("data"))
+#         self.assertEqual(error, expected_error)
 #         validate(actual, MonitorAlertsOutput.schema)
+
+# @parameterized.expand(
+#         [
+#             [
+#                 "400",
+#                 {},
+#                 400,
+#                 "The server is unable to process the request.",
+#                 "Verify your plugin input is correct and not malformed and try again. If the issue persists, please contact support.",
+#             ]
+#         ]
+# )
+# @patch("requests.Session.send", side_effect=mock_request_error)
+
+
+# def test_monitor_logs_api_errors(
+
+#     self,
+#     test_name,
+#     start_time,
+#     input_state,
+#     expected_status_code,
+#     expected_cause,
+#     expected_assistance,
+#     mock_post
+# ):
+#     with freeze_time(start_time):
+
+#         output, state, has_more_pages, status_code, error = self.task.run(state=input_state)
+
+#         mocked_request(mock_post)
+
+#         self.assertEqual(False, has_more_pages)
+#         self.assertEqual(expected_status_code, status_code)
+#         self.assertEqual(expected_cause, error.cause)
+#         self.assertEqual(expected_assistance, error.assistance)
