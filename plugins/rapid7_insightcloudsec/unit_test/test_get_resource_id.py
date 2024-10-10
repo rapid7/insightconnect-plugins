528b085b80a835f1c89dcc4ca02ee25271b4e8a1
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2024-10-10 14:54:47 +0200

    resolving comments from CR

--- plugins/rapid7_insightcloudsec/unit_test/test_get_resource_id.py
+++ plugins/rapid7_insightcloudsec/unit_test/test_get_resource_id.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from insightconnect_plugin_runtime.exceptions import PluginException
 
@@ -21,13 +21,30 @@ class TestGetResourceId(TestCase):
         cls.action = Util.default_connector(GetResourceId())
 
     @parameterized.expand(Util.load_parameters("get_resource_id").get("parameters"))
-    def test_get_resource_id(self, mock_request, name, limit, offset, search_string, expected):
+    def test_get_resource_id(
+        self,
+        mock_request: MagicMock,
+        name: str,
+        limit: int,
+        offset: int,
+        search_string: str,
+        expected: dict,
+    ):
         actual = self.action.run({Input.LIMIT: limit, Input.OFFSET: offset, Input.SEARCH_STRING: search_string})
         validate(actual, self.action.output.schema)
         self.assertEqual(actual, expected)
 
     @parameterized.expand(Util.load_parameters("get_resource_id_bad").get("parameters"))
-    def test_get_resource_id_bad(self, mock_request, name, limit, offset, search_string, cause, assistance):
+    def test_get_resource_id_bad(
+        self,
+        mock_request: MagicMock,
+        name: str,
+        limit: int,
+        offset: int,
+        search_string: str,
+        cause: str,
+        assistance: str,
+    ):
         with self.assertRaises(PluginException) as error:
             self.action.run({Input.LIMIT: limit, Input.OFFSET: offset, Input.SEARCH_STRING: search_string})
         self.assertEqual(error.exception.cause, cause)
