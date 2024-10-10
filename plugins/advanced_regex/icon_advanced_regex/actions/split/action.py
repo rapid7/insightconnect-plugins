bceabcddac802365294eb4fd767b25aed22e4355
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-10-10 19:00:30 +0200

    Advanced Regex - 17800 - Initial updates for fedramp compliance | Updated SDK to the latest version (#2839) (#2856)

--- plugins/advanced_regex/icon_advanced_regex/actions/split/action.py
+++ plugins/advanced_regex/icon_advanced_regex/actions/split/action.py
@@ -16,8 +16,11 @@ class Split(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        flags = shared.constructFlags(params)
-        regex = params.get(Input.IN_REGEX)
-        split = re.split(regex, params.get(Input.IN_STRING), maxsplit=params.get(Input.MAX_SPLIT), flags=flags)
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
+        regex = params.get(Input.IN_REGEX, "")
+        input_string = params.get(Input.IN_STRING, "")
+        max_split = params.get(Input.MAX_SPLIT)
+        # END INPUT BINDING - DO NOT REMOVE
 
+        split = re.split(regex, input_string, maxsplit=max_split, flags=shared.construct_flags(params))
         return {Output.RESULT: split}
---
+++ plugins/advanced_regex/unit_test/test_replace.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from typing import Any, Dict
+from unittest import TestCase
+
+from icon_advanced_regex.actions.replace import Replace
+from icon_advanced_regex.actions.replace.schema import Input, Output
+from jsonschema import validate
+from parameterized import parameterized
+
+
+class TestReplace(TestCase):
+    def setUp(self) -> None:
+        self.action = Replace()
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    Input.ASCII: False,
+                    Input.DOTALL: False,
+                    Input.IGNORECASE: False,
+                    Input.IN_REGEX: "lorem",
+                    Input.IN_STRING: "Lorem ipsum dolor sit amet, consectetur \nadipiscing elit. Aliquam sapien ex, lorems odales",
+                    Input.MAX_REPLACE: 0,
+                    Input.MULTILINE: False,
+                    Input.REPLACE_STRING: "REPLACED",
+                },
+                {
+                    Output.RESULT: "Lorem ipsum dolor sit amet, consectetur \nadipiscing elit. Aliquam sapien ex, REPLACEDs odales"
+                },
+            )
+        ]
+    )
+    def test_replace(self, parameters: Dict[str, Any], expected: Dict[str, Any]) -> None:
+        response = self.action.run(parameters)
+        validate(response, self.action.output.schema)
+        self.assertEqual(response, expected)
