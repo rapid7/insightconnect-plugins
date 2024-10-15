ae5b385205be321bce468b1f4930fa22673b6f01
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-10-15 14:03:12 +0200

    Hashit - 17803 - Initial updates for fedramp compliance | Updated SDK to the latest version (#2847) (#2861)

--- plugins/hashit/icon_hashit/actions/string/action.py
+++ plugins/hashit/icon_hashit/actions/string/action.py
@@ -5,6 +5,7 @@ from .schema import StringInput, StringOutput, Input, Output, Component
 
 
 # Custom imports below
+DEFAULT_ENCODING = "utf-8"
 
 
 class String(insightconnect_plugin_runtime.Action):
@@ -17,16 +18,13 @@ class String(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
-        md5 = hashlib.md5(params[Input.STRING].encode("utf-8")).hexdigest()  # nosec
-        sha1 = hashlib.sha1(params[Input.STRING].encode("utf-8")).hexdigest()  # nosec
-        sha256 = hashlib.sha256(params[Input.STRING].encode("utf-8")).hexdigest()
-        sha512 = hashlib.sha512(params[Input.STRING].encode("utf-8")).hexdigest()
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
+        input_string = params.get(Input.STRING, "")
+        # END INPUT BINDING - DO NOT REMOVE
 
-        hashes = {
-            Output.MD5: md5,
-            Output.SHA1: sha1,
-            Output.SHA256: sha256,
-            Output.SHA512: sha512,
+        return {
+            Output.MD5: hashlib.md5(input_string.encode(DEFAULT_ENCODING)).hexdigest(),  # nosec
+            Output.SHA1: hashlib.sha1(input_string.encode(DEFAULT_ENCODING)).hexdigest(),  # nosec
+            Output.SHA256: hashlib.sha256(input_string.encode(DEFAULT_ENCODING)).hexdigest(),
+            Output.SHA512: hashlib.sha512(input_string.encode(DEFAULT_ENCODING)).hexdigest(),
         }
-
-        return hashes
