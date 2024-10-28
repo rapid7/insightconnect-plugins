4bcdc375c02b9fba14b31422eb9643feb169d679
rmurray-r7 <ryanj_murray@rapid7.com>
2024-10-28 14:42:41 +0000

    [SOAR-18025] Cloudflare - Snyk Vulnerability, SDK Bump and Unit test fix (#2905)
    * snyk, sdk and unit test fix
    
    * fixing unit test paths
    
    * \n
    
    * Fixing help.md

--- plugins/cloudflare/unit_test/test_get_zones.py
+++ plugins/cloudflare/unit_test/test_get_zones.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase
 from icon_cloudflare.actions.getZones import GetZones
-from unit_test.util import Util
+from util import Util
 from unittest.mock import patch
 from parameterized import parameterized
 
