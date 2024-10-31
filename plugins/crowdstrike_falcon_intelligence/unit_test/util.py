40b305a8c21c01cd35992e02d989dad35d372dbe
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-10-31 16:39:34 +0100

    Crowdstrike Falcon Intelligence - 16877 - New action added: Upload Malware Sample | Updated SDK to the latest version (#2918)

--- plugins/crowdstrike_falcon_intelligence/unit_test/util.py
+++ plugins/crowdstrike_falcon_intelligence/unit_test/util.py
@@ -1,7 +1,7 @@
 import json
 import logging
-import sys
 import os
+import sys
 
 import insightconnect_plugin_runtime
 
@@ -10,16 +10,18 @@ sys.path.append(os.path.abspath("../"))
 from icon_crowdstrike_falcon_intelligence.connection import Connection
 from icon_crowdstrike_falcon_intelligence.connection.schema import Input
 
+DEFAULT_CONNECTION_PARAMS = {
+    Input.BASEURL: "https://crowdstrike_url",
+    Input.CLIENTSECRET: {"secretKey": "my-api-key"},
+    Input.CLIENTID: "my_id",
+}
+
 
 class Util:
     @staticmethod
     def default_connector(action: insightconnect_plugin_runtime.Action, params=None):
         if not params:
-            params = {
-                Input.BASEURL: "https://crowdstrike_url",
-                Input.CLIENTSECRET: {"secretKey": "my-api-key"},
-                Input.CLIENTID: "my_id",
-            }
+            params = DEFAULT_CONNECTION_PARAMS
         action.connection = Connection()
         action.connection.meta = "{}"
         action.connection.logger = logging.getLogger("connection logger")
@@ -54,45 +56,52 @@ class Util:
         url = kwargs.get("url")
         params = kwargs.get("params", {})
 
-        if url == "https://crowdstrike_url/oauth2/token":
+        if url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/oauth2/token":
             return MockResponse(201, "auth_token.json.resp")
-        if url == "https://crowdstrike_url/falconx/entities/submissions/v1" and method == "GET":
+        if url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/falconx/entities/submissions/v1" and method == "GET":
             if params.get("ids")[0].startswith("404"):
                 return MockResponse(200, "check_analysis_status_invalid_id.json.resp")
             if len(params.get("ids")) == 1:
                 return MockResponse(200, "check_analysis_status.json.resp")
             if len(params.get("ids")) == 5:
                 return MockResponse(200, "check_analysis_status_many_ids.json.resp")
-        if url == "https://crowdstrike_url/falconx/entities/artifacts/v1" and method == "GET":
+        if url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/falconx/entities/artifacts/v1" and method == "GET":
             if params.get("id").startswith("404"):
                 return MockResponse(500, "")
             else:
                 return MockResponse(200, "download_artifact.json.resp")
-        if url == "https://crowdstrike_url/falconx/entities/reports/v1" and method == "GET":
+        if url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/falconx/entities/reports/v1" and method == "GET":
             if params.get("ids")[0].startswith("404"):
                 return MockResponse(200, "get_full_report_invalid_id.json.resp")
             if len(params.get("ids")) == 1:
                 return MockResponse(200, "get_full_report.json.resp")
             if len(params.get("ids")) == 2:
                 return MockResponse(200, "get_full_report_many_ids.json.resp")
-        if url == "https://crowdstrike_url/falconx/queries/reports/v1" and method == "GET":
+        if url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/falconx/queries/reports/v1" and method == "GET":
             if params.get("filter") == "verdict: 'no verdict'":
                 return MockResponse(200, "get_reports_ids.json.resp")
             elif params.get("filter") == "invalid: filter":
                 return MockResponse(400, "get_reports_ids_invalid_filter.json.resp")
-        if url == "https://crowdstrike_url/falconx/entities/report-summaries/v1" and method == "GET":
+        if (
+            url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/falconx/entities/report-summaries/v1"
+            and method == "GET"
+        ):
             if params.get("ids")[0].startswith("404"):
                 return MockResponse(200, "get_short_report_invalid_id.json.resp")
             if len(params.get("ids")) == 1:
                 return MockResponse(200, "get_short_report.json.resp")
             if len(params.get("ids")) == 4:
                 return MockResponse(200, "get_short_report_many_ids.json.resp")
-        if url == "https://crowdstrike_url/falconx/queries/submissions/v1" and method == "GET":
+        if url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/falconx/queries/submissions/v1" and method == "GET":
             if params.get("filter") == "state: 'error'":
                 return MockResponse(200, "get_submissions_ids.json.resp")
             elif params.get("filter") == "invalid: filter":
                 return MockResponse(400, "get_submissions_ids_invalid_filter.json.resp")
-        if url == "https://crowdstrike_url/falconx/entities/submissions/v1" and method == "POST":
+        if (
+            url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/falconx/entities/submissions/v1"
+            and method == "POST"
+        ):
             return MockResponse(200, "submit_analysis.json.resp")
-
+        if url == f"{DEFAULT_CONNECTION_PARAMS.get(Input.BASEURL)}/samples/entities/samples/v2" and method == "POST":
+            return MockResponse(200, "upload_malware_sample.json.resp")
         raise NotImplementedError("Not implemented", kwargs)
