7419d159001394ae2e167033f3b891c0d4ffb126
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2024-11-06 16:25:43 +0100

    InsightVM - 17939 - Trigger: New Exception Request - Added retry mechanism (#2917) (#2932)

--- plugins/rapid7_insightvm/komand_rapid7_insightvm/triggers/new_exception_request/trigger.py
+++ plugins/rapid7_insightvm/komand_rapid7_insightvm/triggers/new_exception_request/trigger.py
@@ -1,11 +1,16 @@
-import insightconnect_plugin_runtime
 import time
-from .schema import NewExceptionRequestInput, NewExceptionRequestOutput, Input, Output, Component
+from typing import Any, Dict, List
+
+import insightconnect_plugin_runtime
 
 # Custom imports below
 from komand_rapid7_insightvm.util.endpoints import VulnerabilityException
 from komand_rapid7_insightvm.util.resource_requests import ResourceRequests
-from typing import List
+from komand_rapid7_insightvm.util.util import retry_request
+
+from .schema import Component, Input, NewExceptionRequestInput, NewExceptionRequestOutput, Output
+
+MAXIMUM_TRIES = 30
 
 
 class NewExceptionRequest(insightconnect_plugin_runtime.Trigger):
@@ -38,27 +43,16 @@ class NewExceptionRequest(insightconnect_plugin_runtime.Trigger):
             if new_ids:
                 self.logger.info(f"Found new {len(new_ids)} exceptions. Returning results...")
                 for id_ in new_ids:
-                    try:
-                        self.send(
-                            {
-                                Output.EXCEPTION: resource_helper.resource_request(
-                                    endpoint=VulnerabilityException.vulnerability_exception(
-                                        self.connection.console_url, id_
-                                    )
-                                )
-                            }
-                        )
-                    except Exception as error:
-                        self.logger.error(
-                            f"Unexpected exception during trigger execution occurs. The error is: '{error}'"
-                        )
+                    self.send({Output.EXCEPTION: self._get_exception(id_, resource_helper)})
                 previous_ids = current_ids
             else:
-                self.logger.info(f"No new exceptions found. Sleeping for {frequency} minutes...")
+                self.logger.info("No new exceptions found.")
 
             # Sleep for configured frequency in minutes
+            self.logger.info(f"Sleeping for {frequency} minutes...\n")
             time.sleep(frequency * 60)
 
+    @retry_request(maximum_tries=MAXIMUM_TRIES)
     def _get_ids(self, status_filters: List[str], resource_helper: ResourceRequests) -> List[int]:
         """
         Get IDs. This method allows to get a list of vulnerability exception IDs from the API where the
@@ -83,3 +77,22 @@ class NewExceptionRequest(insightconnect_plugin_runtime.Trigger):
             for element in response
             if element.get("state", "").lower() in map(str.lower, status_filters)
         ]
+
+    @retry_request(maximum_tries=MAXIMUM_TRIES)
+    def _get_exception(self, identifier: str, resource_helper: ResourceRequests) -> Dict[str, Any]:
+        """
+        Get Exception. This method allows you to get details about a vulnerability exception with its given identifier.
+
+        :param identifier: The identifier of the vulnerability exception.
+        :type identifier: str
+
+        :param resource_helper: The resource helper object to send requests.
+        :type resource_helper: ResourceRequests
+
+        :return: Dictionary that contains vulnerability exception data.
+        :rtype: Dict[str, Any]
+        """
+
+        return resource_helper.resource_request(
+            endpoint=VulnerabilityException.vulnerability_exception(self.connection.console_url, identifier)
+        )
