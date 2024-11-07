f378a2c55e8742a4ce568f3a09a083e5702aa5de
rjmurray <ryanj_murray@rapid7.com>
2024-11-07 16:37:36 +0000

    snyk and sdk fortinet-6.0.2

--- plugins/fortinet_fortigate/unit_test/test_check_if_address_in_group.py
+++ plugins/fortinet_fortigate/unit_test/test_check_if_address_in_group.py
@@ -1,15 +1,16 @@
 import sys
 import os
 
+sys.path.append(os.path.abspath("../"))
+
+
 from unittest import TestCase
 from icon_fortinet_fortigate.actions.check_if_address_in_group import CheckIfAddressInGroup
 from icon_fortinet_fortigate.actions.check_if_address_in_group.schema import Input, Output
-from unit_test.util import Util
+from util import Util
 from unittest.mock import patch
 from parameterized import parameterized
 from insightconnect_plugin_runtime.exceptions import PluginException
-
-sys.path.append(os.path.abspath("../"))
 
 
 @patch("requests.Session.request", side_effect=Util.mocked_requests)
