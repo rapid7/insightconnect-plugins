792867139ff1e6d85ed6510e6bc5c5ac51fd855b
Dympna Laverty <dympna_laverty@rapid7.com>
2024-11-19 16:57:02 +0000

    Remove unused files

--- tools/Makefiles/Colors.mk
+++
@@ -1,7 +0,0 @@
-Q 		= @
-REPO		= $(shell grep url .git/config)
-BOLD   		= $(shell tput bold)
-UNDERLINE 	= $(shell tput smul)
-NORMAL 		= $(shell tput sgr0)
-RED		= $(shell tput setaf 1)
-YELLOW	 	= $(shell tput setaf 3)
