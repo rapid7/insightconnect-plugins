a06d5109f8c89e89095fbe81a67338cce46a45b6
rmurray-r7 <ryanj_murray@rapid7.com>
2024-11-21 10:48:21 +0000

    [SOAR-18171] Redis - Snyk Vulnerability and SDK Bump (#2950)
    * early commit - utf-8
    
    * Snyk and unit tests
    
    * Fixing unit test expected outcome
    
    * black
    
    * rename test
    
    * Renaming all unit tests with test_
    
    * new line

---
+++ plugins/redis/unit_test/test_delete.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+from util import Util
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+from jsonschema import validate
+from komand_redis.actions.delete import Delete
+
+
+class TestDelete(TestCase):
+    def setUp(self) -> None:
+        self.action = None
+
+        @patch("redis.StrictRedis", return_value=Util.mock_redis())
+        def mocked_connector(mocked_redis: MagicMock) -> None:
+            self.action = Util.default_connector(Delete())
+
+        mocked_connector()
+
+    def test_delete(self):
+        input_param = Util.load_json("inputs/delete.json.exp")
+        expect = Util.load_json("expected/delete_exp.json.exp")
+        actual = self.action.run(input_param)
+        validate(input_param, self.action.input.schema)
+        self.assertEqual(expect, actual)
+        validate(actual, self.action.output.schema)
---
+++ plugins/redis/unit_test/util.py
@@ -0,0 +1,79 @@
+import json
+import logging
+import os.path
+import sys
+
+sys.path.append(os.path.abspath("../"))
+from komand_redis.connection import Connection
+
+
+class Util:
+    @staticmethod
+    def default_connector(action):
+        default_connection = Connection()
+        default_connection.logger = logging.getLogger("connection logger")
+        params = {"host": "127.0.0.1", "port": 8080, "db": 0}
+        default_connection.connect(params)
+        action.connection = default_connection
+        action.logger = logging.getLogger("action logger")
+        return action
+
+    @staticmethod
+    def load_json(filename):
+        with open((os.path.join(os.path.dirname(os.path.realpath(__file__)), filename))) as file:
+            return json.loads(file.read())
+
+    @staticmethod
+    def mock_redis():
+        class MockResponse:
+            def __init__(self):
+                pass
+
+            @staticmethod
+            def delete(key_name: str):
+                return 10
+
+            @staticmethod
+            def hmget(key_name: str, fields: list):
+                return [b"20", b"Test User"]
+
+            @staticmethod
+            def hgetall(key_name: str):
+                return {b"age": b"20", b"email": b"Test@example.com", b"name": b"Test User"}
+
+            @staticmethod
+            def hmset(key_name: str, key_name2: dict):
+                # Differentiate between hmset and hash_set action
+                if key_name == "user:4567":
+                    return True
+                elif key_name == "user:1234":
+                    return "OK"
+
+            @staticmethod
+            def hincrby(key_name: str, key_name2: str, key_name3: str):
+                return 0
+
+            @staticmethod
+            def keys(key_name: str):
+                return {b"count": 2, b"keys": [b"count", b"keys"]}
+
+            @staticmethod
+            def lrange(key_name: str, key_name2: int, key_name3: int):
+                return []
+
+            @staticmethod
+            def setex(key_name: str, key_name2: str, key_name3: str):
+                return "OK"
+
+            @staticmethod
+            def rpush(key_name: str, *values: any):
+                return "OK"
+
+            @staticmethod
+            def get(params: str):
+                if params == "test":
+                    return True
+                if params == "key":
+                    return ""
+
+        return MockResponse()
