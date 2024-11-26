957514214f0a7dc5105ff2a8f620fc44b0312ada
Brad Chiappetta <38439955+bradchiappetta@users.noreply.github.com>
2024-11-26 06:18:54 -0500

    [GreyNoise] Updates + New Actions + New Trigger (#2916)
    * update spec
    
    * updates
    
    * validation fixes
    
    * updates
    
    * updates
    
    * formatting
    
    * clean up
    
    * add vuln action
    
    * add timeline lookup
    
    * add similar_lookup
    
    * help sum
    
    * add trigger
    
    * trigger fixes
    
    * trigger fixes
    
    * linting
    
    * updates
    
    * updates
    
    * unit tests and fixes
    
    * fix trigger output
    
    * fix trigger
    
    * review feedback
    
    * update all action outputs
    
    * fix typos
    
    * update examples
    
    * example updates

---
+++ plugins/greynoise/icon_greynoise/actions/timeline_lookup/action.py
@@ -0,0 +1,36 @@
+import insightconnect_plugin_runtime
+from .schema import TimelineLookupInput, TimelineLookupOutput, Input, Output, Component
+
+# Custom imports below
+
+
+class TimelineLookup(insightconnect_plugin_runtime.Action):
+    def __init__(self):
+        super(self.__class__, self).__init__(
+            name="timeline_lookup",
+            description=Component.DESCRIPTION,
+            input=TimelineLookupInput(),
+            output=TimelineLookupOutput(),
+        )
+
+    def run(self, params={}):
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
+        ip_address = params.get(Input.IP_ADDRESS)
+        # END INPUT BINDING - DO NOT REMOVE
+
+        try:
+            resp = self.connection.gn_client.timelinedaily(ip_address)
+
+        except RequestFailure as error:
+            raise PluginException(
+                cause=f"API responded with ERROR: {error.args[0]} - {error.args[1]}.",
+                assistance="Please check error and try again.",
+            )
+
+        except ValueError as error:
+            raise PluginException(
+                cause=f"Input does not appear to be valid: {ip_address}. Error Message: {error.args[0]}",
+                assistance="Please provide a valid IPv4 Address.",
+            )
+
+        return {Output.IP: resp.get("ip"), Output.ACTIVITY: resp.get("activity"), Output.METADATA: resp.get("metadata")}
---
+++ plugins/greynoise/unit_test/test_community_lookup.py
@@ -0,0 +1,31 @@
+import logging
+from unittest import TestCase, mock
+
+from icon_greynoise.actions.community_lookup import CommunityLookup
+
+from .util import MockConnection, mocked_requests_get
+
+
+class TestCommunityLookup(TestCase):
+    @mock.patch("greynoise.GreyNoise.ip", side_effect=mocked_requests_get)
+    def test_community_lookup(self, mock_get):
+        log = logging.getLogger("Test")
+        test_community = CommunityLookup()
+        test_community.connection = MockConnection()
+        test_community.logger = log
+
+        working_params = {"ip_address": "community_lookup"}
+        results = test_community.run(working_params)
+        expected = {
+            "ip": "1.2.3.4",
+            "noise": False,
+            "riot": True,
+            "classification": "benign",
+            "name": "Acme, Inc",
+            "link": "https://viz.greynoise.io/ip/1.2.3.4",
+            "last_seen": "2020-01-01T00:00:00+00:00",
+            "message": "Success",
+        }
+
+        self.assertNotEqual({}, results, "returns non - empty results")
+        self.assertEqual(expected, results)
