17b77f9941fb76b8497583c875d24f12306c80a8
Conor <93926445+cmcnally-r7@users.noreply.github.com>
2024-12-04 15:38:56 +0000

    Greynoise unit test fix (#2983)
    * Fix unit tests
    
    * Update SDK
    
    * Fix final unit test
    
    * Last unit tests fix hopefully

--- plugins/greynoise/unit_test/test_timeline_lookup.py
+++ plugins/greynoise/unit_test/test_timeline_lookup.py
@@ -3,7 +3,7 @@ from unittest import TestCase, mock
 
 from icon_greynoise.actions.timeline_lookup import TimelineLookup
 
-from unit_test.util import MockConnection, mocked_requests_get
+from util import MockConnection, mocked_requests_get
 
 
 class TestTimelineLookup(TestCase):
@@ -16,50 +16,48 @@ class TestTimelineLookup(TestCase):
 
         working_params = {"ip_address": "timeline_lookup"}
         results = test_timeline.run(working_params)
-        expected = [
-            {
-                "activity": [
-                    {
-                        "asn": "AS12345",
-                        "category": "hosting",
-                        "city": "Berlin",
-                        "classification": "benign",
-                        "country": "Germany",
-                        "country_code": "DE",
-                        "destinations": [{"country": "South Africa", "country_code": "ZA"}],
-                        "hassh_fingerprints": [],
-                        "http_paths": ["/favicon.ico"],
-                        "http_user_agents": ["Mozilla/5.0"],
-                        "ja3_fingerprints": ["04b3f524166caafd433b6864250945be"],
-                        "organization": "Alpha Strike Labs GmbH",
-                        "protocols": [{"port": 80, "transport_protocol": "TCP"}],
-                        "rdns": "",
-                        "region": "Berlin",
-                        "spoofable": True,
-                        "tags": [
-                            {
-                                "category": "actor",
-                                "description": "description.",
-                                "intention": "benign",
-                                "name": "Acme, Inc.",
-                            }
-                        ],
-                        "timestamp": "2024-11-03T00:00:00Z",
-                        "tor": False,
-                        "vpn": False,
-                        "vpn_service": "",
-                    }
-                ],
+        expected = {
+            "activity": [
+                {
+                    "asn": "AS12345",
+                    "category": "hosting",
+                    "city": "Berlin",
+                    "classification": "benign",
+                    "country": "Germany",
+                    "country_code": "DE",
+                    "destinations": [{"country": "South Africa", "country_code": "ZA"}],
+                    "hassh_fingerprints": [],
+                    "http_paths": ["/favicon.ico"],
+                    "http_user_agents": ["Mozilla/5.0"],
+                    "ja3_fingerprints": ["04b3f524166caafd433b6864250945be"],
+                    "organization": "Alpha Strike Labs GmbH",
+                    "protocols": [{"port": 80, "transport_protocol": "TCP"}],
+                    "rdns": "",
+                    "region": "Berlin",
+                    "spoofable": True,
+                    "tags": [
+                        {
+                            "category": "actor",
+                            "description": "description.",
+                            "intention": "benign",
+                            "name": "Acme, Inc.",
+                        }
+                    ],
+                    "timestamp": "2024-11-03T00:00:00Z",
+                    "tor": False,
+                    "vpn": False,
+                    "vpn_service": "",
+                }
+            ],
+            "ip": "1.2.3.4",
+            "metadata": {
+                "end_time": "2024-11-04T19:13:35.892189739Z",
                 "ip": "1.2.3.4",
-                "metadata": {
-                    "end_time": "2024-11-04T19:13:35.892189739Z",
-                    "ip": "1.2.3.4",
-                    "limit": 50,
-                    "next_cursor": "",
-                    "start_time": "2024-11-03T00:00:00Z",
-                },
-            }
-        ]
+                "limit": 50,
+                "next_cursor": "",
+                "start_time": "2024-11-03T00:00:00Z",
+            },
+        }
 
         self.assertNotEqual({}, results, "returns non - empty results")
         self.assertEqual(expected, results)
