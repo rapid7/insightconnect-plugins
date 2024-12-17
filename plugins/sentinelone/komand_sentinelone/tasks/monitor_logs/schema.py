06d9000d4e2d3d625acc649f79be45b23a5362e8
rjmurray <ryanj_murray@rapid7.com>
2024-12-17 16:04:13 +0000

    fix schema

--- plugins/sentinelone/komand_sentinelone/tasks/monitor_logs/schema.py
+++ plugins/sentinelone/komand_sentinelone/tasks/monitor_logs/schema.py
@@ -44,7 +44,7 @@ class MonitorLogsOutput(insightconnect_plugin_runtime.Output):
   "title": "Logs",
   "description": "List of activity, device control event, and threat logs within the specified time range",
   "items": {
-    "$ref": {}
+    "type": "object"
   },
   "definitions": {}
 }
