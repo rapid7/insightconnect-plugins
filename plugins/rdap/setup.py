da01fbdb85b1f1e00650666cb85deb594e83e8ca
komandci-automate-github-pr <jenkins@rapid7.com>
2025-02-11 16:10:11 +0000

    SDK BUMP TEST

--- plugins/rdap/setup.py
+++ plugins/rdap/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="rdap-rapid7-plugin",
-      version="2.0.4",
+      version="2.0.5",
       description="The Registration Data Access Protocol (RDAP) is the successor to WHOIS. Like WHOIS, RDAP provides access to information about Internet resources (domain names, autonomous system numbers, and IP addresses)",
       author="rapid7",
       author_email="",
