bd31e971dc3b1661b43b96dbc05784a21c4ade62
joneill-r7 <joneill@rapid7.com>
2025-02-18 09:34:52 +0000

    SOAR-18908: bump SDK and tidy up spec

--- plugins/microsoft_teams/setup.py
+++ plugins/microsoft_teams/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="microsoft_teams-rapid7-plugin",
-      version="7.0.0",
+      version="7.0.1",
       description="[Microsoft Teams](https://products.office.com/en-us/microsoft-teams/group-chat-software) is a unified communications platform that combines persistent workplace chat, video meetings, file storage, and application integration. The Microsoft Teams plugin allows you to send and trigger workflows on new messages. The plugin will also allow for teams management with the ability to add and remove teams, channels, and users. This plugin uses the [Microsoft Teams API](https://docs.microsoft.com/en-us/graph/api/resources/teams-api-overview?view=graph-rest-1.0) to interact with Microsoft Teams",
       author="rapid7",
       author_email="",
