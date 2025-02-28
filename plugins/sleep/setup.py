8d92620c9a89243f6694fac11cbbee4cd4033de3
rjmurray <ryanj_murray@rapid7.com>
2025-02-28 18:27:29 +0000

    Sleep SDK PREP

--- plugins/sleep/setup.py
+++ plugins/sleep/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="sleep-rapid7-plugin",
-      version="1.0.3",
+      version="1.0.4",
       description="Sleep allows Rapid7 InsightConnect users to suspend workflow execution for a specified period of time. Popular uses are to avoid rate limiting by a service or to wait for processing by a service to complete",
       author="rapid7",
       author_email="",
