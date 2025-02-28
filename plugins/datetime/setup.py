7d85729a512dd4bb2c312cf17c3653f0572bbef9
rmurray-r7 <ryanj_murray@rapid7.com>
2025-02-28 12:36:40 +0000

    SDK automation prep (#3216)

--- plugins/datetime/setup.py
+++ plugins/datetime/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 
 setup(name="datetime-rapid7-plugin",
-      version="3.0.1",
-      description="This plugin manipulates timestamps using Python's maya library",
+      version="3.0.2",
+      description="Timestamps, timezones, and Datetimes can be difficult to work with, especially when dealing with different locales on different systems. The Datetime InsightConnect plugin manipulates timestamps using Python's [Maya](https://pypi.org/project/maya/) library, which makes the simple things much easier while admitting that time is an illusion (timezones doubly so)",
       author="rapid7",
       author_email="",
       url="",
