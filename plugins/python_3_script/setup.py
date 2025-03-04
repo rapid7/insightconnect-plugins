5337a4f65cccafe17b258497b36a9b9a10f990ed
rmurray-r7 <ryanj_murray@rapid7.com>
2025-03-04 09:43:13 +0000

    Python 3 Script -  SDK Automation prep (#3232) (#3260)
    * python sdk prep
    
    * Adding troubleshooting in
    
    * Update docs for new Python version

--- plugins/python_3_script/setup.py
+++ plugins/python_3_script/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 
 setup(name="python_3_script-rapid7-plugin",
-      version="5.1.0",
-      description="[Python](https://www.python.org/) is a programming language that lets you work quickly and integrate systems more effectively. This plugin allows you to run Python 3 code. It includes Python 3.9.19 and its standard library as well as the following 3rd party libraries:",
+      version="5.1.1",
+      description="[Python](https://www.python.org/) is a language for fast development and system integration. This plugin runs Python 3.12.8 with its standard library and other libraries such as:* [requests](https://requests.readthedocs.io/en/latest/)* [arrow](https://pypi.org/project/arrow/)* [lxml](http://lxml.de/)* [beautifulsoup](https://www.crummy.com/software/BeautifulSoup/)It supports loading custom modules and passing credentials (`username`, `password`, `secret_key`)",
       author="rapid7",
       author_email="",
       url="",
