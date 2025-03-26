eaa04a70298f9b48f7fccb4e24c2318c6fb4008f
rmurray-r7 <ryanj_murray@rapid7.com>
2025-03-26 14:22:56 +0000

    Adding additional determination values (#3291)

--- plugins/microsoft_defender_incidents/icon_microsoft_defender_incidents/actions/get_incident/schema.py
+++ plugins/microsoft_defender_incidents/icon_microsoft_defender_incidents/actions/get_incident/schema.py
@@ -27,7 +27,8 @@ class Output:
 
 
 class GetIncidentInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -44,14 +45,16 @@ class GetIncidentInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class GetIncidentOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -556,7 +559,8 @@ class GetIncidentOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/microsoft_defender_incidents/icon_microsoft_defender_incidents/actions/list_incidents/schema.py
+++ plugins/microsoft_defender_incidents/icon_microsoft_defender_incidents/actions/list_incidents/schema.py
@@ -19,7 +19,8 @@ class Output:
 
 
 class ListIncidentsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -66,14 +67,16 @@ class ListIncidentsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ListIncidentsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -588,7 +591,8 @@ class ListIncidentsOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
