2140476b21c6420ec5bfaa78371bc8a443098b60
rmurray-r7 <ryanj_murray@rapid7.com>
2025-03-27 11:43:41 +0000

    Adding additional determination values (#3291) (#3293)

--- plugins/microsoft_defender_incidents/setup.py
+++ plugins/microsoft_defender_incidents/setup.py
@@ -2,13 +2,16 @@
 from setuptools import setup, find_packages
 
 
-setup(name="microsoft_defender_incidents-rapid7-plugin",
-      version="2.0.0",
-      description="Manage security incidents with Microsoft Defender 365",
-      author="rapid7",
-      author_email="",
-      url="",
-      packages=find_packages(),
-      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/icon_microsoft_defender_incidents']
-      )
+setup(
+    name="microsoft_defender_incidents-rapid7-plugin",
+    version="2.0.1",
+    description="Manage security incidents with Microsoft Defender 365",
+    author="rapid7",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["insightconnect-plugin-runtime"],  # Add third-party dependencies to requirements.txt, not here!
+    entry_points={
+        "console_scripts": ["icon_microsoft_defender_incidents = bin.icon_microsoft_defender_incidents:main"]
+    },
+)
