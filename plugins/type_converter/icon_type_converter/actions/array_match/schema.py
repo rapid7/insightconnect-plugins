c2b62bad34d57c845a6c1f2ccfc5fb93caf0d53d
komandci-automate-github-pr <jenkins@rapid7.com>
2025-04-08 10:46:38 +0000

    SDK Automation BUMP to 6.3.2 for type_converter

--- plugins/type_converter/icon_type_converter/actions/array_match/schema.py
+++ plugins/type_converter/icon_type_converter/actions/array_match/schema.py
@@ -19,7 +19,8 @@ class Output:
 
 
 class ArrayMatchInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -56,14 +57,16 @@ class ArrayMatchInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ArrayMatchOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -90,7 +93,8 @@ class ArrayMatchOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
