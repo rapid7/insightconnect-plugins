052a4ff13def72dddc2f83f638fefca6bd2062b8
komandci-automate-github-pr <jenkins@rapid7.com>
2025-04-09 13:20:21 +0000

    SDK Automation BUMP to 6.3.3 for csv

--- plugins/csv/komand_csv/actions/to_json/schema.py
+++ plugins/csv/komand_csv/actions/to_json/schema.py
@@ -17,7 +17,8 @@ class Output:
 
 
 class ToJsonInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -44,14 +45,16 @@ class ToJsonInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ToJsonOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -71,7 +74,8 @@ class ToJsonOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
