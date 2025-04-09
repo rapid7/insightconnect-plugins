cb67c759250b64a44a0333fc615a0c77707890cb
komandci-automate-github-pr <jenkins@rapid7.com>
2025-04-09 13:23:35 +0000

    SDK Automation BUMP to 6.3.3 for whois

--- plugins/whois/komand_whois/actions/domain/schema.py
+++ plugins/whois/komand_whois/actions/domain/schema.py
@@ -31,7 +31,8 @@ class Output:
 
 
 class DomainInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -48,14 +49,16 @@ class DomainInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class DomainOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -171,7 +174,8 @@ class DomainOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
