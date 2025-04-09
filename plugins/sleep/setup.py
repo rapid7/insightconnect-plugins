e040f29a14344589ade4648cd81ae6945ba23535
komandci-automate-github-pr <jenkins@rapid7.com>
2025-04-09 13:22:55 +0000

    SDK Automation BUMP to 6.3.3 for sleep

--- plugins/sleep/setup.py
+++ plugins/sleep/setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup, find_packages
 
 
-setup(name="sleep-rapid7-plugin",
-      version="1.0.4",
-      description="Sleep allows Rapid7 InsightConnect users to suspend workflow execution for a specified period of time. Popular uses are to avoid rate limiting by a service or to wait for processing by a service to complete",
-      author="rapid7",
-      author_email="",
-      url="",
-      packages=find_packages(),
-      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/komand_sleep']
-      )
+setup(
+    name="sleep-rapid7-plugin",
+    version="1.0.5",
+    description="Sleep allows Rapid7 InsightConnect users to suspend workflow execution for a specified period of time. Popular uses are to avoid rate limiting by a service or to wait for processing by a service to complete",
+    author="rapid7",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["insightconnect-plugin-runtime"],  # Add third-party dependencies to requirements.txt, not here!
+    entry_points={"console_scripts": ["komand_sleep = bin.komand_sleep:main"]},
+)
