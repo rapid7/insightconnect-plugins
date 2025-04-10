874854969d145f9c505cc748dbe34d76bd5ccfaf
komandci-automate-github-pr <jenkins@rapid7.com>
2025-04-10 12:12:31 +0000

    SDK Automation BUMP to 6.3.2 for sleep

--- plugins/sleep/komand_sleep/actions/sleep/schema.py
+++ plugins/sleep/komand_sleep/actions/sleep/schema.py
@@ -16,7 +16,8 @@ class Output:
 
 
 class SleepInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -30,14 +31,16 @@ class SleepInput(insightconnect_plugin_runtime.Input):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class SleepOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -51,7 +54,8 @@ class SleepOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
