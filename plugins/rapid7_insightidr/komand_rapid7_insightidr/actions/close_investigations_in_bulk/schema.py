cbdd9d0b191159d42c15c145d6a688d86e2042b4
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2025-04-10 13:46:43 +0200

    [SOAR-19208] Adding a log entry validator using a regex (#3305) (#3313)
    * Adding a log entry validator using a regex
    
    * Reverting unit test changes

--- plugins/rapid7_insightidr/komand_rapid7_insightidr/actions/close_investigations_in_bulk/schema.py
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/actions/close_investigations_in_bulk/schema.py
@@ -21,7 +21,8 @@ class Output:
 
 
 class CloseInvestigationsInBulkInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -69,14 +70,16 @@ class CloseInvestigationsInBulkInput(insightconnect_plugin_runtime.Input):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class CloseInvestigationsInBulkOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -103,7 +106,8 @@ class CloseInvestigationsInBulkOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
