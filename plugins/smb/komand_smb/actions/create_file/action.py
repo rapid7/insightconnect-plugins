fb4ca7161038865f2104ecf8a76328702b2d4eaa
rmurray-r7 <ryanj_murray@rapid7.com>
2025-04-10 09:37:04 +0100

    [SOAR-19161] SMB - Plugin Refactor + Support for SMB2+ (#3294)
    * Initial PR to update versions and add unit test
    
    * Adding additional actions and improving exception handling
    
    * Refactor
    
    * SDK 6.3.2
    
    * Unit tests added

--- plugins/smb/komand_smb/actions/create_file/action.py
+++ plugins/smb/komand_smb/actions/create_file/action.py
@@ -1,79 +1,98 @@
-import komand
+import insightconnect_plugin_runtime
+from insightconnect_plugin_runtime.exceptions import PluginException
+from smbprotocol.exceptions import SMBException
 from .schema import CreateFileInput, CreateFileOutput, Input, Output, Component
-from komand.exceptions import PluginException
 
-# Custom imports below
-import io
-import smb
 import base64
 import binascii
+from smbprotocol.open import Open, CreateDisposition, ShareAccess, ImpersonationLevel
+from smbprotocol.file_info import FileAttributes
 
 
-class CreateFile(komand.Action):
+class CreateFile(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
-            name="create_file", description=Component.DESCRIPTION, input=CreateFileInput(), output=CreateFileOutput()
+            name="create_file",
+            description=Component.DESCRIPTION,
+            input=CreateFileInput(),
+            output=CreateFileOutput(),
         )
 
-    def run(self, params={}):
-        file_path = params.get(Input.FILE_PATH)
-        share_name = params.get(Input.SHARE_NAME)
+    def run(self, params={}):  # noqa: MC0001
+        connection = self.connection
+        if not connection:
+            raise PluginException(
+                cause="No active connection",
+                assistance="Ensure that a valid connection has been established.",
+            )
+
+        file_path = params.get(Input.FILE_PATH, "").strip()
+        share_name = params.get(Input.SHARE_NAME, "").strip()
         file_content = params.get(Input.FILE_CONTENT)
         overwrite_existing = params.get(Input.OVERWRITE_EXISTING)
-        timeout = params.get(Input.TIMEOUT, 30)
 
         try:
             file_content = base64.b64decode(file_content)
-        except binascii.Error as e:
+        except binascii.Error:
             raise PluginException(
-                cause="Invalid file content input provided.", assistance="Please double-check the file content input."
+                cause="Invalid file content provided.",
+                assistance="Please ensure the file content is valid before running.",
             )
-
-        if not overwrite_existing:
-            try:
-                self.connection.conn.getAttributes(share_name, file_path, timeout=timeout)
-                raise PluginException(
-                    cause="Cannot overwrite existing file.",
-                    assistance="Please set the 'Overwrite Existing' input to true and re-run.",
-                )
-            except smb.smb_structs.OperationFailure as e:
-                self.logger.info(f"File {file_path} does not exist. Continuing with file creation...")
-            except smb.base.SMBTimeout as e:
-                raise PluginException(
-                    cause="Timeout reached when connecting to SMB endpoint.",
-                    assistance="Validate network connectivity.",
-                    data=e,
-                )
-            except smb.base.NotReadyError as e:
-                raise PluginException(
-                    cause="The SMB connection is not authenticated or the authentication has failed.",
-                    assistance="Verify the credentials of the connection in use.",
-                    data=e,
-                )
 
         try:
-            file_obj = io.BytesIO(file_content)
-            self.connection.conn.storeFile(share_name, file_path, file_obj, timeout=timeout)
+            # Calls method to establish a connection
+            tree = self.connection._connect_to_smb_share(share_name)  # noqa: E1101
+
+            open_file = Open(tree, file_path)
+            open_file.create(
+                create_disposition=(
+                    CreateDisposition.FILE_OVERWRITE_IF if overwrite_existing else CreateDisposition.FILE_CREATE
+                ),
+                desired_access=0xF000F,  # access permissions
+                file_attributes=FileAttributes.FILE_ATTRIBUTE_NORMAL,
+                share_access=ShareAccess.FILE_SHARE_READ | ShareAccess.FILE_SHARE_WRITE,
+                create_options=0,
+                impersonation_level=ImpersonationLevel.Impersonation,
+            )
+
+        except SMBException as error:
+            self.logger.error(f"SMBException encountered: {error}")
+            if "STATUS_OBJECT_NAME_COLLISION" in str(error) and not overwrite_existing:
+                raise PluginException(
+                    cause="File already exists and overwrite_existing is set to False.",
+                    assistance=f"File: {file_path} already exists, and overwrite_existing is False. Please set overwrite_existing to True if you want to overwrite the file.",
+                )
+            elif "STATUS_OBJECT_NAME_INVALID" in str(error) or "STATUS_OBJECT_NAME_COLLISION" in str(error):
+                raise PluginException(
+                    cause=f"The requested file: {file_path} may not be an acceptable file name, or the file content is invalid.",
+                    assistance=f"Please ensure the file: {file_path} is not null, contains any illegal filename characters and that the file content is type base64 before trying again.",
+                )
+            elif "STATUS_BAD_NETWORK_NAME" in str(error):
+                raise PluginException(
+                    cause=f"The requested share: {share_name} is not a valid share name.",
+                    assistance=f"Please ensure the share name: {share_name} is correct and try again.",
+                )
+
+            raise PluginException(
+                cause="SMB file creation failed.", assistance="Check logs for further information", data=error
+            )
+
+        try:
+            open_file.write(file_content, 0)
+            open_file.flush()
             self.logger.info(f"File {file_path} created successfully")
+
+            open_file.close()
+            tree.disconnect()
+
             return {Output.CREATED: True}
-        except smb.smb_structs.OperationFailure as e:
+
+        except PluginException as e:
             raise PluginException(
-                cause="Failed to create a file.", assistance="This may occur when the file does not exist.", data=e
+                cause="Failed to create a file",
+                assistance=f"Ensure the share name: {share_name} and file path {file_path} are correct, and that the correct permissions have been granted.",
+                data=str(e),
             )
-        except smb.base.SMBTimeout as e:
-            raise PluginException(
-                cause="Timeout reached when connecting to SMB endpoint.",
-                assistance="Validate network connectivity.",
-                data=e,
-            )
-        except smb.base.NotReadyError as e:
-            raise PluginException(
-                cause="The SMB connection is not authenticated or the authentication has failed.",
-                assistance="Verify the credentials of the connection in use.",
-                data=e,
-            )
-        except Exception as e:
-            raise PluginException(
-                preset=PluginException.Preset.UNKNOWN,
-                data=f"Error: {str(e)}.",
-            )
+        finally:
+            if open_file:
+                open_file.close()
