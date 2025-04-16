24410d696e8b0aa402bb73063333178f243c2b7d
rjmurray <ryanj_murray@rapid7.com>
2025-04-16 15:22:55 +0100

    JQ Cloud Enabled | Unit test and SDK added

--- plugins/jq/icon_jq/actions/run_jq/action.py
+++ plugins/jq/icon_jq/actions/run_jq/action.py
@@ -1,14 +1,18 @@
 import json
+import re
 
 # Custom imports below
 import subprocess  # noqa: B404
 from subprocess import PIPE  # noqa: B404
 
-import komand
+import insightconnect_plugin_runtime
+from insightconnect_plugin_runtime.exceptions import PluginException
+from insightconnect_plugin_runtime.telemetry import auto_instrument
+
 from .schema import RunJqInput, RunJqOutput, Input, Output
 
 
-class RunJq(komand.Action):
+class RunJq(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="run_jq",
@@ -17,6 +21,7 @@ class RunJq(komand.Action):
             output=RunJqOutput(),
         )
 
+    @auto_instrument
     def run(self, params=None):
         if params is None:
             params = {}
@@ -24,9 +29,20 @@ class RunJq(komand.Action):
         json_in = params.get(Input.JSON_IN)
         flags = params.get(Input.FLAGS)
         filter_ = params.get(Input.FILTER)
-        timeout = params.get(Input.TIMEOUT)
+        timeout = params.get(Input.TIMEOUT, 15)
+
+        # Max time is 5 minutes
+        if timeout > 300:
+            raise PluginException("JQ failed due to the timeout exceeding the maximum limit of 300 seconds.")
+        elif timeout < 1:
+            raise PluginException("The timeout must be greater than 0 seconds.")
 
         jq_cmd_array = ["jq"]
+        blocked_patterns = [r"/proc/", r"/dev/", r'import\s+"']
+        #  Prevents possible malicious injections
+        for pattern in blocked_patterns:
+            if re.search(pattern, filter_, re.IGNORECASE):
+                raise PluginException(f"Blocked pattern found in filter: {pattern}")
 
         if len(flags) > 0:
             string_flags = " ".join(flags)
@@ -34,16 +50,16 @@ class RunJq(komand.Action):
 
         jq_cmd_array.append(filter_)
 
-        self.logger.info("Command to Run: {}".format(jq_cmd_array))
+        self.logger.info(f"Command to Run: {jq_cmd_array}")
         process = subprocess.Popen(jq_cmd_array, stdout=PIPE, stderr=PIPE, stdin=PIPE)  # noqa: B603
         std_out, std_err = process.communicate(input=json.dumps(json_in).encode(), timeout=timeout)
         return_code = process.returncode
 
-        self.logger.info("Return Code: {}".format(return_code))
+        self.logger.info(f"Command to Run: {return_code}")
         if return_code > 0:
-            self.logger.info("JQ Standard Output: {}".format(std_out.decode()))
-            self.logger.info("JQ Standard Error: {}".format(std_err.decode()))
-            raise Exception("JQ failed with return code: {}".format(return_code))
+            self.logger.info(f"JQ Standard Output: {std_out.decode()}")
+            self.logger.info(f"JQ Standard Error: {std_err.decode}")
+            raise PluginException(f"JQ failed with return code: {return_code}")
 
         output_string = std_out.decode("utf-8").strip()
         return {Output.JSON_OUT: output_string}
