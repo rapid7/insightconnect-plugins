0799954c6cb3d040baa234256e1d30b43fbf3515
rjmurray <ryanj_murray@rapid7.com>
2025-04-17 15:40:13 +0100

    Adding unit test to check flag isn't available on cloud

--- plugins/jq/unit_test/test_run_jq.py
+++ plugins/jq/unit_test/test_run_jq.py
@@ -11,7 +11,7 @@ class TestRunJq(TestCase):
         self.action = RunJq()
 
     @patch("subprocess.Popen")
-    def test_run_jq_action(self, mock_popen):
+    def test_valid_jq(self, mock_popen):
         mock_process = MagicMock()
         mock_process.communicate.return_value = (b'"Alice"', b"")
         mock_process.returncode = 0
@@ -32,3 +32,25 @@ class TestRunJq(TestCase):
         validate(actual, self.action.output.schema)
 
         self.assertEqual(expected, actual)
+
+    @patch("subprocess.Popen")
+    @patch.dict("os.environ", {"PLUGIN_RUNTIME_ENVIRONMENT": "cloud"})
+    def test_flag_not_passed_in_cloud(self, mock_popen):
+
+        mock_process = MagicMock()
+        mock_process.communicate.return_value = (b'"Alice"', b"")
+        mock_process.returncode = 0
+        mock_popen.return_value = mock_process
+
+        input_param = {
+            "json_in": {"user": {"name": "Alice", "age": 30}},
+            "flags": ["--rawfile"],
+            "filter": ".user.name",
+            "timeout": 1,
+        }
+
+        self.action.run(input_param)
+
+        mock_popen.assert_called_once()
+        args, _ = mock_popen.call_args
+        self.assertNotIn("--rawfile", args[0])
