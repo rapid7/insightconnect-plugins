794026c3a8868f3ac371fb0e09b55253e0d02a8d
ghconnector-r7 <github_serviceaccounts+ghconnector@rapid7.com>
2025-04-30 07:27:43 -0400

    SDK Automation BUMP to 6.3.3 for string (#3383)
    Co-authored-by: komandci-automate-github-pr <jenkins@rapid7.com>

--- plugins/string/komand_string/actions/set_encoding/schema.py
+++ plugins/string/komand_string/actions/set_encoding/schema.py
@@ -18,7 +18,8 @@ class Output:
 
 
 class SetEncodingInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -56,14 +57,16 @@ class SetEncodingInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class SetEncodingOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -80,7 +83,8 @@ class SetEncodingOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/string/komand_string/actions/split_to_object/schema.py
+++ plugins/string/komand_string/actions/split_to_object/schema.py
@@ -18,7 +18,8 @@ class Output:
 
 
 class SplitToObjectInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -47,14 +48,16 @@ class SplitToObjectInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class SplitToObjectOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -68,7 +71,8 @@ class SplitToObjectOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
