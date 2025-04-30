9d347e9600bca04495241233014e0c66bc48746f
komandci-automate-github-pr <jenkins@rapid7.com>
2025-04-30 08:19:47 +0000

    SDK Automation BUMP to 6.3.3 for microsoft_atp

--- plugins/microsoft_atp/komand_microsoft_atp/actions/get_machine_information/schema.py
+++ plugins/microsoft_atp/komand_microsoft_atp/actions/get_machine_information/schema.py
@@ -16,7 +16,8 @@ class Output:
 
 
 class GetMachineInformationInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -33,14 +34,16 @@ class GetMachineInformationInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class GetMachineInformationOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -163,7 +166,8 @@ class GetMachineInformationOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/microsoft_atp/komand_microsoft_atp/triggers/__init__.py
+++ plugins/microsoft_atp/komand_microsoft_atp/triggers/__init__.py
@@ -3,4 +3,3 @@
 from .get_alert_matching_key.trigger import GetAlertMatchingKey
 
 from .get_alerts.trigger import GetAlerts
-
