06af11bca9b758fb59763971350ccc7a377ab71c
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2025-05-15 11:04:15 +0200

    Reverting resource helper

--- plugins/rapid7_insightidr/komand_rapid7_insightidr/util/resource_helper.py
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/util/resource_helper.py
@@ -2,7 +2,7 @@ import asyncio
 import base64
 import json
 import re
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List
 
 import aiohttp
 import requests
@@ -19,14 +19,10 @@ def _get_async_session(headers) -> aiohttp.ClientSession:
     :return: aiohttp ClientSession
     """
 
-    return aiohttp.ClientSession(
-        connector=aiohttp.TCPConnector(verify_ssl=False), headers=headers
-    )
+    return aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False), headers=headers)
 
 
-async def get_label_for_id(
-    label_id: str, url: str, session: aiohttp.ClientSession
-) -> dict:
+async def get_label_for_id(label_id: str, url: str, session: aiohttp.ClientSession) -> dict:
     response = await session.get(f"{url}log_search/management/labels/{label_id}")
     if response.status == 200:
         try:
@@ -95,13 +91,7 @@ class ResourceHelper(object):
         self.logger = logger
         self.session = session
 
-    def resource_request(
-        self,
-        endpoint: str,
-        method: str = "get",
-        params: dict = None,
-        payload: dict = None,
-    ) -> dict:
+    def resource_request(self, endpoint: str, method: str = "get", params: dict = None, payload: dict = None) -> dict:
         """
         Sends a request to API with the provided endpoint and optional method/payload
         :param endpoint: Endpoint for the API call defined in endpoints.py
@@ -117,9 +107,7 @@ class ResourceHelper(object):
             if not payload:
                 response = request_method(url=endpoint, params=params, verify=False)
             else:
-                response = request_method(
-                    url=endpoint, params=params, json=payload, verify=False
-                )
+                response = request_method(url=endpoint, params=params, json=payload, verify=False)
         except requests.RequestException as error:
             self.logger.error(error)
             raise
@@ -134,35 +122,23 @@ class ResourceHelper(object):
             except json.decoder.JSONDecodeError:
                 message, correlation_id = DEFAULT_ERROR_MESSAGE, "None"
 
-            status_code_message = self._ERRORS.get(
-                response.status_code, self._ERRORS[000]
-            )
+            status_code_message = self._ERRORS.get(response.status_code, self._ERRORS[000])
             self.logger.error(f"The endpoint is {endpoint}")
             self.logger.error(
                 f"{status_code_message} (Status Code: {response.status_code}, Correlation ID: {correlation_id}): {message}"
             )
-            raise PluginException(
-                f"InsightIDR returned a status code of {response.status_code}: {status_code_message}"
-            )
+            raise PluginException(f"InsightIDR returned a status code of {response.status_code}: {status_code_message}")
 
-    def _handle_response_status(
-        self, response: requests.Response, method: str, path: str
-    ) -> None:
+    def _handle_response_status(self, response: requests.Response) -> None:
         """
         Handles the response status code and raises appropriate exceptions
         :param response: Response object from the request
-        :param method: string representing the HTTP method used (GET, POST, etc.)
-        :param path: string representing the API endpoint path
         :return: None
         """
         if response.status_code == 400:
-            raise PluginException(
-                preset=PluginException.Preset.BAD_REQUEST, data=response.text
-            )
+            raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.text)
         if response.status_code in [401, 403]:
-            raise PluginException(
-                preset=PluginException.Preset.API_KEY, data=response.text
-            )
+            raise PluginException(preset=PluginException.Preset.API_KEY, data=response.text)
         if response.status_code == 404:
             raise PluginException(
                 cause="Resource not found.",
@@ -171,31 +147,12 @@ class ResourceHelper(object):
                 data=response.text,
             )
         if 400 < response.status_code < 500:
-            raise PluginException(
-                preset=PluginException.Preset.UNKNOWN, data=response.text
-            )
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
         if response.status_code >= 500:
-            raise PluginException(
-                preset=PluginException.Preset.SERVER_ERROR, data=response.text
-            )
-        if response.status_code == 204:
-            return response
-        if 200 <= response.status_code < 300:
-            if (
-                method == "GET"
-                and "attachments/" in path
-                and not path.endswith("/metadata")
-            ):
-                return response.content
-            return clean(response.json())
+            raise PluginException(preset=PluginException.Preset.SERVER_ERROR, data=response.text)
 
     def make_request(  # noqa: C901
-        self,
-        path: str,
-        method: str = "GET",
-        params: dict = None,
-        json_data: dict = None,
-        files: dict = None,
+        self, path: str, method: str = "GET", params: dict = None, json_data: dict = None, files: dict = None
     ):
         try:
             response = self.session.request(
@@ -205,15 +162,31 @@ class ResourceHelper(object):
                 params=params,
                 files=files,
             )
-            self._handle_response_status(response, method, path)
+            if response.status_code == 400:
+                raise PluginException(preset=PluginException.Preset.BAD_REQUEST, data=response.text)
+            if response.status_code in [401, 403]:
+                raise PluginException(preset=PluginException.Preset.API_KEY, data=response.text)
+            if response.status_code == 404:
+                raise PluginException(
+                    cause="Resource not found.",
+                    assistance="Verify your input is correct and not malformed and try again. If the issue persists, "
+                    "please contact support.",
+                    data=response.text,
+                )
+            if 400 < response.status_code < 500:
+                raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
+            if response.status_code >= 500:
+                raise PluginException(preset=PluginException.Preset.SERVER_ERROR, data=response.text)
+            if response.status_code == 204:
+                return response
+            if 200 <= response.status_code < 300:
+                if method == "GET" and "attachments/" in path and not path.endswith("/metadata"):
+                    return response.content
+                return clean(response.json())
 
-            raise PluginException(
-                preset=PluginException.Preset.UNKNOWN, data=response.text
-            )
+            raise PluginException(preset=PluginException.Preset.UNKNOWN, data=response.text)
         except json.decoder.JSONDecodeError as error:
-            raise PluginException(
-                preset=PluginException.Preset.INVALID_JSON, data=error
-            )
+            raise PluginException(preset=PluginException.Preset.INVALID_JSON, data=error)
         except requests.exceptions.HTTPError as error:
             raise PluginException(preset=PluginException.Preset.UNKNOWN, data=error)
 
@@ -269,9 +242,7 @@ class ResourceHelper(object):
                 if raw_value.startswith('["') and raw_value.endswith('"]'):
                     inner = raw_value[1:-1]
                     inner = inner.replace('"', '\\"')  # Escape inner quotes
-                    inner = inner.replace("\n", "\\n").replace(
-                        "\r", "\\r"
-                    )  # Escape control characters
+                    inner = inner.replace("\n", "\\n").replace("\r", "\\r")  # Escape control characters
                     fixed_value = f'"{inner}"'
                     return f'"{key}":{fixed_value}'
 
@@ -290,9 +261,7 @@ class ResourceHelper(object):
             return json.loads(json.dumps(fixed_json_str))
 
     @staticmethod
-    async def _get_log_entries_with_labels(
-        connection: Connection, log_entries: [dict]
-    ) -> [dict]:  # noqa: C901
+    async def _get_log_entries_with_labels(connection: Connection, log_entries: [dict]) -> [dict]:  # noqa: C901
         label_ids = set()
         for log_entry in log_entries:
             for label in log_entry.get("labels", []):
@@ -303,9 +272,7 @@ class ResourceHelper(object):
             for label_id in label_ids:
                 tasks.append(
                     asyncio.ensure_future(
-                        get_label_for_id(
-                            label_id=label_id, url=connection.url, session=async_session
-                        )
+                        get_label_for_id(label_id=label_id, url=connection.url, session=async_session)
                     )
                 )
 
@@ -319,9 +286,7 @@ class ResourceHelper(object):
         new_log_entries: [dict] = []
         for log_entry in log_entries:
             new_log_entry = dict(log_entry)
-            new_log_entry["message"] = ResourceHelper.extract_and_fix_json_all_strings(
-                log_entry.get("message", "{}")
-            )
+            new_log_entry["message"] = ResourceHelper.extract_and_fix_json_all_strings(log_entry.get("message", "{}"))
 
             entry_labels = []
             for label in log_entry.get("labels", []):
@@ -334,11 +299,5 @@ class ResourceHelper(object):
         return new_log_entries
 
     @staticmethod
-    def get_log_entries_with_new_labels(
-        connection: Connection, log_entries: [dict]
-    ) -> [dict]:
-        return asyncio.run(
-            ResourceHelper._get_log_entries_with_labels(
-                connection=connection, log_entries=log_entries
-            )
-        )
+    def get_log_entries_with_new_labels(connection: Connection, log_entries: [dict]) -> [dict]:
+        return asyncio.run(ResourceHelper._get_log_entries_with_labels(connection=connection, log_entries=log_entries))
