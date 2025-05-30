61c024083708969595323e5d511ba9707c6ceb0f
rmurray-r7 <ryanj_murray@rapid7.com>
2025-05-30 12:09:27 +0100

    Removing deprecated action from documentation (#3432)

--- plugins/rapid7_insightcloudsec/icon_rapid7_insightcloudsec/actions/detach_policy/schema.py
+++ plugins/rapid7_insightcloudsec/icon_rapid7_insightcloudsec/actions/detach_policy/schema.py
@@ -17,7 +17,8 @@ class Output:
 
 
 class DetachPolicyInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -41,14 +42,16 @@ class DetachPolicyInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class DetachPolicyOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -65,7 +68,8 @@ class DetachPolicyOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
