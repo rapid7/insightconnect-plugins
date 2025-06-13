2cc47294d4a0196d7811a707ea71e69334837294
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2025-06-13 16:51:12 +0200

    Conditioning getting logger context to fix unit tests

--- plugins/rapid7_insightidr/komand_rapid7_insightidr/util/util.py
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/util/util.py
@@ -1,10 +1,12 @@
+import os
 from flask import request
 from insightconnect_plugin_runtime.helper import clean_dict
 
 
-# when running in cloud enabled mode, loggers from within a thread will not have the correct context from the SDK
-# we then need to read in this context from the request object and add it back to loggers from with the thread
 def get_logging_context():
     log_values = {}
-    log_values["R7-Correlation-Id"] = request.headers.get("X-REQUEST-ID")
-    return clean_dict(log_values)
+    if os.environ.get("PLUGIN_RUNTIME_ENVIRONMENT", "") == "cloud":
+        log_values["R7-Correlation-Id"] = request.headers.get("X-REQUEST-ID")
+        return clean_dict(log_values)
+    else:
+        return log_values
