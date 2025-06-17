89f656fb46844cbd4b2700da17a739f12ec1d759
rmurray-r7 <ryanj_murray@rapid7.com>
2025-06-17 09:49:41 +0100

    Resolving Snyk Vulnerability (#3459) (#3460)

--- plugins/cylance_protect/icon_cylance_protect/actions/delete_asset/schema.py
+++ plugins/cylance_protect/icon_cylance_protect/actions/delete_asset/schema.py
@@ -19,7 +19,8 @@ class Output:
 
 
 class DeleteAssetInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -48,14 +49,16 @@ class DeleteAssetInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class DeleteAssetOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -90,7 +93,8 @@ class DeleteAssetOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
