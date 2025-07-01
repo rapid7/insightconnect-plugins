f17dfc99937285922235bb168fa844c9db26bf73
rjmurray <ryanj_murray@rapid7.com>
2025-07-01 12:02:58 +0100

    Resolving Snyk Vulnerabilities

--- plugins/powershell/setup.py
+++ plugins/powershell/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="powershell-rapid7-plugin",
-    version="3.0.9",
+    version="3.0.10",
     description="[PowerShell](https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-6) is a task-based command-line shell and scripting language from Microsoft that helps system administrators, power-users, and InsightConnect customers rapidly automate tasks that manage operating systems and processes. This plugin runs a PowerShell script on a remote host or locally on an InsightConnect Orchestrator",
     author="rapid7",
     author_email="",
