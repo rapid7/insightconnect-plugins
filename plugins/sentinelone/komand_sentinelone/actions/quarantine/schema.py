6b9bf370ca7e0964369c5494dde33441850e9a9d
ablakley <adam_blakley@rapid7.com>
2025-07-02 11:41:57 +0100

    Lagging decorator

--- plugins/sentinelone/komand_sentinelone/actions/quarantine/schema.py
+++ plugins/sentinelone/komand_sentinelone/actions/quarantine/schema.py
@@ -18,7 +18,8 @@ class Output:
 
 
 class QuarantineInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -51,14 +52,16 @@ class QuarantineInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class QuarantineOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -72,7 +75,8 @@ class QuarantineOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
