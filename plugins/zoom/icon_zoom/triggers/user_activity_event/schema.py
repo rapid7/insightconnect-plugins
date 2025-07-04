b88abcf32f1858ade650febf815fe758be8747e4
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2025-07-04 10:06:44 +0100

    [SOAR-19688] Zoom Lagging Decorator (#3499)
    * Decorator
    
    * Refactor
    
    * Refactor

--- plugins/zoom/icon_zoom/triggers/user_activity_event/schema.py
+++ plugins/zoom/icon_zoom/triggers/user_activity_event/schema.py
@@ -16,7 +16,8 @@ class Output:
 
 
 class UserActivityEventInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -38,14 +39,16 @@ class UserActivityEventInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class UserActivityEventOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -112,7 +115,8 @@ class UserActivityEventOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
