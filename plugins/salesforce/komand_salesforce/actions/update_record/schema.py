84f5f211f8e2c5a94479b33caf4b0493274f9cdc
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2025-07-04 09:53:41 +0100

    [SOAR-19650] Salesforce Task Delay Decorator (#3487)
    * Add lagging decorator
    
    * Refactor
    
    * Fix error raise
    
    * Fix error raise

--- plugins/salesforce/komand_salesforce/actions/update_record/schema.py
+++ plugins/salesforce/komand_salesforce/actions/update_record/schema.py
@@ -18,7 +18,8 @@ class Output:
 
 
 class UpdateRecordInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -50,14 +51,16 @@ class UpdateRecordInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class UpdateRecordOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -71,7 +74,8 @@ class UpdateRecordOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
