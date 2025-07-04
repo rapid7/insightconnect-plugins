0199d1be3bd9999283ab442bc529267931ca63a3
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2025-07-04 10:06:44 +0100

    [SOAR-19688] Zoom Lagging Decorator (#3499)
    * Decorator
    
    * Refactor
    
    * Refactor

--- plugins/zoom/icon_zoom/actions/get_user/schema.py
+++ plugins/zoom/icon_zoom/actions/get_user/schema.py
@@ -16,7 +16,8 @@ class Output:
 
 
 class GetUserInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -33,14 +34,16 @@ class GetUserInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class GetUserOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -220,7 +223,8 @@ class GetUserOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/zoom/setup.py
+++ plugins/zoom/setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup, find_packages
 
 
-setup(name="zoom-rapid7-plugin",
-      version="4.1.16",
-      description="[Zoom](https://zoom.us) is a cloud platform for video and audio conferencing, chat, and webinars. The Zoom plugin allows you to add and remove users as part of of workflow, while also providing the ability to trigger workflows on new user sign-in and sign-out activity events. This plugin uses the [Zoom API](https://marketplace.zoom.us/docs/api-reference/introduction) and requires a Pro, Business, or Enterprise plan",
-      author="rapid7",
-      author_email="",
-      url="",
-      packages=find_packages(),
-      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/icon_zoom']
-      )
+setup(
+    name="zoom-rapid7-plugin",
+    version="4.1.17",
+    description="[Zoom](https://zoom.us) is a cloud platform for video and audio conferencing, chat, and webinars. The Zoom plugin allows you to add and remove users as part of of workflow, while also providing the ability to trigger workflows on new user sign-in and sign-out activity events. This plugin uses the [Zoom API](https://marketplace.zoom.us/docs/api-reference/introduction) and requires a Pro, Business, or Enterprise plan",
+    author="rapid7",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["insightconnect-plugin-runtime"],  # Add third-party dependencies to requirements.txt, not here!
+    entry_points={"console_scripts": ["icon_zoom = bin.icon_zoom:main"]},
+)
