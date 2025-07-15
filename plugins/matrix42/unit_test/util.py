c129af5b02990f8db1aa4cc5fafc7a1e560a42d8
Philipp Behmer <kontakt@philippbehmer.de>
2025-07-15 16:20:31 +0200

    Add Matrix42 plugin for creating incidents and service requests (#3440)
    * Add Matrix42 plugin for creating incidents and service requests
    
    * Change Version History to Policy
    
    * Implement PR changes
    
    * Remove requests from requirements as it is already included in the SDK
    
    * Create unit tests for CreateTicket action
    
    * Fix typo
    
    * Refactor unit test to abstract mock setup to be reusable
    
    * Refactor CreateTicket action and enhance unit tests with abstractions
    
    * Refresh
    
    * Refactor header assertion in test_create_service_request_ticket_success for clarity
    
    * Remove trailing dot in plugin description according to specification
    
    * Save request header in connection
    
    * Remove default User and Category values
    
    * Remove "cloud_enabled" tag from plugin spec
    
    * Add JSON decode error handling
    
    * Update unit test
    
    * Trim whitespace from API key and URL in connection parameters
    
    * Add error handling for access token parsing in Matrix42 connection
    
    * Plugin refresh
    
    * Update SDK version to 6.3.7
    
    * Add new line to the end of the plugin spec file
    
    * Update "Additional Fields" example in plugins/matrix42/plugin.spec.yaml
    
    Co-authored-by: igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
    
    * Add new lines at the end of JSON files
    
    * Change SDK to version 6.3.8
    
    * Remove @auto_instrument
    
    * Refactor Matrix42 plugin to use server name instead of API URL; update related documentation and tests
    
    * Initialize instance variables in Connection class
    
    ---------
    
    Co-authored-by: igorski-r7 <99184344+igorski-r7@users.noreply.github.com>

---
+++ plugins/matrix42/unit_test/util.py
@@ -0,0 +1,70 @@
+from unittest.mock import MagicMock
+from icon_matrix42.connection.connection import Connection
+from insightconnect_plugin_runtime.exceptions import PluginException
+import json
+import os
+
+
+class Util:
+    @staticmethod
+    def read_file_to_string(filename):
+        with open(filename) as my_file:
+            return my_file.read()
+
+    @staticmethod
+    def load_parameters(filename):
+        return json.loads(
+            Util.read_file_to_string(
+                os.path.join(os.path.dirname(os.path.realpath(__file__)), f"parameters/{filename}.json.resp")
+            )
+        )
+
+    @staticmethod
+    def mocked_requests(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, filename, status_code=200):
+                self.filename = filename
+                self.status_code = status_code
+
+            def raise_for_status(self):
+                if self.status_code != 200:
+                    raise PluginException(
+                        cause="Failed to create ticket in Matrix42.",
+                        assistance="Please check your Matrix42 connection, credentials, and input parameters.",
+                        data=f"Mock request failed with status code {self.status_code}",
+                    )
+                return None
+
+            def json(self):
+                return json.loads(
+                    Util.read_file_to_string(
+                        os.path.join(
+                            os.path.dirname(os.path.realpath(__file__)), f"responses/{self.filename}.json.resp"
+                        )
+                    )
+                )
+
+        url = kwargs.get("url")
+
+        if kwargs.get("json", {}).get("Subject") == 1:
+            return MockResponse("bad request", 400)
+
+        if url.endswith("ticket/create?activityType=6"):
+            return MockResponse("create_service_request_ticket")
+
+    @staticmethod
+    def default_connection(action):
+        server_name = "my.fake.url"
+        connection = Connection()
+        connection.server_name = server_name
+        connection.api_url = f"https://{server_name}/m42Services/api/"
+        connection.api_key = "fake-key"
+        connection.access_token = "mocked-access-token"
+        connection.request_header = {
+            "Authorization": f"Bearer {connection.access_token}",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+        action.connection = connection
+        action.logger = MagicMock()
+        return action
