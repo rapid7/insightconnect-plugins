727ef01fc4ba5d807661c8332c254cc7d046a8a8
rmurray-r7 <ryanj_murray@rapid7.com>
2025-07-28 16:37:22 +0100

    Matrix42 1.0.0 Release - (plugin for creating incidents and service requests)  (#3516)
    * Add Matrix42 plugin for creating incidents and service requests (#3440)
    
    * Add Matrix42 plugin for creating incidents and service requests
    
    * Change Version History to Policy
    
    * Implement PR changes
    
    * Remove requests from requirements as it is already included in the SDK
    
    * Create unit tests for CreateTicket action
    
    * Fix typo
    
    * Refactor unit test to abstract mock setup to be reusable
    
    * Refactor CreateTicket action and enhance unit tests with abstractions
    
    * Refresh
    
    * Refactor header assertion in test_create_service_request_ticket_success for clarity
    
    * Remove trailing dot in plugin description according to specification
    
    * Save request header in connection
    
    * Remove default User and Category values
    
    * Remove "cloud_enabled" tag from plugin spec
    
    * Add JSON decode error handling
    
    * Update unit test
    
    * Trim whitespace from API key and URL in connection parameters
    
    * Add error handling for access token parsing in Matrix42 connection
    
    * Plugin refresh
    
    * Update SDK version to 6.3.7
    
    * Add new line to the end of the plugin spec file
    
    * Update "Additional Fields" example in plugins/matrix42/plugin.spec.yaml
    
    Co-authored-by: igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
    
    * Add new lines at the end of JSON files
    
    * Change SDK to version 6.3.8
    
    * Remove @auto_instrument
    
    * Refactor Matrix42 plugin to use server name instead of API URL; update related documentation and tests
    
    * Initialize instance variables in Connection class
    
    ---------
    
    Co-authored-by: igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
    
    * Matrix42 1.0.0 final pre-release fixes (#3520)
    
    * Update requirements.txt to include parameterized==0.8.1 dependency
    
    * Improve error handling in create_ticket action by providing clearer response data in exceptions
    
    * Update parameterized dependency version to 0.9.0 in requirements.txt
    
    * Add security comment to requests.post in create_ticket action
    
    * Add security comment to requests.post for access token generation
    
    * Fixing Unit Test
    
    ---------
    
    Co-authored-by: rjmurray <ryanj_murray@rapid7.com>
    
    * Updating path in Unit Test (#3522)
    
    ---------
    
    Co-authored-by: Philipp Behmer <kontakt@philippbehmer.de>
    Co-authored-by: igorski-r7 <99184344+igorski-r7@users.noreply.github.com>

---
+++ plugins/matrix42/unit_test/test_create_ticket.py
@@ -0,0 +1,75 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase
+from unittest.mock import patch
+from icon_matrix42.actions.create_ticket import CreateTicket
+from icon_matrix42.actions.create_ticket.schema import Input
+from util import Util
+from insightconnect_plugin_runtime.exceptions import PluginException
+from parameterized import parameterized
+
+
+@patch("requests.post", side_effect=Util.mocked_requests)
+class TestCreateTicket(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.action = Util.default_connection(CreateTicket())
+
+    @parameterized.expand(Util.load_parameters("create_service_request_ticket").get("parameters"))
+    def test_create_service_request_ticket_success(
+        self, mock_request, activity_type, subject, description_html, additional_fields
+    ):
+
+        result = self.action.run(
+            {
+                Input.ACTIVITY_TYPE: activity_type,
+                Input.SUBJECT: subject,
+                Input.DESCRIPTION_HTML: description_html,
+                Input.ADDITIONAL_FIELDS: additional_fields,
+            }
+        )
+
+        self.assertEqual(result["id"], "3db8e0a1-1d53-f011-1887-000d3aed261c2")
+
+        args, kwargs = mock_request.call_args
+
+        # Check the URL called
+        self.assertEqual(kwargs["url"], "https://my.fake.url/m42Services/api/ticket/create?activityType=6")
+
+        # Check the headers sent in the request
+        expected_headers = {
+            "Authorization": f"Bearer {self.action.connection.access_token}",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+        self.assertEqual(kwargs["headers"], expected_headers)
+
+        # Check the JSON body sent in the request
+        expected_body = {
+            "User": "00000000-0000-1337-0000-000000000000",
+            "Category": "00000000-0000-1337-0000-000000000000",
+            "Subject": "Unit Test Ticket",
+            "DescriptionHTML": "<h1>Unit Test</h1>",
+        }
+        self.assertEqual(kwargs["json"], expected_body)
+
+    @parameterized.expand(Util.load_parameters("create_service_request_ticket_bad").get("parameters"))
+    def test_create_service_request_ticket_bad(
+        self, mock_request, activity_type, subject, description_html, additional_fields, cause, assistance, data
+    ):
+        with self.assertRaises(PluginException) as error:
+            self.action.run(
+                {
+                    Input.ACTIVITY_TYPE: activity_type,
+                    Input.SUBJECT: subject,
+                    Input.DESCRIPTION_HTML: description_html,
+                    Input.ADDITIONAL_FIELDS: additional_fields,
+                }
+            )
+
+        self.assertEqual(error.exception.cause, cause)
+        self.assertEqual(error.exception.assistance, assistance)
+        self.assertEqual(error.exception.data, data)
