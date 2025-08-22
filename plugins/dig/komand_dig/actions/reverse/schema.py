bc5d7bc88875bb9db6b56236ec95a1c4281ed180
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2025-08-22 13:19:45 +0200

    Dig - 19984 - Updated SDK to the latest version (6.3.9) (#3536)

--- plugins/dig/komand_dig/actions/reverse/schema.py
+++ plugins/dig/komand_dig/actions/reverse/schema.py
@@ -21,7 +21,8 @@ class Output:
 
 
 class ReverseInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -44,14 +45,16 @@ class ReverseInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ReverseOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -89,7 +92,8 @@ class ReverseOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/dig/setup.py
+++ plugins/dig/setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup, find_packages
 
 
-setup(name="dig-rapid7-plugin",
-      version="2.0.5",
-      description="The DNS plugin is used for forward and reverse DNS lookups. This plugin uses [Dig](https://linux.die.net/man/1/dig), or Domain Information Groper, which is a network administration command-line tool for querying Domain Name System (DNS) name servers",
-      author="rapid7",
-      author_email="",
-      url="",
-      packages=find_packages(),
-      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/komand_dig']
-      )
+setup(
+    name="dig-rapid7-plugin",
+    version="2.0.6",
+    description="The DNS plugin is used for forward and reverse DNS lookups. This plugin uses [Dig](https://linux.die.net/man/1/dig), or Domain Information Groper, which is a network administration command-line tool for querying Domain Name System (DNS) name servers",
+    author="rapid7",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["insightconnect-plugin-runtime"],  # Add third-party dependencies to requirements.txt, not here!
+    entry_points={"console_scripts": ["komand_dig = bin.komand_dig:main"]},
+)
