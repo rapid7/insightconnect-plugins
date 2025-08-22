aa681f4101bc839fe68b697069f5c095812ac4ca
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2025-08-22 13:19:45 +0200

    Dig - 19984 - Updated SDK to the latest version (6.3.9) (#3536)

--- plugins/dig/komand_dig/actions/forward/schema.py
+++ plugins/dig/komand_dig/actions/forward/schema.py
@@ -24,7 +24,8 @@ class Output:
 
 
 class ForwardInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -64,14 +65,16 @@ class ForwardInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ForwardOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -124,7 +127,8 @@ class ForwardOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
