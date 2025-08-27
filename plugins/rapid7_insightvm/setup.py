545081749643542296888e126a87bf3599cd33b8
rmurray-r7 <ryanj_murray@rapid7.com>
2025-08-27 09:51:10 +0100

    [SOAR-20017] Rapid7 InsightVM 8.0.13 release (#3542)
    * [SOAR-19978] InsightVM - Tag Assets (Convert 'Tag Source' input to correct case) (#3530)
    
    * Resolving Snyk Vulnerability | Removing updates (#3537)
    
    * [SOAR-19978] InsightVM - Tag Assets (Convert 'Tag Source' input to correct case) (#3530)
    
    * [SOAR-20017] Rapid7 InsightVM - SDK Bump to 6.3.10 (#3543)

--- plugins/rapid7_insightvm/setup.py
+++ plugins/rapid7_insightvm/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rapid7_insightvm-rapid7-plugin",
-    version="8.0.12",
+    version="8.0.13",
     description="InsightVM is a powerful vulnerability management tool which finds, prioritizes, and remediates vulnerabilities. This plugin uses an orchestrator to get top remediations, scan results and start scans",
     author="rapid7",
     author_email="",
