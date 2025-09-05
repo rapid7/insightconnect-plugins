947dc8455193ddc54c7a863401fad740d5c6a45a
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2025-09-05 13:01:57 +0200

    [SOAR-20060] Fortinet FortiGate headers authorization (#3555)
    * Fortinet FortiGate headers authorization
    
    * Adding authentication type to connection parameters
    
    * Bumping plugin version
    
    * Resolving comments
    
    * Resolving black linting

--- plugins/fortinet_fortigate/setup.py
+++ plugins/fortinet_fortigate/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fortinet_fortigate-rapid7-plugin",
-    version="6.0.3",
+    version="7.0.0",
     description="[FortiGate Next Generation Firewalls (NGFWs)](https://www.fortinet.com/) enable security-driven networking and consolidate industry-leading security capabilities such as intrusion prevention system (IPS), web filtering, secure sockets layer (SSL) inspection, and automated threat protection",
     author="rapid7",
     author_email="",
