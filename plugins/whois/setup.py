09e032bad1ae6c55468f1b5a9645420ccea550cf
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2025-09-30 12:39:11 +0200

    Whois - 20096 - Updated SDK to the latest version (6.3.10) (#3582) (#3602)

--- plugins/whois/setup.py
+++ plugins/whois/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="whois-rapid7-plugin",
-    version="3.1.9",
+    version="3.1.10",
     description="[WHOIS](https://en.wikipedia.org/wiki/WHOIS) is a query and response protocol that is widely used for querying databases that store the registered users or assignee's of an Internet resource, such as a domain name, an IP address block, or an autonomous system",
     author="rapid7",
     author_email="",
