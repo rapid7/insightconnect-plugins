ce96794dfa0d18d34b8bd7a8608f8cd78ed41aae
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2025-10-08 10:22:09 +0200

    [SOAR-20185] Hybrid analysis connection URL (#3610)
    * Adjusted the way the connection URL is passed to improve compatibility
    
    * redundant logger

--- plugins/hybrid_analysis/icon_hybrid_analysis/util/api.py
+++ plugins/hybrid_analysis/icon_hybrid_analysis/util/api.py
@@ -1,11 +1,15 @@
 import json
 import urllib.parse
 from logging import Logger
+from typing import Literal
 
 import requests
 
 from icon_hybrid_analysis.util import constants
 from insightconnect_plugin_runtime.exceptions import PluginException
+
+
+URL_PREFIX_TYPE = Literal[constants.DEFAULT_HTTP_PREFIX, constants.DEFAULT_HTTP_WWW_PREFIX]
 
 
 class HybridAnalysisAPI:
@@ -16,22 +20,37 @@ class HybridAnalysisAPI:
         self.logger = logger
 
     def lookup_by_hash(self, analyzed_hash: str):
-        return self._send_request(method="GET", path="/search/hash", params={"hash": analyzed_hash})
+        return self._send_request(
+            method="GET",
+            path="/search/hash",
+            url_prefix=constants.DEFAULT_HTTP_PREFIX,
+            params={"hash": analyzed_hash},
+        )
 
     def lookup_by_terms(self, data: dict):
-        return self._send_request(method="POST", path="/search/terms", data=data)
+        return self._send_request(
+            method="POST", path="/search/terms", url_prefix=constants.DEFAULT_HTTP_PREFIX, data=data
+        )
 
     def report(self, analyzed_hash: str):
-        return self._send_request(method="GET", path=f"/report/{urllib.parse.quote(analyzed_hash)}/state")
+        return self._send_request(
+            method="GET",
+            path=f"/report/{urllib.parse.quote(analyzed_hash)}/state",
+            url_prefix=constants.DEFAULT_HTTP_WWW_PREFIX,
+        )
 
     def submit(self, files: dict, data: dict):
-        return self._send_request(method="POST", path="/submit/file", files=files, data=data)
+        return self._send_request(
+            method="POST", path="/submit/file", url_prefix=constants.DEFAULT_HTTP_PREFIX, files=files, data=data
+        )
 
-    def _send_request(self, method: str, path: str, data: dict = None, params=None, files: dict = None) -> dict:
+    def _send_request(
+        self, method: str, path: str, url_prefix: URL_PREFIX_TYPE, data: dict = None, params=None, files: dict = None
+    ) -> dict:
         try:
             response = requests.request(
                 method.upper(),
-                f"{self.base_url}{path}",
+                f"{url_prefix}{self.base_url}{path}",
                 data=data,
                 params=params,
                 files=files,
