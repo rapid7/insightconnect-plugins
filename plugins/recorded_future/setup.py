b7e5465602c483661076e3ab2c830373548b3e1e
igorski-r7 <igor_gorski@rapid7.com>
2025-10-22 16:05:57 +0200

    Recorded Future - Addressed Snyk Vulnerability | Updated SDK to the latest version (6.3.10)

--- plugins/recorded_future/setup.py
+++ plugins/recorded_future/setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup, find_packages
 
 
-setup(name="recorded_future-rapid7-plugin",
-      version="7.0.0",
-      description="Recorded Future arms threat analysts, security operators, and incident responders to rapidly connect the dots and reveal unknown threats. Using the Recorded Future plugin for Rapid7 InsightConnect, users can search domain lists, entity lists, and more",
-      author="rapid7",
-      author_email="",
-      url="",
-      packages=find_packages(),
-      install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/komand_recorded_future']
-      )
+setup(
+    name="recorded_future-rapid7-plugin",
+    version="7.0.1",
+    description="[Recorded Future](https://www.recordedfuture.com/) arms threat analysts, security operators, and incident responders to rapidly connect the dots and reveal unknown threats. Using the Recorded Future plugin for Rapid7 InsightConnect, users can search domain lists, entity lists, and more.Use Recorded Future within an automation workflow to quickly assist with threat analysis, incident response, and vulnerability management.Note: When a plugin action that causes a file to be downloaded is invoked, the file data is parsed internally and returned in the [STIX](https://stixproject.github.io/about/) format",
+    author="rapid7",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=["insightconnect-plugin-runtime"],  # Add third-party dependencies to requirements.txt, not here!
+    entry_points={"console_scripts": ["komand_recorded_future = bin.komand_recorded_future:main"]},
+)
