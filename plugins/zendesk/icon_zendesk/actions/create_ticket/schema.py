d79d0e22f3fb5d33b3af626f27bfed1853b2d632
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2025-12-02 10:10:03 +0100

    [SOAR-20280] Zendesk dependency version and SDK bump (#3649)
    * dependency version and SDK bump
    
    * SDK bump to the latest
    
    * Update help.md
    
    * SDK version revert

--- plugins/zendesk/icon_zendesk/actions/create_ticket/schema.py
+++ plugins/zendesk/icon_zendesk/actions/create_ticket/schema.py
@@ -30,7 +30,8 @@ class Output:
 
 
 class CreateTicketInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -183,14 +184,16 @@ class CreateTicketInput(insightconnect_plugin_runtime.Input):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class CreateTicketOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -370,7 +373,8 @@ class CreateTicketOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/zendesk/icon_zendesk/actions/show_memberships/schema.py
+++ plugins/zendesk/icon_zendesk/actions/show_memberships/schema.py
@@ -16,7 +16,8 @@ class Output:
 
 
 class ShowMembershipsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -33,14 +34,16 @@ class ShowMembershipsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ShowMembershipsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -107,7 +110,8 @@ class ShowMembershipsOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
