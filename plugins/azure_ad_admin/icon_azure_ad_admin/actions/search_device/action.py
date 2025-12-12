48633795581d9a81a6a20ef277299bbeb81c1a96
igorski-r7 <igor_gorski@rapid7.com>
2025-12-12 12:20:24 +0100

    Azure AD Admin - Search Devices: Fixed issue where devices output was limited | Updated SDK to the latest version (6.4.1)

--- plugins/azure_ad_admin/icon_azure_ad_admin/actions/search_device/action.py
+++ plugins/azure_ad_admin/icon_azure_ad_admin/actions/search_device/action.py
@@ -19,23 +19,54 @@ class SearchDevice(insightconnect_plugin_runtime.Action):
         )
 
     def run(self, params={}):
+        # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
+        filter_ = params.get(Input.FILTER, "")
+        order_by = params.get(Input.ORDERBY, "")
+        search = params.get(Input.SEARCH, "")
+        select = params.get(Input.SELECT, [])
+        # END INPUT BINDING - DO NOT REMOVE
+
+        # Get headers with authentication token
         headers = self.connection.get_headers(self.connection.get_auth_token())
         headers["ConsistencyLevel"] = "eventual"
-        search = params.get(Input.SEARCH)
-        select = params.get(Input.SELECT)
+
+        # Prepare search parameters
         search_params = {
             "$search": f'"{search}"' if search else "",
-            "$filter": params.get(Input.FILTER),
-            "$orderBy": params.get(Input.ORDERBY),
+            "$filter": filter_,
+            "$orderBy": order_by,
             "$select": ", ".join(select) if select else [],
             "$count": "true",
         }
-        response = requests.request(
-            method="GET",
-            url=Endpoint.DEVICES.format(self.connection.tenant),
-            params=clean(search_params),
-            headers=headers,
-        )
-        raise_for_status(response)
-        devices = response.json().get("value", [])
+
+        # Configure initial URL
+        url = Endpoint.DEVICES.format(self.connection.tenant)
+
+        # Limit to 1,000 iterations to avoid infinite loops (which gives total of max 100 000 devices)
+        devices, total_devices = [], 0
+        for _ in range(1_000):
+            response = requests.request(
+                method="GET",
+                url=url,
+                params=clean(search_params) if url == Endpoint.DEVICES.format(self.connection.tenant) else None,
+                headers=headers,
+            )
+            raise_for_status(response)
+            response_data = response.json()
+            devices.extend(response_data.get("value", []))
+
+            # Check for next page
+            if not (url := response_data.get("@odata.nextLink")):
+                self.logger.info("No more pages found. Ending pagination.")
+                break
+
+            # Parse total count if available
+            if "@odata.count" in response_data:
+                total_devices = response_data.get("@odata.count", 0)
+
+            if total_devices:
+                self.logger.info(
+                    f"The next page of results has been found. Fetched {len(devices)} out of {total_devices} devices so far."
+                )
+        self.logger.info(f"Found total of {len(devices)} devices matching the criteria.")
         return {Output.DEVICES: clean(devices)}
--- plugins/azure_ad_admin/unit_test/test_search_device.py
+++ plugins/azure_ad_admin/unit_test/test_search_device.py
@@ -3,21 +3,22 @@ import sys
 
 sys.path.append(os.path.abspath("../"))
 
+from typing import Any, Dict
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from icon_azure_ad_admin.actions.search_device import SearchDevice
-
-from util import Util
 from insightconnect_plugin_runtime.exceptions import PluginException
 from parameterized import parameterized
+
+from util import Util
 
 
 @patch("requests.request", side_effect=Util.mocked_requests)
 class TestSearchDevice(TestCase):
     @classmethod
     @patch("requests.post", side_effect=Util.mocked_requests)
-    def setUpClass(cls, mock_request) -> None:
+    def setUpClass(cls, mock_request: MagicMock) -> None:
         cls.action = Util.default_connector(SearchDevice())
 
     @parameterized.expand(
@@ -34,7 +35,9 @@ class TestSearchDevice(TestCase):
             ],
         ]
     )
-    def test_search_device(self, mock_request, test_name, input_params, expected):
+    def test_search_device(
+        self, mock_request: MagicMock, test_name: str, input_params: Dict[str, Any], expected: Dict[str, Any]
+    ) -> None:
         actual = self.action.run(input_params)
         self.assertEqual(actual, expected)
 
@@ -48,7 +51,9 @@ class TestSearchDevice(TestCase):
             ],
         ]
     )
-    def test_search_device_raise_exception(self, mock_request, test_name, input_params, cause, assistance):
+    def test_search_device_raise_exception(
+        self, mock_request: MagicMock, test_name: str, input_params: Dict[str, Any], cause: str, assistance: str
+    ) -> None:
         with self.assertRaises(PluginException) as error:
             self.action.run(input_params)
         self.assertEqual(error.exception.cause, cause)
