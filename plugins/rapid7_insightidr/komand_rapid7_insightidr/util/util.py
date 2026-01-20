b4e029af637526cd632cbefb256028ee0b76964e
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-01-20 16:16:36 +0100

    Added request id to Log Search actions (#3689)

--- plugins/rapid7_insightidr/komand_rapid7_insightidr/util/util.py
+++ plugins/rapid7_insightidr/komand_rapid7_insightidr/util/util.py
@@ -2,6 +2,7 @@ import requests
 import os
 from flask import request
 from insightconnect_plugin_runtime.helper import clean_dict
+from typing import Any
 
 
 def get_logging_context():
@@ -12,12 +13,19 @@ def get_logging_context():
     return clean_dict(log_values)
 
 
-def send_session_request(req_url, req_headers, req_params=None):
-    if req_params is None:
-        req_params = {}
+def send_session_request(
+    request_url: str, request_headers: dict[str, Any], request_id: str, request_params: dict[str, Any] = None
+):
+    if request_params is None:
+        request_params = {}
     with requests.Session() as session:
         prepared_request = session.prepare_request(
-            request=requests.Request(method="GET", headers=req_headers, url=req_url, params=req_params)
+            request=requests.Request(
+                method="GET",
+                headers={**request_headers, "R7-Correlation-Id": request_id},
+                url=request_url,
+                params=request_params,
+            )
         )
         response = session.send(prepared_request)
     return response
