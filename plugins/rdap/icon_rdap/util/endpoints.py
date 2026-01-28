e3944f3627d75e3f363430ad5fa3fe2c91805dd7
ghconnector-r7 <github_serviceaccounts+ghconnector@rapid7.com>
2026-01-28 06:55:20 -0500

    Release RDAP 2.0.8

--- plugins/rdap/icon_rdap/util/endpoints.py
+++ plugins/rdap/icon_rdap/util/endpoints.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://rdap.org"
+BASE_URL = "https://rdap.net"
 ASN_ENDPOINT = BASE_URL + "/autnum/{asn}"
 DOMAIN_LOOKUP_ENDPOINT = BASE_URL + "/domain/{domain}"
 IP_ENDPOINT = BASE_URL + "/ip/{ip_address}"
