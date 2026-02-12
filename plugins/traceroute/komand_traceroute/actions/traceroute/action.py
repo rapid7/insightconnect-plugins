227e0d8304b436a4c83c075e72f19f1dd305962a
joneill-r7 <joneill@rapid7.com>
2026-02-12 13:50:20 +0000

    [SOAR-20669]: SDK refresh and code tidy up

--- plugins/traceroute/komand_traceroute/actions/traceroute/action.py
+++ plugins/traceroute/komand_traceroute/actions/traceroute/action.py
@@ -1,4 +1,5 @@
-import komand
+import insightconnect_plugin_runtime
+from insightconnect_plugin_runtime.exceptions import PluginException
 from .schema import TracerouteInput, TracerouteOutput
 
 # Custom imports below
@@ -6,7 +7,7 @@ import subprocess  # noqa: B404
 import re
 
 
-class Traceroute(komand.Action):
+class Traceroute(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="traceroute",
@@ -16,13 +17,66 @@ class Traceroute(komand.Action):
         )
 
     def run(self, params={}):
-        host = params.get("host")
+        host = self._validate_host(params.get("host"))
+        count, max_ttl, time_out, set_ack, resolve_hostname, port = self._get_inputs(params)
+
+        cmd = ["tcptraceroute", "-m", f"{max_ttl}", "-q", f"{count}", "-w", f"{time_out}", host, f"{port}"]
+        if set_ack:
+            cmd.insert(-2, "-A")
+        if not resolve_hostname:
+            cmd.insert(1, "-n")
+
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as response:  # nosec B603
+            (output, err) = response.communicate()
+            output = output.decode("utf-8")
+            err = err.decode("utf-8")
+            resp_code = response.returncode
+            self.logger.info("Standard Error: %s", err)
+
+        if resp_code != 0:
+            self.logger.error(f"Received a non-zero exit code, please see error. status_code={resp_code}")
+            raise PluginException(
+                preset=PluginException.Preset.UNKNOWN, data=f"Return code={resp_code}, output={output}, error={err}"
+            )
+
+        try:
+            ip, path = [], [p.lstrip(" ") for p in output.splitlines()]
+            ip = re.findall(r"\d*\.\d*\.\d*\.\d*", output)
+
+            if not ip:
+                self.logger.error("No IP addresses found in the output.")
+                raise PluginException(
+                    cause="Unable to find any IP addresses in the output.",
+                    assistance="Please check output of traceroute command.",
+                    data=output,
+                )
+
+            # if the work open found it means the final IP address responded.
+            reply = re.search(r"open", output) is not None
+            return {"reply": reply, "response": output, "path": path, "ip": ip}
+        except:
+            self.logger.error("Unexpected error occurred while parsing output.")
+            raise PluginException(
+                cause=PluginException.Preset.UNKNOWN,
+                data=f"Return code={resp_code}, output={output}, error={err}",
+            )
+
+    @staticmethod
+    def _validate_host(host: str):
+        if not re.match(r"^[a-zA-Z0-9][a-zA-Z0-9\-\.]+$", host):
+            raise PluginException(
+                cause="Invalid hostname format, please check input.",
+                assistance="Hostname can only contain alphanumeric characters, hyphens, and dots.",
+                data=f"Supplied hostname: {host}",
+            )
+        return host
+
+    @staticmethod
+    def _get_inputs(params: dict) -> tuple[int, int, int, bool, bool, int]:
+        # ensure customer hasn't provided a zero value for these inputs
         count = params.get("count")
         max_ttl = params.get("max_ttl")
         time_out = params.get("time_out")
-        set_ack = params.get("set_ack")
-        resolve_hostname = params.get("resolve_hostname")
-        port = params.get("port")
 
         if count == 0:
             count = 3
@@ -30,114 +84,12 @@ class Traceroute(komand.Action):
             max_ttl = 30
         if time_out == 0:
             time_out = 3
+
+        set_ack = params.get("set_ack")
+        resolve_hostname = params.get("resolve_hostname")
+
+        port = params.get("port")
         if port < 1 or port > 65535:
             port = 80
 
-        count = str(count)
-        max_ttl = str(max_ttl)
-        time_out = str(time_out)
-        port = str(port)
-
-        if set_ack and resolve_hostname:
-            # both set_ack and resolve_hostname are true
-            request = "-m {max_ttl} -q {count} ".format(
-                max_ttl=max_ttl, count=count
-            ) + "-w {time_out} -A {host} {port}".format(time_out=time_out, host=host, port=port)
-        elif set_ack:
-            # set_ack is true but resolve_hostname is false
-            request = "-n -m {max_ttl} -q {count} ".format(
-                max_ttl=max_ttl, count=count
-            ) + "-w {time_out} -A {host} {port}".format(time_out=time_out, host=host, port=port)
-        elif resolve_hostname:
-            # resolve_hostname is true but set_ack is false
-            request = "-m {max_ttl} -q {count} ".format(
-                max_ttl=max_ttl, count=count
-            ) + "-w {time_out} {host} {port}".format(time_out=time_out, host=host, port=port)
-        else:
-            # both set_ack and resolve_hostname are false
-            request = "-n -m {max_ttl} -q {count} ".format(
-                max_ttl=max_ttl, count=count
-            ) + "-w {time_out} {host} {port}".format(time_out=time_out, host=host, port=port)
-
-        response = subprocess.Popen(
-            ["tcptraceroute " + request], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True  # noqa: B602
-        )
-        (output, err) = response.communicate()
-
-        output = output.decode("utf-8")
-        err = err.decode("utf-8")
-
-        self.logger.info("Standard Error: %s", err)
-
-        if response.returncode == 0:
-            ip = []
-            path = []
-            temp_path = output.splitlines()
-            # remove white space from the start of each path line
-            for x in temp_path:
-                temp = x.lstrip(" ")
-                path.append(temp)
-            # parse out IP addresses
-            try:
-                for x in path:
-                    temp = re.search(r"\d*\.\d*\.\d*\.\d*", x)
-                    ip.append(temp.group())
-
-                temp = re.search(r"open", output)
-                # if the work open found it means the final IP address responded.
-                if temp:
-                    return {"reply": True, "response": output, "path": path, "ip": ip}
-                return {"reply": False, "response": output, "path": path, "ip": ip}
-            except AttributeError:
-                self.logger.error("The regular expression search for IP addresses failed")
-                self.logger.error("Standard Output: %s", output)
-                raise
-            except:
-                self.logger.error("Return Code: %s" % response.returncode)
-                self.logger.error("Standard Output: %s" % output)
-                self.logger.error("Standard Error: %s" % err)
-                raise Exception("Something went wrong, see self.logger")
-        else:
-            self.logger.error("Return Code: %s" % response.returncode)
-            self.logger.error("Standard Output: %s" % output)
-            self.logger.error("Standard Error: %s" % err)
-            raise Exception("Something went wrong, see self.logger")
-
-    def test(self):
-        request = "tcptraceroute 127.0.0.1"
-        response = subprocess.Popen([request], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)  # noqa: B602
-        (output, err) = response.communicate()
-        self.logger.info("Standard Error: %s", err)
-
-        output = output.decode("utf-8")
-
-        if response.returncode == 0:
-            ip = []
-            path = []
-            temp_path = output.splitlines()
-            for x in temp_path:
-                temp = x.lstrip(" ")
-                path.append(temp)
-            try:
-                for x in path:
-                    temp = re.search(r"\d*\.\d*\.\d*\.\d*", x)
-                    ip.append(temp.group())
-
-                temp = re.search(r"open", output)
-                if temp:
-                    return {"reply": True, "response": output, "path": path, "ip": ip}
-                return {"reply": False, "response": output, "path": path, "ip": ip}
-            except AttributeError:
-                self.logger.error("The regular expression search for IP addresses failed")
-                self.logger.error("Standard Output: %s", output)
-                raise
-            except:
-                self.logger.error("Return Code: %s" % response.returncode)
-                self.logger.error("Standard Output: %s" % output)
-                self.logger.error("Standard Error: %s" % err)
-                raise Exception("Something went wrong, see self.logger")
-        else:
-            self.logger.error("Return Code: %s" % response.returncode)
-            self.logger.error("Standard Output: %s" % output)
-            self.logger.error("Standard Error: %s" % err)
-            raise Exception("Something went wrong, see self.logger")
+        return count, max_ttl, time_out, set_ack, resolve_hostname, port
