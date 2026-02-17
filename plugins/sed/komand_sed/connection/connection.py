154a6c93ce742915316806502e9afaf4f9f83589
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-02-17 16:31:55 +0100

    Release sed v2.0.4

--- plugins/sed/komand_sed/connection/connection.py
+++ plugins/sed/komand_sed/connection/connection.py
@@ -1,13 +1,15 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import ConnectionSchema
+from typing import Dict
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
-    def connect(self, params):
+    def connect(self, params={}):
         pass
 
-    def test(self):
-        pass
+    @staticmethod
+    def test() -> Dict[str, bool]:
+        return {"success": True}
