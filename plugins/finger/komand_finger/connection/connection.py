16ee394a6ac12f039a22fb00921e3d266e2e7b3f
ablakley-r7 <96182471+ablakley-r7@users.noreply.github.com>
2026-02-19 08:43:27 +0000

    [SOAR-20674] Finger: Address input validation / update SDK (#3730) (#3738)
    * Address input validation / update SDK
    
    * Unit test
    
    * Update unit test to run in server
    
    * Update unit test to run in server
    
    * Add connection test
    
    * Update docker
    
    * Update docker
    
    * Update docker
    
    * Update pluginspec

--- plugins/finger/komand_finger/connection/connection.py
+++ plugins/finger/komand_finger/connection/connection.py
@@ -1,12 +1,18 @@
-import komand
+import insightconnect_plugin_runtime
+
 from .schema import ConnectionSchema
+from typing import Dict
 
 # Custom imports below
 
 
-class Connection(komand.Connection):
+class Connection(insightconnect_plugin_runtime.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
     def connect(self, params):
         pass
+
+    @staticmethod
+    def test() -> Dict[str, bool]:
+        return {"success": True}
