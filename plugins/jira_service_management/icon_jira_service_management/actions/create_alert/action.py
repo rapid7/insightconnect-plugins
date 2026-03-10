73429e55929b46a4e7f65b5cae683be57ff2b3bc
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2026-03-10 15:31:27 +0100

    Create alert and basic token auth instead of oauth

--- plugins/jira_service_management/icon_jira_service_management/actions/create_alert/action.py
+++ plugins/jira_service_management/icon_jira_service_management/actions/create_alert/action.py
@@ -17,8 +17,32 @@ class CreateAlert(insightconnect_plugin_runtime.Action):
     @auto_instrument
     def run(self, params={}):
         # START INPUT BINDING - DO NOT REMOVE - ANY INPUTS BELOW WILL UPDATE WITH YOUR PLUGIN SPEC AFTER REGENERATION
+        data = {
+            "message": params.get(Input.MESSAGE),
+            "alias": params.get(Input.ALIAS),
+            "description": params.get(Input.DESCRIPTION),
+            "responders": params.get(Input.RESPONDERS),
+            "visibleTo": params.get(Input.VISIBLETO),
+            "actions": params.get(Input.ACTIONS),
+            "tags": params.get(Input.TAGS),
+            "details": params.get(Input.DETAILS),
+            "entity": params.get(Input.ENTITY),
+            "source": params.get(Input.SOURCE),
+            "priority": params.get(Input.PRIORITY),
+            "user": params.get(Input.USER),
+            "note": params.get(Input.NOTE),
+        }
         # END INPUT BINDING - DO NOT REMOVE
+        token = self.connection.api.encode_basic_auth()
+
+        response = self.connection.api.create_alert(
+            token=token,
+            data=data,
+        )
 
         return {
-            Output.MESSAGE: None,
+            Output.RESULT: response.get("result"),
+            Output.REQUESTID: response.get("requestId"),
+            Output.ELAPSED_TIME: response.get("took"),
+            Output.ALERTID: response.get("alertId"),
         }
--- plugins/jira_service_management/icon_jira_service_management/util/api.py
+++ plugins/jira_service_management/icon_jira_service_management/util/api.py
@@ -1,61 +1,141 @@
+import base64
+import time
 from logging import Logger
+from typing import Callable
 
 import requests
 
 from icon_jira_service_management.util.constants import REQUESTS_TIMEOUT
+from icon_jira_service_management.util.validators import InputDataValidator
 from insightconnect_plugin_runtime.exceptions import PluginException
 from insightconnect_plugin_runtime.helper import make_request
 
 
+RETRY_MESSAGE = "Rate limiting error occurred. Retrying in {delay:.1f} seconds ({attempts_counter}/{max_tries})"
+GET_REQUEST_ID_RETRY_MESSAGE = (
+    "Request is still being processed. Retrying in {delay:.1f} seconds ({attempts_counter}/{max_tries})"
+)
+MAX_REQUEST_TRIES = 10
+
+
 class JiraServiceManagementApi:
 
-    def __init__(self, client_id: str, client_secret: str, instance: str, logger: Logger) -> None:
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.instance = instance
+    def __init__(self, api_token: str, cloud_id: str, email: str, logger: Logger) -> None:
+        self.api_token = api_token
+        self.cloud_id = cloud_id
+        self.email = email
         self.logger = logger
+        self.validator = InputDataValidator()
 
-    def _get_token(self) -> str:
-        try:
-            return (
-                make_request(
-                    _request=requests.Request(
-                        method="POST",
-                        url="https://auth.atlassian.com/oauth/token",
-                        json={
-                            "grant_type": "client_credentials",
-                            "client_id": self.client_id,
-                            "client_secret": self.client_secret,
-                            "audience": "api.atlassian.com",
-                        },
-                    ),
-                    timeout=REQUESTS_TIMEOUT,
-                )
-                .json()
-                .get("access_token", "")
-            )
-        except Exception:
-            raise PluginException(
-                cause="Failed to obtain access token for provided client id and client secret.",
-                assistance="Please check your credentials and try again.",
-            )
+    def encode_basic_auth(self):
+        credentials = f"{self.email}:{self.api_token}"
+        return base64.b64encode(credentials.encode()).decode()
 
-    def _get_cloud_id(self, authorization: str) -> str:
-        resources = make_request(
+    def _rate_limiting(max_tries: int = 5):
+        def decorator(func: Callable):
+            def wrapper(self, *args, **kwargs):
+                attempts, delay = 0, 0
+                while attempts < max_tries:
+                    if attempts > 0:
+                        time.sleep(delay)
+                    try:
+                        return func(self, *args, **kwargs)
+                    except PluginException as e:
+                        attempts += 1
+                        delay = 2 ** (attempts * 0.6)
+                        if e.cause == PluginException.causes[PluginException.Preset.RATE_LIMIT]:
+                            self.logger.info(
+                                RETRY_MESSAGE.format(delay=delay, attempts_counter=attempts, max_tries=max_tries)
+                            )
+                            continue
+                        raise
+                return func(self, *args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+    def create_alert(self, token: str, data: dict) -> dict:
+        url = f"https://api.atlassian.com/jsm/ops/api/{self.cloud_id}/v1/alerts"
+        self.validator.validate(data)
+
+        create_alert_response = self._call_api(
+            method="POST",
+            url=url,
+            token=token,
+            json_data=data,
+        )
+        request_alert_id = self._retry_request(
+            token=token,
+            request_id=create_alert_response.get("requestId"),
+            max_tries=MAX_REQUEST_TRIES,
+        )
+
+        return {**create_alert_response, "alertId": request_alert_id}
+
+    @_rate_limiting(max_tries=MAX_REQUEST_TRIES)
+    def _call_api(self, method: str, url: str, token: str, json_data: dict = None, params: dict = None) -> dict:
+        return make_request(
             _request=requests.Request(
-                method="GET",
-                url="https://api.atlassian.com/oauth/token/accessible-resources",
+                method=method,
+                url=url,
+                json=json_data,
                 headers={
                     "Accept": "application/json",
-                    "Authorization": f"Bearer {authorization}",
+                    "Authorization": f"Basic {token}",
                 },
-            )
+                params=params,
+            ),
+            timeout=REQUESTS_TIMEOUT,
         ).json()
 
-        for resource in resources:
-            if resource.get("name", "") == self.instance:
-                return resource.get("id", "")
-        raise PluginException(
-            cause=f"No accessible resource found for instance: {self.instance}.",
-            assistance="Please provide a valid instance name.",
-        )
+    def _retry_request(self, token: str, request_id: str, max_tries: int) -> str:
+        request_response = {}
+        attempts_counter, delay = -1, 0
+        while not request_response.get("alertId", "") and attempts_counter < max_tries:
+            time.sleep(delay)
+            try:
+                request_response = self._get_request_status(
+                    token=token,
+                    identifier=request_id,
+                )
+            except PluginException:
+                attempts_counter += 1
+                delay = 2 ** (attempts_counter * 0.6)
+                self.logger.info(
+                    GET_REQUEST_ID_RETRY_MESSAGE.format(
+                        delay=delay, attempts_counter=attempts_counter, max_tries=max_tries
+                    )
+                )
+        alert_id = request_response.get("alertId")
+        if alert_id:
+            return alert_id
+        raise PluginException(preset=PluginException.Preset.NOT_FOUND)
+
+    def _get_request_status(self, token: str, identifier: str) -> dict:
+        url = f"https://api.atlassian.com/jsm/ops/api/{self.cloud_id}/v1/alerts/requests/{identifier}"
+
+        return make_request(
+            _request=requests.Request(
+                method="GET",
+                url=url,
+                headers={
+                    "Accept": "application/json",
+                    "Authorization": f"Basic {token}",
+                },
+            ),
+            timeout=REQUESTS_TIMEOUT,
+        ).json()
+
+    def test_api(self) -> dict:
+        return make_request(
+            _request=requests.Request(
+                method="GET",
+                url=f"https://api.atlassian.com/jsm/ops/api/{self.cloud_id}/v1/alerts",
+                headers={
+                    "Accept": "application/json",
+                    "Authorization": f"Basic {self.encode_basic_auth()}",
+                },
+            ),
+            timeout=REQUESTS_TIMEOUT,
+        ).status_code
