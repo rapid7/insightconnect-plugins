238dccd9d7b12dd2bb1f919f9c1aed75fb80e45a
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2026-03-13 12:21:14 +0100

    cr fixes

--- plugins/jira_service_management/icon_jira_service_management/util/api.py
+++ plugins/jira_service_management/icon_jira_service_management/util/api.py
@@ -1,21 +1,14 @@
 import base64
-import time
-from functools import wraps
+
 from logging import Logger
-from typing import Callable
 
 import requests
 
 from icon_jira_service_management.util.constants import REQUESTS_TIMEOUT
 from icon_jira_service_management.util.validators import InputDataValidator
-from insightconnect_plugin_runtime.exceptions import PluginException
+from icon_jira_service_management.util.retry import rate_limiting
 from insightconnect_plugin_runtime.helper import make_request
 
-
-RETRY_MESSAGE = "Rate limiting error occurred. Retrying in {delay:.1f} seconds ({attempts_counter}/{max_tries})"
-GET_REQUEST_ID_RETRY_MESSAGE = (
-    "Request is still being processed. Retrying in {delay:.1f} seconds ({attempts_counter}/{max_tries})"
-)
 MAX_REQUEST_TRIES = 10
 
 
@@ -38,32 +31,6 @@ class JiraServiceManagementApi:
             "Authorization": f"Basic {self.encode_basic_auth()}",
         }
 
-    @staticmethod
-    def _rate_limiting(max_tries: int = 5):
-        def decorator(func: Callable):
-            @wraps(func)
-            def wrapper(self, *args, **kwargs):
-                attempts, delay = 0, 0
-                while attempts < max_tries:
-                    if attempts > 0:
-                        time.sleep(delay)
-                    try:
-                        return func(self, *args, **kwargs)
-                    except PluginException as error:
-                        attempts += 1
-                        delay = 2 ** (attempts * 0.6)
-                        if error.cause == PluginException.causes[PluginException.Preset.RATE_LIMIT]:
-                            self.logger.info(
-                                RETRY_MESSAGE.format(delay=delay, attempts_counter=attempts, max_tries=max_tries)
-                            )
-                            continue
-                        raise
-                return func(self, *args, **kwargs)
-
-            return wrapper
-
-        return decorator
-
     def create_alert(self, data: dict) -> dict:
         url = f"https://api.atlassian.com/jsm/ops/api/{self.cloud_id}/v1/alerts"
         self.validator.validate(data)
@@ -77,7 +44,7 @@ class JiraServiceManagementApi:
 
         return {**create_alert_response, "alertId": alert_id}
 
-    @_rate_limiting(max_tries=MAX_REQUEST_TRIES)
+    @rate_limiting(max_tries=MAX_REQUEST_TRIES)
     def _call_api(self, method: str, url: str, json_data: dict = None, params: dict = None) -> dict:
         return make_request(
             _request=requests.Request(
--- plugins/jira_service_management/unit_test/util.py
+++ plugins/jira_service_management/unit_test/util.py
@@ -1,45 +1,45 @@
 import json
 import logging
 import os
+from typing import Any, Dict
 
 from icon_jira_service_management.connection.connection import Connection
 from icon_jira_service_management.connection.schema import Input
+from insightconnect_plugin_runtime import Action
 
 
 class Util:
     @staticmethod
-    def default_connector(action, connect_params: object = None):
+    def default_connector(action: Action) -> Action:
         default_connection = Connection()
         default_connection.logger = logging.getLogger("connection logger")
-        if connect_params:
-            params = connect_params
-        else:
-            params = {
-                Input.API_TOKEN: {"secretKey": "1234567890abcdef1234567890abcdef"},
-                Input.EMAIL: "test@test.com",
-                Input.CLOUD_ID: "123456789",
-            }
+
+        params = {
+            Input.API_TOKEN: {"secretKey": "1234567890abcdef1234567890abcdef"},
+            Input.EMAIL: "test@test.com",
+            Input.CLOUD_ID: "123456789",
+        }
         default_connection.connect(params)
         action.connection = default_connection
         action.logger = logging.getLogger("action logger")
         return action
 
     @staticmethod
-    def read_file_to_string(filename):
+    def read_file_to_string(filename: str) -> str:
         with open(filename) as my_file:
             return my_file.read()
 
     @staticmethod
-    def read_file_to_bytes(filename):
+    def read_file_to_bytes(filename: str) -> bytes:
         with open(filename, "rb") as my_file:
             return my_file.read()
 
     @staticmethod
-    def read_file_to_dict(filename):
+    def read_file_to_dict(filename: str) -> Dict[str, Any]:
         return json.loads(Util.read_file_to_string(os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)))
 
     @staticmethod
-    def mocked_requests(*args, **kwargs):
+    def mocked_requests(*args: Any, **kwargs: Any) -> Any:
         class MockResponse:
             def __init__(self, filename, status_code, headers=None):
                 if headers is None:
@@ -52,10 +52,10 @@ class Util:
                     )
                     self.text = self.content.decode("UTF-8")
 
-            def json(self):
+            def json(self) -> Dict[str, Any]:
                 return json.loads(self.text)
 
-            def raise_for_status(self):
+            def raise_for_status(self) -> None:
                 if self.status_code >= 400:
                     raise Exception(f"HTTP {self.status_code}")
 
