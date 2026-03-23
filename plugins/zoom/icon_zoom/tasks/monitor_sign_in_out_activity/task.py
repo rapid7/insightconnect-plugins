0bc89ee1db89a1b3d37dfd48cc2cf321be7cffde
ablakley <adam_blakley@rapid7.com>
2026-03-23 12:05:57 +0000

    Rebase

--- plugins/zoom/icon_zoom/tasks/monitor_sign_in_out_activity/task.py
+++ plugins/zoom/icon_zoom/tasks/monitor_sign_in_out_activity/task.py
@@ -307,7 +307,9 @@ class MonitorSignInOutActivity(insightconnect_plugin_runtime.Task):
         previous_completed_date = datetime.strptime(previous_completed_query_date, self.ZOOM_TIME_FORMAT)
         if previous_completed_date.time() == time(23, 59, 59):
             query_start_time = datetime.strftime(end_date.replace(hour=0, minute=0, second=0), self.ZOOM_TIME_FORMAT)
-            self.logger.info(f"Date range completed for {previous_completed_date.date()}, adjusting query start time to be 00:00:00 of the next day")
+            self.logger.info(
+                f"Date range completed for {previous_completed_date.date()}, adjusting query start time to be 00:00:00 of the next day"
+            )
             return query_start_time, now
         if start_date.date() != end_date.date():
             # If the start and end time are not on the same day, we adjust the end time to be 1 second before midnight of that day
