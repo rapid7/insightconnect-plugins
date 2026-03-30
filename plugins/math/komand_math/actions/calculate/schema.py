c2fb340c784368735fa8a515c0d4da6f0411812e
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2026-03-30 16:36:50 +0200

    Math deps update (#3827) (#3828)

--- plugins/math/komand_math/actions/calculate/schema.py
+++ plugins/math/komand_math/actions/calculate/schema.py
@@ -16,8 +16,7 @@ class Output:
 
 
 class CalculateInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -34,16 +33,14 @@ class CalculateInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class CalculateOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -60,8 +57,7 @@ class CalculateOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
