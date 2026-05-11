63572abc266184ee15915589fe408681122669ce
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-05-11 09:36:49 +0200

    InsightVM Cloud - Action Vulnerability Search: Fixed an issue where actions could fail due to JSON schema validation errors | Updated SDK to the latest version (6.5.0) (#3862)

--- plugins/rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/asset_search/schema.py
+++ plugins/rapid7_insightvm_cloud/icon_rapid7_insightvm_cloud/actions/asset_search/schema.py
@@ -21,8 +21,7 @@ class Output:
 
 
 class AssetSearchInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -71,16 +70,14 @@ class AssetSearchInput(insightconnect_plugin_runtime.Input):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class AssetSearchOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -368,8 +365,7 @@ class AssetSearchOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
