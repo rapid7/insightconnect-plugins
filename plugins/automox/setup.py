7988c3239ca62064e59320686a2e28105b55d971
Scott Pinkelman <scott.pinkelman@automox.com>
2026-05-13 15:08:44 -0600

    PR Feedback: run insight-plugin refresh

--- plugins/automox/setup.py
+++ plugins/automox/setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 
 
 setup(name="automox-automox-plugin",
-      version="3.0.0",
+      version="3.1.0",
       description="Automox is modernizing IT operations with continuous visibility, insight, and agility for your entire IT environment",
       author="automox",
       author_email="",
       url="",
       packages=find_packages(),
       install_requires=['insightconnect-plugin-runtime'],  # Add third-party dependencies to requirements.txt, not here!
-      scripts=['bin/icon_automox']
-      )
+      entry_points={
+            "console_scripts": [
+                'icon_automox = bin.icon_automox:main'
+            ]
+      }
+)
