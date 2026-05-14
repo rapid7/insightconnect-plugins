6cb720a892442add4e8603deb36b3f1b381fa5b3
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-05-14 14:52:03 +0200

    Sed - Updated command validation from denied commands to allow commands | Updated SDK to the latest version (6.5.0) (#3871)

--- plugins/sed/komand_sed/actions/process_bytes/schema.py
+++ plugins/sed/komand_sed/actions/process_bytes/schema.py
@@ -18,8 +18,7 @@ class Output:
 
 
 class ProcessBytesInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -54,16 +53,14 @@ class ProcessBytesInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ProcessBytesOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -77,8 +74,7 @@ class ProcessBytesOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
