34580a99388443b92809500cfd6bcbdb9eacf576
igorski-r7 <igor_gorski@rapid7.com>
2026-05-19 16:23:51 +0200

    Base64 - Updated SDK to the latest version (6.5.1)

--- plugins/base64/komand_base64/actions/decode/schema.py
+++ plugins/base64/komand_base64/actions/decode/schema.py
@@ -17,8 +17,7 @@ class Output:
 
 
 class DecodeInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -47,16 +46,14 @@ class DecodeInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class DecodeOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -73,8 +70,7 @@ class DecodeOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
