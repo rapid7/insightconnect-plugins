b5d0df75a06e1a3e90b416f131a91d3844862161
igorski-r7 <igor_gorski@rapid7.com>
2026-05-22 15:43:23 +0200

    Extractit - Updated SDK to the latest version (6.5.1)

--- plugins/extractit/icon_extractit/actions/date_extractor/schema.py
+++ plugins/extractit/icon_extractit/actions/date_extractor/schema.py
@@ -18,8 +18,7 @@ class Output:
 
 
 class DateExtractorInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -92,16 +91,14 @@ class DateExtractorInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class DateExtractorOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -120,8 +117,7 @@ class DateExtractorOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
