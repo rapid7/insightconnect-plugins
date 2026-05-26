8ef692d55e56f0c5085161bb0fe41fe91535245c
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-05-26 14:37:11 +0200

    Html - Updated SDK to the latest version (6.5.1) (#3895)

--- plugins/html/icon_html/actions/validate/schema.py
+++ plugins/html/icon_html/actions/validate/schema.py
@@ -16,8 +16,7 @@ class Output:
 
 
 class ValidateInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -34,16 +33,14 @@ class ValidateInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ValidateOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -57,8 +54,7 @@ class ValidateOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
