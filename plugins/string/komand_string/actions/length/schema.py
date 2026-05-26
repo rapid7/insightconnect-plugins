018532bead2e5fa3224ce16dbae3011b1554492f
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2026-05-26 14:05:31 +0200

    SDK bump

--- plugins/string/komand_string/actions/length/schema.py
+++ plugins/string/komand_string/actions/length/schema.py
@@ -16,8 +16,7 @@ class Output:
 
 
 class LengthInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -34,16 +33,14 @@ class LengthInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class LengthOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -60,8 +57,7 @@ class LengthOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/string/komand_string/actions/replace/schema.py
+++ plugins/string/komand_string/actions/replace/schema.py
@@ -18,8 +18,7 @@ class Output:
 
 
 class ReplaceInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -49,16 +48,14 @@ class ReplaceInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ReplaceOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -75,8 +72,7 @@ class ReplaceOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
