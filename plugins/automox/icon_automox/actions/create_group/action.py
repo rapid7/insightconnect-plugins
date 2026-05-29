757f50ff1d5a4ff5a424407da6d172ec90a0e12c
igorski-r7 <igor_gorski@rapid7.com>
2026-05-29 15:34:42 +0200

    Fixed linters and SDK version

--- plugins/automox/icon_automox/actions/create_group/action.py
+++ plugins/automox/icon_automox/actions/create_group/action.py
@@ -1,7 +1,6 @@
 import insightconnect_plugin_runtime
 from .schema import CreateGroupInput, CreateGroupOutput, Input, Output, Component
 
-
 # Custom imports below
 
 
--- plugins/automox/icon_automox/actions/delete_device/action.py
+++ plugins/automox/icon_automox/actions/delete_device/action.py
@@ -1,7 +1,6 @@
 import insightconnect_plugin_runtime
 from .schema import DeleteDeviceInput, DeleteDeviceOutput, Input, Output, Component
 
-
 # Custom imports below
 
 
