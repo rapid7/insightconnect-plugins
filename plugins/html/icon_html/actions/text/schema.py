cd26dbf49d3ad89bf6130f71a6445315fc266187
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-01 12:05:51 +0200

    Release Html v2.0.4

--- plugins/html/icon_html/actions/text/schema.py
+++ plugins/html/icon_html/actions/text/schema.py
@@ -17,8 +17,7 @@ class Output:
 
 
 class TextInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -41,16 +40,14 @@ class TextInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class TextOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -64,8 +61,7 @@ class TextOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
