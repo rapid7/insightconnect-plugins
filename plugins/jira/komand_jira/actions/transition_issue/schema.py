cf4b96c5df3cb63458b65dab4a16386f9e0392af
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-01 14:11:11 +0200

    Release Jira v6.7.1

--- plugins/jira/komand_jira/actions/transition_issue/schema.py
+++ plugins/jira/komand_jira/actions/transition_issue/schema.py
@@ -19,8 +19,7 @@ class Output:
 
 
 class TransitionIssueInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -56,16 +55,14 @@ class TransitionIssueInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class TransitionIssueOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -79,8 +76,7 @@ class TransitionIssueOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
