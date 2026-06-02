4654087157fb2585c766f89c0db00b1099928352
igorski-r7 <igor_gorski@rapid7.com>
2026-06-02 14:11:00 +0200

    Type Converter - Updated SDK to the latest version (6.5.1)

--- plugins/type_converter/icon_type_converter/actions/array_match/schema.py
+++ plugins/type_converter/icon_type_converter/actions/array_match/schema.py
@@ -19,8 +19,7 @@ class Output:
 
 
 class ArrayMatchInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -57,16 +56,14 @@ class ArrayMatchInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ArrayMatchOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -93,8 +90,7 @@ class ArrayMatchOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
