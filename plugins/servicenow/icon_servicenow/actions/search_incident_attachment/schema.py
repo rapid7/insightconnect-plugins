9379a51afa51e2e543ec5a329b18776b08db66de
igorski-r7 <igor_gorski@rapid7.com>
2026-06-02 14:00:56 +0200

    Servicenow - Updated SDK to the latest version (6.5.1)

--- plugins/servicenow/icon_servicenow/actions/search_incident_attachment/schema.py
+++ plugins/servicenow/icon_servicenow/actions/search_incident_attachment/schema.py
@@ -16,8 +16,7 @@ class Output:
 
 
 class SearchIncidentAttachmentInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -34,16 +33,14 @@ class SearchIncidentAttachmentInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class SearchIncidentAttachmentOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -63,8 +60,7 @@ class SearchIncidentAttachmentOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
