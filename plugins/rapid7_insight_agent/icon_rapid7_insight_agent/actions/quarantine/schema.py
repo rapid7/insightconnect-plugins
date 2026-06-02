6b07348ecfe40f2461e39c7370fab8310d0ec38c
igorski-r7 <igor_gorski@rapid7.com>
2026-06-02 11:43:28 +0200

    Rapid7 Insight Agent - Updated SDK to the latest version (6.5.1)

--- plugins/rapid7_insight_agent/icon_rapid7_insight_agent/actions/quarantine/schema.py
+++ plugins/rapid7_insight_agent/icon_rapid7_insight_agent/actions/quarantine/schema.py
@@ -18,8 +18,7 @@ class Output:
 
 
 class QuarantineInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -52,16 +51,14 @@ class QuarantineInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class QuarantineOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -78,8 +75,7 @@ class QuarantineOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
