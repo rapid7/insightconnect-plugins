4eae7c8a3f341d7901f0750255a370a2a6b79733
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-02 15:44:01 +0200

    RDAP - Updated SDK to the latest version (6.5.1) (#3914)

--- plugins/rdap/icon_rdap/actions/asnLookup/schema.py
+++ plugins/rdap/icon_rdap/actions/asnLookup/schema.py
@@ -25,8 +25,7 @@ class Output:
 
 
 class AsnLookupInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -43,16 +42,14 @@ class AsnLookupInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class AsnLookupOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -271,8 +268,7 @@ class AsnLookupOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
