4d328d1d5294877e2cd392dc46951079914735ea
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-03 13:23:16 +0200

    Release RDAP v2.0.10

--- plugins/rdap/icon_rdap/actions/ipLookup/schema.py
+++ plugins/rdap/icon_rdap/actions/ipLookup/schema.py
@@ -34,8 +34,7 @@ class Output:
 
 
 class IpLookupInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -59,16 +58,14 @@ class IpLookupInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class IpLookupOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -335,8 +332,7 @@ class IpLookupOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
