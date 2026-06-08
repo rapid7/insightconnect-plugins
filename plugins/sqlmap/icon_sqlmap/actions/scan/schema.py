88144c816904cd14cc622a893e90f0fa55a5bbfa
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-08 14:56:19 +0200

    Sqlmap - Updated SDK version to 6.5.1 (#3943)

--- plugins/sqlmap/icon_sqlmap/actions/scan/schema.py
+++ plugins/sqlmap/icon_sqlmap/actions/scan/schema.py
@@ -158,8 +158,7 @@ class Output:
 
 
 class ScanInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -1107,16 +1106,14 @@ class ScanInput(insightconnect_plugin_runtime.Input):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ScanOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -1130,8 +1127,7 @@ class ScanOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
