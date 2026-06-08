648c9555099c3ffca1ef46fb64dd07adf2c2433e
igorski-r7 <igor_gorski@rapid7.com>
2026-06-08 14:06:06 +0200

    Compression - Updated SDK version to 6.5.1

--- plugins/compression/komand_compression/actions/create_archive/schema.py
+++ plugins/compression/komand_compression/actions/create_archive/schema.py
@@ -18,8 +18,7 @@ class Output:
 
 
 class CreateArchiveInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -77,16 +76,14 @@ class CreateArchiveInput(insightconnect_plugin_runtime.Input):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class CreateArchiveOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -120,8 +117,7 @@ class CreateArchiveOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
