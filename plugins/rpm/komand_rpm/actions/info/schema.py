029189b272b56297cc22c9b384651c0967788518
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-08 14:56:16 +0200

    RPM - Updated SDK version to 6.5.1 (#3942)

--- plugins/rpm/komand_rpm/actions/info/schema.py
+++ plugins/rpm/komand_rpm/actions/info/schema.py
@@ -40,8 +40,7 @@ class Output:
 
 
 class InfoInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -116,16 +115,14 @@ class InfoInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class InfoOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -294,8 +291,7 @@ class InfoOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
