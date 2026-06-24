d16564890c18e702239ede5ff4f89a595886eb9e
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-24 13:07:19 +0200

    Release Compression v2.0.4 (#3962)

--- plugins/compression/komand_compression/actions/compress_bytes/schema.py
+++ plugins/compression/komand_compression/actions/compress_bytes/schema.py
@@ -19,8 +19,7 @@ class Output:
 
 
 class CompressBytesInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -53,16 +52,14 @@ class CompressBytesInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class CompressBytesOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -78,8 +75,7 @@ class CompressBytesOutput(insightconnect_plugin_runtime.Output):
   },
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
