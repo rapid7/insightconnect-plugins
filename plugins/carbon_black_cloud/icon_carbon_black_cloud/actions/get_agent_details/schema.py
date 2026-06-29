9e99f01abe0db5bbe457219c63b9133017309d08
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-06-29 16:14:33 +0200

    Release Carbon Black Cloud v2.2.14 (#3994)

--- plugins/carbon_black_cloud/icon_carbon_black_cloud/actions/get_agent_details/schema.py
+++ plugins/carbon_black_cloud/icon_carbon_black_cloud/actions/get_agent_details/schema.py
@@ -16,8 +16,7 @@ class Output:
 
 
 class GetAgentDetailsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -34,16 +33,14 @@ class GetAgentDetailsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class GetAgentDetailsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -526,8 +523,7 @@ class GetAgentDetailsOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
