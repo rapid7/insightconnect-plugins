4fc37c54921afe552c798dba24d6b7fcd8431869
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2026-07-03 14:32:55 +0200

    Dependency update, SDK bump

--- plugins/cylance_protect/icon_cylance_protect/actions/search_threats/schema.py
+++ plugins/cylance_protect/icon_cylance_protect/actions/search_threats/schema.py
@@ -17,8 +17,7 @@ class Output:
 
 
 class SearchThreatsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -44,16 +43,14 @@ class SearchThreatsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class SearchThreatsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -152,8 +149,7 @@ class SearchThreatsOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
