8922ea68954eb46e2edf67bbfab229898b1a537f
lcwiklinski-r7 <lukasz_cwiklinski@rapid7.com>
2026-07-06 11:17:00 +0200

    Dependency update, SDK bump (#3997) (#3998)

--- plugins/cylance_protect/icon_cylance_protect/actions/search_agents/schema.py
+++ plugins/cylance_protect/icon_cylance_protect/actions/search_agents/schema.py
@@ -16,8 +16,7 @@ class Output:
 
 
 class SearchAgentsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -34,16 +33,14 @@ class SearchAgentsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class SearchAgentsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -142,8 +139,7 @@ class SearchAgentsOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/cylance_protect/icon_cylance_protect/actions/search_threat_agents/schema.py
+++ plugins/cylance_protect/icon_cylance_protect/actions/search_threat_agents/schema.py
@@ -16,8 +16,7 @@ class Output:
 
 
 class SearchThreatAgentsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -34,16 +33,14 @@ class SearchThreatAgentsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class SearchThreatAgentsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -136,8 +133,7 @@ class SearchThreatAgentsOutput(insightconnect_plugin_runtime.Output):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
