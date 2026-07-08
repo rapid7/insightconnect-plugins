31a8328294e436220c68a49b7aa0f733ba5d74c0
igorski-r7 <igor_gorski@rapid7.com>
2026-07-08 15:52:41 +0200

    Cisco Firepower Management Center - Bumped cryptography to the latest version | Updated SDK to the latest version (6.6.0)

--- plugins/cisco_firepower_management_center/icon_cisco_firepower_management_center/actions/block_url_policy/schema.py
+++ plugins/cisco_firepower_management_center/icon_cisco_firepower_management_center/actions/block_url_policy/schema.py
@@ -18,8 +18,7 @@ class Output:
 
 
 class BlockUrlPolicyInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -76,16 +75,14 @@ class BlockUrlPolicyInput(insightconnect_plugin_runtime.Input):
     }
   }
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class BlockUrlPolicyOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(
-        r"""
+    schema = json.loads(r"""
    {
   "type": "object",
   "title": "Variables",
@@ -102,8 +99,7 @@ class BlockUrlPolicyOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """
-    )
+    """)
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
