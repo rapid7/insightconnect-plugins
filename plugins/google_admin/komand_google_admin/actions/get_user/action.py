8f11f3cc2fa208fb91e54a20aced01c5f2ef8427
igorski-r7 <99184344+igorski-r7@users.noreply.github.com>
2026-08-27 14:33:29 +0200

    Release Google Admin v1.0.5 (#4050)

--- plugins/google_admin/komand_google_admin/actions/get_user/action.py
+++ plugins/google_admin/komand_google_admin/actions/get_user/action.py
@@ -1,10 +1,10 @@
-import komand
+import insightconnect_plugin_runtime
 from .schema import GetUserInput, GetUserOutput
 
 # Custom imports below
 
 
-class GetUser(komand.Action):
+class GetUser(insightconnect_plugin_runtime.Action):
     def __init__(self):
         super(self.__class__, self).__init__(
             name="get_user", description="Get a User", input=GetUserInput(), output=GetUserOutput()
@@ -12,8 +12,4 @@ class GetUser(komand.Action):
 
     def run(self, params={}):
         user = self.connection.service.users().get(userKey=params["user"]).execute()
-        return {"user": user, "found": (not not user)}
-
-    def test(self):
-        # TODO: Implement test function
-        return {}
+        return {"user": user, "found": bool(user)}
