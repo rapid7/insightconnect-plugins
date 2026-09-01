0c3e39a6c7322b6b898af3fe3fee53ae9e4482e3
igorski-r7 <igor_gorski@rapid7.com>
2026-09-01 12:32:01 +0200

    revert(netmiko): revert netmiko to pre-#4047 state

--- plugins/netmiko/komand_netmiko/actions/configuration_commands/schema.py
+++ plugins/netmiko/komand_netmiko/actions/configuration_commands/schema.py
@@ -17,7 +17,8 @@ class Output:
 
 
 class ConfigurationCommandsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -43,14 +44,16 @@ class ConfigurationCommandsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ConfigurationCommandsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -67,7 +70,8 @@ class ConfigurationCommandsOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
--- plugins/netmiko/komand_netmiko/actions/show_commands/schema.py
+++ plugins/netmiko/komand_netmiko/actions/show_commands/schema.py
@@ -17,7 +17,8 @@ class Output:
 
 
 class ShowCommandsInput(insightconnect_plugin_runtime.Input):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -40,14 +41,16 @@ class ShowCommandsInput(insightconnect_plugin_runtime.Input):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
 
 
 class ShowCommandsOutput(insightconnect_plugin_runtime.Output):
-    schema = json.loads(r"""
+    schema = json.loads(
+        r"""
    {
   "type": "object",
   "title": "Variables",
@@ -64,7 +67,8 @@ class ShowCommandsOutput(insightconnect_plugin_runtime.Output):
   ],
   "definitions": {}
 }
-    """)
+    """
+    )
 
     def __init__(self):
         super(self.__class__, self).__init__(self.schema)
